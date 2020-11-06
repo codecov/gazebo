5c20af271b33ff4812990c5cdd1a6309b9ed39c0
Dorian Amouroux <dor.amouroux@gmail.com>
2020-11-06 13:39:58 +0100

    boilerplate for React router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg'
-import './App.css'
+import { Suspense, lazy } from 'react'
+
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+const Home = lazy(() => import('./pages/Home'))
+const About = lazy(() => import('./pages/About'))
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+    <Suspense fallback="loading...">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </Suspense>
+  );
 }
 
 export default App
---
+++ src/pages/About.js
@@ -0,0 +1,9 @@
+function About() {
+  return (
+    <div>
+      Im the about page
+    </div>
+  )
+}
+
+export default About
