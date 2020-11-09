59dd46b62c38eef536c89f2e37e3ff692372cd61
Dorian Amouroux <dor.amouroux@gmail.com>
2020-11-09 16:07:27 +0100

    iownoirqwer

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React :)
         </a>
       </header>
     </div>
