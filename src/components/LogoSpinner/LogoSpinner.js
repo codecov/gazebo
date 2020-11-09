cb123a0c7ed1c8699bcaa694c20172b0ea53d30c
Dorian Amouroux <dor.amouroux@gmail.com>
2020-11-09 11:41:23 +0100

    add logo spinner component

---
+++ src/components/LogoSpinner/LogoSpinner.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types'
+
+import styles from './LogoSpinner.module.css'
+import { ReactComponent as Logo } from './logo.svg'
+
+function LogoSpinner({ size = 100 }) {
+  const height = `${size}px`
+  return (
+    <div className={styles.spinner} style={{ height }}>
+      <Logo />
+    </div>
+  )
+}
+
+LogoSpinner.propTypes = {
+  size: PropTypes.number,
+}
+
+export default LogoSpinner
