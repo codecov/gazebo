d7b2cdbea3cd11a72ffdf89d41755ea850e751c7
Dorian Amouroux <dor.amouroux@gmail.com>
2020-11-10 15:20:10 +0100

    home page fetch the user information

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,18 @@
+import { useEffect } from 'react'
+
+import Server from 'shared/server'
+
 function Home() {
-  return (
-    <div>
-      Im the home page
-    </div>
-  )
+  useEffect(() => {
+    Server.get({
+      path: '/profile',
+      provider: 'gh',
+    })
+      .then(console.log)
+      .catch(console.error)
+  }, [])
+
+  return <div>Im the home page</div>
 }
 
 export default Home
