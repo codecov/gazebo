dbb1a197b98307cd54a125e17948480d9a39c5c0
Dorian Amouroux <dor.amouroux@gmail.com>
2020-11-10 16:17:23 +0100

    remove REACT_APP_ prefix from config object and add API URL to production build

--- src/config.js
+++ src/config.js
@@ -1,10 +1,19 @@
+import mapKeys from 'lodash/mapKeys'
+
 const defaultConfig = {
   API_URL: '',
 }
 
+function removeReactAppPrefix(obj) {
+  // in .env file, the variable must start with REACT_APP_
+  // to be injected in the application, so we remove that
+  // prefix to be more convenient for us
+  return mapKeys(obj, (_, key) => key.replace('REACT_APP_', ''))
+}
+
 const config = {
   ...defaultConfig,
-  ...process.env,
+  ...removeReactAppPrefix(process.env),
   ...window.configEnv,
 }
 
