b70c84a3af4ffcade08fdbe94faf983fdbf681bc
Dorian Amouroux <dor.amouroux@gmail.com>
2020-11-12 11:27:10 +0100

    remove api call on the homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,4 @@
-import { useEffect } from 'react'
-
-import Server from 'shared/server'
-
 function Home() {
-  useEffect(() => {
-    Server.get({
-      path: '/profile',
-      provider: 'gh',
-    })
-      .then(console.log)
-      .catch(console.error)
-  }, [])
-
-  useEffect(() => {
-    Server.get({
-      path: '/github/codecov/repos/java-standard/',
-      provider: 'gh',
-    })
-      .then(console.log)
-      .catch(console.error)
-  }, [])
-
   return <div>Im the home page</div>
 }
 
