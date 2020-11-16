f2b0a2ebb859f7095c396e2f69afffecf323f25d
Dorian Amouroux <dor.amouroux@gmail.com>
2020-11-16 14:01:03 +0100

    remove pages boilerplate

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,12 @@
-import { Suspense, lazy } from 'react'
+import { Suspense } from 'react'
 
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-
-const Home = lazy(() => import('./pages/Home'))
-const About = lazy(() => import('./pages/About'))
+import { BrowserRouter, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <Suspense fallback="loading...">
       <BrowserRouter>
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
+        <Switch></Switch>
       </BrowserRouter>
     </Suspense>
   )
