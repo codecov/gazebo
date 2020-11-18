88c774535bdfaa9dc8e4f42f99e3d21d2f8c4cb2
Dorian Amouroux <dor.amouroux@gmail.com>
2020-11-18 12:12:31 +0100

    setup react-query library

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,30 @@
 import { Suspense, lazy } from 'react'
+import { QueryCache, ReactQueryCacheProvider } from 'react-query'
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 const AccountSettings = lazy(() => import('./pages/AccountSettings'))
 
+const queryCache = new QueryCache({
+  defaultConfig: {
+    queries: {
+      suspense: true,
+    },
+  },
+})
+
 function App() {
   return (
     <Suspense fallback="loading...">
-      <BrowserRouter>
-        <Switch>
-          <Route path="/account/:provider/:owner/">
-            <AccountSettings />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      <ReactQueryCacheProvider queryCache={queryCache}>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/account/:provider/:owner/">
+              <AccountSettings />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </ReactQueryCacheProvider>
     </Suspense>
   )
 }
