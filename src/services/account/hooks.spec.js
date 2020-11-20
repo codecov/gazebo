b1f2e1b648a3a4df0ebf25671aeaf5ed047735eb
Dorian Amouroux <dor.amouroux@gmail.com>
2020-11-20 16:52:02 +0100

    add specs for cancel plan page

--- src/services/account/hooks.spec.js
+++ src/services/account/hooks.spec.js
@@ -2,7 +2,7 @@ import { rest } from 'msw'
 import { setupServer } from 'msw/node'
 import { renderHook } from '@testing-library/react-hooks'
 
-import { useAccountDetails } from './hooks'
+import { useAccountDetails, useAccountsAndPlans } from './hooks'
 
 const provider = 'gh'
 const owner = 'codecov'
@@ -25,7 +25,10 @@ const server = setupServer(
     (req, res, ctx) => {
       return res(ctx.status(200), ctx.json(accountDetails))
     }
-  )
+  ),
+  rest.get(`/internal/plans`, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(getPlans()))
+  })
 )
 
 beforeAll(() => server.listen())
@@ -35,13 +38,13 @@ afterAll(() => server.close())
 describe('useAccountDetails', () => {
   let hookData
 
-  function setup(currentUrl) {
+  function setup() {
     hookData = renderHook(() => useAccountDetails({ provider, owner }))
   }
 
-  describe('when called without a / at the end of URL', () => {
+  describe('when called', () => {
     beforeEach(() => {
-      setup('/hello')
+      setup()
     })
 
     it('renders isLoading true', () => {
@@ -50,7 +53,6 @@ describe('useAccountDetails', () => {
 
     describe('when data is loaded', () => {
       beforeEach(() => {
-        // console.log(hookData.result.current
         return hookData.waitFor(() => hookData.result.current.isSuccess)
       })
 
@@ -60,3 +62,74 @@ describe('useAccountDetails', () => {
     })
   })
 })
+
+describe('useAccountsAndPlans', () => {
+  let hookData
+
+  function setup(currentUrl) {
+    hookData = renderHook(() => useAccountsAndPlans({ provider, owner }))
+  }
+
+  describe('when called', () => {
+    beforeEach(() => {
+      setup()
+    })
+
+    it('renders isLoading true', () => {
+      expect(hookData.result.current.isLoading).toBeTruthy()
+    })
+
+    describe('when data is loaded', () => {
+      beforeEach(() => {
+        return hookData.waitFor(() => hookData.result.current.isSuccess)
+      })
+
+      it('returns the data', () => {
+        expect(hookData.result.current.data).toEqual({
+          accountDetails,
+          plans: getPlans(),
+        })
+      })
+    })
+  })
+})
+
+function getPlans() {
+  return [
+    {
+      marketingName: 'Basic',
+      value: 'users-free',
+      billingRate: null,
+      basUnitprice: 0,
+      benefits: [
+        'Up to 5 users',
+        'Unlimited public repositories',
+        'Unlimited private repositories',
+      ],
+    },
+    {
+      marketingName: 'Pro Team',
+      value: 'users-pr-inappm',
+      billingRate: 'monthly',
+      baseUnitPrice: 12,
+      benefits: [
+        'Configureable # of users',
+        'Unlimited public repositories',
+        'Unlimited private repositories',
+        'Priorty Support',
+      ],
+    },
+    {
+      marketingName: 'Pro Team',
+      value: 'users-pr-inappy',
+      billingRate: 'annually',
+      baseUnitPrice: 10,
+      benefits: [
+        'Configureable # of users',
+        'Unlimited public repositories',
+        'Unlimited private repositories',
+        'Priorty Support',
+      ],
+    },
+  ]
+}
