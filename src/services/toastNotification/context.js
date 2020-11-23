95bca2fd5e7ba5c06e8f2146f45c2cb9c9ebdf18
Dorian Amouroux <dor.amouroux@gmail.com>
2020-11-23 14:54:15 +0100

    add toast notification service

---
+++ src/services/toastNotification/context.js
@@ -0,0 +1,71 @@
+import * as React from 'react'
+import noop from 'lodash/noop'
+
+const ToastContext = React.createContext({
+  addNofitication: noop,
+  removeNotification: noop,
+  notifications: [],
+})
+
+export function ToastNotificationProvider({ children }) {
+  const [notifications, setNotifications] = React.useState([
+    {
+      id: 1,
+      type: 'error',
+      text: 'enter the text',
+      disappearAfter: 5000,
+    },
+  ])
+  const currentId = React.useRef(0)
+
+  function addNofitication(newNotification) {
+    currentId.current++
+
+    const baseNotification = {
+      id: currentId.current,
+      type: 'success',
+      text: 'enter the text',
+      disappearAfter: 5000,
+    }
+
+    const mergedNotification = {
+      ...baseNotification,
+      ...newNotification,
+    }
+
+    setNotifications((oldNotifications) => [
+      ...oldNotifications,
+      mergedNotification,
+    ])
+  }
+
+  function removeNotification(id) {
+    setNotifications((oldNotifications) => {
+      return oldNotifications.filter((notification) => notification.id !== id)
+    })
+  }
+
+  const contextValue = {
+    notifications,
+    addNofitication,
+    removeNotification,
+  }
+
+  return (
+    <ToastContext.Provider value={contextValue}>
+      {children}
+    </ToastContext.Provider>
+  )
+}
+
+export function useNotifications() {
+  return React.useContext(ToastContext).notifications
+}
+
+export function useAddNotification() {
+  return React.useContext(ToastContext).addNofitication
+}
+
+export function useRemoveNotification() {
+  return React.useContext(ToastContext).removeNotification
+}
