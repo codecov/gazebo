5b2a2d193dadfe6840be701da519e2d11a49362c
Dorian Amouroux <dor.amouroux@gmail.com>
2020-11-23 10:43:27 +0100

    add some specs to Button

---
+++ src/components/Button/Button.spec.js
@@ -0,0 +1,28 @@
+import { render, fireEvent } from '@testing-library/react'
+import Button from '.'
+
+describe('Button', () => {
+  let wrapper
+
+  const onClick = jest.fn()
+
+  describe('when rendered', () => {
+    beforeEach(() => {
+      wrapper = render(<Button onClick={onClick}>Click me</Button>)
+    })
+
+    it('renders a button', () => {
+      expect(wrapper.container.querySelector('button')).not.toBeNull()
+    })
+
+    describe('when clicking', () => {
+      beforeEach(() => {
+        fireEvent.click(wrapper.getByText('Click me'))
+      })
+
+      it('calls the handler', () => {
+        expect(onClick).toHaveBeenCalled()
+      })
+    })
+  })
+})
