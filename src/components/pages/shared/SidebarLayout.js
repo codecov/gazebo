4025ae16fb61c8f868c85ee6905a6eedb3f5f4e3
Terry Smith <terry@codecov.io>
2020-11-25 12:15:18 -0400

    Fix sidebar layout to work with billing and users

--- src/components/pages/shared/SidebarLayout.js
+++ src/components/pages/shared/SidebarLayout.js
@@ -2,9 +2,9 @@ import PropType from 'prop-types'
 
 function SidebarLayout({ sidebar, children }) {
   return (
-    <div className="flex-grow grid grid-cols-1 sm:grid-cols-7 bg-gray-200">
+    <div className="flex-grow grid grid-cols-1 sm:grid-cols-7 p-4 bg-gray-200">
       {sidebar}
-      <article className="col-span-6 grid sm:grid-cols-5 grid-cols-1 sm:gap-4 gap-0 p-0 sm:p-4 px-4 sm:px-0">
+      <article className="col-span-6 grid sm:grid-cols-12 grid-cols-1 sm:gap-4 gap-0 p-0 sm:p-4 px-4 sm:px-0">
         {children}
       </article>
     </div>
