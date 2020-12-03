2f03ab0d7e1c4d3c9faa61a4dbd5d4edeae49141
Dorian Amouroux <dor.amouroux@gmail.com>
2020-12-03 13:10:05 +0100

    apply the network error boundary on the account settings

--- src/components/pages/AccountSettings/AccountSettings.js
+++ src/components/pages/AccountSettings/AccountSettings.js
@@ -3,6 +3,7 @@ import { useParams, Switch, Route } from 'react-router-dom'
 
 import { useBaseUrl } from 'shared/router'
 import LogoSpinner from 'components/LogoSpinner'
+import NetworkErrorBoundary from 'components/NetworkErrorBoundary'
 import PageLayout from 'components/pages/shared/SidebarLayout'
 
 import SideMenu from './SideMenu'
@@ -22,25 +23,27 @@ function AccountSettings() {
 
   return (
     <PageLayout sidebar={<SideMenu baseUrl={baseUrl} />}>
-      <Suspense fallback={<LogoSpinner />}>
-        <Switch>
-          <Route path={baseUrl + ''} exact>
-            <BillingAndUsersTab provider={provider} owner={owner} />
-          </Route>
-          <Route path={baseUrl + 'billing/upgrade'}>
-            <UpgradePlanTab provider={provider} owner={owner} />
-          </Route>
-          <Route path={baseUrl + 'billing/cancel'}>
-            <CancelPlanTab provider={provider} owner={owner} />
-          </Route>
-          <Route path={baseUrl + 'yaml'}>
-            <YAMLTab />
-          </Route>
-          <Route path={baseUrl + 'admin'}>
-            <AdminTab />
-          </Route>
-        </Switch>
-      </Suspense>
+      <NetworkErrorBoundary>
+        <Suspense fallback={<LogoSpinner />}>
+          <Switch>
+            <Route path={baseUrl + ''} exact>
+              <BillingAndUsersTab provider={provider} owner={owner} />
+            </Route>
+            <Route path={baseUrl + 'billing/upgrade'}>
+              <UpgradePlanTab provider={provider} owner={owner} />
+            </Route>
+            <Route path={baseUrl + 'billing/cancel'}>
+              <CancelPlanTab provider={provider} owner={owner} />
+            </Route>
+            <Route path={baseUrl + 'yaml'}>
+              <YAMLTab />
+            </Route>
+            <Route path={baseUrl + 'admin'}>
+              <AdminTab />
+            </Route>
+          </Switch>
+        </Suspense>
+      </NetworkErrorBoundary>
     </PageLayout>
   )
 }
