8eb263fb2a2ed8652061caaa2f5d4cd25468be31
Dorian Amouroux <dor.amouroux@gmail.com>
2020-12-10 10:06:17 -1000

    suspense in base layout

--- src/layouts/FullLayout/FullLayout.js
+++ src/layouts/FullLayout/FullLayout.js
@@ -1,21 +1,13 @@
 import ErrorBoundary from '../shared/ErrorBoundary'
 import NetworkErrorBoundary from '../shared/NetworkErrorBoundary'
-import BaseLayout from '../BaseLayout'
 
 function FullLayout({ children }) {
   return (
-    <BaseLayout>
-      <main className="flex-grow bg-gray-200 mt-16">
-        <article
-          className="container py-10 px-4 sm:px-0"
-          data-testid="full-layout"
-        >
-          <ErrorBoundary sentryScopes={[['layout', 'full']]}>
-            <NetworkErrorBoundary>{children}</NetworkErrorBoundary>
-          </ErrorBoundary>
-        </article>
-      </main>
-    </BaseLayout>
+    <article className="container py-10 px-4 sm:px-0" data-testid="full-layout">
+      <ErrorBoundary sentryScopes={[['layout', 'full']]}>
+        <NetworkErrorBoundary>{children}</NetworkErrorBoundary>
+      </ErrorBoundary>
+    </article>
   )
 }
 
