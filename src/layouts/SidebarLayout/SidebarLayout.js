a693c5a9e48ffa2833219ef0494b54cb499abe4c
Dorian Amouroux <dor.amouroux@gmail.com>
2020-12-10 09:14:58 -1000

    hide the sidebar if error on sidebar layout

--- src/layouts/SidebarLayout/SidebarLayout.js
+++ src/layouts/SidebarLayout/SidebarLayout.js
@@ -8,12 +8,14 @@ function SidebarLayout({ sidebar, children }) {
   return (
     <BaseLayout>
       <main className="flex-grow grid grid-cols-1 sm:grid-cols-7 p-4 bg-gray-200 mt-16">
-        {sidebar}
-        <article className="col-span-6 grid sm:grid-cols-12 grid-cols-1 sm:gap-4 gap-0 p-0 sm:p-4 px-4 sm:px-0">
-          <ErrorBoundary sentryScopes={[['layout', 'sidebar']]}>
-            <NetworkErrorBoundary>{children}</NetworkErrorBoundary>
-          </ErrorBoundary>
-        </article>
+        <ErrorBoundary sentryScopes={[['layout', 'sidebar']]}>
+          <NetworkErrorBoundary>
+            {sidebar}
+            <article className="col-span-6 grid sm:grid-cols-12 grid-cols-1 sm:gap-4 gap-0 p-0 sm:p-4 px-4 sm:px-0">
+              {children}
+            </article>
+          </NetworkErrorBoundary>
+        </ErrorBoundary>
       </main>
     </BaseLayout>
   )
