f4ee0953884bc5d16005236a7d4dc5161fd83c91
Terry Smith <terry@codecov.io>
2020-12-12 12:23:52 -0400

    Move SidebarLayout to use flex for sizebar sizing

--- src/layouts/SidebarLayout/SidebarLayout.js
+++ src/layouts/SidebarLayout/SidebarLayout.js
@@ -1,15 +1,22 @@
 import PropType from 'prop-types'
+import cs from 'classnames'
 
 import ErrorBoundary from '../shared/ErrorBoundary'
 import NetworkErrorBoundary from '../shared/NetworkErrorBoundary'
 
 function SidebarLayout({ sidebar, children }) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-7 p-4">
+    <div className="flex flex-col sm:flex-row p-4">
       <ErrorBoundary sentryScopes={[['layout', 'sidebar']]}>
         <NetworkErrorBoundary>
           {sidebar}
-          <article className="col-span-6 grid sm:grid-cols-12 grid-cols-1 sm:gap-4 gap-0 p-0 sm:p-4 px-4 sm:px-0">
+          <article
+            className={cs(
+              'flex-1', // parent
+              'grid grid-flow-col auto-cols-max md:auto-cols-min gap-0 sm:gap-4', // contents
+              'p-0 sm:p-4 px-4 sm:px-0'
+            )}
+          >
             {children}
           </article>
         </NetworkErrorBoundary>
