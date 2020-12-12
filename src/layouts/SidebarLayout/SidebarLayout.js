cdd45842f5e755bda38aa17c152ea3c95f6953a9
Terry Smith <terry@codecov.io>
2020-12-12 13:28:24 -0400

    Optionally pass classes to sidebar content

--- src/layouts/SidebarLayout/SidebarLayout.js
+++ src/layouts/SidebarLayout/SidebarLayout.js
@@ -1,15 +1,21 @@
 import PropType from 'prop-types'
+import cs from 'classnames'
 
 import ErrorBoundary from '../shared/ErrorBoundary'
 import NetworkErrorBoundary from '../shared/NetworkErrorBoundary'
 
-function SidebarLayout({ sidebar, children }) {
+function SidebarLayout({ sidebar, children, className }) {
   return (
     <div className="flex flex-col sm:flex-row p-4">
       <ErrorBoundary sentryScopes={[['layout', 'sidebar']]}>
         <NetworkErrorBoundary>
           {sidebar}
-          <div className="flex-1 pl-0 sm:pl-4">{children}</div>
+          <div
+            data-testid="sidebar-content"
+            className={cs('flex-1', className, { 'pl-0 sm:pl-4': !className })}
+          >
+            {children}
+          </div>
         </NetworkErrorBoundary>
       </ErrorBoundary>
     </div>
