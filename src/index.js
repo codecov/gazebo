f4157d59760ffbbff856887fb440786d32bdc457
Terry Smith <terry@codecov.io>
2021-01-06 12:44:45 -0400

    Setup browser mock api for development

--- src/index.js
+++ src/index.js
@@ -9,6 +9,11 @@ import './globals.css'
 import reportWebVitals from './reportWebVitals'
 import './sentry.js'
 
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = require('./mocks/browser')
+  worker.start()
+}
+
 ReactModal.setAppElement('#root')
 
 ReactDOM.render(
