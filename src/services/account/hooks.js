9e3be0449eddaf2bfb50bc68e379222c2ed8ba47
Dorian Amouroux <dor.amouroux@gmail.com>
2021-01-12 18:59:20 +0100

    add specs for useInvoice hook

--- src/services/account/hooks.js
+++ src/services/account/hooks.js
@@ -35,10 +35,8 @@ export function useInvoices({ provider, owner }) {
 }
 
 export function useInvoice({ provider, owner, id }) {
-  console.log('bro')
   return useQuery(['invoice', provider, owner, id], () => {
     const path = `/${provider}/${owner}/invoices/${id}`
-    console.log(path)
     return Api.get({ path, provider })
   })
 }
