3a40db507ebaba14a61816dba1e59a8ed2b5ec11
Dorian Amouroux <dor.amouroux@gmail.com>
2021-01-14 14:36:06 +0100

    start printing process if ?print is in the URL on invoice detail page

--- src/pages/AccountSettings/tabs/InvoiceDetail/InvoiceDetail.js
+++ src/pages/AccountSettings/tabs/InvoiceDetail/InvoiceDetail.js
@@ -1,3 +1,6 @@
+import qs from 'qs'
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { useParams, Link } from 'react-router-dom'
 import cs from 'classnames'
@@ -16,11 +19,29 @@ const classNameSection =
 // make the Invoice container full screen so only that part is printed
 const printClassnames = 'print:absolute print:inset-0 print:z-50'
 
+function usePrintPage() {
+  const urlParams = qs.parse(useLocation().search, {
+    ignoreQueryPrefix: true,
+  })
+
+  const shouldPrint = 'print' in urlParams
+
+  useEffect(() => {
+    if (shouldPrint) {
+      // close window after printing
+      window.addEventListener('afterprint', window.close)
+      window.print()
+    }
+  }, [shouldPrint])
+}
+
 function InvoiceDetail({ provider, owner }) {
   const { id } = useParams()
   const { data: accountDetails } = useAccountDetails({ provider, owner })
   const { data: invoice } = useInvoice({ provider, owner, id })
   const backLinkUrl = `/account/${provider}/${owner}/invoices`
+
+  usePrintPage()
 
   return (
     <>
