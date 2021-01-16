b3a7620875123137d9d4b4e3cbdfb2422bf5bca6
Terry Smith <terry@codecov.io>
2021-01-16 16:33:57 -0400

    Add user update hook

--- src/services/users/hooks.js
+++ src/services/users/hooks.js
@@ -1,9 +1,15 @@
-import { useQuery } from 'react-query'
+import { useQuery, useMutation, useQueryClient } from 'react-query'
+import update from 'lodash/update'
+import findIndex from 'lodash/findIndex'
 
 import Api from 'shared/api'
 
 function getPathUsers({ provider, owner }) {
   return `/${provider}/${owner}/users/`
+}
+
+function patchPathUsers({ provider, owner, targetUser }) {
+  return `/${provider}/${owner}/users/${targetUser}/`
 }
 
 function fetchUsers({ provider, owner, query }) {
@@ -18,6 +24,42 @@ export function useUsers({ provider, owner, query }) {
     {
       keepPreviousData: true,
       staleTime: 5000,
+    }
+  )
+}
+
+export function useUpdateUser({ provider, owner, params }) {
+  const queryClient = useQueryClient()
+
+  return useMutation(
+    ({ targetUser, ...body }) => {
+      const path = patchPathUsers({ provider, owner, targetUser })
+      return Api.patch({ path, provider, body })
+    },
+    {
+      onSuccess: (user) => {
+        const prevData = queryClient.getQueryData([
+          'users',
+          provider,
+          owner,
+          params,
+        ])
+        const index = findIndex(
+          prevData.results,
+          ({ username }) => username === user.username
+        )
+        const currentData = update(
+          prevData,
+          `results[${index}].activated`,
+          () => user.activated
+        )
+
+        // update the local cache of account details from what the server returns
+        queryClient.setQueryData(
+          ['users', provider, owner, params],
+          currentData
+        )
+      },
     }
   )
 }
