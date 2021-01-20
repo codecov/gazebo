204c9b96ca69da26ba2746c88902b4b0a7d4f401
Dorian Amouroux <dor.amouroux@gmail.com>
2021-01-20 11:10:34 +0100

    text input forward the ref

--- src/ui/TextInput/TextInput.js
+++ src/ui/TextInput/TextInput.js
@@ -1,13 +1,16 @@
+import { forwardRef } from 'react'
 import cs from 'classnames'
 
-function TextInput({ className = '', ...props }) {
+const TextInput = forwardRef(({ className = '', ...props }, ref) => {
   const classes = cs(
     className,
     'block w-full px-6 py-2 rounded-full',
     'bg-gray-100 text-gray-900 caret-pink-500'
   )
 
-  return <input type="text" className={classes} {...props} />
-}
+  return <input ref={ref} type="text" className={classes} {...props} />
+})
+
+TextInput.displayName = 'TextInput'
 
 export default TextInput
