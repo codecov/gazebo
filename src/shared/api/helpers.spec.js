0d33d50ae082d6243785c38c22c8774a8f89681d
Terry Smith <terry@codecov.io>
2021-01-21 15:39:12 -0400

    Add snakeifyKeys test because asserting fetches was getting hairy.

---
+++ src/shared/api/helpers.spec.js
@@ -0,0 +1,16 @@
+import { snakeifyKeys } from './helpers'
+
+describe('snakeifyKeys', () => {
+  it('converts an object from CamelCast to SnakeCase', () => {
+    expect(snakeifyKeys({ fooBar: 1, bizBaz: 3 })).toStrictEqual({
+      foo_bar: 1,
+      biz_baz: 3,
+    })
+  })
+
+  it('else passes through', () => {
+    expect(snakeifyKeys([1, 2, 3])).toStrictEqual([1, 2, 3])
+    expect(snakeifyKeys(1)).toStrictEqual(1)
+    expect(snakeifyKeys('test')).toStrictEqual('test')
+  })
+})
