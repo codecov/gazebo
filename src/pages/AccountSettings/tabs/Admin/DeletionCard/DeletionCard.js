70796ed2d4282c7eec437e2f7742e82d7bad3d1f
Dorian Amouroux <dor.amouroux@gmail.com>
2021-01-26 13:08:12 +0100

    ui for the deletion card

--- src/pages/AccountSettings/tabs/Admin/DeletionCard/DeletionCard.js
+++ src/pages/AccountSettings/tabs/Admin/DeletionCard/DeletionCard.js
@@ -1,7 +1,37 @@
-import Card from 'ui/Card'
+import PropTypes from 'prop-types'
 
-function DeletionCard() {
-  return <Card className="p-10">DeletionCard</Card>
+import Card from 'ui/Card'
+import Button from 'ui/Button'
+
+function DeletionCard({ isPersonalSettings }) {
+  return (
+    <Card className="p-10 text-codecov-red">
+      <h2 className="border-b text-2xl pb-4 bold border-codecov-red">
+        Danger!
+      </h2>
+      {isPersonalSettings ? (
+        <>
+          <p className="mt-4 mb-6">
+            Erase all my personal content and personal projects.
+          </p>
+          <Button color="red">Erase account</Button>
+        </>
+      ) : (
+        <>
+          <p className="mt-4 mb-6">
+            Erase all my organization content and projects.
+          </p>
+          <Button Component="a" href="https://codecov.io/support" color="red">
+            Contact support
+          </Button>
+        </>
+      )}
+    </Card>
+  )
+}
+
+DeletionCard.propTypes = {
+  isPersonalSettings: PropTypes.bool.isRequired,
 }
 
 export default DeletionCard
