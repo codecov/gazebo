2b3086ff5e3025d96f45e67dbfecdf8ef14db670
Terry Smith <terry@codecov.io>
2021-01-27 15:00:43 -0400

    Add ability to embed elements to input + search icon

--- src/ui/TextInput/TextInput.stories.js
+++ src/ui/TextInput/TextInput.stories.js
@@ -1,10 +1,20 @@
+/* eslint-disable react/display-name */
 import TextInput from './TextInput'
+import Icon from '../Icon'
 
-const Template = (args) => <TextInput {...args} />
+const Template = (args) => {
+  return <TextInput {...args} />
+}
 
 export const NormalTextInput = Template.bind({})
 NormalTextInput.args = {
   placeholder: 'Text placeholder',
+}
+
+export const ExtraTextInput = Template.bind({})
+ExtraTextInput.args = {
+  ...NormalTextInput.args,
+  embedded: () => <Icon name="search" />,
 }
 
 export default {
