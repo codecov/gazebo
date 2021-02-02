0f8684e6f3f2e6e205ba243c5de0ea2a0a7c3bdd
Dorian Amouroux <dor.amouroux@gmail.com>
2021-02-02 14:59:25 +0100

    handle Gitlab subgroup on the billing UI

---
+++ src/pages/AccountSettings/tabs/BillingAndUsers/CurrentPlanCard/ActionsBilling.js
@@ -0,0 +1,88 @@
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+
+import Button from 'ui/Button'
+import githubLogo from 'assets/githublogo.png'
+import { useBaseUrl } from 'shared/router'
+import { accountDetailsPropType } from 'services/account'
+
+function shouldRenderCancelLink(accountDetails, isFreePlan) {
+  // cant cancel a free plan
+  if (isFreePlan) return false
+
+  // plan is already set for cancellation
+  if (accountDetails.subscriptionDetail?.cancelAtPeriodEnd) return false
+
+  return true
+}
+
+function ActionsBilling({ accountDetails, isFreePlan }) {
+  const baseUrl = useBaseUrl()
+
+  if (accountDetails.planProvider === 'github') {
+    return (
+      <div className="border-t border-gray-200 pb-4">
+        <p className="mt-4 mb-6 flex items-center text-sm">
+          <img
+            className="block mr-4"
+            alt="Github"
+            src={githubLogo}
+            height={32}
+            width={32}
+          />
+          Your account is configured via GitHub Marketplace
+        </p>
+        <div className="text-center">
+          <Button Component="a" href="https://github.com/marketplace/codecov">
+            Manage billing in GitHub
+          </Button>
+        </div>
+      </div>
+    )
+  }
+
+  if (accountDetails.rootOrganization) {
+    return (
+      <div className="border-t border-gray-200 pb-4">
+        <p className="mt-4 mb-6 text-sm">
+          This subgroup’s billing is managed by{' '}
+          {accountDetails.rootOrganization.username}.
+        </p>
+        <div className="text-center">
+          <Button
+            Component={Link}
+            to={`/account/gl/${accountDetails.rootOrganization.username}/billing`}
+          >
+            View Billing
+          </Button>
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <>
+      <Button Component={Link} to={`${baseUrl}upgrade`}>
+        {isFreePlan ? 'Upgrade plan to pro' : 'Change plan'}
+      </Button>
+      {shouldRenderCancelLink(accountDetails, isFreePlan) && (
+        <Button
+          to={`${baseUrl}cancel`}
+          Component={Link}
+          variant="text"
+          color="gray"
+          className="mt-4"
+        >
+          Cancel Plan
+        </Button>
+      )}
+    </>
+  )
+}
+
+ActionsBilling.propTypes = {
+  accountDetails: accountDetailsPropType.isRequired,
+  isFreePlan: PropTypes.bool.isRequired,
+}
+
+export default ActionsBilling
--- src/pages/AccountSettings/tabs/BillingAndUsers/CurrentPlanCard/CurrentPlanCard.js
+++ src/pages/AccountSettings/tabs/BillingAndUsers/CurrentPlanCard/CurrentPlanCard.js
@@ -1,92 +1,40 @@
-import { Link } from 'react-router-dom'
-
-import Button from 'ui/Button'
 import Card from 'ui/Card'
-import githubLogo from 'assets/githublogo.png'
-import { useBaseUrl } from 'shared/router'
-
 import { accountDetailsPropType } from 'services/account'
 
+import ActionsBilling from './ActionsBilling'
 import BenefitList from '../../../shared/BenefitList'
 
-function shouldRenderCancelLink(accountDetails, isFreePlan) {
-  // cant cancel a free plan
-  if (isFreePlan) return false
-
-  // plan is already set for cancellation
-  if (accountDetails.subscriptionDetail?.cancelAtPeriodEnd) return false
-
-  return true
-}
-
 function CurrentPlanCard({ accountDetails }) {
-  const isFreePlan = accountDetails.plan.value === 'users-free'
-  const baseUrl = useBaseUrl()
+  const plan = accountDetails.rootOrganization?.plan ?? accountDetails.plan
+  const isFreePlan = plan.value === 'users-free'
 
   return (
     <Card className="px-12 py-10 pb-4 mb-4">
-      <h3 className="text-lg text-pink-500 font-bold">
-        {accountDetails.plan.marketingName}
-      </h3>
+      <h3 className="text-lg text-pink-500 font-bold">{plan.marketingName}</h3>
       <h2 className="text-4xl uppercase">
-        {isFreePlan ? 'Free' : `$${accountDetails.plan.baseUnitPrice}`}
+        {isFreePlan ? 'Free' : `$${plan.baseUnitPrice}`}
       </h2>
       <div className="mt-8 text-sm border-gray-200">
         <BenefitList
           iconName="check"
           iconColor="text-pink-500"
-          benefits={accountDetails.plan.benefits}
+          benefits={plan.benefits}
         />
       </div>
       <hr className="my-6" />
       <p className="mt-4">
-        {accountDetails.activatedUserCount} / {accountDetails.plan.quantity}{' '}
-        Active users
+        {accountDetails.activatedUserCount ?? 0} /{' '}
+        {accountDetails.plan.quantity ?? 0} Active users
       </p>
       <p className="mt-3 text-codecov-red font-bold" data-test="inactive-users">
         {accountDetails.inactiveUserCount} Inactive users
       </p>
 
       <div className="flex flex-col items-center mt-6">
-        {accountDetails.planProvider === 'github' ? (
-          <div className="border-t border-gray-200 pb-4">
-            <p className="mt-4 mb-6 flex items-center text-sm">
-              <img
-                className="block mr-4"
-                alt="Github"
-                src={githubLogo}
-                height={32}
-                width={32}
-              />
-              Your account is configured via GitHub Marketplace
-            </p>
-            <div className="text-center">
-              <Button
-                Component="a"
-                href="https://github.com/marketplace/codecov"
-              >
-                Manage billing in GitHub
-              </Button>
-            </div>
-          </div>
-        ) : (
-          <>
-            <Button Component={Link} to={`${baseUrl}upgrade`}>
-              {isFreePlan ? 'Upgrade plan to pro' : 'Change plan'}
-            </Button>
-            {shouldRenderCancelLink(accountDetails, isFreePlan) && (
-              <Button
-                to={`${baseUrl}cancel`}
-                Component={Link}
-                variant="text"
-                color="gray"
-                className="mt-4"
-              >
-                Cancel Plan
-              </Button>
-            )}
-          </>
-        )}
+        <ActionsBilling
+          accountDetails={accountDetails}
+          isFreePlan={isFreePlan}
+        />
       </div>
     </Card>
   )
