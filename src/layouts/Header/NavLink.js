02e09760d7b9944d736d0437062522a5973a90c7
Felipe Ballesteros <felipe@codecov.io>
2021-02-19 09:23:28 -0800

    initial commit

--- src/layouts/Header/NavLink.js
+++ src/layouts/Header/NavLink.js
@@ -1,13 +1,17 @@
 import PropType from 'prop-types'
 import cs from 'classnames'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useParams } from 'react-router-dom'
 
-import Icon from 'ui/Icon'
+import githubLogo from 'assets/githublogo.png'
 
 const UserNav = {
   label: PropType.string.isRequired,
   imageUrl: PropType.string,
   iconName: PropType.string,
+}
+
+const providerIcons = {
+    'gh': githubLogo
 }
 
 export function UserNavLink({
@@ -18,6 +22,9 @@ export function UserNavLink({
   LinkComponent = 'a',
   ...props
 }) {
+
+  const { provider } = useParams()
+
   return (
     <LinkComponent
       className={cs('flex items-center py-2 text-sm', className)}
@@ -27,12 +34,7 @@ export function UserNavLink({
         <img className="h-4 w-4 rounded-full" src={imageUrl} alt={label} />
       )}
       {iconName && (
-        <Icon
-          testId="nav-link-icon"
-          name={iconName}
-          className="h-4 w-4"
-          color="text-white"
-        />
+        <img height={32} width={32} src={providerIcons[provider]} alt="provider-logo"/>
       )}
       <span className="pl-3">{label}</span>
     </LinkComponent>
@@ -51,18 +53,16 @@ export function MainNavLink({
   className,
   ...props
 }) {
+
+  const { provider } = useParams()
+
   return (
     <NavLink className={cs('flex items-center', className)} {...props}>
       {imageUrl && (
         <img className="h-6 w-6 rounded-full" src={imageUrl} alt={label} />
       )}
       {iconName && (
-        <Icon
-          testId="nav-link-icon"
-          name={iconName}
-          className="h-6 w-6"
-          color="text-white"
-        />
+        <img height={32} width={32} src={providerIcons[provider]} alt="provider-logo"/>
       )}
       <span className="pl-3">{label}</span>
     </NavLink>
