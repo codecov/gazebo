ed1a9b37de6b8035c6a8217580a92327ad34cff7
Terry Smith <terry@codecov.io>
2021-03-05 17:26:44 -0400

    Make AppLink pass a "to" prop as a href

--- src/ui/AppLink/AppLink.js
+++ src/ui/AppLink/AppLink.js
@@ -1,16 +1,31 @@
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
-function AppLink({ Component = Link, useRouter = true, children, ...props }) {
+function AppLink({
+  Component = Link,
+  useRouter = true,
+  children,
+  to,
+  ...props
+}) {
   if (!useRouter) {
-    return <a {...props}>{children}</a>
+    return (
+      <a href={to} {...props}>
+        {children}
+      </a>
+    )
   }
-  return <Component {...props}>{children}</Component>
+  return (
+    <Component to={to} {...props}>
+      {children}
+    </Component>
+  )
 }
 
 AppLink.propTypes = {
   useRouter: PropTypes.bool,
   Component: PropTypes.elementType,
+  to: PropTypes.string,
 }
 
 export default AppLink
