e40509e198da0aa3ce9d4e7c0c2f45e0e057716a
Dorian Amouroux <dor.amouroux@gmail.com>
2021-03-22 13:41:57 -0600

    add very basic button

---
+++ src/ui/Button/Button.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types'
+
+import AppLink from 'ui/AppLink'
+
+function Button({ to, ...props }) {
+  const className = 'bg-blue-500'
+
+  return to ? (
+    <AppLink {...to} {...props} className={className} />
+  ) : (
+    <button {...props} className={className} />
+  )
+}
+
+Button.propTypes = {
+  to: PropTypes.shape(AppLink.propTypes),
+}
+
+export default Button
