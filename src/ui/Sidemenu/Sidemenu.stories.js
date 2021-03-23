70c78cd16b60542711a07e4b807dc4172e052041
Dorian Amouroux <dor.amouroux@gmail.com>
2021-03-23 11:50:47 -0600

    add sidemenu component

---
+++ src/ui/Sidemenu/Sidemenu.stories.js
@@ -0,0 +1,36 @@
+import Sidemenu from './Sidemenu'
+import { MemoryRouter, Route, Switch } from 'react-router-dom'
+
+const Template = (args) => (
+  <MemoryRouter initialEntries={['/account/gh/codecov']}>
+    <Switch>
+      <Route path="/account/:provider/:owner" exact>
+        <Sidemenu {...args} />
+      </Route>
+      <Route path="/account/:provider/:owner/billing" exact>
+        <Sidemenu {...args} />
+      </Route>
+      <Route path="/account/:provider/:owner/access" exact>
+        <Sidemenu {...args} />
+      </Route>
+      <Route path="/account/:provider/:owner/yaml" exact>
+        <Sidemenu {...args} />
+      </Route>
+    </Switch>
+  </MemoryRouter>
+)
+
+export const SimpleSidemenu = Template.bind({})
+SimpleSidemenu.args = {
+  links: [
+    { pageName: 'accountAdmin', exact: true },
+    { pageName: 'billingAndUsers' },
+    { pageName: 'internalAccessTab' },
+    { pageName: 'internalYamlTab', children: 'Global Yaml' },
+  ],
+}
+
+export default {
+  title: 'Components/Sidemenu',
+  component: Sidemenu,
+}
