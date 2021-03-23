e7fcac9172b81093bc7e7648bd69e1b29d2dc7bb
Dorian Amouroux <dor.amouroux@gmail.com>
2021-03-23 13:29:25 -0600

    text input can take an icon prop

--- src/ui/TextInput/TextInput.js
+++ src/ui/TextInput/TextInput.js
@@ -3,13 +3,17 @@ import cs from 'classnames'
 import uniqueId from 'lodash/uniqueId'
 import defaultTo from 'lodash/defaultTo'
 
+import Icon from 'ui/Icon'
+
 const styles = {
   input:
     'block rounded border border-ds-gray-secondary px-3 py-1 text-sm w-full',
   label: 'block font-semibold mb-2',
+  iconWrapper:
+    'absolute text-ds-gray-quanternary left-2 h-full flex items-center',
 }
 
-function TextInput({ type = 'text', label, placeholder, ...props }) {
+function TextInput({ type = 'text', icon, label, placeholder, ...props }) {
   const id = uniqueId('text-input')
 
   // If no label, the placeholder is used as a hidden label for a11y
@@ -25,13 +29,22 @@ function TextInput({ type = 'text', label, placeholder, ...props }) {
       >
         {textLabel}
       </label>
-      <input
-        id={id}
-        type={type}
-        className={styles.input}
-        placeholder={placeholder}
-        {...props}
-      />
+      <div className="relative">
+        {icon && (
+          <div className={styles.iconWrapper}>
+            <Icon size="sm" variant="outline" name={icon} />
+          </div>
+        )}
+        <input
+          id={id}
+          type={type}
+          className={cs(styles.input, {
+            'pl-7': Boolean(icon),
+          })}
+          placeholder={placeholder}
+          {...props}
+        />
+      </div>
     </div>
   )
 }
@@ -39,6 +52,7 @@ function TextInput({ type = 'text', label, placeholder, ...props }) {
 TextInput.propTypes = {
   label: PropTypes.string,
   type: PropTypes.string,
+  icon: PropTypes.string,
   placeholder: PropTypes.string,
   onChange: PropTypes.func,
   value: PropTypes.string,
