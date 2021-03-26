c221606f6c0b4ff9ba56266bf8ad39119804262a
Dorian Amouroux <dor.amouroux@gmail.com>
2021-03-26 11:10:28 -0600

    add nav link spec and comment header

--- src/pages/AccountSettings/shared/Header/Header.js
+++ src/pages/AccountSettings/shared/Header/Header.js
@@ -1,26 +1,26 @@
 // import { useParams } from 'react-router-dom'
 
 // import MyContextSwitcher from 'layouts/MyContextSwitcher'
-import TabNavigation from 'ui/TabNavigation'
+// import TabNavigation from 'ui/TabNavigation'
 
+// Commented for now, as it consumes GraphQL endpoint which is not 100% ready for production
 function Header() {
+  return null
   // const { owner } = useParams()
 
-  return (
-    <>
-      {/* Commented for now, as it consumes GraphQL endpoint which is not
-        100% ready for production */}
-      {/* <MyContextSwitcher pageName="ownerInternal" activeContext={owner} /> */}
-      <div className="my-4">
-        <TabNavigation
-          tabs={[
-            { pageName: 'owner', children: 'Repos' },
-            { pageName: 'accountAdmin', children: 'Settings' },
-          ]}
-        />
-      </div>
-    </>
-  )
+  // return (
+  //   <>
+  // {/* <MyContextSwitcher pageName="ownerInternal" activeContext={owner} /> */}
+  //     <div className="my-4">
+  //       <TabNavigation
+  //         tabs={[
+  //           { pageName: 'owner', children: 'Repos' },
+  //           { pageName: 'accountAdmin', children: 'Settings' },
+  //         ]}
+  //       />
+  //     </div>
+  //   </>
+  // )
 }
 
 export default Header
