b30b80f16998a75168cb3f510b9de22934740619
Sabreen Noah <sabreen.noah@gmail.com>
2021-03-26 00:57:07 -0400

    add fields and defaults to segment tracking

--- src/services/tracking/hooks.js
+++ src/services/tracking/hooks.js
@@ -1,22 +1,51 @@
-import pick from 'lodash/pick'
-
 import { useUser } from 'services/user'
 
+/* eslint-disable */
 function setDataLayer(user) {
+  const user_data = user
+    ? {
+        ownerid: user.ownerid,
+        avatar: user.avatarUrl,
+        service_id: user.serviceId,
+        plan: user.plan,
+        staff: user.staff,
+        has_yaml: Boolean(user.yaml),
+        guest: false,
+        // Set default values so fields can be parsed by Salesforce
+        email: user.email || 'unknown@codecov.io',
+        name: user.name || 'unknown',
+        username: user.username || 'unknown',
+        student: user.student || false,
+        bot: user.bot || false,
+        delinquent: user.delinquent || false,
+        did_trial: user.didTrial || false,
+        private_access: user.privateAccess || false,
+        plan_provider: user.planProvider || '',
+        plan_user_count:
+          (typeof user.planUserCount == 'number' && user.planUserCount) || 5,
+        createdAt:
+          user.createstamp || new Date('2014-01-01 12:00:00').toISOString(),
+        updatedAt:
+          user.updatestamp || new Date('2014-01-01 12:00:00').toISOString(),
+        student_created_at:
+          user.studentCreatedAt ||
+          new Date('2014-01-01 12:00:00').toISOString(),
+        student_updated_at:
+          user.studentUpdatedAt ||
+          new Date('2014-01-01 12:00:00').toISOString(),
+      }
+    : { guest: true }
   const layer = {
     codecov: {
       app: {
         version: 'react-app',
       },
-      user: {
-        ...pick(user, 'ownerid', 'email', 'username', 'service', 'serviceId'),
-        guest: !user,
-      },
+      user: user_data,
     },
   }
   window.dataLayer = [layer]
 }
-
+/*eslint-enable*/
 export function useTracking() {
   return useUser({
     onSuccess: (user) => setDataLayer(user),
--- src/services/tracking/hooks.spec.js
+++ src/services/tracking/hooks.spec.js
@@ -38,14 +38,32 @@ describe('useTracking', () => {
     })
   }
 
-  describe('when the user is logged-in', () => {
+  describe('when the user is logged-in and has all data', () => {
     const user = {
       ownerid: 1,
+      yaml: { comment: 'on' },
+      avatar_url: 'avatar',
+      service_id: '123',
+      plan: 'plan',
+      staff: true,
       email: 'fake@test.com',
-      username: 'fake',
-      service: 'github',
-      serviceId: '1',
+      name: 'Eugene Onegin',
+      username: 'eugene_onegin',
+      student: true,
+      bot: true,
+      delinquent: true,
+      did_trial: true,
+      private_access: true,
+      plan_provider: 'provider',
+      plan_user_count: 1000,
+      createstamp: new Date('2017-01-01 12:00:00').toISOString(),
+      updatestamp: new Date('2018-01-01 12:00:00').toISOString(),
+      student_created_at: new Date('2019-01-01 12:00:00').toISOString(),
+      student_updated_at: new Date('2020-01-01 12:00:00').toISOString(),
     }
+
+    console.log('User: ' + JSON.stringify(user))
+
     beforeEach(() => {
       server.use(
         rest.get(`/internal/profile`, (req, res, ctx) => {
@@ -62,7 +80,93 @@ describe('useTracking', () => {
             version: 'react-app',
           },
           user: {
-            ...user,
+            ownerid: 1,
+            has_yaml: true,
+            avatar: 'avatar',
+            service_id: '123',
+            plan: 'plan',
+            staff: true,
+            email: 'fake@test.com',
+            name: 'Eugene Onegin',
+            username: 'eugene_onegin',
+            student: true,
+            bot: true,
+            delinquent: true,
+            did_trial: true,
+            private_access: true,
+            plan_provider: 'provider',
+            plan_user_count: 1000,
+            createdAt: new Date('2017-01-01 12:00:00').toISOString(),
+            updatedAt: new Date('2018-01-01 12:00:00').toISOString(),
+            student_created_at: new Date('2019-01-01 12:00:00').toISOString(),
+            student_updated_at: new Date('2020-01-01 12:00:00').toISOString(),
+            guest: false,
+          },
+        },
+      })
+    })
+  })
+
+  describe('when the user is logged-in but missing data', () => {
+    const user = {
+      ownerid: 3,
+      yaml: null,
+      avatar_url: 'avatar',
+      service_id: '123',
+      plan: 'plan',
+      staff: false,
+      email: null,
+      name: null,
+      username: null,
+      student: null,
+      bot: null,
+      delinquent: null,
+      did_trial: null,
+      private_access: null,
+      plan_provider: null,
+      plan_user_count: null,
+      createdAt: null,
+      updatedAt: null,
+      student_created_at: null,
+      student_updated_at: null,
+    }
+
+    beforeEach(() => {
+      server.use(
+        rest.get(`/internal/profile`, (req, res, ctx) => {
+          return res(ctx.status(200), ctx.json(user))
+        })
+      )
+      return setup()
+    })
+
+    it('set the user data in the dataLayer', () => {
+      expect(window.dataLayer[0]).toEqual({
+        codecov: {
+          app: {
+            version: 'react-app',
+          },
+          user: {
+            ownerid: 3,
+            has_yaml: false,
+            avatar: 'avatar',
+            service_id: '123',
+            plan: 'plan',
+            staff: false,
+            email: 'unknown@codecov.io',
+            name: 'unknown',
+            username: 'unknown',
+            student: false,
+            bot: false,
+            delinquent: false,
+            did_trial: false,
+            private_access: false,
+            plan_provider: '',
+            plan_user_count: 5,
+            createdAt: new Date('2014-01-01 12:00:00').toISOString(),
+            updatedAt: new Date('2014-01-01 12:00:00').toISOString(),
+            student_created_at: new Date('2014-01-01 12:00:00').toISOString(),
+            student_updated_at: new Date('2014-01-01 12:00:00').toISOString(),
             guest: false,
           },
         },
