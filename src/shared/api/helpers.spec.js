8367f560a5231e0cc760d36735b8fa3a944deefa
Terry Smith <terry@codecov.io>
2021-04-01 16:03:32 -0300

    Additional tests for generatePath just because.

--- src/shared/api/helpers.spec.js
+++ src/shared/api/helpers.spec.js
@@ -1,5 +1,23 @@
-import { snakeifyKeys, getHeaders } from './helpers'
+import { snakeifyKeys, getHeaders, generatePath } from './helpers'
 import * as Cookie from 'js-cookie'
+
+import config from 'config'
+describe('generatePath', () => {
+  it('generates a path without a query', () => {
+    expect(generatePath({ path: '/epic' })).toStrictEqual(
+      `${config.API_URL}/internal/epic`
+    )
+  })
+
+  it('generates a path with a query', () => {
+    expect(
+      generatePath({ path: '/epic', query: { rocket: 'league' } })
+    ).toStrictEqual(`${config.API_URL}/internal/epic?rocket=league`)
+    expect(
+      generatePath({ path: '/epic', query: { rocket: 'league', fort: 'nite' } })
+    ).toStrictEqual(`${config.API_URL}/internal/epic?rocket=league&fort=nite`)
+  })
+})
 
 describe('snakeifyKeys', () => {
   it('converts an object from CamelCast to SnakeCase', () => {
