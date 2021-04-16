4730e3ca2f291c2babc4504dec5aadf475f9dd37
Felipe Ballesteros <felipe@codecov.io>
2021-04-16 13:46:44 -0400

    Access tab main structure

--- src/pages/AccountSettings/tabs/Access/Access.js
+++ src/pages/AccountSettings/tabs/Access/Access.js
@@ -1,5 +1,51 @@
-function Access() {
-  return <div>Access</div>
+import Button from 'ui/Button'
+import Table from 'ui/Table'
+import PropTypes from 'prop-types'
+
+function Access({ tokens = [] }) {
+  const renderTokens = () => {
+    if (tokens.length <= 0)
+      return (
+        <span className="text-sm text-gray-octonary">
+          No tokens created yet
+        </span>
+      )
+    return <Table />
+  }
+
+  return (
+    <div className="flex flex-col">
+      <span className="text-lg font-semibold text-gray-octonary">
+        API Tokens
+      </span>
+      <div className="flex justify-between items-center">
+        <span
+          data-testid="tokens-summary"
+          className="text-sm text-gray-octonary"
+        >
+          Tokens created to access Codecov{"'"}s API as an authenticated user{' '}
+          <a
+            data-testid="tokens-docs-link"
+            rel="noreferrer"
+            target="_blank"
+            href="https://docs.codecov.io/reference#authorization"
+          >
+            learn more
+          </a>
+        </span>
+        <Button>Generate Token</Button>
+      </div>
+      <hr className="mt-3 mb-3 border-ds-gray-secondary" />
+      {renderTokens()}
+      <span className="mt-5 text-lg font-semibold text-gray-octonary">
+        Login Sessions
+      </span>
+    </div>
+  )
+}
+
+Access.propTypes = {
+  tokens: PropTypes.array.isRequired,
 }
 
 export default Access
