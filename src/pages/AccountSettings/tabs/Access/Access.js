e5802e10e55b50e9d5befcc3dfffe4c337fffa95
Felipe Ballesteros <felipe@codecov.io>
2021-04-20 16:18:51 -0400

    added session table

--- src/pages/AccountSettings/tabs/Access/Access.js
+++ src/pages/AccountSettings/tabs/Access/Access.js
@@ -1,8 +1,21 @@
 import Button from 'ui/Button'
 import Table from 'ui/Table'
 import PropTypes from 'prop-types'
+import { useSessions } from 'services/access'
+import formatDistance from 'date-fns/formatDistance'
 
-function Access({ tokens = [] }) {
+function Access({ tokens = [], owner, provider }) {
+  const { data } = useSessions({
+    provider,
+    owner,
+  })
+
+  const formatLastSeen = (lastseen) => {
+    const date = new Date(lastseen)
+    const today = new Date()
+    return formatDistance(date, today, 'MM/dd/yyyy')
+  }
+
   const renderTokens = () => {
     if (tokens.length <= 0)
       return (
@@ -11,6 +24,48 @@ function Access({ tokens = [] }) {
         </span>
       )
     return <Table />
+  }
+
+  const getSessionsTableData = () => {
+    return data.sessions.map((s) => ({
+      col1: (
+        <p className="text-center font-mono bg-ds-gray-secondary text-ds-gray-octonary font-bold">
+          {s.ip}
+        </p>
+      ),
+      col2: (
+        <span data-testid="sessions-lastseen">
+          {formatLastSeen(s.lastseen)}
+        </span>
+      ),
+      col3: <span data-testid="sessions-useragent">{s.useragent}</span>,
+      col4: <Button variant="danger">Revoke</Button>,
+    }))
+  }
+
+  const getSessionsTableColumns = () => {
+    return [
+      {
+        Header: 'IP',
+        accessor: 'col1',
+        width: 'w-3/12',
+      },
+      {
+        Header: 'Last Seen',
+        accessor: 'col2',
+        width: 'w-2/12',
+      },
+      {
+        Header: 'User Agent',
+        accessor: 'col3',
+        width: 'w-6/12',
+      },
+      {
+        Header: '',
+        accessor: 'col4',
+        width: 'w-1/6',
+      },
+    ]
   }
 
   return (
@@ -32,15 +87,23 @@ function Access({ tokens = [] }) {
       </div>
       <hr className="mt-4 mb-4 border-ds-gray-secondary" />
       {renderTokens()}
-      <h2 className="mt-8 text-lg font-semibold text-gray-octonary">
+      <h2 className="mt-8 mb-4 text-lg font-semibold text-gray-octonary">
         Login Sessions
       </h2>
+      <div className="max-w-screen-md">
+        <Table
+          data={getSessionsTableData()}
+          columns={getSessionsTableColumns()}
+        />
+      </div>
     </div>
   )
 }
 
 Access.propTypes = {
-  tokens: PropTypes.array.isRequired,
+  tokens: PropTypes.array,
+  provider: PropTypes.string.isRequired,
+  owner: PropTypes.string.isRequired,
 }
 
 export default Access
--- src/pages/AccountSettings/tabs/Access/Access.spec.js
+++ src/pages/AccountSettings/tabs/Access/Access.spec.js
@@ -1,6 +1,8 @@
-import { render, screen } from '@testing-library/react'
-
 import Access from './Access'
+import { render, screen } from '@testing-library/react'
+import { useSessions } from 'services/access'
+
+jest.mock('services/access')
 
 describe('AccessTab', () => {
   const defaultProps = {
@@ -9,6 +11,31 @@ describe('AccessTab', () => {
   }
 
   function setup(props) {
+    useSessions.mockReturnValue({
+      data: {
+        sessions: [
+          {
+            sessionid: 32,
+            ip: '172.21.0.1',
+            lastseen: '2021-04-19T18:35:05.451136Z',
+            useragent: null,
+            owner: 2,
+            type: 'login',
+            name: null,
+          },
+          {
+            sessionid: 6,
+            ip: '172.23.0.2',
+            lastseen: '2020-07-29T18:36:06.443999Z',
+            useragent:
+              'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
+            owner: 2,
+            type: 'login',
+            name: null,
+          },
+        ],
+      },
+    })
     const _props = { ...defaultProps, ...props }
     render(<Access {..._props} />)
   }
@@ -40,6 +67,27 @@ describe('AccessTab', () => {
       it('renders no tokens message', () => {
         const sessionsTitle = screen.getByText(/No tokens created yet/)
         expect(sessionsTitle).toBeInTheDocument()
+      })
+    })
+
+    describe('renders sessions table', () => {
+      it('renders sessions table revoke button', () => {
+        const buttons = screen.getAllByText(/Revoke/)
+        expect(buttons.length).toBe(2)
+      })
+      it('renders sessions table user agent', () => {
+        const useragents = screen.getAllByTestId('sessions-useragent')
+        expect(useragents.length).toBe(2)
+      })
+      it('renders sessions table lastseen', () => {
+        const lastseens = screen.getAllByTestId('sessions-lastseen')
+        expect(lastseens.length).toBe(2)
+      })
+      it('renders sessions table ips', () => {
+        const ip_1 = screen.getByText(/172.21.0.1/)
+        const ip_2 = screen.getByText(/172.23.0.2/)
+        expect(ip_1).toBeInTheDocument()
+        expect(ip_2).toBeInTheDocument()
       })
     })
   })
