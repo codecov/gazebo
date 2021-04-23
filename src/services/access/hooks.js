fec097a7678e50a25ce1aa9b15d0debec4061d4a
Felipe Ballesteros <felipe@codecov.io>
2021-04-23 14:30:36 -0400

    added create/destroy services

--- src/services/access/hooks.js
+++ src/services/access/hooks.js
@@ -1,6 +1,7 @@
 import Api from 'shared/api'
-import { useQuery } from 'react-query'
+import { useMutation, useQuery, useQueryClient } from 'react-query'
 import { mapEdges } from 'shared/utils/graphql'
+import _ from 'lodash'
 
 export function useSessions({ provider }) {
   const query = `
@@ -9,6 +10,7 @@ export function useSessions({ provider }) {
           sessions {
             edges {
               node{
+                sessionid
                 name
                 ip
                 lastseen
@@ -25,11 +27,47 @@ export function useSessions({ provider }) {
   return useQuery(['sessions', provider], () => {
     return Api.graphql({ provider, query }).then((res) => {
       const me = res?.data?.me
+      const data = _.groupBy(mapEdges(me.sessions), 'type')
       return me
         ? {
-            sessions: mapEdges(me.sessions),
+            sessions: data.login || [],
+            tokens: data.api || [],
           }
         : null
+    })
+  })
+}
+
+export function useDeleteSession({ provider }) {
+  const queryClient = useQueryClient()
+  return useMutation(({ sessionid }) => {
+    const query = `
+    mutation {
+        deleteSession(input: { sessionid: ${sessionid}}) {
+          error
+        }
+      }
+  `
+    return Api.graphql({ provider, query }).then((res) => {
+      queryClient.invalidateQueries('sessions')
+      return res?.data?.deleteSession?.error
+    })
+  })
+}
+
+export function useGenerateToken({ provider }) {
+  const queryClient = useQueryClient()
+  return useMutation(({ name }) => {
+    const query = `
+      mutation {
+        createApiToken(input: { name: ${name}}) {
+            error
+          }
+        }
+    `
+    return Api.graphql({ provider, query }).then((res) => {
+      queryClient.invalidateQueries('sessions')
+      return res?.data?.deleteSession?.error
     })
   })
 }
