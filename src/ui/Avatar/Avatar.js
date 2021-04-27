82f14e49b3ba90aa6f76536668b1bc5d749ec094
Pierce McEntagart <pierce@codecov.io>
2021-04-27 14:57:53 -0400

    Separate avatar from svg avatar; update stories.

--- src/ui/Avatar/Avatar.js
+++ src/ui/Avatar/Avatar.js
@@ -1,40 +1,19 @@
-let baseClasses =
-  'rounded-full h-8 w-8 flex items-center justify-center border-ds-grey-secondary border-2'
+import cs from 'classnames'
+import PropTypes from 'prop-types'
 
-// TODO -- what improvements are needed over existing avatar
+let baseClasses = 'rounded-full h-6 w-6 flex items-center justify-center'
+let borderedClasses = 'border-ds-grey-secondary border-2'
 
-function Avatar({ avatarUrl, alt, userName }) {
-  return (
-    <>
-      {avatarUrl && <img src={avatarUrl} alt={alt} className={baseClasses} />}
-      {!avatarUrl && (
-        <svg
-          data-testid="bb-avatar"
-          className="h-7 w-7 rounded-full"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 40 40"
-        >
-          <rect width="50px" height="50px" fill="#2684FF" />
-          <text
-            x="50%"
-            y="50%"
-            textAnchor="middle"
-            fill="white"
-            fontSize="17px"
-            dy=".3em"
-          >
-            {userName[0]}
-          </text>
-        </svg>
-      )}
-    </>
-  )
+function Avatar({ avatarUrl, alt, bordered }) {
+  const classes = cs(baseClasses, bordered ? borderedClasses : '')
+
+  return <img src={avatarUrl} alt={alt} className={classes} />
 }
 
 Avatar.propTypes = {
-  avatarUrl: String,
-  alt: String,
-  userName: String,
+  avatarUrl: PropTypes.string,
+  alt: PropTypes.string,
+  bordered: PropTypes.bool,
 }
 
 export default Avatar
--- src/ui/Avatar/Avatar.stories.js
+++ src/ui/Avatar/Avatar.stories.js
@@ -2,19 +2,16 @@ import Avatar from '.'
 
 const Template = (args) => <Avatar {...args} />
 
-export const WithUserImage = Template.bind({})
-WithUserImage.args = {
+const constantArgs = {
   avatarUrl: 'https://avatars0.githubusercontent.com/u/1060902?v=3&s=55',
   alt: 'avatar',
-  userName: 'andrewjaeger',
 }
 
-export const WithoutUserImage = Template.bind({})
-WithoutUserImage.args = {
-  avatarUrl: null,
-  alt: 'avatar',
-  userName: 'andrewjaeger',
-}
+export const Default = Template.bind({})
+Default.args = { ...constantArgs }
+
+export const Bordered = Template.bind({})
+Bordered.args = { bordered: true, ...constantArgs }
 
 export default {
   title: 'Components/Avatar',
