8f379c3530b8a59b9a8f522966d50f50c9caa7f0
Pierce McEntagart <pierce@codecov.io>
2021-05-17 18:04:56 -0400

    Update height to 56 px.

--- src/layouts/Header/Header.js
+++ src/layouts/Header/Header.js
@@ -3,7 +3,7 @@ import DesktopMenu from './DesktopMenu'
 function Header() {
   return (
     <header className="bg-ds-gray-octonary text-white">
-      <nav className="h-12 flex items-center container justify-between mx-auto">
+      <nav className="h-14 flex items-center container justify-between mx-auto">
         <DesktopMenu />
       </nav>
     </header>
