3472e747a3b7143bf5ed0cb6768de051702fb98a
Dorian Amouroux <dor.amouroux@gmail.com>
2021-05-18 17:58:32 +0200

    refactor active and inactive repo table

--- src/pages/HomePage/ReposTable/ReposTable.js
+++ src/pages/HomePage/ReposTable/ReposTable.js
@@ -1,9 +1,71 @@
 import PropTypes from 'prop-types'
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 
+import Progress from 'ui/Progress'
+import Table from 'ui/Table'
 import { useRepos } from 'services/repos/hooks'
+import AppLink from 'shared/AppLink'
+import OrgBreadcrumb from './OrgBreadcrumb'
 
-import ActiveReposTable from './ActiveReposTable'
-import InactiveReposTable from './InactiveReposTable'
+const tableActive = [
+  {
+    Header: 'Name',
+    accessor: 'title',
+    width: 'w-7/12',
+  },
+  {
+    Header: 'Last Updated',
+    accessor: 'lastUpdated',
+    width: 'w-2/12',
+  },
+  {
+    Header: 'Test Coverage',
+    accessor: 'coverage',
+    width: 'w-3/12',
+  },
+]
+
+const tableInactive = [
+  {
+    Header: 'Name',
+    accessor: 'title',
+    width: 'w-9/12',
+  },
+  {
+    Header: '',
+    accessor: 'notEnabled',
+    width: 'w-3/12',
+  },
+]
+
+function transformRepoToTable(repos, owner) {
+  // if we have an owner, then we don't need to show it on the repo title
+  const showRepoOwner = !owner
+
+  return repos.map((repo) => ({
+    title: <OrgBreadcrumb repo={repo} showRepoOwner={showRepoOwner} />,
+    lastUpdated: formatDistanceToNow(new Date(repo.updatedAt)),
+    coverage: repo?.coverage ? (
+      <Progress amount={repo.coverage} label={true} />
+    ) : (
+      <span className="text-gray-quinary text-sm">No data available</span>
+    ),
+    notEnabled: (
+      <span>
+        Not yet enabled{' '}
+        <AppLink
+          pageName="repo"
+          options={{
+            owner: repo.author.username,
+            repo: repo.name,
+          }}
+        >
+          setup repo
+        </AppLink>
+      </span>
+    ),
+  }))
+}
 
 function ReposTable({ active, searchValue, owner }) {
   const { data } = useRepos({
@@ -12,10 +74,10 @@ function ReposTable({ active, searchValue, owner }) {
     owner,
   })
 
-  return active ? (
-    <ActiveReposTable repos={data.repos} />
-  ) : (
-    <InactiveReposTable repos={data.repos} />
+  const dataTable = transformRepoToTable(data.repos, owner)
+
+  return (
+    <Table data={dataTable} columns={active ? tableActive : tableInactive} />
   )
 }
 
