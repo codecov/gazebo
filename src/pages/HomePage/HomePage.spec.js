4f74c00a69b402e10f15d1238563b204b811fe62
Dorian Amouroux <dor.amouroux@gmail.com>
2021-05-19 12:04:58 +0200

    fix some specs on homepage and add spec for repo title link

--- src/pages/HomePage/HomePage.spec.js
+++ src/pages/HomePage/HomePage.spec.js
@@ -6,6 +6,8 @@ import { MemoryRouter, Route } from 'react-router-dom'
 jest.mock('layouts/MyContextSwitcher', () => () => 'MyContextSwitcher')
 jest.mock('services/repos/hooks')
 jest.mock('./OrgControlTable/ResyncButton', () => () => 'ResyncButton')
+jest.mock('./Header', () => () => 'Header')
+jest.mock('./ReposTable', () => () => 'ReposTable')
 
 describe('HomePage', () => {
   function setup() {
@@ -34,6 +36,14 @@ describe('HomePage', () => {
 
     it('renders the children', () => {
       expect(screen.getByText(/Enabled/)).toBeInTheDocument()
+    })
+
+    it('renders the repo table', () => {
+      expect(screen.getByText(/ReposTable/)).toBeInTheDocument()
+    })
+
+    it('renders the header', () => {
+      expect(screen.getByText(/Header/)).toBeInTheDocument()
     })
   })
 })
--- src/pages/HomePage/ReposTable/RepoTitleLink.spec.js
+++ src/pages/HomePage/ReposTable/RepoTitleLink.spec.js
@@ -1,51 +1,92 @@
 import { render, screen } from 'custom-testing-library'
 import RepoTitleLink from './RepoTitleLink'
+import { MemoryRouter, Route } from 'react-router-dom'
 
 describe('RepoTitleLink', () => {
   let props
 
-  function setup(over = {}) {
-    props = {
-      ...over,
-    }
-    render(<RepoTitleLink {...props} />)
+  const repo = {
+    name: 'repo1',
+    author: {
+      username: 'owner1',
+    },
+    private: true,
   }
 
-  describe('display private', () => {
+  function setup(over = {}) {
+    props = {
+      showRepoOwner: true,
+      repo,
+      ...over,
+    }
+    render(
+      <MemoryRouter initialEntries={['/gh']}>
+        <Route path="/:provider">
+          <RepoTitleLink {...props} />
+        </Route>
+      </MemoryRouter>
+    )
+  }
+
+  describe('when the repository is private', () => {
     beforeEach(() => {
       setup({
         repo: {
-          name: 'repo1',
-          author: {
-            username: 'owner1',
-          },
+          ...repo,
           private: true,
         },
       })
     })
 
-    it('renders bar', () => {
+    it('renders the title', () => {
       expect(screen.getByText(/repo1/)).toBeInTheDocument()
-      expect(screen.getByText(/owner1/)).toBeInTheDocument()
+    })
+
+    it('renders the private tag', () => {
+      expect(screen.getByText(/Private/)).toBeInTheDocument()
     })
   })
 
-  describe('display public', () => {
+  describe('when the repo is public', () => {
     beforeEach(() => {
       setup({
         repo: {
-          name: 'repo2',
-          author: {
-            username: 'owner2',
-          },
+          ...repo,
           private: false,
         },
       })
     })
 
-    it('renders bar', () => {
-      expect(screen.getByText(/repo2/)).toBeInTheDocument()
-      expect(screen.getByText(/owner2/)).toBeInTheDocument()
+    it('renders the title', () => {
+      expect(screen.getByText(/repo1/)).toBeInTheDocument()
+    })
+
+    it('doesnt render the private tag', () => {
+      expect(screen.queryByText(/Private/)).not.toBeInTheDocument()
+    })
+  })
+
+  describe('when showRepoOwner is true', () => {
+    beforeEach(() => {
+      setup({
+        showRepoOwner: true,
+      })
+    })
+
+    it('renders the org name', () => {
+      expect(screen.getByText(/owner1/)).toBeInTheDocument()
+    })
+  })
+
+  describe('when showRepoOwner is false', () => {
+    beforeEach(() => {
+      setup({
+        showRepoOwner: false,
+      })
+    })
+
+    it('desont render the org name', () => {
+      expect(screen.queryByText(/owner1/)).not.toBeInTheDocument()
     })
   })
 })
