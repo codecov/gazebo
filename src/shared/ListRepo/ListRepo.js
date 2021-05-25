73c6b7d6e4caafce56dc497fb1fcbfc991459b8b
Felipe Ballesteros <felipe@codecov.io>
2021-05-25 16:07:00 +0400

    wip

--- src/shared/ListRepo/ListRepo.js
+++ src/shared/ListRepo/ListRepo.js
@@ -2,15 +2,34 @@ import PropTypes from 'prop-types'
 import { useState, Suspense } from 'react'
 
 import { orderingOptions } from 'services/repos'
+import qs from 'qs'
 
 import Spinner from 'ui/Spinner'
 import OrgControlTable from './OrgControlTable'
 import ReposTable from './ReposTable'
+import { useLocation, useHistory } from 'react-router-dom'
 
 function ListRepo({ owner }) {
   const [active, setActive] = useState(true)
   const [sortItem, setSortItem] = useState(orderingOptions[0])
   const [searchValue, setSearchValue] = useState('')
+  const { pathname } = useLocation()
+  const { push } = useHistory()
+
+  function handleQueryChange(item) {
+    const queryType = Object.keys(item)[0]
+    if (queryType === 'active') setActive(item.queryType)
+    else if (queryType === 'sort') setSortItem(item.queryType)
+    else if (queryType === 'search') setSearchValue(item.queryType)
+
+    const query = {
+      active,
+      sort: sortItem,
+      search: searchValue,
+      ...item,
+    }
+    push(`${pathname}?${qs.stringify(query)}`)
+  }
 
   const loadingState = (
     <div className="flex justify-center py-8">
@@ -22,10 +41,10 @@ function ListRepo({ owner }) {
     <>
       <OrgControlTable
         sortItem={sortItem}
-        setSortItem={setSortItem}
+        setSortItem={(sort) => handleQueryChange({ sort })}
         active={active}
-        setActive={setActive}
-        setSearchValue={setSearchValue}
+        setActive={(active) => handleQueryChange({ active })}
+        setSearchValue={(search) => handleQueryChange({ search })}
       />
       <Suspense fallback={loadingState}>
         <ReposTable
