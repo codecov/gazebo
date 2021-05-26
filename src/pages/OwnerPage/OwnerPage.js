65062a0d8416f1ecb3cd6fb359850449c377e786
Dorian Amouroux <dor.amouroux@gmail.com>
2021-05-26 18:18:48 +0200

    fetch owner in owner page and handle not found owner

--- src/pages/OwnerPage/OwnerPage.js
+++ src/pages/OwnerPage/OwnerPage.js
@@ -1,16 +1,23 @@
 import { useParams } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import ListRepo from 'shared/ListRepo'
+import { useOwner } from 'services/user'
+import NotFound from 'pages/NotFound'
 
 import Header from './Header'
 
 function HomePage({ active = false }) {
   const { owner } = useParams()
+  const { data: ownerData } = useOwner({ username: owner })
+
+  if (!ownerData) {
+    return <NotFound />
+  }
 
   return (
     <>
       <Header owner={owner} />
-      <ListRepo active={active} owner={owner} />
+      <ListRepo active={active} owner={ownerData.username} />
     </>
   )
 }
