e8e00a98d098ff85aa803edeef6dcd8a7ccb6491
Dorian Amouroux <dor.amouroux@gmail.com>
2021-05-26 10:42:17 +0200

    401 when unauthenticated on the homepage repo list

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -4,9 +4,7 @@ import ListRepo from 'shared/ListRepo'
 import Header from './Header'
 
 function HomePage() {
-  const { data: currentUser } = useUser({
-    onError: console.log,
-  })
+  const { data: currentUser } = useUser()
 
   return (
     <>
