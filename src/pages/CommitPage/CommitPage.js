32ec6dd23e510627c69ab832bc762cc27377f74a
Felipe Ballesteros <felipe@codecov.io>
2021-06-16 15:38:26 -0500

    Added lazy loading for YAML modal

--- src/pages/CommitPage/CommitPage.js
+++ src/pages/CommitPage/CommitPage.js
@@ -1,11 +1,14 @@
-import { useState } from 'react'
+import { useState, lazy, Suspense } from 'react'
 import { useParams } from 'react-router-dom'
 import Modal from 'ui/Modal'
 import CoverageReportCard from './CoverageReportCard'
 import UploadsCard from './UploadsCard'
 import CommitsTable from './CommitsTable'
 import Breadcrumb from 'ui/Breadcrumb'
-import YAMLViewer from './YAMLViewer'
+import LogoSpinner from 'old_ui/LogoSpinner'
+
+const YAMLViewer = lazy(() => import('./YAMLViewer'))
+
 const exampleCode = `
 codecov:\n  max_report_age: false\n  require_ci_to_pass: true\ncomment:\n  behavior: default\n  layout: reach,diff,flags,tree,reach\n  show_carryforward_flags: false\ncoverage:\n  precision: 2\n  range:\n  - 70.0\n  - 100.0\n  round: down\n  status:\n    changes: false\n    default_rules:\n      flag_coverage_not_uploaded_behavior: include\n    patch: true\n    project: true\ngithub_checks:\n  annotations: true\n
 `
@@ -13,6 +16,7 @@ codecov:\n  max_report_age: false\n  require_ci_to_pass: true\ncomment:\n  behav
 function CommitPage() {
   const { owner, repo } = useParams()
   const [showYAMLModal, setShowYAMLModal] = useState(false)
+  const loadingState = <LogoSpinner size={40} />
 
   return (
     <div className="flex flex-col">
@@ -51,7 +55,11 @@ function CommitPage() {
                 isOpen={true}
                 onClose={() => setShowYAMLModal(false)}
                 title="Yaml"
-                body={<YAMLViewer YAML={exampleCode} />}
+                body={
+                  <Suspense fallback={loadingState}>
+                    <YAMLViewer YAML={exampleCode} />
+                  </Suspense>
+                }
                 footer={
                   <span className="text-sm w-full text-left">
                     Includes default yaml, global yaml, and repo{' '}
