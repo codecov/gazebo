2c8ae0b2575667919217e2164dd70945ac3c1ad7
Terry Smith <terry@codecov.io>
2021-06-21 16:00:29 -0300

    Revert "Adds file view and a tree view link"

--- src/services/navigation/useNavLinks.js
+++ src/services/navigation/useNavLinks.js
@@ -127,17 +127,6 @@ function useNavLinks() {
       isExternalLink: true,
       text: 'Commits',
     },
-    treeView: {
-      path: (
-        { provider = p, owner = o, repo = r, tree = '' } = {
-          provider: p,
-          owner: o,
-          repo: r,
-        }
-      ) => `/${provider}/${owner}/${repo}/tree/${tree}`,
-      isExternalLink: true,
-      text: 'Tree View',
-    },
   }
 }
 
