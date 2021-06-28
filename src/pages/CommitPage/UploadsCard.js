cbe393f29f733b7be65fd307d60abddf462900e6
Felipe Ballesteros <felipe@codecov.io>
2021-06-28 11:15:44 -0500

    Added jobCode & use object.keys()

--- src/pages/CommitPage/UploadsCard.js
+++ src/pages/CommitPage/UploadsCard.js
@@ -3,56 +3,57 @@ import Icon from 'ui/Icon'
 import PropTypes from 'prop-types'
 import groupBy from 'lodash/groupBy'
 import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+import config from 'config'
 
 function UploadsCard({ setShowYAMLModal, data = [] }) {
   const uploads = groupBy(data, 'provider')
   function renderUploads() {
-    const _uploads = []
-    for (const key in uploads) {
-      _uploads.push(
-        <Fragment key={key}>
-          <span className="text-sm font-semibold w-full py-1 px-4">{key}</span>
-          {uploads[key].map((d, i) => (
-            <div
-              className="border-t border-ds-gray-secondary py-2 px-4 flex flex-col"
-              key={i}
-            >
-              <div className="flex justify-between">
-                <a
-                  href={d?.ciUrl}
-                  className="text-ds-blue-darker flex items-center mr-1"
-                >
-                  {d?.ciUrl?.split('/').pop()}
-                  <div className="mx-1 text-ds-gray-quinary">
-                    <Icon size="sm" name="external-link" />
-                  </div>
-                </a>
-                <span className="text-xs text-ds-gray-quinary">
-                  {formatDistanceToNow(new Date(d.createdAt), {
-                    addSuffix: true,
-                  })}
-                </span>
-              </div>
-              <div className="flex justify-between mt-1">
-                <div className="flex">
-                  {d.flags.length > 0 && (
-                    <>
-                      <Icon variant="solid" size="sm" name="flag" />
-                      <span className="text-xs ml-1">{d.flags[0]}</span>
-                    </>
-                  )}
+    return Object.keys(uploads).map((key) => (
+      <Fragment key={key}>
+        <span className="text-sm font-semibold w-full py-1 px-4">{key}</span>
+        {uploads[key].map((d, i) => (
+          <div
+            className="border-t border-ds-gray-secondary py-2 px-4 flex flex-col"
+            key={i}
+          >
+            <div className="flex justify-between">
+              <a
+                href={d?.ciUrl}
+                className="text-ds-blue-darker flex items-center mr-1"
+              >
+                {d?.jobCode}
+                <div className="mx-1 text-ds-gray-quinary">
+                  <Icon size="sm" name="external-link" />
                 </div>
-                <span className="text-xs justify-self-end text-ds-blue-darker">
-                  Download
-                </span>
-              </div>
+              </a>
+              <span className="text-xs text-ds-gray-quinary">
+                {formatDistanceToNow(new Date(d.createdAt), {
+                  addSuffix: true,
+                })}
+              </span>
             </div>
-          ))}
-        </Fragment>
-      )
-    }
-    return _uploads
+            <div className="flex justify-between mt-1">
+              <div className="flex">
+                {d?.flags?.length > 0 && (
+                  <>
+                    <Icon variant="solid" size="sm" name="flag" />
+                    <span className="text-xs ml-1">{d.flags[0]}</span>
+                  </>
+                )}
+              </div>
+              <a
+                href={`${config.API_URL}${d?.downloadUrl}`}
+                className="text-xs justify-self-end text-ds-blue-darker"
+              >
+                Download
+              </a>
+            </div>
+          </div>
+        ))}
+      </Fragment>
+    ))
   }
+
   return (
     <div className="flex w-full flex-col border border-ds-gray-secondary text-ds-gray-octonary">
       <div className="flex p-4 border-b border-ds-gray-secondary flex-col">
@@ -93,6 +94,7 @@ UploadsCard.propTypes = {
       flags: PropTypes.arrayOf(PropTypes.string),
       uploadType: PropTypes.string,
       updatedAt: PropTypes.string,
+      jobCode: PropTypes.string,
     })
   ),
   setShowYAMLModal: PropTypes.func.isRequired,
