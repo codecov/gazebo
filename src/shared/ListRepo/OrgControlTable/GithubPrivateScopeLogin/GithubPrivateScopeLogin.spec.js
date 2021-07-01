9243bae05d6fccec76a9f3cdaa6a0350bb8dd64e
Andrew Jaeger <mrjaeger00@gmail.com>
2021-07-01 14:50:19 -0400

    Allow users to login with private scope on org page

---
+++ src/shared/ListRepo/OrgControlTable/GithubPrivateScopeLogin/GithubPrivateScopeLogin.spec.js
@@ -0,0 +1,58 @@
+import { render, screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+
+import { useUser } from 'services/user'
+
+import GithubPrivateScopeLogin from './GithubPrivateScopeLogin'
+
+jest.mock('services/user')
+
+const user = {
+  privateAccess: false,
+  service: 'github',
+}
+
+describe('GithubPrivateScopeLogin', () => {
+  describe('should not render', () => {
+    it('if no user exists', () => {
+      useUser.mockReturnValue({ data: undefined })
+      render(<GithubPrivateScopeLogin />, { wrapper: MemoryRouter })
+
+      expect(screen.queryByText('add private')).not.toBeInTheDocument()
+    })
+
+    it('if the user has a service other than github', () => {
+      useUser.mockReturnValue({
+        data: {
+          ...user,
+          service: 'bitbucket',
+        },
+      })
+      render(<GithubPrivateScopeLogin />, { wrapper: MemoryRouter })
+
+      expect(screen.queryByText('add private')).not.toBeInTheDocument()
+    })
+
+    it('if the user already has private access', () => {
+      useUser.mockReturnValue({
+        data: {
+          ...user,
+          privateAccess: true,
+        },
+      })
+      render(<GithubPrivateScopeLogin />, { wrapper: MemoryRouter })
+
+      expect(screen.queryByText('add private')).not.toBeInTheDocument()
+    })
+  })
+
+  it('renders', () => {
+    useUser.mockReturnValue({ data: user })
+    render(<GithubPrivateScopeLogin />, { wrapper: MemoryRouter })
+
+    expect(screen.queryByText('add private')).toHaveAttribute(
+      'href',
+      'https://stage-web.codecov.dev/login/undefined?private=t'
+    )
+  })
+})
