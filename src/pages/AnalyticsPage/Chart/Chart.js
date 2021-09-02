fb06f798fa59fbba9654eb7022061319eee41c04
Terry Smith <terry@codecov.io>
2021-09-02 12:38:44 -0300

    Chart polish / ask feedback

--- src/pages/AnalyticsPage/Chart/Chart.js
+++ src/pages/AnalyticsPage/Chart/Chart.js
@@ -10,20 +10,38 @@ import { format } from 'date-fns'
 
 const TailwindFontFamily = 'Poppins, ui-sans-serif, system-ui'
 
+/* 
+  Victory uses inline styles (style={}) for charts. While we can disable inline
+  styling, for the first itteration I've just extending the default theme
+  rather then rolling our own.
+*/
+
 function Chart({ data = [] }) {
-  const formatDate = (d) => format(new Date(d), 'do MMM, yyyy')
+  const formatDate = (d) => format(new Date(d), 'MMM d, yyyy')
+  const formatDateShort = (d) => format(new Date(d), 'MMM do')
 
   return (
     <VictoryChart
-      width={768}
-      height={300}
       yDomain={[0, 100]}
       scale={{ x: 'time', y: 'linear' }}
+      // Custom padding tightens the whitespace around the chart.
+      padding={{
+        top: 20,
+        bottom: 80,
+        left: 45,
+        right: 0,
+      }}
       containerComponent={
+        // Veronoi is a algorythem that defines invisible mouse hover regions for data points.
+        // For line charts this is a better tooltip then using a normal hover target
+        // which is hard/tiny to hit.
+        // Refrence: https://en.wikipedia.org/wiki/Voronoi_diagram
         <VictoryVoronoiContainer
           voronoiDimension="x"
           labels={({ datum }) =>
-            `Coverage: ${datum.coverage}%, ${formatDate(datum.date)}`
+            `Coverage: ${Math.floor(datum.coverage, 2)}% ${formatDate(
+              datum.date
+            )}`
           }
           labelComponent={
             <VictoryTooltip
@@ -35,8 +53,21 @@ function Chart({ data = [] }) {
         />
       }
     >
-      <VictoryAxis tickFormat={(t) => ''} />
       <VictoryAxis
+        // Dates (x)
+        labelPlacement="vertical"
+        tickFormat={(t) => formatDateShort(t)}
+        fixLabelOverlap={true}
+        style={{
+          tickLabels: {
+            fontFamily: TailwindFontFamily,
+            angle: -45,
+            textAnchor: 'end',
+          },
+        }}
+      />
+      <VictoryAxis
+        // Coverage (y)
         style={{
           tickLabels: {
             fontFamily: TailwindFontFamily,
@@ -51,7 +82,6 @@ function Chart({ data = [] }) {
         y="coverage"
         style={{
           data: { stroke: '#F01F7A' },
-          parent: { border: '1px solid #ccc' },
         }}
         data={data}
       />
