372e4084266f5b6f98941d728a1d8622c663962b
Adrian Viquez <adrian@codecov.io>
2021-09-02 17:06:47 -0700

    initial implementation of multiselect and adjustments to repoTable to make it work

--- src/pages/AnalyticsPage/AnalyticsPage.js
+++ src/pages/AnalyticsPage/AnalyticsPage.js
@@ -4,17 +4,19 @@ import Tabs from './Tabs'
 import { orderingOptions, nonActiveOrderingOptions } from 'services/repos'
 import { useLocationParams } from 'services/navigation'
 import ReposTable from 'shared/ListRepo/ReposTable'
+import ChartSelectors from './ChartSelectors'
 import { useParams } from 'react-router-dom'
 import { useOwner } from 'services/user'
 
 const defaultQueryParams = {
   search: '',
+  repos: [],
   ordering: orderingOptions[0]['ordering'],
   direction: orderingOptions[0]['direction'],
 }
 
 function AnalyticsPage() {
-  const { params } = useLocationParams(defaultQueryParams)
+  const { params, updateParams } = useLocationParams(defaultQueryParams)
   const { owner } = useParams()
   const { data: ownerData } = useOwner({ username: owner })
 
@@ -35,11 +37,19 @@ function AnalyticsPage() {
     <div className="flex flex-col gap-4">
       <Header owner={ownerData} />
       <div>{ownerData?.isCurrentUserPartOfOrg && <Tabs />}</div>
+      <ChartSelectors
+        params={params}
+        updateParams={updateParams}
+        owner={owner}
+        active={true}
+        sortItem={sortItem}
+      />
       <ReposTable
         owner={owner}
         active={true}
         sortItem={sortItem}
         searchValue={params.search}
+        filterValues={params.repos}
       />
     </div>
   )
---
+++ src/pages/AnalyticsPage/ChartSelectors/ChartSelectors.js
@@ -0,0 +1,50 @@
+import PropTypes from 'prop-types'
+import { useRepos } from 'services/repos/hooks'
+import MultiSelect from 'ui/MultiSelect'
+import { useState } from 'react'
+
+function formatDataForMultiselect(repos) {
+  return repos.map((repo) => repo.name)
+}
+
+function ChartSelectors({ params, updateParams, owner, active, sortItem }) {
+  const { search, repos } = params
+  const [selectedRepos, setSelectedRepos] = useState(repos)
+  const { data } = useRepos({
+    active,
+    sortItem,
+    term: search,
+    owner,
+  })
+
+  // TODO: Ensure this brings all data and bypasses pagination
+  const items = formatDataForMultiselect(data?.repos)
+
+  const onChangeHandler = (item) => {
+    setSelectedRepos(item)
+    updateParams({ repos: item })
+  }
+
+  return (
+    <div className="flex flex-col w-52 gap-3">
+      <span className="font-semibold">Repositories</span>
+      <MultiSelect
+        ariaName="Select flags to filter"
+        items={items}
+        onChange={onChangeHandler}
+        resourceName="Repo"
+        selectedItems={selectedRepos}
+      />
+    </div>
+  )
+}
+
+ChartSelectors.propTypes = {
+  params: PropTypes.object.isRequired,
+  updateParams: PropTypes.func.isRequired,
+  owner: PropTypes.string.isRequired,
+  active: PropTypes.bool.isRequired,
+  sortItem: PropTypes.object.isRequired,
+}
+
+export default ChartSelectors
