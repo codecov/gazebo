f7d755f08d5a2c968e94759e3542466e55d7a127
Adrian Viquez <adrian@codecov.io>
2021-09-16 11:10:21 -0600

    properly cover the datepicker test

--- src/ui/Datepicker/Datepicker.spec.js
+++ src/ui/Datepicker/Datepicker.spec.js
@@ -3,19 +3,22 @@ import { render, screen, fireEvent } from '@testing-library/react'
 import Datepicker from './Datepicker'
 
 describe('Datepicker', () => {
+  let props
+
   function setup({ params, updateParams }) {
     render(<Datepicker params={params} updateParams={updateParams} />)
   }
 
   describe('DateRangePicker component with start and end date', () => {
     beforeEach(() => {
-      setup({
+      props = {
         params: {
           startDate: '10/02/2021',
           endDate: '12/05/2021',
         },
-        updateParams: () => {},
-      })
+        updateParams: jest.fn(),
+      }
+      setup(props)
     })
 
     it('has a changeable start date', () => {
@@ -31,24 +34,15 @@ describe('Datepicker', () => {
       fireEvent.change(endDate, { target: { value: '10/01/2021' } })
       expect(endDate.value).toBe('10/01/2021')
     })
-  })
 
-  describe('DateRangePicker component with null start and end dates', () => {
-    beforeEach(() => {
-      setup({
-        params: {
-          startDate: null,
-          endDate: null,
-        },
-        updateParams: () => {},
-      })
-    })
-
-    it('renders an empty string for start and end date', () => {
+    it('renders an empty string for start and end date when value is changed to null', () => {
       const startDate = screen.getByRole('textbox', { name: 'Start Date' })
       const endDate = screen.getByRole('textbox', { name: 'End Date' })
+      fireEvent.change(startDate, { target: { value: null } })
+      fireEvent.change(endDate, { target: { value: null } })
       expect(startDate.value).toBe('')
       expect(endDate.value).toBe('')
+      expect(props.updateParams).toBeCalled()
     })
   })
 })
