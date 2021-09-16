f9a7f0ccb62d8db77fa87419b439dc2d23af233e
Adrian Viquez <adrian@codecov.io>
2021-09-16 10:54:11 -0600

    covered untested addition to datepicker

--- src/ui/Datepicker/Datepicker.spec.js
+++ src/ui/Datepicker/Datepicker.spec.js
@@ -7,7 +7,7 @@ describe('Datepicker', () => {
     render(<Datepicker params={params} updateParams={updateParams} />)
   }
 
-  describe('renders the DateRangePicker component', () => {
+  describe('DateRangePicker component with start and end date', () => {
     beforeEach(() => {
       setup({
         params: {
@@ -30,6 +30,25 @@ describe('Datepicker', () => {
       expect(endDate.value).toBe('12/05/2021')
       fireEvent.change(endDate, { target: { value: '10/01/2021' } })
       expect(endDate.value).toBe('10/01/2021')
+    })
+  })
+
+  describe('DateRangePicker component with null start and end dates', () => {
+    beforeEach(() => {
+      setup({
+        params: {
+          startDate: null,
+          endDate: null,
+        },
+        updateParams: () => {},
+      })
+    })
+
+    it('renders an empty string for start and end date', () => {
+      const startDate = screen.getByRole('textbox', { name: 'Start Date' })
+      const endDate = screen.getByRole('textbox', { name: 'End Date' })
+      expect(startDate.value).toBe('')
+      expect(endDate.value).toBe('')
     })
   })
 })
