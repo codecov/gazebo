9649e5a59812de2397cbee16284cbc2c4fdb93fb
RulaKhaled <rula@codecov.io>
2021-10-06 17:32:38 +0300

    use flex to fix mobile bug

--- src/shared/ListRepo/OrgControlTable/OrgControlTable.js
+++ src/shared/ListRepo/OrgControlTable/OrgControlTable.js
@@ -38,7 +38,7 @@ function OrgControlTable({
   )
 
   return (
-    <div className="flex items-center h-8 my-4">
+    <div className="flex items-center h-auto my-4 flex-col md:flex-row">
       <div className="flex">
         <div className="w-52 mr-2">
           <Select
@@ -56,17 +56,19 @@ function OrgControlTable({
           />
         </div>
       </div>
-      {canRefetch && <ResyncButton />}
-      <div className="ml-auto flex items-center">
-        <GithubPrivateScopeLogin />
-        <OptionButton
-          active={active ? optionButtonOptions[0] : optionButtonOptions[1]}
-          onChange={(option) =>
-            setActive(option.text === optionButtonOptions[0].text)
-          }
-          options={optionButtonOptions}
-        />
-      </div>
+      <span className="flex flex-auto gap-4 mt-2 md:gap-0 md:m-0">
+        {canRefetch && <ResyncButton />}
+        <div className="ml-auto flex items-center">
+          <GithubPrivateScopeLogin />
+          <OptionButton
+            active={active ? optionButtonOptions[0] : optionButtonOptions[1]}
+            onChange={(option) =>
+              setActive(option.text === optionButtonOptions[0].text)
+            }
+            options={optionButtonOptions}
+          />
+        </div>
+      </span>
     </div>
   )
 }
