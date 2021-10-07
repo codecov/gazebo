fd5a3a02bbac4d6ef8d0eedfd56719772328a485
RulaKhaled <rula@codecov.io>
2021-10-07 17:12:45 +0300

    new site footer

--- src/layouts/Footer/Footer.js
+++ src/layouts/Footer/Footer.js
@@ -6,21 +6,29 @@ function Footer() {
   const { terms, privacy, security, gdpr, pricing, support, docs, enterprise } =
     useStaticNavLinks()
 
-  const leftMenu = [{ text: '© 2021 Codecov' }, terms, privacy, security, gdpr]
+  const leftMenu = [
+    { text: '© 2021 Codecov', classes: 'text-ds-gray-quinary' },
+    terms,
+    privacy,
+    security,
+    gdpr,
+  ]
   const rightMenu = [pricing, support, docs, enterprise]
   return (
-    <footer className="flex-none bg-codecov-footer">
-      <nav className="container flex flex-wrap flex-col lg:flex-row justify-center sm:justify-between text-white py-6 px-3 items-center">
+    <footer className="flex-none">
+      <nav className="container flex flex-wrap flex-col lg:flex-row justify-center sm:justify-between text-white py-6 px-3 items-center border-t border-ds-gray-tertiary">
         <ul className="flex-1 flex justify-center lg:justify-start items-center">
-          {leftMenu.map((data, i) => (
-            <FooterItem key={`footer-left-${i}`} {...data} />
-          ))}
+          {leftMenu.map((data, i) => {
+            data.classes = data.classes || 'text-ds-blue-darker'
+            return <FooterItem key={`footer-left-${i}`} {...data} />
+          })}
         </ul>
-        <CodecovIcon className="text-pink-500 mr-2" />
+        <CodecovIcon className="text-ds-gray-quinary mr-2 cursor-pointer" />
         <ul className="flex-1 flex justify-center lg:justify-end items-center">
-          {rightMenu.map((data, i) => (
-            <FooterItem key={`footer-right-${i}`} {...data} />
-          ))}
+          {rightMenu.map((data, i) => {
+            data.classes = data.classes || 'text-ds-blue-darker'
+            return <FooterItem key={`footer-right-${i}`} {...data} />
+          })}
         </ul>
       </nav>
     </footer>
--- src/layouts/Footer/FooterItem.js
+++ src/layouts/Footer/FooterItem.js
@@ -1,8 +1,8 @@
 import PropType from 'prop-types'
 
-export function FooterItem({ text, path }) {
+export function FooterItem({ text, path, classes }) {
   return (
-    <li className="flex justify-center">
+    <li className={`flex justify-center ${classes} `}>
       {path ? (
         <a
           className="p-4 no-underline hover:underline hover:text-blue-400"
@@ -20,4 +20,5 @@ export function FooterItem({ text, path }) {
 FooterItem.propTypes = {
   text: PropType.string.isRequired,
   path: PropType.func,
+  classes: PropType.string,
 }
