f917af86addbc0f9bcbd2cfb9e75165a9da696cf
RulaKhaled <rula@codecov.io>
2021-10-09 14:49:26 +0300

    add tests to repoPage

---
+++ src/pages/RepoPage/RepoPage.spec.js
@@ -0,0 +1,53 @@
+import { render, screen } from '@testing-library/react'
+import { useLocation, Route, MemoryRouter } from 'react-router-dom'
+import RepoPage from '.'
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useParams: () => ({ provider: 'gh', owner: 'codecov', repo: 'test' }),
+  useRouteMatch: () => ({
+    path: '/:provider/:owner/:repo',
+    url: '/gh/codecov/test',
+  }),
+  useLocation: jest.fn(),
+}))
+
+describe('RepoPage', () => {
+  function setup() {
+    useLocation.mockReturnValue({ pathname: 'gh/codecov/test' })
+    render(
+      <MemoryRouter initialEntries={['/gh']}>
+        <Route>
+          <RepoPage />
+        </Route>
+      </MemoryRouter>
+    )
+  }
+
+  describe('when rendered with a repo and an owner', () => {
+    beforeEach(() => {
+      setup()
+    })
+
+    it('renders the title with the owner name', () => {
+      const owner = screen.getByText(/codecov/)
+      expect(owner).toBeInTheDocument()
+    })
+
+    it('renders the title with the repo name', () => {
+      screen.debug()
+      const repo = screen.getByText(/test/)
+      expect(repo).toBeInTheDocument()
+    })
+
+    it('renders with overview tap', () => {
+      const tab = screen.getByText(/Overview/)
+      expect(tab).toBeInTheDocument()
+    })
+
+    it('renders with settings tap', () => {
+      const tab = screen.getByText(/Settings/)
+      expect(tab).toBeInTheDocument()
+    })
+  })
+})
