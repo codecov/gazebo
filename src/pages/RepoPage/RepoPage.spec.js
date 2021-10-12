50fe746005dc9e1d40f8b40e5046db9873437f60
RulaKhaled <rula@codecov.io>
2021-10-12 21:54:35 +0300

    check the private test

--- src/pages/RepoPage/RepoPage.spec.js
+++ src/pages/RepoPage/RepoPage.spec.js
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react'
 import { useLocation, Route, MemoryRouter } from 'react-router-dom'
 import RepoPage from '.'
 import { QueryClientProvider, QueryClient } from 'react-query'
+import { useRepo } from 'services/repo/hooks'
 
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
@@ -13,8 +14,40 @@ jest.mock('react-router-dom', () => ({
   useLocation: jest.fn(),
 }))
 
+jest.mock('services/repo/hooks')
+
+// const result = {
+//   activated: false,
+//   active: false,
+//   author: {
+//     avatarUrl: 'https://avatars0.githubusercontent.com/u/8226205?v=3&s=55',
+//     service: 'github',
+//     username: 'codecov',
+//     name: 'codecov',
+//     stats: {},
+//   },
+//   bot: null,
+//   branch: 'master',
+//   canEdit: true,
+//   canView: true,
+//   fork: null,
+//   hookid: null,
+//   imageToken: 'XQV51VID4C',
+//   language: 'python',
+//   latestCommit: null,
+//   name: 'codecov-circleci-orb',
+//   private: false,
+//   repoid: 17,
+//   serviceId: '157147600',
+//   updatestamp: '2021-10-11T15:56:18.422715Z',
+//   uploadToken: 'token',
+//   usingIntegration: false,
+//   yaml: null,
+// }
+
 describe('RepoPage', () => {
-  function setup() {
+  function setup(repo) {
+    useRepo.mockReturnValue({ data: repo })
     const queryClient = new QueryClient()
     useLocation.mockReturnValue({ pathname: 'gh/codecov/test' })
     render(
@@ -30,7 +63,9 @@ describe('RepoPage', () => {
 
   describe('when rendered', () => {
     beforeEach(() => {
-      setup()
+      setup({
+        private: false,
+      })
     })
 
     it('renders the title with the owner name', () => {
@@ -41,6 +76,34 @@ describe('RepoPage', () => {
     it('renders the title with the repo name', () => {
       const repo = screen.getByText(/test/)
       expect(repo).toBeInTheDocument()
+    })
+
+    it('does not render Private span', () => {
+      const privateSpan = screen.queryByText(/Private/)
+      expect(privateSpan).not.toBeInTheDocument()
+    })
+  })
+
+  describe('when rendered with private repo', () => {
+    beforeEach(() => {
+      setup({
+        private: true,
+      })
+    })
+
+    it('renders the title with the owner name', () => {
+      const owner = screen.getByText(/codecov/)
+      expect(owner).toBeInTheDocument()
+    })
+
+    it('renders the title with the repo name', () => {
+      const repo = screen.getByText(/test/)
+      expect(repo).toBeInTheDocument()
+    })
+
+    it('renders the block private', () => {
+      const owner = screen.getByText(/Private/)
+      expect(owner).toBeInTheDocument()
     })
   })
 })
