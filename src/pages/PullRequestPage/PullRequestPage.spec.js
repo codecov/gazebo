9cf8fda58e286106bce395adb7ed17fed07b2bfa
Dorian Amouroux <dorian@codecov.io>
2021-10-14 11:49:32 +0200

    add simple spec on the pull request page boilerplate

---
+++ src/pages/PullRequestPage/PullRequestPage.spec.js
@@ -0,0 +1,40 @@
+import { render, screen } from 'custom-testing-library'
+import { MemoryRouter, Route } from 'react-router-dom'
+
+import PullRequestPage from './PullRequestPage'
+
+describe('PullRequestPage', () => {
+  function setup() {
+    render(
+      <MemoryRouter initialEntries={['/gh/test-org/test-repo/pull/12']}>
+        <Route path="/:provider/:owner/:repo/pull/:pullid/">
+          <PullRequestPage />
+        </Route>
+      </MemoryRouter>
+    )
+  }
+
+  describe('when rendered', () => {
+    beforeEach(() => {
+      setup()
+    })
+
+    it('renders the Breadcrumb', () => {
+      expect(
+        screen.getByRole('link', {
+          name: /test-org/i,
+        })
+      ).toBeInTheDocument()
+      expect(
+        screen.getByRole('link', {
+          name: /test-repo/i,
+        })
+      ).toBeInTheDocument()
+      expect(
+        screen.getByRole('link', {
+          name: /pulls/i,
+        })
+      ).toBeInTheDocument()
+    })
+  })
+})
