b6e68cfe4e9b61dc878debc42463e932b82c3d2a
RulaKhaled <rula@codecov.io>
2021-11-03 14:33:32 +0200

    fix change check

--- src/pages/RepoPage/PullsPage/PullsTable/PullsTable.js
+++ src/pages/RepoPage/PullsPage/PullsTable/PullsTable.js
@@ -79,7 +79,7 @@ Coverage.propTypes = {
 }
 
 const Change = ({ change }) =>
-  !isNaN(change) && (
+  typeof change === 'number' && (
     <div className="flex justify-end w-full font-semibold">
       <span className={change <= 0 ? 'nf bg-red-100' : 'bg-green-100'}>
         {change}%
