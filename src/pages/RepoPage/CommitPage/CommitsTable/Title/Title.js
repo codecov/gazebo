a9121722c6b6bd91ddaf2706cf78fefbeb50c4d3
RulaKhaled <rula@codecov.io>
2021-11-16 16:03:37 +0200

    resolve warning

--- src/pages/RepoPage/CommitPage/CommitsTable/Title/Title.js
+++ src/pages/RepoPage/CommitPage/CommitsTable/Title/Title.js
@@ -23,7 +23,7 @@ const Title = ({ commit }) => {
           user={
             ownerData || {
               avatarUrl: 'https://avatars0.githubusercontent.com/u/?v=3&s=55',
-              username: null,
+              username: 'default',
             }
           }
           bordered
