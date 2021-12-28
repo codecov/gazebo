1197669debab8765c9816f6f512562ea77754741
RulaKhaled <rula@codecov.io>
2021-12-28 12:42:59 +0200

    add more tests

--- src/pages/RepoPage/RepoPage.spec.js
+++ src/pages/RepoPage/RepoPage.spec.js
@@ -1,12 +1,14 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import { Route, MemoryRouter } from 'react-router-dom'
 import RepoPage from '.'
 import { QueryClientProvider, QueryClient } from 'react-query'
 import { useRepo } from 'services/repo/hooks'
 import { useCommits } from 'services/commits'
+import { useBranches } from 'services/branches'
 
 jest.mock('services/repo/hooks')
 jest.mock('services/commits')
+jest.mock('services/branches')
 
 const commits = [
   {
@@ -32,10 +34,23 @@ const commits = [
   },
 ]
 
+const branches = [
+  {
+    name: 'main',
+  },
+  {
+    name: 'test1',
+  },
+  {
+    name: 'test2',
+  },
+]
+
 describe('RepoPage', () => {
   function setup({ repo, commits = [], path = '/' }) {
     useRepo.mockReturnValue({ data: { repo } })
     useCommits.mockReturnValue({ data: commits })
+    useBranches.mockReturnValue({ data: branches })
     const queryClient = new QueryClient()
     render(
       <MemoryRouter initialEntries={[`/gh/codecov/Test/${path}`]}>
@@ -71,6 +86,11 @@ describe('RepoPage', () => {
     it('does not render the branch in the breadcrumb', () => {
       const branch = screen.queryByText(/main/)
       expect(branch).not.toBeInTheDocument()
+    })
+
+    it('does not render the branch context selector', () => {
+      const select = screen.queryByText('Branch Context')
+      expect(select).not.toBeInTheDocument()
     })
   })
 
@@ -152,6 +172,64 @@ describe('RepoPage', () => {
     it('renders the branch in the breadcrumb', () => {
       const branch = screen.getAllByText(/main/)[0]
       expect(branch).toBeInTheDocument()
+    })
+
+    it('renders the branch context selector label', () => {
+      const label = screen.getByText('Branch Context')
+      expect(label).toBeInTheDocument()
+    })
+
+    it('renders the branch context selector', () => {
+      const select = screen.getByRole('button', {
+        name: 'main chevron-down.svg',
+      })
+      expect(select).toBeInTheDocument()
+    })
+  })
+
+  describe('when click on the selector in the commits page', () => {
+    beforeEach(() => {
+      setup({
+        repo: {
+          private: true,
+        },
+        path: 'commits',
+        commits,
+      })
+      const select = screen.getByRole('button', {
+        name: 'main chevron-down.svg',
+      })
+      fireEvent.click(select)
+    })
+
+    it('renders the options of select branch', () => {
+      const branch = screen.getByText(/test1/)
+      expect(branch).toBeInTheDocument()
+      const branch2 = screen.getByText(/test2/)
+      expect(branch2).toBeInTheDocument()
+    })
+  })
+
+  describe('when seelct a branch of the selector in the commits page', () => {
+    beforeEach(() => {
+      setup({
+        repo: {
+          private: true,
+        },
+        path: 'commits',
+        commits,
+      })
+      const select = screen.getByRole('button', {
+        name: 'main chevron-down.svg',
+      })
+      fireEvent.click(select)
+      const branch = screen.getByText(/test1/)
+      fireEvent.click(branch)
+    })
+
+    it('renders the name of the branch in the breadcrumb', () => {
+      const branch = screen.getAllByText(/test1/)
+      expect(branch.length).toEqual(2)
     })
   })
 })
