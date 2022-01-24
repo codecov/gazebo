e328dc98019b99156e09bd3169270f5ba8742e04
Terry Smith <terry@codecov.io>
2022-01-24 11:31:55 -0400

    Test the token components

--- src/pages/RepoPage/NewRepoTab/Token/PrivateRepoScope.js
+++ src/pages/RepoPage/NewRepoTab/Token/PrivateRepoScope.js
@@ -7,7 +7,7 @@ const PrivateRepoScope = ({ token }) => (
     <p className="text-base">
       Copy the below token and set it in your CI environment variables.
     </p>
-    <p className="flex flex-row justify-center text-s mt-4">
+    <p className="flex flex-row justify-center text-sm mt-4">
       Codecov Token={' '}
       <span className="font-mono bg-ds-gray-secondary text-ds-gray-octonary h-auto xl:h-5">
         {token}
