19993b190c1d1b7444b1898906b7354ed93200d7
Terry Smith <terry@codecov.io>
2022-01-24 12:54:08 -0400

    Fix some markup, not worth rewritting till we know the pavillion version but it at least renders correctly on mobile now.

--- src/pages/AccountSettings/tabs/Admin/NameEmailCard/NameEmailCard.js
+++ src/pages/AccountSettings/tabs/Admin/NameEmailCard/NameEmailCard.js
@@ -57,9 +57,19 @@ function NameEmailCard({ currentUser, provider }) {
   return (
     <Card className="p-10">
       {/* Define the field first and the submit/title after so the TAB order makes sense for accessibility but we reverse the two so it looks like the correct UI */}
-      <form onSubmit={handleSubmit(submit)} className="flex flex-col-reverse">
-        <div className="flex justify-between mt-8 flex-col md:flex-row">
-          <div className="w-full md:w-1/2 mr-2">
+      <form onSubmit={handleSubmit(submit)} className="flex flex-col gap-8">
+        <div className="flex justify-between items-center">
+          <h1 className="text-2xl bold">Your details</h1>
+          <Button
+            type="submit"
+            disabled={isButtonDisabled}
+            hook="Update personal information"
+          >
+            Save changes
+          </Button>
+        </div>
+        <div className="flex justify-between flex-col md:flex-row gap-2 md:gap-4">
+          <div className="w-full md:w-1/2">
             <label htmlFor="name-edit" className="bold">
               Name
             </label>
@@ -75,7 +85,7 @@ function NameEmailCard({ currentUser, provider }) {
               </p>
             )}
           </div>
-          <div className="w-full md:w-1/2 ml-2 mt-4 md:mt-0">
+          <div className="w-full md:w-1/2 md:mt-0">
             <label htmlFor="email-edit" className="bold">
               Email
             </label>
@@ -91,16 +101,6 @@ function NameEmailCard({ currentUser, provider }) {
               </p>
             )}
           </div>
-        </div>
-        <div className="flex justify-between items-center">
-          <h1 className="text-2xl bold">Your details</h1>
-          <Button
-            type="submit"
-            disabled={isButtonDisabled}
-            hook="Update personal information"
-          >
-            Save changes
-          </Button>
         </div>
       </form>
     </Card>
