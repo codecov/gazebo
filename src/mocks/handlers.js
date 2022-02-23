e206663d2502dc53d00c82a33c379d27110be951
Terry Smith <terry@codecov.io>
2022-02-23 15:18:02 -0400

    this is it

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,11 +1,11 @@
 import { randomOrgCoverageHandler } from 'services/charts/mocks'
 import { randomAccountDetailsHandler } from 'services/account/mocks'
 import { randomUsersHandler } from 'services/users/mocks'
-import { commitErrored } from 'services/commit/mocks'
+import { myExample } from 'services/commit/mocks'
 
 export const handlers = [
   randomOrgCoverageHandler,
   randomAccountDetailsHandler,
   randomUsersHandler,
-  commitErrored,
+  myExample,
 ]
--- src/services/commit/mocks.js
+++ src/services/commit/mocks.js
@@ -1,5 +1,9 @@
 import { graphql } from 'msw'
 
+export const myExample = graphql.query('Commit', (req, res, ctx) => {
+  return res(ctx.status(200), ctx.data(myExampleData))
+})
+
 export const commitErrored = graphql.query('Commit', (req, res, ctx) => {
   return res(
     ctx.status(200),
@@ -228,6 +232,109 @@ const commitDataEmpty = {
     commitid: 'd773f5bc170caec7f6e64420b0967e7bac978a8f',
     totals: {
       coverage: 38.30846,
+    },
+  },
+}
+
+const myExampleData = {
+  owner: {
+    repository: {
+      commit: {
+        totals: { coverage: 99.53 },
+        state: 'complete',
+        commitid: '64caa0cdbcce06af3af69299ae5af2f3e0a3245a',
+        pullId: null,
+        createdAt: '2022-02-23T02:07:02',
+        author: { username: 'adrian-codecov' },
+        uploads: {
+          edges: [
+            {
+              node: {
+                state: 'PROCESSED',
+                provider: 'circleci',
+                createdAt: '2022-02-23T02:19:32.469473+00:00',
+                updatedAt: '2022-02-23T02:19:32.964842+00:00',
+                flags: [],
+                jobCode: '2',
+                downloadUrl:
+                  '/upload/gh/codecov/gazebo/download?path=v4/raw/2022-02-23/91DC663110CD105B1FB05FA7F5636233/64caa0cdbcce06af3af69299ae5af2f3e0a3245a/f110ae78-f68d-49f1-9375-b58b5223b9a0.txt',
+                ciUrl:
+                  'https://circleci.com/gh/codecov/gazebo/17797#tests/containers/2',
+                uploadType: 'UPLOADED',
+                buildCode: '17797',
+              },
+            },
+            {
+              node: {
+                state: 'PROCESSED',
+                provider: 'circleci',
+                createdAt: '2022-02-23T02:19:32.472375+00:00',
+                updatedAt: '2022-02-23T02:19:33.474310+00:00',
+                flags: [],
+                jobCode: '0',
+                downloadUrl:
+                  '/upload/gh/codecov/gazebo/download?path=v4/raw/2022-02-23/91DC663110CD105B1FB05FA7F5636233/64caa0cdbcce06af3af69299ae5af2f3e0a3245a/3c720890-e102-4090-8154-cc520ae2b974.txt',
+                ciUrl:
+                  'https://circleci.com/gh/codecov/gazebo/17797#tests/containers/0',
+                uploadType: 'UPLOADED',
+                buildCode: '17797',
+              },
+            },
+            {
+              node: {
+                state: 'PROCESSED',
+                provider: 'circleci',
+                createdAt: '2022-02-23T02:19:43.780176+00:00',
+                updatedAt: '2022-02-23T02:19:44.584505+00:00',
+                flags: [],
+                jobCode: '1',
+                downloadUrl:
+                  '/upload/gh/codecov/gazebo/download?path=v4/raw/2022-02-23/91DC663110CD105B1FB05FA7F5636233/64caa0cdbcce06af3af69299ae5af2f3e0a3245a/681f2cc6-ab56-4f17-a2fb-813f84a6c108.txt',
+                ciUrl:
+                  'https://circleci.com/gh/codecov/gazebo/17797#tests/containers/1',
+                uploadType: 'UPLOADED',
+                buildCode: '17797',
+              },
+            },
+          ],
+        },
+        message: 'See if lint works',
+        ciPassed: true,
+        parent: {
+          commitid: '9c3cd20f2e790b0d334d3c2afe411367a561924c',
+          totals: { coverage: 99.6 },
+        },
+        compareWithParent: {
+          state: 'processed',
+          patchTotals: { coverage: 0.84615386 },
+          impactedFiles: [
+            {
+              patchCoverage: { coverage: 100.0 },
+              headName: 'src/services/navigation/useNavLinks.js',
+              baseCoverage: { coverage: 100.0 },
+              headCoverage: { coverage: 100.0 },
+            },
+            {
+              patchCoverage: { coverage: 100.0 },
+              headName: 'src/App.js',
+              baseCoverage: { coverage: 100.0 },
+              headCoverage: { coverage: 100.0 },
+            },
+            {
+              patchCoverage: { coverage: 100.0 },
+              headName: 'src/shared/utils/url.js',
+              baseCoverage: null,
+              headCoverage: { coverage: 100.0 },
+            },
+            {
+              patchCoverage: { coverage: 66.66666666666667 },
+              headName: 'src/services/tracking/utm.js',
+              baseCoverage: null,
+              headCoverage: { coverage: 66.66666666666667 },
+            },
+          ],
+        },
+      },
     },
   },
 }
