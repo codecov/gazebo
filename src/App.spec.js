e1a5d0b4545ca38ead66e6116e27e675be3cf08b
Rabee AbuBaker <rabee@codecov.io>
2022-02-27 22:48:30 +0200

    - Fixing failing tests ✅ - Upgrading more dependencies ⬆️ - Fixing ️app.spec.js failing lint rules (as an example on how to fix the render usage with beforeEach) 🚨

--- src/App.spec.js
+++ src/App.spec.js
@@ -8,17 +8,19 @@ jest.mock('react-query/devtools', () => ({
 
 describe('App', () => {
   describe('when rendering', () => {
+    const setup = () => render(<App />)
     beforeEach(() => {
       window.history.pushState({}, 'Test page', '/account/gh/codecov/')
-      render(<App />)
     })
 
     it('renders the loading state', () => {
+      setup()
       const loading = screen.getByTestId('logo-spinner')
       expect(loading).toBeInTheDocument()
     })
 
     it('renders the AccountSettings page', () => {
+      setup()
       return waitFor(() => {
         const page = screen.getByText(/AccountSettings/i)
         expect(page).toBeInTheDocument()
--- src/old_ui/ControlGroup/ControlGroup.js
+++ src/old_ui/ControlGroup/ControlGroup.js
@@ -1,9 +1,9 @@
 import cs from 'classnames'
 
-import controlGroup from './controlGroup.module.css'
+import styles from './controlGroup.module.css'
 
 // Overrides ui components to render a horiztonal a crontorl group
 // Removes borders and rounded corners to create a control group.
 export default function ControlGroup({ children, className }) {
-  return <div className={cs(className, controlGroup)}>{children}</div>
+  return <div className={cs(className, styles.controlGroup)}>{children}</div>
 }
