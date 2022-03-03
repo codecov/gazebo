8c20047016ec432bca5d3f3de597def73d04ee15
Terry Smith <terry@codecov.io>
2022-03-02 23:24:33 -0400

    I noticed lint staged hadnt caught the formatting for storybook, I enabled it more boardly to cover all js files. Also testing husky

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,9 +2,7 @@ module.exports = {
   core: {
     builder: 'webpack5',
   },
-  features: {
-    storyStoreV7: true,
-  },
+  features: { storyStoreV7: true },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-a11y',
