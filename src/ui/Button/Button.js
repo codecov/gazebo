3a74bd63ca8acbfcc38c39b69771a027a719d2eb
Terry Smith <terry@codecov.io>
2022-03-03 01:44:28 -0400

    fix a bit of bad test linting code

--- src/ui/Button/Button.js
+++ src/ui/Button/Button.js
@@ -2,7 +2,6 @@ import cs from 'classnames'
 import PropTypes from 'prop-types'
 
 import AppLink from 'shared/AppLink'
-import { getOwnerImg, getProviderPullURL } from 'shared/utils'
 import Spinner from 'ui/Spinner'
 
 const baseClass = `
