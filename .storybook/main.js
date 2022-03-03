0304d32d7eb13619dcc561dedfe7bf40840529a0
Terry Smith <terry@codecov.io>
2022-03-02 23:17:42 -0400

    fix style and test husky

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,17 @@
 module.exports = {
   core: {
-    builder: "webpack5"
+    builder: 'webpack5',
   },
   features: {
-    storyStoreV7: true
+    storyStoreV7: true,
   },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-a11y', '@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-create-react-app', '@storybook/addon-controls'],
-  framework: "@storybook/react"
-};
+  addons: [
+    '@storybook/addon-a11y',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-controls',
+  ],
+  framework: '@storybook/react',
+}
