af8e38c85d9689bb25490d65524336eed33881b2
Terry Smith <terry@codecov.io>
2022-05-11 19:38:31 -0300

    Fix the segments prop type definition

--- src/pages/PullRequestPage/subroute/Root/FileDiff/FileDiff.js
+++ src/pages/PullRequestPage/subroute/Root/FileDiff/FileDiff.js
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types'
 import { Fragment } from 'react'
 
+import { LINE_TYPE } from 'shared/FileViewer/lineStates'
+
 const FileDiff = ({ headName, segments = [] }) => {
   return (
     <div>
@@ -19,7 +21,22 @@ const FileDiff = ({ headName, segments = [] }) => {
 
 FileDiff.propTypes = {
   headName: PropTypes.string,
-  segments: PropTypes.arrayOf(PropTypes.object), // Todo define the shape when we have the final data/components
+  segments: PropTypes.arrayOf(
+    PropTypes.shape({
+      header: PropTypes.string,
+      lines: PropTypes.shape({
+        baseNumber: PropTypes.string,
+        headNumber: PropTypes.string,
+        baseCoverage: PropTypes.PropTypes.objectOf(
+          PropTypes.oneOf([LINE_TYPE.HIT, LINE_TYPE.MISS, LINE_TYPE.PARTIAL])
+        ),
+        headCoverage: PropTypes.PropTypes.objectOf(
+          PropTypes.oneOf([LINE_TYPE.HIT, LINE_TYPE.MISS, LINE_TYPE.PARTIAL])
+        ),
+        content: PropTypes.string,
+      }),
+    })
+  ),
 }
 
 export default FileDiff
