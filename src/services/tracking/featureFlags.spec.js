ec29c8f51753e5157e2d13eeee303b3307426184
Terry Smith <terry@codecov.io>
2022-05-18 15:22:08 -0300

    Cleanup tests

--- src/services/tracking/featureFlags.spec.js
+++ src/services/tracking/featureFlags.spec.js
@@ -19,8 +19,6 @@ describe('useTrackFeatureFlags', () => {
   describe('normal use', () => {
     beforeEach(() => {
       useIdentifyUser.mockImplementation(mockIdentifyUser)
-      Cookie.set('github-username', 'doggo')
-
       setup({
         email: 'test@test.com',
         user: { avatarUrl: 'doggo.picz', name: 'doggo', username: 'doggo' },
@@ -29,7 +27,6 @@ describe('useTrackFeatureFlags', () => {
     })
     afterEach(() => {
       jest.clearAllMocks()
-      Cookie.remove('github-username')
     })
 
     it('Creates the expected user and key identified', () => {
@@ -63,7 +60,6 @@ describe('useTrackFeatureFlags', () => {
   describe('impersonating on github', () => {
     beforeEach(() => {
       useIdentifyUser.mockImplementation(mockIdentifyUser)
-      Cookie.set('github-username', 'laudna')
       Cookie.set('staff_user', 'doggo')
       setup({
         email: 'test@test.com',
@@ -73,7 +69,6 @@ describe('useTrackFeatureFlags', () => {
     })
     afterEach(() => {
       jest.clearAllMocks()
-      Cookie.remove('github-username')
       Cookie.remove('staff_user')
     })
     it('Creates the expected user and key identified', () => {
@@ -107,7 +102,6 @@ describe('useTrackFeatureFlags', () => {
   describe('impersonating on bitbucket', () => {
     beforeEach(() => {
       useIdentifyUser.mockImplementation(mockIdentifyUser)
-      Cookie.set('bitbucket-username', 'laudna')
       Cookie.set('staff_user', 'doggo')
       setup({
         email: 'test@test.com',
@@ -117,7 +111,6 @@ describe('useTrackFeatureFlags', () => {
     })
     afterEach(() => {
       jest.clearAllMocks()
-      Cookie.remove('bitbucket-username')
       Cookie.remove('staff_user')
     })
 
@@ -152,7 +145,6 @@ describe('useTrackFeatureFlags', () => {
   describe('impersonating on gitlab', () => {
     beforeEach(() => {
       useIdentifyUser.mockImplementation(mockIdentifyUser)
-      Cookie.set('gitlab-username', 'laudna')
       Cookie.set('staff_user', 'doggo')
       setup({
         email: 'test@test.com',
@@ -162,7 +154,6 @@ describe('useTrackFeatureFlags', () => {
     })
     afterEach(() => {
       jest.clearAllMocks()
-      Cookie.remove('gitlab-username')
       Cookie.remove('staff_user')
     })
     it('Creates the expected user and key identified', () => {
@@ -170,45 +161,6 @@ describe('useTrackFeatureFlags', () => {
         name: 'doggo',
         email: 'test@test.com',
         key: 'impersonated',
-        avatar: 'doggo.picz',
-        custom: {
-          guest: false,
-          student: false,
-          username: 'doggo',
-          service: null,
-          ownerid: 'hello',
-          service_id: null,
-          plan: null,
-          staff: true,
-          has_yaml: false,
-          bot: null,
-          delinquent: null,
-          did_trial: null,
-          plan_provider: null,
-          plan_user_count: null,
-          created_at: null,
-          updated_at: null,
-        },
-      })
-    })
-  })
-
-  describe('In the unlikely event there is no cookie', () => {
-    beforeEach(() => {
-      useIdentifyUser.mockImplementation(mockIdentifyUser)
-      setup({
-        email: 'test@test.com',
-        user: { avatarUrl: 'doggo.picz', name: 'doggo', username: 'doggo' },
-        trackingMetadata: { ownerid: 'hello', staff: true },
-      })
-    })
-    afterEach(() => jest.clearAllMocks())
-
-    it('Creates the expected user and key identified', () => {
-      expect(mockIdentifyUser).lastCalledWith({
-        name: 'doggo',
-        email: 'test@test.com',
-        key: 'hello',
         avatar: 'doggo.picz',
         custom: {
           guest: false,
