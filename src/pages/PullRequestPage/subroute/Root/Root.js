c6f6cd5393fb18bc1ea98293fa12282ae5f63e5f
Terry Smith <terry@codecov.io>
2022-06-02 00:37:50 -0300

    handle pull request with no impacted files (mocked case)

--- src/pages/PullRequestPage/subroute/Root/Root.js
+++ src/pages/PullRequestPage/subroute/Root/Root.js
@@ -53,15 +53,33 @@ const Root = () => {
             lineCoverageStatesAndSetters={lineCoverageStatesAndSetters}
           />
         </div>
-        {diff?.files?.map((file, i) => {
-          return (
-            <FileDiff
-              key={`impacted-file-${i}`}
-              {...file}
-              lineCoverageStatesAndSetters={lineCoverageStatesAndSetters}
-            />
-          )
-        })}
+        {diff?.files?.map((file, i) => (
+          <FileDiff
+            key={`impacted-file-${i}`}
+            {...file}
+            lineCoverageStatesAndSetters={lineCoverageStatesAndSetters}
+          />
+        ))}
+        {(!diff?.files || diff?.files?.length === 0) && (
+          <>
+            <p className="m-2">
+              Everything is accounted for! No changes detected that need to be
+              reviewed.
+            </p>
+            <p className="mx-2 font-medium">
+              What changes does Codecov check for?
+            </p>
+            <ul className="list-disc mx-2 ml-6">
+              <li>
+                Lines, not adjusted in diff, that have changed coverage data.
+              </li>
+              <li>Files that introduced coverage data that had none before.</li>
+              <li>
+                Files that have missing coverage data that once were tracked.
+              </li>
+            </ul>
+          </>
+        )}
       </div>
     )
   )
