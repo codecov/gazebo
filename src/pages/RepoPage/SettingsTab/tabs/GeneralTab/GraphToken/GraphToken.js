da5e1670026bcfe24d0dd0ca45973b4090b3fd0c
RulaKhaled <rula@codecov.io>
2022-06-06 15:18:31 +0300

    conflicts leftovers

--- src/pages/RepoPage/SettingsTab/tabs/GeneralTab/GraphToken/GraphToken.js
+++ src/pages/RepoPage/SettingsTab/tabs/GeneralTab/GraphToken/GraphToken.js
@@ -1,31 +1,33 @@
 import PropTypes from 'prop-types'
 
 import A from 'ui/A'
+import SettingsDescriptor from 'ui/SettingsDescriptor'
 import TokenWrapper from 'ui/TokenWrapper'
 
 function GraphToken({ graphToken }) {
-    return (
-        <div className="flex flex-col gap-4">
-            <div className="flex flex-col gap-2">
-                <h1 className="text-lg font-semibold">Repository graphing token</h1>
-                <p>
-                    Token is used for viewing graphs{' '}
-                    <A to={{ pageName: 'graphAuthorization' }} isExternal>
-                        learn more
-                    </A>
-                </p>
-                <hr />
-            </div>
-            <div className="flex flex-col border-2 border-gray-100 p-4 xl:w-4/5 2xl:w-3/5 gap-4">
-                <p>Use this token in API request to repository graphs</p>
-                <TokenWrapper token={graphToken} />
-            </div>
-        </div>
-    )
+  return (
+    <SettingsDescriptor
+      title="Repository graphing token"
+      description={
+        <span>
+          Token is used for viewing graphs{' '}
+          <A to={{ pageName: 'graphAuthorization' }} isExternal>
+            learn more
+          </A>
+        </span>
+      }
+      content={
+        <>
+          <p>Use this token in API request to repository graphs</p>
+          <TokenWrapper token={graphToken} />
+        </>
+      }
+    />
+  )
 }
 
 GraphToken.propTypes = {
-    graphToken: PropTypes.string.isRequired,
+  graphToken: PropTypes.string.isRequired,
 }
 
 export default GraphToken
