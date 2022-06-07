d6d4e72e6ca6fcd999491ad6ec8faf77d83f2fb9
RulaKhaled <rula@codecov.io>
2022-06-07 17:43:36 +0300

    use useRepoSettings - clean up styles

--- src/pages/RepoPage/SettingsTab/tabs/YamlTab/YAML/YAML.js
+++ src/pages/RepoPage/SettingsTab/tabs/YamlTab/YAML/YAML.js
@@ -1,23 +1,22 @@
-import { useParams } from 'react-router-dom'
-
 import YamlEditor from 'pages/AccountSettings/tabs/YAML/YamlEditor'
-import { useRepo } from 'services/repo'
+import { useRepoSettings } from 'services/repo'
 
 function YAML() {
-  const { provider, owner, repo } = useParams()
-  const { data } = useRepo({ provider, owner, repo })
+  const { data } = useRepoSettings()
 
   return (
-    <div className="flex flex-col gap-4">
-      <div className="flex flex-col gap-2 xl:w-3/4">
-        <h1 className="text-lg font-semibold">Repository yaml</h1>
-        <p>
-          This is the default yaml for the current repository, after validation.
-          This yaml takes precedence over the global yaml, but will be
-          overwritten if a yaml change is included in a commit.
-        </p>
+    <div className="flex flex-col gap-1">
+      <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-2 xl:w-3/4">
+          <h1 className="text-lg font-semibold">Repository yaml</h1>
+          <p>
+            This is the default yaml for the current repository, after
+            validation. This yaml takes precedence over the global yaml, but
+            will be overwritten if a yaml change is included in a commit.
+          </p>
+        </div>
+        <hr />
       </div>
-      <hr />
       <YamlEditor
         value={data?.repository?.yaml}
         readOnly
--- src/pages/RepoPage/SettingsTab/tabs/YamlTab/YamlTab.spec.js
+++ src/pages/RepoPage/SettingsTab/tabs/YamlTab/YamlTab.spec.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { MemoryRouter, Route } from 'react-router-dom'
 
-import { useRepo } from 'services/repo'
+import { useRepoSettings } from 'services/repo'
 
 import YamlTab from './YamlTab'
 
@@ -10,35 +10,35 @@ jest.mock('services/repo')
 const queryClient = new QueryClient()
 
 describe('YamlTab', () => {
-    function setup() {
-        useRepo.mockReturnValue({ data: { repository: { yaml: 'test' } } })
+  function setup() {
+    useRepoSettings.mockReturnValue({ data: { repository: { yaml: 'test' } } })
 
-        render(
-            <MemoryRouter initialEntries={['/gh/codecov/codecov-client/yaml']}>
-                <QueryClientProvider client={queryClient}>
-                    <Route path="/:provider/:owner/:repo/yaml">
-                        <YamlTab />
-                    </Route>
-                </QueryClientProvider>
-            </MemoryRouter>
-        )
-    }
+    render(
+      <MemoryRouter initialEntries={['/gh/codecov/codecov-client/yaml']}>
+        <QueryClientProvider client={queryClient}>
+          <Route path="/:provider/:owner/:repo/yaml">
+            <YamlTab />
+          </Route>
+        </QueryClientProvider>
+      </MemoryRouter>
+    )
+  }
 
-    describe('renders yaml section', () => {
-        beforeEach(() => {
-            setup()
-        })
-
-        it('renders Repository Yaml compoenent', () => {
-            const title = screen.getByText(/Repository yaml/)
-            expect(title).toBeInTheDocument()
-        })
-
-        it('renders body', () => {
-            const p = screen.getByText(
-                'This is the default yaml for the current repository, after validation. This yaml takes precedence over the global yaml, but will be overwritten if a yaml change is included in a commit.'
-            )
-            expect(p).toBeInTheDocument()
-        })
+  describe('renders yaml section', () => {
+    beforeEach(() => {
+      setup()
     })
+
+    it('renders Repository Yaml compoenent', () => {
+      const title = screen.getByText(/Repository yaml/)
+      expect(title).toBeInTheDocument()
+    })
+
+    it('renders body', () => {
+      const p = screen.getByText(
+        'This is the default yaml for the current repository, after validation. This yaml takes precedence over the global yaml, but will be overwritten if a yaml change is included in a commit.'
+      )
+      expect(p).toBeInTheDocument()
+    })
+  })
 })
