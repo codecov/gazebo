8f3d8ffa0c6f6c507ef64f479f69440189f57c68
nicholas-codecov <nicholas.deschenes@codecov.io>
2022-06-07 13:54:00 -0300

    missed one conflict

--- src/services/navigation/useNavLinks.spec.js
+++ src/services/navigation/useNavLinks.spec.js
@@ -10,7 +10,6 @@ describe('useNavLinks', () => {
   let hookData
 
   function setup(location) {
-<<<<<<< HEAD
     hookData = renderHook(() => useNavLinks(), {
       wrapper: ({ children }) => (
         <MemoryRouter initialEntries={location} initialIndex={0}>
@@ -25,21 +24,6 @@ describe('useNavLinks', () => {
         </MemoryRouter>
       ),
     })
-=======
-    const wrapper = ({ children }) => (
-      <MemoryRouter initialEntries={location} initialIndex={0}>
-        <Route path="/:provider">{children}</Route>
-        <Route path="/:provider/:owner">{children}</Route>
-        <Route path="/:provider/:owner/:repo">{children}</Route>
-        <Route path="/:provider/:owner/:repo/:id">{children}</Route>
-        <Route path="/:provider/:owner/:repo/commit/:commit/file/:path">
-          {children}
-        </Route>
-        <Route path="/:provider/:owner/:repo/pull/:pullId">{children}</Route>
-      </MemoryRouter>
-    )
-    hookData = renderHook(() => useNavLinks(), { wrapper })
->>>>>>> update useNavLinks to support tree/ref
   }
 
   describe('provider link', () => {
@@ -716,11 +700,6 @@ describe('useNavLinks', () => {
 })
 
 describe('useStaticNavLinks', () => {
-  const wrapper = ({ children }) => (
-    <MemoryRouter initialEntries={['/gh']} initialIndex={0}>
-      <Route path="/:provider">{children}</Route>
-    </MemoryRouter>
-  )
   const view = renderHook(() => useStaticNavLinks(), {
     wrapper: ({ children }) => (
       <MemoryRouter initialEntries={['/gh']} initialIndex={0}>
@@ -730,32 +709,32 @@ describe('useStaticNavLinks', () => {
   })
   const links = view.result.current
   describe.each`
-    link                       | outcome
-    ${links.root}              | ${`${config.MARKETING_BASE_URL}`}
-    ${links.terms}             | ${`${config.MARKETING_BASE_URL}/terms-and-conditions`}
-    ${links.privacy}           | ${`${config.MARKETING_BASE_URL}/privacy`}
-    ${links.security}          | ${`${config.MARKETING_BASE_URL}/security`}
-    ${links.gdpr}              | ${`${config.MARKETING_BASE_URL}/gdpr`}
-    ${links.pricing}           | ${`${config.MARKETING_BASE_URL}/pricing`}
-    ${links.support}           | ${`https://codecov.freshdesk.com/support/home`}
-    ${links.docs}              | ${`https://docs.codecov.io/`}
-    ${links.enterprise}        | ${`${config.MARKETING_BASE_URL}/self-hosted`}
-    ${links.githubMarketplace} | ${`https://github.com/marketplace/codecov`}
-    ${links.freshdesk}         | ${`https://codecov.freshdesk.com/support/home`}
-    ${links.freeTrial}         | ${`${config.MARKETING_BASE_URL}/trial`}
-    ${links.demo}              | ${`${config.MARKETING_BASE_URL}/demo`}
-    ${links.oauthTroubleshoot} | ${'https://docs.codecov.com/docs/github-oauth-application-authorization#troubleshooting'}
-    ${links.flags}             | ${'https://docs.codecov.com/docs/flags'}
-    ${links.unexpectedChanges} | ${'https://docs.codecov.com/docs/unexpected-coverage-changes'}
-    ${links.userAppManagePage} | ${'https://github.com/settings/connections/applications/c68c81cbfd179a50784a'}
-    ${links.blog}              | ${`${config.MARKETING_BASE_URL}/blog`}
-    ${links.sales}             | ${`${config.MARKETING_BASE_URL}/sales`}
-    ${links.uploader}          | ${'https://docs.codecov.com/docs/codecov-uploader'}
-    ${links.integrityCheck}    | ${'https://docs.codecov.com/docs/codecov-uploader#integrity-checking-the-uploader'}
-    ${links.codecovGithubApp}  | ${'https://github.com/apps/codecov'}
-    ${links.teamBot}           | ${'https://docs.codecov.com/docs/team-bot'}
-    ${links.runtimeInsights}   | ${'https://docs.codecov.com/docs/runtime-insights'}
-    ${links.graphAuthorization}| ${'https://docs.codecov.com/reference/authorization#about-graphs'}
+    link                        | outcome
+    ${links.root}               | ${`${config.MARKETING_BASE_URL}`}
+    ${links.terms}              | ${`${config.MARKETING_BASE_URL}/terms-and-conditions`}
+    ${links.privacy}            | ${`${config.MARKETING_BASE_URL}/privacy`}
+    ${links.security}           | ${`${config.MARKETING_BASE_URL}/security`}
+    ${links.gdpr}               | ${`${config.MARKETING_BASE_URL}/gdpr`}
+    ${links.pricing}            | ${`${config.MARKETING_BASE_URL}/pricing`}
+    ${links.support}            | ${`https://codecov.freshdesk.com/support/home`}
+    ${links.docs}               | ${`https://docs.codecov.io/`}
+    ${links.enterprise}         | ${`${config.MARKETING_BASE_URL}/self-hosted`}
+    ${links.githubMarketplace}  | ${`https://github.com/marketplace/codecov`}
+    ${links.freshdesk}          | ${`https://codecov.freshdesk.com/support/home`}
+    ${links.freeTrial}          | ${`${config.MARKETING_BASE_URL}/trial`}
+    ${links.demo}               | ${`${config.MARKETING_BASE_URL}/demo`}
+    ${links.oauthTroubleshoot}  | ${'https://docs.codecov.com/docs/github-oauth-application-authorization#troubleshooting'}
+    ${links.flags}              | ${'https://docs.codecov.com/docs/flags'}
+    ${links.unexpectedChanges}  | ${'https://docs.codecov.com/docs/unexpected-coverage-changes'}
+    ${links.userAppManagePage}  | ${'https://github.com/settings/connections/applications/c68c81cbfd179a50784a'}
+    ${links.blog}               | ${`${config.MARKETING_BASE_URL}/blog`}
+    ${links.sales}              | ${`${config.MARKETING_BASE_URL}/sales`}
+    ${links.uploader}           | ${'https://docs.codecov.com/docs/codecov-uploader'}
+    ${links.integrityCheck}     | ${'https://docs.codecov.com/docs/codecov-uploader#integrity-checking-the-uploader'}
+    ${links.codecovGithubApp}   | ${'https://github.com/apps/codecov'}
+    ${links.teamBot}            | ${'https://docs.codecov.com/docs/team-bot'}
+    ${links.runtimeInsights}    | ${'https://docs.codecov.com/docs/runtime-insights'}
+    ${links.graphAuthorization} | ${'https://docs.codecov.com/reference/authorization#about-graphs'}
   `('static links return path', ({ link, outcome }) => {
     it('Returns the correct link', () => {
       expect(link.path()).toBe(outcome)
