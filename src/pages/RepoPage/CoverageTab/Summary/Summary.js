e770603ac3ddd721931ae49d30de1a48928c3979
Terry Smith <terry@codecov.io>
2022-06-07 17:53:20 -0300

    Remove some work to make the PR easier to review.

--- src/pages/RepoPage/CoverageTab/Summary/Summary.js
+++ src/pages/RepoPage/CoverageTab/Summary/Summary.js
@@ -1,10 +1,7 @@
-import { useLayoutEffect } from 'react'
 import { Redirect } from 'react-router-dom'
 
-import { useSetCrumbs } from 'pages/RepoPage/context'
 import A from 'ui/A'
 import Icon from 'ui/Icon'
-import Progress from 'ui/Progress'
 import Select from 'ui/Select'
 import { SummaryField, SummaryRoot } from 'ui/Summary'
 
@@ -12,34 +9,11 @@ import { useSummary } from './hooks'
 
 const Summary = () => {
   const {
-    coverage,
-    data,
-    defaultBranch,
     currenBranchSelected,
-    privateRepo,
     branchSelectorProps,
     newPath,
     enableRedirection,
   } = useSummary()
-  const setCrumbs = useSetCrumbs()
-
-  useLayoutEffect(() => {
-    setCrumbs([
-      {
-        pageName: '',
-        readOnly: true,
-        children: (
-          <span className="flex items-center gap-1">
-            <Icon name="branch" variant="developer" size="sm" />
-            {currenBranchSelected?.name}
-            {privateRepo && (
-              <Icon name="lock-closed" variant="solid" size="sm" />
-            )}
-          </span>
-        ),
-      },
-    ])
-  }, [currenBranchSelected?.name, setCrumbs, privateRepo])
 
   return (
     <>
@@ -76,28 +50,6 @@ const Summary = () => {
             </p>
           )}
         </SummaryField>
-        <SummaryField>
-          <h3 className="text-ds-gray-octonary text-sm font-semibold  min-w-[16rem]">
-            Repo Coverage
-          </h3>
-          <Progress label amount={coverage} variant="tall" />
-          {/* Not yet in api*/}
-          <p className="text-xs">-- of -- lines covered</p>
-        </SummaryField>
-        {data?.name !== defaultBranch && data?.head?.totals?.percentCovered && (
-          <SummaryField>
-            <h3 className="text-ds-gray-octonary text-sm font-semibold  min-w-[16rem]">
-              Branch Coverage
-            </h3>
-            <Progress
-              label
-              amount={data?.head?.totals?.percentCovered}
-              variant="tall"
-            />
-            {/* Not yet in api*/}
-            <p className="text-xs">-- of -- lines covered</p>
-          </SummaryField>
-        )}
       </SummaryRoot>
     </>
   )
