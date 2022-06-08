c02e72646f0b26842fe4326968395d03a2a9c9db
nicholas-codecov <105234307+nicholas-codecov@users.noreply.github.com>
2022-06-08 13:35:10 -0300

    Setup Breadcrumb Component for Repo Overview (#1367)
    * create hook for getting tree paths
    
    * update useNavLinks to support tree/ref
    
    * update hooks to use branch in treeView
    
    * initial implementation of BreadcrumbSearch
    
    * missed one conflict

---
+++ src/pages/RepoPage/CoverageTab/BreadcrumbSearch/BreadcrumbSearch.spec.js
@@ -0,0 +1,44 @@
+import { render, screen } from 'custom-testing-library'
+
+import { MemoryRouter, Route } from 'react-router-dom'
+
+import BreadcrumbSearch from './BreadcrumbSearch'
+
+describe('BreadcrumbSearch', () => {
+  function setup({ entries, path }) {
+    render(
+      <MemoryRouter initialEntries={entries}>
+        <Route path={path}>
+          <BreadcrumbSearch />
+        </Route>
+      </MemoryRouter>
+    )
+  }
+
+  describe('path is provided in route', () => {
+    beforeEach(() => {
+      setup({
+        entries: ['/gh/owner/coolrepo/tree/main/src/tests'],
+        path: '/:provider/:owner/:repo/tree/:branch/:path+',
+      })
+    })
+
+    it('renders the breadcrumb', () => {
+      const repo = screen.getByRole('link', { name: 'coolrepo' })
+      expect(repo).toBeInTheDocument()
+      expect(repo).toHaveAttribute('href', '/gh/owner/coolrepo/tree/main/')
+
+      const src = screen.getByRole('link', { name: 'src' })
+      expect(src).toBeInTheDocument()
+      expect(src).toHaveAttribute('href', '/gh/owner/coolrepo/tree/main/src')
+
+      const tests = screen.getByText('tests')
+      expect(tests).toBeInTheDocument()
+    })
+
+    it('renders the search', () => {
+      const searchMsg = screen.getByText('Search Goes Here')
+      expect(searchMsg).toBeInTheDocument()
+    })
+  })
+})
