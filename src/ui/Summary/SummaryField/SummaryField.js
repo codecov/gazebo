6f57718b6162c4572ed21280d6b29b8aadf81d8c
Terry Smith <terry@codecov.io>
2022-06-13 13:42:08 -0300

    Update summary to match design

--- src/ui/Summary/SummaryField/SummaryField.js
+++ src/ui/Summary/SummaryField/SummaryField.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 
 export default function SummaryField({ children, title }) {
   return (
-    <div className="flex flex-col gap-1 justify-between px-4 first:pl-0 last:pr-0 text-xl font-light">
+    <div className="flex flex-col gap-1 justify-between px-8 first:pl-0 last:pr-0 text-xl font-light">
       {title && (
         <h4 className="flex gap-2 text-ds-gray-quinary font-mono text-xs">
           {title}
