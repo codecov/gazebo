4f52755b107b301d38adfc235bfec4cddd8337c0
nicholas-codecov <nicholas.deschenes@codecov.io>
2022-06-14 13:47:05 -0300

    add BreadcrumbSearch and url params into CoverageTab

--- src/pages/RepoPage/CoverageTab/CoverageTab.js
+++ src/pages/RepoPage/CoverageTab/CoverageTab.js
@@ -1,14 +1,21 @@
 import { lazy, Suspense } from 'react'
 import { Route, Switch } from 'react-router-dom'
 
+import { useLocationParams } from 'services/navigation'
 import Spinner from 'ui/Spinner'
 
+import BreadcrumbSearch from './BreadcrumbSearch'
 import Summary from './Summary'
 
 const FileViewer = lazy(() => import('./subroute/Fileviewer'))
 const RepoContentsTable = lazy(() => import('./subroute/RepoContents'))
 
+const defaultQueryParams = {
+  search: '',
+}
+
 function CoverageTab() {
+  const { params, updateParams } = useLocationParams(defaultQueryParams)
   const Loader = (
     <div className="flex items-center justify-center py-16">
       <Spinner />
@@ -18,6 +25,10 @@ function CoverageTab() {
   return (
     <div className="flex flex-col gap-4 mx-4 md:mx-0">
       <Summary />
+      <BreadcrumbSearch
+        searchValue={params.search}
+        setSearchValue={(search) => updateParams({ search })}
+      />
       <div className="flex flex-1 flex-col gap-4 border-t border-solid border-ds-gray-secondary">
         <Switch>
           <Route path="/:provider/:owner/:repo/tree/:branch/:path+" exact>
