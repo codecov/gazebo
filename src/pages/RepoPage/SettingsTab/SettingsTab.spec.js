21aed8ce2b33de6b0b395f9b2eca0b6859305466
RulaKhaled <rula@codecov.io>
2022-06-16 18:09:23 +0300

    test erorr 404

--- src/pages/RepoPage/SettingsTab/SettingsTab.spec.js
+++ src/pages/RepoPage/SettingsTab/SettingsTab.spec.js
@@ -1,7 +1,18 @@
 import { render, screen } from '@testing-library/react'
 import { MemoryRouter, Route } from 'react-router-dom'
 
+import { useUser } from 'services/user'
+
 import SettingsTab from './SettingsTab'
+
+jest.mock('services/user')
+
+const loggedInUser = {
+  user: {
+    username: 'RulaKhaled',
+    avatarUrl: '',
+  },
+}
 
 describe('AccountSettings', () => {
   function setup({ url }) {
@@ -16,6 +27,7 @@ describe('AccountSettings', () => {
 
   describe('Render for a repo', () => {
     beforeEach(() => {
+      useUser.mockReturnValue({ data: loggedInUser })
       setup({ url: '/gh/codecov/codecov-client/settings' })
     })
 
@@ -28,6 +40,7 @@ describe('AccountSettings', () => {
 
   describe('Render with an uknown path', () => {
     beforeEach(() => {
+      useUser.mockReturnValue({ data: loggedInUser })
       setup({ url: '/gh/codecov/codecov-client/settings/random' })
     })
 
@@ -38,6 +51,17 @@ describe('AccountSettings', () => {
     })
 
     it('renders 404 not found', () => {
+      expect(screen.getByText('Error 404')).toBeInTheDocument()
+    })
+  })
+
+  describe('Render with Unauthorized user', () => {
+    beforeEach(() => {
+      useUser.mockReturnValue({ data: null })
+      setup({ url: '/gh/codecov/codecov-client/settings' })
+    })
+
+    it('renders error', () => {
       expect(screen.getByText('Error 404')).toBeInTheDocument()
     })
   })
