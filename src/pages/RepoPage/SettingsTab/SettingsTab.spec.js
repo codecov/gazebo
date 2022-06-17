08ffc0527fc828019d79d74a6f30e972eafcbff9
RulaKhaled <rula@codecov.io>
2022-06-17 17:03:29 +0300

    handle 401 error in settings page

--- src/pages/RepoPage/SettingsTab/SettingsTab.spec.js
+++ src/pages/RepoPage/SettingsTab/SettingsTab.spec.js
@@ -1,11 +1,10 @@
 import { render, screen } from '@testing-library/react'
 import { MemoryRouter, Route } from 'react-router-dom'
 
+import NetworkErrorBoundary from 'layouts/shared/NetworkErrorBoundary'
 import { useUser } from 'services/user'
 
 import SettingsTab from './SettingsTab'
-
-jest.mock('services/user')
 
 const loggedInUser = {
   user: {
@@ -14,12 +13,22 @@ const loggedInUser = {
   },
 }
 
-describe('AccountSettings', () => {
-  function setup({ url }) {
+jest.spyOn(console, 'error').mockImplementation()
+
+jest.mock('services/user', () => ({
+  ...jest.requireActual('services/user'), // import and retain the original functionalities
+  useUser: jest.fn(),
+}))
+
+describe('SettingsTab', () => {
+  function setup(url, ErrorRender) {
     render(
       <MemoryRouter initialEntries={[url]}>
         <Route path="/:provider/:owner/:repo/settings">
-          <SettingsTab />
+          <NetworkErrorBoundary>
+            <SettingsTab />
+            <ErrorRender />
+          </NetworkErrorBoundary>
         </Route>
       </MemoryRouter>
     )
@@ -28,7 +37,7 @@ describe('AccountSettings', () => {
   describe('Render for a repo', () => {
     beforeEach(() => {
       useUser.mockReturnValue({ data: loggedInUser })
-      setup({ url: '/gh/codecov/codecov-client/settings' })
+      setup('/gh/codecov/codecov-client/settings', () => null)
     })
 
     it('renders the right links', () => {
@@ -41,13 +50,33 @@ describe('AccountSettings', () => {
   describe('Render with an uknown path', () => {
     beforeEach(() => {
       useUser.mockReturnValue({ data: loggedInUser })
-      setup({ url: '/gh/codecov/codecov-client/settings/random' })
+      setup('/gh/codecov/codecov-client/settings/random', () => null)
     })
 
     it('renders the right links', () => {
       expect(screen.getByRole('link', { name: /General/ })).toBeInTheDocument()
       expect(screen.getByRole('link', { name: /Yaml/ })).toBeInTheDocument()
       expect(screen.getByRole('link', { name: /Badge/ })).toBeInTheDocument()
+    })
+  })
+
+  describe('Render with Unauthenticated user', () => {
+    beforeEach(() => {
+      function ErrorRender() {
+        //mocking error promise reject within then promise handle
+        throw Object.assign({
+          status: 401,
+          data: {
+            detail: 'Unauthenticated',
+          },
+        })
+      }
+      useUser.mockReturnValue({ data: undefined })
+      setup('/gh/codecov/codecov-client/settings', ErrorRender)
+    })
+
+    it('renders 401', () => {
+      expect(screen.getByText('Error 401')).toBeInTheDocument()
     })
   })
 })
