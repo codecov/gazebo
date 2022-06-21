ea37fb88164cbf36cf59cc1b3fa68b6535adb4f0
nicholas-codecov <nicholas.deschenes@codecov.io>
2022-06-21 10:45:32 -0300

    update Fileviewer to use FileBreadcrumb

--- src/pages/RepoPage/CoverageTab/subroute/Fileviewer/Fileviewer.js
+++ src/pages/RepoPage/CoverageTab/subroute/Fileviewer/Fileviewer.js
@@ -1,21 +1,16 @@
-// import { useParams } from 'react-router-dom'
-
 import RawFileviewer from 'shared/RawFileviewer'
-// import { getFilenameFromFilePath } from 'shared/utils/url'
-import Breadcrumb from 'ui/Breadcrumb'
 
-import { useTreePaths } from '../../BreadcrumbSearch/hooks'
+import FileBreadcrumb from '../../FileBreadcrumb'
+import { useTreePaths } from '../../FileBreadcrumb/hooks'
 
 function FileView() {
-  // const { path } = useParams()
-  // const title = getFilenameFromFilePath(path)
   const { treePaths } = useTreePaths()
 
   return (
     <RawFileviewer
       title={
         <div className="text-sm font-normal">
-          <Breadcrumb paths={[...treePaths]} />
+          <FileBreadcrumb paths={[...treePaths]} />
         </div>
       }
     />
