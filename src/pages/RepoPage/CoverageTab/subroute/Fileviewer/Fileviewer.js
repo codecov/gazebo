931eabc21ec386043e8fd97520c3ef9c9c09373d
nicholas-codecov <105234307+nicholas-codecov@users.noreply.github.com>
2022-06-28 13:46:53 -0300

    Initial Unify File Viewers (#1411)
    * initial poking
    
    * Add new variant to A
    
    * use new feature flag in code header
    
    * update Fileviewer to use FileBreadcrumb
    
    * update id to be targetted by # in url
    
    * set tree view links to internal
    
    * remove check for id that has moved
    
    * add pink border
    
    * change colour to match button focus colour
    
    * update to just use ring
    
    * rename variant
    
    * use new url split function
    
    * add todo reminder

--- src/pages/RepoPage/CoverageTab/subroute/Fileviewer/Fileviewer.js
+++ src/pages/RepoPage/CoverageTab/subroute/Fileviewer/Fileviewer.js
@@ -1,13 +1,20 @@
-import { useParams } from 'react-router-dom'
-
 import RawFileviewer from 'shared/RawFileviewer'
-import { getFilenameFromFilePath } from 'shared/utils/url'
+
+import FileBreadcrumb from '../../FileBreadcrumb'
+import { useTreePaths } from '../../FileBreadcrumb/hooks'
 
 function FileView() {
-  const { path } = useParams()
-  const title = getFilenameFromFilePath(path)
+  const { treePaths } = useTreePaths()
 
-  return <RawFileviewer title={title} />
+  return (
+    <RawFileviewer
+      title={
+        <div className="text-sm font-normal">
+          <FileBreadcrumb paths={[...treePaths]} />
+        </div>
+      }
+    />
+  )
 }
 
 export default FileView
