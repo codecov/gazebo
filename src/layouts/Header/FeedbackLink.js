a7be3688351572caa988da3e011219601fed3a8d
nicholas-codeocv <nicholas.deschenes@codecov.io>
2022-07-11 15:46:29 -0300

    update FeedbackLink component to include ref

--- src/layouts/Header/FeedbackLink.js
+++ src/layouts/Header/FeedbackLink.js
@@ -5,14 +5,17 @@ import A from 'ui/A'
 function FeedbackLink() {
   const { pathname } = useLocation()
 
+  let ref = ''
   let header = 'header'
   if (pathname.includes('feedback')) {
     header = 'headerHighlight'
+  } else {
+    ref = pathname
   }
 
   return (
     <div className="border-l py-1 pl-4">
-      <A to={{ pageName: 'feedback' }} variant={header}>
+      <A to={{ pageName: 'feedback', options: { ref } }} variant={header}>
         Feedback
       </A>
     </div>
