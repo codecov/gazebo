630a6ff715c8872840271b1fd64024f03c87635b
nicholas-codeocv <nicholas.deschenes@codecov.io>
2022-07-11 15:45:57 -0300

    add feedback link to footer

--- src/layouts/Footer/Footer.js
+++ src/layouts/Footer/Footer.js
@@ -1,9 +1,17 @@
+import { useLocation } from 'react-router-dom'
+
 import { ReactComponent as CodecovIcon } from 'assets/svg/codecov.svg'
+import { useUser } from 'services/user'
 import A from 'ui/A'
 
 import { FooterItem } from './FooterItem'
 
 function Footer() {
+  const { pathname } = useLocation()
+  const { data: currentUser } = useUser({
+    suspense: false,
+  })
+
   const year = new Date().getUTCFullYear()
   const leftMenu = [
     { text: `© ${year} Codecov` },
@@ -12,12 +20,18 @@ function Footer() {
     { to: { pageName: 'security' } },
     { to: { pageName: 'gdpr' } },
   ]
+
   const rightMenu = [
     { to: { pageName: 'pricing' } },
     { to: { pageName: 'support' } },
     { to: { pageName: 'docs' } },
     { to: { pageName: 'enterprise' } },
   ]
+
+  if (!!currentUser) {
+    rightMenu.push({ to: { pageName: 'feedback', options: { ref: pathname } } })
+  }
+
   return (
     <footer className="flex-none">
       <nav className="container flex flex-wrap flex-col lg:flex-row justify-center sm:justify-between text-white py-6 px-3 items-center border-t border-ds-gray-tertiary">
