c9406e5d33150fb6468c20fe1c2592eaa75e9c67
nicholas-codeocv <nicholas.deschenes@codecov.io>
2022-07-12 15:39:11 -0300

    remove ref from Header/FeedbackLink

--- src/layouts/Header/FeedbackLink.js
+++ src/layouts/Header/FeedbackLink.js
@@ -5,17 +5,11 @@ import A from 'ui/A'
 function FeedbackLink() {
   const { pathname } = useLocation()
 
-  let ref = ''
-  let header = 'header'
-  if (pathname.includes('feedback')) {
-    header = 'headerHighlight'
-  } else {
-    ref = pathname
-  }
+  const header = pathname.includes('feedback') ? 'headerHighlight' : 'header'
 
   return (
     <div className="border-l py-1 pl-4">
-      <A to={{ pageName: 'feedback', options: { ref } }} variant={header}>
+      <A to={{ pageName: 'feedback' }} variant={header}>
         Feedback
       </A>
     </div>
