254f43d561755689fc8106e270b9e397dc000926
nicholas-codeocv <nicholas.deschenes@codecov.io>
2022-07-18 08:34:10 -0300

    add tests for footer

--- src/layouts/Footer/Footer.spec.js
+++ src/layouts/Footer/Footer.spec.js
@@ -15,8 +15,9 @@ const loggedInUser = {
 }
 
 describe('Footer', () => {
-  function setup() {
-    useUser.mockReturnValue(loggedInUser)
+  function setup(userData = undefined) {
+    useUser.mockReturnValue({ data: userData })
+
     render(
       <MemoryRouter initialEntries={['/bb/critical-role/bells-hells']}>
         <Route path="/:provider/:owner/:repo">
@@ -25,6 +26,26 @@ describe('Footer', () => {
       </MemoryRouter>
     )
   }
+
+  describe('rendering the feedback link', () => {
+    describe('user is signed in', () => {
+      beforeEach(() => {
+        setup(loggedInUser)
+      })
+      it('renders the link', () => {
+        const feedback = screen.getByText('Feedback')
+        expect(feedback).toBeInTheDocument()
+      })
+    })
+    describe('user is not signed in', () => {
+      beforeEach(() => {
+        setup()
+      })
+      it('does not render link with no signed in user', () => {
+        expect(screen.queryByText('Feedback')).toBeNull()
+      })
+    })
+  })
 
   describe('renders the current years copywrite', () => {
     beforeEach(() => {
