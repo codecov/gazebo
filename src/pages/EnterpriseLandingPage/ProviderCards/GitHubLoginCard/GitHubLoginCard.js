7564dd2c22a871b268f950cc24ea717a423aa59d
nicholas-codeocv <nicholas.deschenes@codecov.io>
2022-07-25 10:43:42 -0300

    add provider logos to headers

--- src/pages/EnterpriseLandingPage/ProviderCards/GitHubLoginCard/GitHubLoginCard.js
+++ src/pages/EnterpriseLandingPage/ProviderCards/GitHubLoginCard/GitHubLoginCard.js
@@ -1,12 +1,22 @@
 import PropTypes from 'prop-types'
 
+import { providerImage, providerToName } from 'shared/utils/provider'
 import Button from 'ui/Button'
 
 function GitHubLoginCard({ github }) {
+  const provider = 'gh'
+
   return (
     <div className="flex flex-col items-center">
-      <div className="w-64">
-        <h2 className="text-2xl mb-2">GitHub</h2>
+      <div className="flex flex-col w-64">
+        <div className="flex flex-row gap-1 items-center justify-center pb-2 w-64">
+          <img
+            alt={`Logo of ${providerToName(provider)}`}
+            className="mx-2 h-6 w-6"
+            src={providerImage(provider)}
+          />
+          <h2 className="text-2xl">GitHub</h2>
+        </div>
         <div className="flex flex-col gap-2">
           {github.map(({ hostingOption }, i) =>
             hostingOption === 'EXTERNAL' ? (
--- src/pages/EnterpriseLandingPage/ProviderCards/GitLabLoginCard/GitLabLoginCard.js
+++ src/pages/EnterpriseLandingPage/ProviderCards/GitLabLoginCard/GitLabLoginCard.js
@@ -1,13 +1,23 @@
 import PropTypes from 'prop-types'
 
+import { providerImage, providerToName } from 'shared/utils/provider'
 import Button from 'ui/Button'
 
 function GitLabLoginCard({ gitlab }) {
+  const provider = 'gl'
+
   return (
     <div className="flex flex-col items-center">
-      <div className="w-64">
-        <h2 className="text-2xl mb-2">GitLab</h2>
-        <div className="flex flex-col gap-2">
+      <div className="flex flex-col w-64">
+        <div className="flex flex-row gap-1 items-center justify-center pb-2 w-64">
+          <img
+            alt={`Logo of ${providerToName(provider)}`}
+            className="mx-2 h-6 w-6"
+            src={providerImage(provider)}
+          />
+          <h2 className="text-2xl">GitLab</h2>
+        </div>
+        <div className="flex flex-col gap-2 w-64">
           {gitlab.map(({ hostingOption }, i) =>
             hostingOption === 'EXTERNAL' ? (
               <Button
