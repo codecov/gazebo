20ce2ca542f9a47095ae86e29ef04bc2053dd9c1
RulaKhaled <rula@codecov.io>
2022-07-25 18:59:16 +0300

    handle launchdarkly flag

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,29 +1,55 @@
 import { render, screen, waitFor } from '@testing-library/react'
 
 import App from './App'
+import { useFlags } from 'shared/featureFlags'
 
 jest.mock('./pages/AccountSettings', () => () => 'AccountSettings')
 jest.mock('react-query/devtools', () => ({
   ReactQueryDevtools: () => 'ReactQueryDevtools',
 }))
+jest.mock('shared/featureFlags')
 
 describe('App', () => {
+  function setup() {
+    useFlags.mockReturnValue({
+      gazeboBillingsTab: true,
+    })
+    render(<App />)
+  }
+
   describe('when rendering', () => {
-    const setup = () => render(<App />)
     beforeEach(() => {
       window.history.pushState({}, 'Test page', '/account/gh/codecov/')
+      setup()
     })
 
     it('renders the loading state', () => {
-      setup()
       const loading = screen.getByTestId('logo-spinner')
       expect(loading).toBeInTheDocument()
     })
 
     it('renders the AccountSettings page', () => {
-      setup()
       return waitFor(() => {
         const page = screen.getByText(/AccountSettings/i)
+        expect(page).toBeInTheDocument()
+      })
+    })
+  })
+
+  describe('',()=>{
+    beforeEach(() => {
+      window.history.pushState({}, 'Test page', '/billing/gh/codecov/')
+      setup()
+    })
+
+    it('renders the loading state', () => {
+      const loading = screen.getByTestId('logo-spinner')
+      expect(loading).toBeInTheDocument()
+    })
+
+    it('renders billing page', () => {
+      return waitFor(() => {
+        const page = screen.getByText(/Current org plan/i)
         expect(page).toBeInTheDocument()
       })
     })
