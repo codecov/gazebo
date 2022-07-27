16fa6428960c33ff5745e031695ca686d5668fc7
nicholas-codeocv <nicholas.deschenes@codecov.io>
2022-07-27 16:20:59 -0300

    add banner image to landing page

--- src/pages/EnterpriseLandingPage/EnterpriseLandingPage.js
+++ src/pages/EnterpriseLandingPage/EnterpriseLandingPage.js
@@ -1,3 +1,5 @@
+import rocketImg from 'assets/enterprise-rocket.png'
+
 import { useServiceProviders } from './hooks'
 import BitbucketLoginCard from './ProviderCards/BitbucketLoginCard'
 import GitHubLoginCard from './ProviderCards/GitHubLoginCard'
@@ -7,21 +9,20 @@ function EnterpriseLandingPage() {
   const { data } = useServiceProviders()
 
   return (
-    <>
-      <div className="flex flex-col gap-4">
-        <div className="h-96">
-          <h2>Banner Image</h2>
-        </div>
-        <hr />
-        <div className="flex md:flex-row md:gap-36 gap-8 flex-col text-center justify-center">
-          {data?.github && <GitHubLoginCard providers={data?.providerList} />}
-          {data?.gitlab && <GitLabLoginCard providers={data?.providerList} />}
-          {data?.bitbucket && (
-            <BitbucketLoginCard providers={data?.providerList} />
-          )}
+    <div className="flex flex-col gap-5">
+      <div className="">
+        <div className="flex justify-center items-center h-96 bg-enterprise-banner-bg relative bg-cover bg-repeat-x bg-center">
+          <img className="h-3/4" alt="codecov space shuttle" src={rocketImg} />
         </div>
       </div>
-    </>
+      <div className="flex md:flex-row md:gap-36 gap-8 flex-col text-center justify-center">
+        {data?.github && <GitHubLoginCard providers={data?.providerList} />}
+        {data?.gitlab && <GitLabLoginCard providers={data?.providerList} />}
+        {data?.bitbucket && (
+          <BitbucketLoginCard providers={data?.providerList} />
+        )}
+      </div>
+    </div>
   )
 }
 
