2b2dd8ebb2d09bd26ba2fae4131737c6e8ac6ed4
RulaKhaled <rula@codecov.io>
2022-08-08 20:39:36 +0300

    leave comment for future refactor

--- src/services/useShouldRenderBillingTabs/hooks.js
+++ src/services/useShouldRenderBillingTabs/hooks.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom'
 import { useUser } from 'services/user'
 import { useFlags } from 'shared/featureFlags'
 
+//once we are done with this feature flag, we should call this useIsPersonalAccount, watch out for this change's ripple effect
 export function useShouldRenderBillingTabs() {
   const { gazeboPlanTab } = useFlags({
     gazeboPlanTab: false,
