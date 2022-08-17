4b67aa14e2653f90cb04a0475fa244de0503ba08
nicholas-codeocv <nicholas.deschenes@codecov.io>
2022-08-17 11:22:26 -0300

    update services/repos hooks

--- src/services/repos/hooks.js
+++ src/services/repos/hooks.js
@@ -1,4 +1,4 @@
-import { useInfiniteQuery } from 'react-query'
+import { useInfiniteQuery } from '@tanstack/react-query'
 import { useParams } from 'react-router-dom'
 
 import Api from 'shared/api'
