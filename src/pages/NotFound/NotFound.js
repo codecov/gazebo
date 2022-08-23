0b215f0479b6e02cf1e636e52c6b30b4cd11efba
nicholas-codecov <nicholas.deschenes@codecov.io>
2022-08-23 09:04:42 -0300

    add new NotFoundErrorMessage component to switch up message depending if running in self-hosted mode

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,7 +1,41 @@
 import cs from 'classnames'
 
+import config from 'config'
+
 import img404 from './assets/error-404.svg'
 import styles from './NotFound.module.css'
+
+const NotFoundErrorMessage = () => {
+  const { origin } = window.location
+
+  const href = config.IS_ENTERPRISE ? origin : 'https://app.codecov.io/'
+
+  if (config.IS_ENTERPRISE) {
+    return (
+      <p className="my-4 px-3 sm:px-0">
+        You may be able to locate this content by browsing for it from{' '}
+        <a
+          rel="noreferrer"
+          className="text-blue-400"
+          href={href}
+          target="_blank"
+        >
+          the home page
+        </a>
+      </p>
+    )
+  }
+
+  return (
+    <p className="my-4 px-3 sm:px-0">
+      You may be able to locate the content by visiting{' '}
+      <a rel="noreferrer" className="text-blue-400" href={href} target="_blank">
+        Codecov’s home page
+      </a>{' '}
+      and browsing to it.
+    </p>
+  )
+}
 
 const NotFound = () => {
   const { illustration, title } = {
@@ -17,18 +51,7 @@ const NotFound = () => {
         src={illustration}
       />
       <h1 className="text-2xl mt-6">{title}</h1>
-      <p className="my-4 px-3 sm:px-0">
-        You may be able to locate the content by visiting{' '}
-        <a
-          rel="noreferrer"
-          className="text-blue-400"
-          href="https://app.codecov.io/"
-          target="_blank"
-        >
-          Codecov’s home page
-        </a>{' '}
-        and browsing to it.
-      </p>
+      <NotFoundErrorMessage />
       <p>
         <strong>Error 404</strong>
       </p>
