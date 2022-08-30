170cee07ec718461c48779864655fa299dff5843
Rula Abu Hasna <rula@codecov.io>
2022-08-30 18:11:17 +0300

    Hook for commit errors

---
+++ src/services/commitErrors/hooks.js
@@ -0,0 +1,50 @@
+import { useQuery } from '@tanstack/react-query'
+import { useParams } from 'react-router-dom'
+
+import Api from 'shared/api'
+import { mapEdges } from 'shared/utils/graphql'
+
+export function useCommitErrors() {
+  const { provider, owner, repo, commit: commitid } = useParams()
+  const query = `
+      query useCommitErrors($owner: String!, $repo: String!, $commitid: String!) {
+        owner(username: $owner) {
+          repository(name: $repo) {
+            commit(id: $commitid) {
+              yamlErrors{
+                edges{
+                    node{
+                        errorCode
+                    }
+                }
+              }
+              botErrors{
+                edges{
+                    node{
+                        errorCode
+                    }
+                }
+              }
+            }
+          }
+        }
+      }
+    `
+
+  return useQuery(['commit-errors', provider, owner, repo, commitid], () => {
+    return Api.graphql({
+      provider,
+      query,
+      variables: {
+        owner,
+        repo,
+        commitid,
+      },
+    }).then((res) => {
+      return {
+        yamlErrors: mapEdges(res?.data?.owner?.repository?.commit?.yamlErrors),
+        botErrors: mapEdges(res?.data?.owner?.repository?.commit?.botErrors),
+      }
+    })
+  })
+}
---
+++ src/services/commitErrors/hooks.spec.js
@@ -0,0 +1,76 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { renderHook } from '@testing-library/react-hooks'
+import { graphql } from 'msw'
+import { setupServer } from 'msw/node'
+import { MemoryRouter, Route } from 'react-router-dom'
+
+import { useCommitErrors } from './hooks'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+})
+const wrapper = ({ children }) => (
+  <MemoryRouter initialEntries={['/gh/codecov/codecov-exe/commit/9']}>
+    <Route path="/:provider/:owner/:repo/commit/:commit">
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </Route>
+  </MemoryRouter>
+)
+
+const dataReturned = {
+  owner: {
+    repository: {
+      commit: {
+        yamlErrors: {
+          edges: [{ node: { errorCode: 'invalid_yaml' } }],
+        },
+        botErrors: {
+          edges: [{ node: { errorCode: 'repo_bot_invalid' } }],
+        },
+      },
+    },
+  },
+}
+
+const server = setupServer()
+
+beforeAll(() => server.listen())
+beforeEach(() => {
+  jest.useRealTimers()
+  server.resetHandlers()
+  queryClient.clear()
+})
+afterAll(() => server.close())
+
+describe('useCommitErrors', () => {
+  let hookData
+
+  function setup() {
+    server.use(
+      graphql.query(`useCommitErrors`, (req, res, ctx) => {
+        return res(ctx.status(200), ctx.data(dataReturned))
+      })
+    )
+    hookData = renderHook(() => useCommitErrors(), {
+      wrapper,
+    })
+  }
+
+  describe('when called and user is authenticated', () => {
+    beforeEach(() => {
+      setup()
+      return hookData.waitFor(() => hookData.result.current.isSuccess)
+    })
+
+    it('returns commit info', () => {
+      expect(hookData.result.current.data).toEqual({
+        botErrors: [{ errorCode: 'repo_bot_invalid' }],
+        yamlErrors: [{ errorCode: 'invalid_yaml' }],
+      })
+    })
+  })
+})
