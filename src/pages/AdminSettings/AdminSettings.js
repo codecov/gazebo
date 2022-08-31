5a11d76fa7300de2a8145c79db511f45438ceeb4
nicholas-codecov <nicholas.deschenes@codecov.io>
2022-08-31 15:22:54 -0300

    add loader to AdminSettings and lazily import AdminAccess

--- src/pages/AdminSettings/AdminSettings.js
+++ src/pages/AdminSettings/AdminSettings.js
@@ -1,9 +1,19 @@
+import { lazy, Suspense } from 'react'
 import { Redirect, Route, Switch, useParams } from 'react-router-dom'
 
 import SidebarLayout from 'layouts/SidebarLayout'
+import LogoSpinner from 'old_ui/LogoSpinner'
 
 import AdminSettingsHeader from './AdminSettingsHeader'
 import AdminSettingsSidebar from './AdminSettingsSidebar'
+
+const AdminAccess = lazy(() => import('./AdminAccess'))
+
+const Loader = () => (
+  <div className="flex-1 flex items-center justify-center mt-16">
+    <LogoSpinner />
+  </div>
+)
 
 function AdminSettings() {
   const { provider } = useParams()
@@ -14,17 +24,19 @@ function AdminSettings() {
     <>
       <AdminSettingsHeader />
       <SidebarLayout sidebar={<AdminSettingsSidebar />}>
-        <Switch>
-          <Route path="/admin/:provider/access" exact>
-            <p>{provider} access</p>
-          </Route>
-          <Route path="/admin/:provider/users" exact>
-            <p>{provider} users</p>
-          </Route>
-          <Route path="/admin/:provider">
-            <Redirect to={redirectTo} />
-          </Route>
-        </Switch>
+        <Suspense fallback={<Loader />}>
+          <Switch>
+            <Route path="/admin/:provider/access" exact>
+              <AdminAccess />
+            </Route>
+            <Route path="/admin/:provider/users" exact>
+              <p>{provider} users</p>
+            </Route>
+            <Route path="/admin/:provider">
+              <Redirect to={redirectTo} />
+            </Route>
+          </Switch>
+        </Suspense>
       </SidebarLayout>
     </>
   )
