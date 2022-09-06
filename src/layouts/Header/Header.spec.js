123639f4d71564ab084de5e5d8ae2e7901327c36
nicholas-codecov <nicholas.deschenes@codecov.io>
2022-09-06 15:50:49 -0300

    revert header tests

--- src/layouts/Header/Header.spec.js
+++ src/layouts/Header/Header.spec.js
@@ -1,8 +1,5 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { render, screen } from '@testing-library/react'
-import { graphql } from 'msw'
-import { setupServer } from 'msw/node'
-import { MemoryRouter, Route } from 'react-router-dom'
+import { MemoryRouter, Route, Switch } from 'react-router-dom'
 
 import { useUser } from 'services/user'
 
@@ -17,42 +14,17 @@ const loggedInUser = {
   },
 }
 
-const mockSeatData = {
-  config: {
-    seatsUsed: 5,
-    seatsLimit: 10,
-  },
-}
-
-const queryClient = new QueryClient()
-const server = setupServer()
-
-beforeAll(() => server.listen())
-beforeEach(() => {
-  server.resetHandlers()
-  queryClient.clear()
-})
-afterAll(() => server.close())
-
 describe('Header', () => {
   function setup({ provider }) {
     useUser.mockReturnValue({ data: loggedInUser })
 
-    server.use(
-      server.use(
-        graphql.query('Seats', (req, res, ctx) =>
-          res(ctx.status(200), ctx.data(mockSeatData))
-        )
-      )
-    )
-
     render(
       <MemoryRouter initialEntries={[`/${provider}`]}>
-        <Route path="/:provider" exact>
-          <QueryClientProvider client={queryClient}>
+        <Switch>
+          <Route path="/:provider" exact>
             <Header />
-          </QueryClientProvider>
-        </Route>
+          </Route>
+        </Switch>
       </MemoryRouter>
     )
   }
