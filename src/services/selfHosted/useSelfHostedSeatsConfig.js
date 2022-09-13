986de9ddb2a7b8bada405c62302d4e7d81ca9d9d
nicholas-codecov <nicholas.deschenes@codecov.io>
2022-09-13 08:12:24 -0300

    move query outside of hook and return useQuery

--- src/services/selfHosted/useSelfHostedSeatsConfig.js
+++ src/services/selfHosted/useSelfHostedSeatsConfig.js
@@ -2,19 +2,17 @@ import { useQuery } from '@tanstack/react-query'
 
 import Api from 'shared/api'
 
-export const useSelfHostedSeatsConfig = (options = {}) => {
-  const query = `
-    query Seats {
-      config {
-        seatsUsed
-        seatsLimit
-      }
-    }
-  `
-  const apiResponse = useQuery(['Seats'], () => Api.graphql({ query }), {
+const query = `
+query Seats {
+  config {
+    seatsUsed
+    seatsLimit
+  }
+}
+`
+
+export const useSelfHostedSeatsConfig = (options = {}) =>
+  useQuery(['Seats'], () => Api.graphql({ query }), {
     select: ({ data }) => data?.config,
     ...options,
   })
-
-  return apiResponse
-}
