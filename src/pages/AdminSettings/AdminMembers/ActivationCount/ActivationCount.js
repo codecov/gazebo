c781a8e2f221fb35a22868f41e71aee74e03fd7c
nicholas-codecov <nicholas.deschenes@codecov.io>
2022-09-14 08:26:10 -0300

    add seat count props to ActivationCount and when equal render ActivationLimit

--- src/pages/AdminSettings/AdminMembers/ActivationCount/ActivationCount.js
+++ src/pages/AdminSettings/AdminMembers/ActivationCount/ActivationCount.js
@@ -1,13 +1,24 @@
-function ActivationCount() {
+import PropTypes from 'prop-types'
+
+import ActivationLimit from './ActivationLimit'
+
+function ActivationCount({ seatsUsed, seatsLimit }) {
   return (
     <div className="flex flex-col p-4 gap-2 border-2 border-ds-gray-primary mt-4">
       <h3 className="font-semibold">Activation count </h3>
       <p>
-        <span className="font-semibold text-lg">0</span> active members of{' '}
-        <span className="font-semibold text-lg">0</span> available seats
+        <span className="font-semibold text-lg">{seatsUsed}</span> active
+        members of <span className="font-semibold text-lg">{seatsLimit}</span>{' '}
+        available seats
       </p>
+      {seatsUsed === seatsLimit && <ActivationLimit />}
     </div>
   )
+}
+
+ActivationCount.propTypes = {
+  seatsUsed: PropTypes.number,
+  seatsLimit: PropTypes.number,
 }
 
 export default ActivationCount
