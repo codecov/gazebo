54958f458a05e5bee52c567a1106248eef6702b2
Rula Abu Hasna <rula@codecov.io>
2022-09-14 18:56:30 +0300

    more tests

--- src/pages/RepoPage/NewRepoTab/Token/Token.spec.js
+++ src/pages/RepoPage/NewRepoTab/Token/Token.spec.js
@@ -1,19 +1,22 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
+import * as Segment from 'services/tracking/segment'
 import { useUser } from 'services/user'
 
 import Token from './Token'
 
+jest.mock('services/user')
+const trackSegmentSpy = jest.spyOn(Segment, 'trackSegmentEvent')
+
 const loggedInUser = {
   user: {
-    username: 'Zerxus Illerez',
+    username: 'Laerryn Coramar-Seelie',
     trackingMetadata: {
-      ownerid: 12345,
+      ownerid: 4,
     },
   },
 }
-
-jest.mock('services/user')
 
 describe('Token', () => {
   function setup(props) {
@@ -61,6 +64,28 @@ describe('Token', () => {
         })
       ).toBeInTheDocument()
       expect(screen.queryByText(/mytoken/)).not.toBeInTheDocument()
+    })
+  })
+
+  describe('when user clicks on copy token', () => {
+    beforeEach(() => {
+      setup({
+        uploadToken: '0414a776-c670-4fc2-b04d-eeedc0d665c0',
+        isCurrentUserPartOfOrg: true,
+        privateRepo: false,
+      })
+      userEvent.click(screen.getByTestId('clipboard'))
+    })
+
+    it('calls the trackSegmentEvent', () => {
+      expect(trackSegmentSpy).toHaveBeenCalledTimes(1)
+      expect(trackSegmentSpy).toHaveBeenCalledWith({
+        event: 'User Onboarding Copied CI Token',
+        data: {
+          category: 'Onboarding',
+          tokenHash: 'c0d665c0',
+        },
+      })
     })
   })
 })
