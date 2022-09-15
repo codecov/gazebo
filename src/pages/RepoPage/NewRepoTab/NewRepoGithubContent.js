c01ecc2bd9132d0674f80d5c72b769f2bce419e8
Rula Abu Hasna <rula@codecov.io>
2022-09-15 15:32:24 +0300

    extra space + reuse html symbols

--- src/pages/RepoPage/NewRepoTab/NewRepoGithubContent.js
+++ src/pages/RepoPage/NewRepoTab/NewRepoGithubContent.js
@@ -4,10 +4,10 @@ function NewRepoGithubContent() {
   return (
     <div className="flex flex-col gap-6 text-base font-light">
       <div className="flex flex-col gap-1">
-        <p>After completing the three steps in this guide, you’ll have:</p>
+        <p>After completing the three steps in this guide, you&apos;ll have:</p>
         <ul className="list-disc pl-6">
-          <li>integrated Codecov into this repo’s CI/CD.</li>
-          <li> uploaded coverage report to Codecov.</li>
+          <li>integrated Codecov into this repo&apos; s CI/CD.</li>
+          <li>uploaded coverage report to Codecov.</li>
           <li>
             viewed coverage reports on Codecov app and in your pull request.
           </li>
@@ -16,7 +16,7 @@ function NewRepoGithubContent() {
 
       <div className="flex flex-col gap-1">
         <h3 className="font-semibold">Prerequisites</h3>
-        <p>In order to get started you’ll need:</p>
+        <p>In order to get started you&apos;ll need:</p>
         <ul className="list-disc pl-6">
           <li>
             familiarity with modifying your{' '}
