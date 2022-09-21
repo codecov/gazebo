8a9586beee913d073249a6af4aac76ad96dcd42b
nicholas-codecov <nicholas.deschenes@codecov.io>
2022-09-21 09:48:48 -0300

    copy DeletionCard from Admin tab

---
+++ src/pages/AccountSettings/tabs/Profile/DeletionCard/DeletionCard.js
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types'
+
+import Card from 'old_ui/Card'
+import A from 'ui/A'
+
+import ErasePersonalAccountButton from './ErasePersonalAccountButton'
+
+function DeletionCard({ isPersonalSettings, provider, owner }) {
+  return (
+    <Card className="p-10 text-codecov-red">
+      <h2 className="border-b text-2xl pb-4 bold border-codecov-red">
+        Delete account
+      </h2>
+      {isPersonalSettings ? (
+        <>
+          <p className="mt-4 mb-6">
+            Erase all my personal content and personal projects.
+          </p>
+          <ErasePersonalAccountButton provider={provider} owner={owner} />
+        </>
+      ) : (
+        <>
+          <p className="mt-4 mb-6">
+            Erase all my organization content and projects.
+          </p>
+          <A to={{ pageName: 'support' }}>Contact support</A>
+        </>
+      )}
+    </Card>
+  )
+}
+
+DeletionCard.propTypes = {
+  isPersonalSettings: PropTypes.bool.isRequired,
+  provider: PropTypes.string.isRequired,
+  owner: PropTypes.string.isRequired,
+}
+
+export default DeletionCard
