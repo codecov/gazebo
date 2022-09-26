d0483f0a40376e83d64faa2b44d96857fc38c237
Rula Abu Hasna <rula@codecov.io>
2022-09-26 14:55:53 +0300

    Use p instead of div

--- src/pages/CommitPage/YamlErrorBanner/YamlErrorBanner.jsx
+++ src/pages/CommitPage/YamlErrorBanner/YamlErrorBanner.jsx
@@ -6,7 +6,7 @@ function YamlErrorBanner() {
   return (
     <Banner variant="warning">
       <BannerHeading>
-        <div className="font-semibold">Commit YAML is invalid</div>
+        <p className="font-semibold">Commit YAML is invalid</p>
       </BannerHeading>
       <BannerContent>
         Coverage data is unable to be displayed, as the commit YAML appears to
