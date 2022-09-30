6d061eacc41320588a2b27d5dc0209249fd7a4cb
Adrian <adrian@codecov.io>
2022-09-29 18:25:29 -0700

    Add processing warning message to commits + impacted files on compare page

--- src/pages/PullRequestPage/Commits/Commits.jsx
+++ src/pages/PullRequestPage/Commits/Commits.jsx
@@ -1,4 +1,6 @@
+import { CommitStateEnum } from 'shared/utils/commit'
 import A from 'ui/A'
+import Icon from 'ui/Icon'
 
 import { useCompareCommits } from './hooks'
 
@@ -6,22 +8,33 @@ import Card from '../Card'
 
 function Commits() {
   const { data: commits } = useCompareCommits()
+
   return (
     <Card title="Commits">
-      {commits.map(({ message, commitid, author }) => (
+      {commits?.map(({ message, commitid, author, state }) => (
         <div
           key={commitid}
-          className="py-4 first:pt-0 last:pb-0 text-ds-gray-octonary text-sm"
+          className="py-4 first:pt-0 last:pb-0 text-ds-gray-octonary text-sm flex flex-col"
         >
           <A
             to={{ pageName: 'commit', options: { commit: commitid } }}
             variant="cardLink"
           >
-            {message}
+            {message ? message : 'Commit Title Unknown'}
           </A>
-          <p>
-            <span className="text-ds-gray-quaternary">by</span> {author}
-          </p>
+          {author ? (
+            <p>
+              <span className="text-ds-gray-quaternary">by</span> {author}
+            </p>
+          ) : (
+            <span className="text-ds-gray-quaternary">Author Unknown</span>
+          )}
+          {state === CommitStateEnum.ERROR && (
+            <span className="text-ds-primary-red flex items-end pt-1">
+              <Icon name="exclamation" size="flex" variant="solid" /> processing
+              failed
+            </span>
+          )}
         </div>
       ))}
       {commits?.length === 0 && (
