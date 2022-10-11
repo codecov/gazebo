7e4aa8522a6c59cdcc6d7834697401a7a158d163
nicholas-codecov <nicholas.deschenes@codecov.io>
2022-10-11 11:52:07 -0300

    pull pull from pullData

--- src/pages/PullRequestPage/Header/Header.jsx
+++ src/pages/PullRequestPage/Header/Header.jsx
@@ -19,45 +19,44 @@ function Header() {
   // TODO: When we update the cicd link and branch link to mobe this to a hook to match the rest of the page.
   const { provider, owner, repo, pullId } = useParams()
   const { data: pullData } = usePull({ provider, owner, repo, pullId })
+  const pull = pullData?.pull
 
   return (
     <div className="border-b border-ds-gray-secondary pb-4 text-xs">
       <h1 className="flex items-center gap-2 text-lg font-semibold">
-        {pullData?.pull?.title}
+        {pull?.title}
         <span
           className={cs(
             'text-white font-bold px-3 py-0.5 text-xs rounded',
-            pullStateToColor[pullData?.pull?.state]
+            pullStateToColor[pull?.state]
           )}
         >
-          {capitalize(pullData?.pull?.state)}
+          {capitalize(pull?.state)}
         </span>
       </h1>
       <p className="flex items-center gap-2">
         <span>
-          {pullData?.pull?.updatestamp &&
-            formatTimeToNow(pullData?.pull?.updatestamp)}{' '}
-          <span className="bold">{pullData?.pull?.author?.username}</span>{' '}
-          authored{' '}
-          {pullData?.pull?.pullId && (
+          {pull?.updatestamp && formatTimeToNow(pull?.updatestamp)}{' '}
+          <span className="bold">{pull?.author?.username}</span> authored{' '}
+          {pull?.pullId && (
             <A
               href={getProviderPullURL({
                 provider,
                 owner,
                 repo,
-                pullId: pullData?.pull?.pullId,
+                pullId: pull?.pullId,
               })}
               hook="provider-pr-link"
               isExternal={true}
             >
-              #{pullData?.pull?.pullId}
+              #{pull?.pullId}
             </A>
           )}
         </span>
-        <CIStatusLabel ciPassed={pullData?.pull?.head?.ciPassed} />
+        <CIStatusLabel ciPassed={pull?.head?.ciPassed} />
         <span className="flex items-center">
           <Icon name="branch" variant="developer" size="sm" />
-          {pullData?.pull?.head?.branchName}
+          {pull?.head?.branchName}
         </span>
       </p>
     </div>
