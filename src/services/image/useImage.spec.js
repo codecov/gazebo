2a082471ffeb3c117d6c1d1574220af0695ac1f1
nicholas-codecov <nicholas.deschenes@codecov.io>
2022-11-10 09:49:02 -0400

    update useImage to use react query so we can cache valid image src

--- src/services/image/useImage.spec.js
+++ src/services/image/useImage.spec.js
@@ -1,15 +1,30 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { renderHook } from '@testing-library/react-hooks'
 
 import { useImage } from './useImage'
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { retry: false } },
+  logger: {
+    error: () => {},
+  },
+})
+
+const wrapper = ({ children }) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+)
+
+afterEach(() => queryClient.clear())
 
 describe('useImage', () => {
   let hookData
 
   it('starts loading an image', () => {
-    hookData = renderHook(() => useImage({ src: 'image.com' }))
+    hookData = renderHook(() => useImage({ src: 'image.com' }), { wrapper })
 
     expect(hookData.result.current.isLoading).toBeTruthy()
   })
+
   describe('successful network request', () => {
     beforeAll(() => {
       global.Image = class {
@@ -18,15 +33,20 @@ describe('useImage', () => {
             this.onload()
           }, 100)
         }
+
         decode() {
           return new Promise((resolve) => resolve())
         }
       }
     })
+
     it('successfully loads an image', async () => {
-      const { result, waitForNextUpdate } = renderHook(() =>
-        useImage({ src: 'https://api.backend.dev/image.png' })
+      const { result, waitForNextUpdate, waitFor } = renderHook(
+        () => useImage({ src: 'https://api.backend.dev/image.png' }),
+        { wrapper }
       )
+
+      await waitFor(() => expect(result.current.isLoading).toBeTruthy())
 
       await waitForNextUpdate()
 
@@ -34,28 +54,31 @@ describe('useImage', () => {
       expect(result.current.src).toEqual('https://api.backend.dev/image.png')
     })
   })
+
   describe('unsuccessful network request', () => {
     beforeAll(() => {
       global.Image = class {
         constructor() {
           setTimeout(() => {
             this.onload()
-          }, 100)
+          }, 1)
         }
+
         decode() {
-          return new Promise((_resolve, reject) => reject())
+          return Promise.reject(new Error('Oh no!'))
         }
       }
     })
+
     it('cannot successful load an image', async () => {
-      const { result, waitForNextUpdate } = renderHook(() =>
-        useImage({ src: 'https://api.backend.dev/image.png' })
+      const { result, waitForNextUpdate } = renderHook(
+        () => useImage({ src: 'https://api.backend.dev/image.png' }),
+        { wrapper }
       )
 
       await waitForNextUpdate()
 
-      expect(result.current.isLoading).toBeFalsy()
-      expect(result.current.error).toBeTruthy()
+      expect(result.current.error).toBe('Unable to load image')
     })
   })
 })
