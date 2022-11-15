0c451beae5448fe77ddf9597880705b6ea17de77
Terry Smith <terry@codecov.io>
2022-11-15 18:47:42 -0400

    missing files whoops

---
+++ src/services/navigation/useNavLinks/useNavLinks.js
@@ -0,0 +1,385 @@
+import Cookie from 'js-cookie'
+import qs from 'qs'
+import { useParams } from 'react-router-dom'
+
+import config from 'config'
+
+import { useFlags } from 'shared/featureFlags'
+
+export function useNavLinks() {
+  const { provider: p, owner: o, repo: r, id: i, pullId: pi } = useParams()
+  const {
+    gazeboRepoTabs,
+    gazeboPullRequestPage,
+    gazeboCommitsTab,
+    gazeboPullsTab,
+    gazeboSettingsTab,
+    gazeboOverviewTab,
+  } = useFlags({
+    gazeboRepoTabs: false,
+    gazeboPullRequestPage: false,
+    gazeboCommitsTab: false,
+    gazeboPullsTab: false,
+    gazeboSettingsTab: false,
+    gazeboOverviewTab: false,
+  })
+
+  const utmCookie = Cookie.get('utmParams')
+  const utmCookieObj = qs.parse(utmCookie, {
+    ignoreQueryPrefix: true,
+  })
+
+  return {
+    signOut: {
+      text: 'Sign Out',
+      path: ({ provider = p } = { provider: p }) =>
+        `${config.BASE_URL}/logout/${provider}`,
+      isExternalLink: true,
+    },
+    signIn: {
+      text: 'Log in',
+      path: ({ provider = p, privateScope, to } = { provider: p }) => {
+        const query = qs.stringify(
+          {
+            to,
+            private: privateScope,
+            ...utmCookieObj,
+          },
+          { addQueryPrefix: true }
+        )
+        return `${config.BASE_URL}/login/${provider}${query}`
+      },
+      isExternalLink: true,
+    },
+    signUp: {
+      text: 'Sign Up',
+      path: () => {
+        const params = qs.stringify(utmCookieObj, {
+          addQueryPrefix: true,
+        })
+        return `${config.MARKETING_BASE_URL}/sign-up/${params}`
+      },
+      isExternalLink: true,
+    },
+    provider: {
+      path: ({ provider = p } = { provider: p }) => `/${provider}`,
+      isExternalLink: false,
+    },
+    providerAddRepo: {
+      path: ({ provider = p } = { provider: p }) => `/${provider}/+`,
+      isExternalLink: false,
+    },
+    owner: {
+      path: ({ provider = p, owner = o } = { provider: p, owner: o }) =>
+        `/${provider}/${owner}`,
+      isExternalLink: false,
+    },
+    ownerAddRepo: {
+      path: ({ provider = p, owner = o } = { provider: p, owner: o }) =>
+        `/${provider}/${owner}/+`,
+      isExternalLink: false,
+    },
+    analytics: {
+      path: ({ provider = p, owner = o } = { provider: p, owner: o }) =>
+        `/analytics/${provider}/${owner}`,
+      isExternalLink: false,
+    },
+    repo: {
+      path: (
+        { provider = p, owner = o, repo = r } = {
+          provider: p,
+          owner: o,
+          repo: r,
+        }
+      ) => `/${provider}/${owner}/${repo}`,
+      isExternalLink: gazeboRepoTabs,
+    },
+    account: {
+      text: 'Personal Settings',
+      path: ({ provider = p, owner = o } = { provider: p, owner: o }) =>
+        `/account/${provider}/${owner}`,
+      isExternalLink: false,
+    },
+    planTab: {
+      text: 'Plan',
+      path: ({ provider = p, owner = o } = { provider: p, owner: o }) =>
+        `/plan/${provider}/${owner}`,
+      isExternalLink: false,
+    },
+    membersTab: {
+      text: 'Members',
+      path: ({ provider = p, owner = o } = { provider: p, owner: o }) =>
+        `/members/${provider}/${owner}`,
+      isExternalLink: false,
+    },
+    upgradeOrgPlan: {
+      text: 'Upgrade Plan',
+      path: ({ provider = p, owner = o } = { provider: p, owner: o }) =>
+        `/plan/${provider}/${owner}/upgrade`,
+      isExternalLink: false,
+    },
+    cancelOrgPlan: {
+      text: 'Cancel Plan',
+      path: ({ provider = p, owner = o } = { provider: p, owner: o }) =>
+        `/plan/${provider}/${owner}/cancel`,
+      isExternalLink: false,
+    },
+    invoicesPage: {
+      path: ({ provider = p, owner = o } = { provider: p, owner: o }) =>
+        `/plan/${provider}/${owner}/invoices`,
+      isExternalLink: false,
+      text: 'Invoices',
+    },
+    invoiceDetailsPage: {
+      path: (
+        { provider = p, owner = o, id = i } = { provider: p, owner: o, id: i }
+      ) => `/plan/${provider}/${owner}/invoices/${id}`,
+      isExternalLink: false,
+    },
+    accountAdmin: {
+      text: 'Admin',
+      path: ({ provider = p, owner = o } = { provider: p, owner: o }) =>
+        `/account/${provider}/${owner}`,
+      isExternalLink: false,
+    },
+    yamlTab: {
+      text: 'Global YAML',
+      path: ({ provider = p, owner = o } = { provider: p, owner: o }) =>
+        `/account/${provider}/${owner}/yaml`,
+      isExternalLink: false,
+    },
+    accessTab: {
+      text: 'Access',
+      path: ({ provider = p, owner = o } = { provider: p, owner: o }) =>
+        `/account/${provider}/${owner}/access`,
+      isExternalLink: true,
+    },
+    internalAccessTab: {
+      text: 'Access',
+      path: ({ provider = p, owner = o } = { provider: p, owner: o }) =>
+        `/account/${provider}/${owner}/access`,
+      isExternalLink: false,
+    },
+    billingAndUsers: {
+      text: 'Billing & Users',
+      path: ({ provider = p, owner = o } = { provider: p, owner: o }) =>
+        `/account/${provider}/${owner}/billing`,
+      isExternalLink: false,
+    },
+    commits: {
+      path: (
+        { provider = p, owner = o, repo = r } = {
+          provider: p,
+          owner: o,
+          repo: r,
+        }
+      ) => `/${provider}/${owner}/${repo}/commits`,
+      isExternalLink: gazeboCommitsTab,
+      text: 'Commits',
+    },
+    commit: {
+      path: (
+        { provider = p, owner = o, repo = r, commit } = {
+          provider: p,
+          owner: o,
+          repo: r,
+        }
+      ) => `/${provider}/${owner}/${repo}/commit/${commit}`,
+      isExternalLink: false,
+      text: 'Commit',
+    },
+    commitFile: {
+      path: (
+        { provider = p, owner = o, repo = r, commit, path } = {
+          provider: p,
+          owner: o,
+          repo: r,
+        }
+      ) => `/${provider}/${owner}/${repo}/commit/${commit}/${path}`,
+      isExternalLink: false,
+      text: 'Commit File',
+    },
+    treeView: {
+      path: (
+        { provider = p, owner = o, repo = r, tree, ref } = {
+          provider: p,
+          owner: o,
+          repo: r,
+        }
+      ) => {
+        if (!tree && !ref) {
+          return `/${provider}/${owner}/${repo}/tree/`
+        } else if (!tree) {
+          return `/${provider}/${owner}/${repo}/tree/${ref}/`
+        } else {
+          return `/${provider}/${owner}/${repo}/tree/${ref}/${tree}`
+        }
+      },
+      isExternalLink: false,
+      text: 'Tree View',
+    },
+    fileViewer: {
+      path: (
+        { provider = p, owner = o, repo = r, ref, tree } = {
+          provider: p,
+          owner: o,
+          repo: r,
+        }
+      ) => `/${provider}/${owner}/${repo}/blob/${ref}/${tree}`,
+      isExternalLink: false,
+      text: 'File Viewer',
+    },
+    new: {
+      path: (
+        { provider = p, owner = o, repo = r } = {
+          provider: p,
+          owner: o,
+          repo: r,
+        }
+      ) => `/${provider}/${owner}/${repo}/new`,
+      isExternalLink: gazeboRepoTabs,
+      text: 'New',
+    },
+    overview: {
+      path: (
+        { provider = p, owner = o, repo = r } = {
+          provider: p,
+          owner: o,
+          repo: r,
+        }
+      ) => `/${provider}/${owner}/${repo}`,
+      isExternalLink: gazeboOverviewTab,
+      text: 'Overview',
+    },
+    flagsTab: {
+      path: (
+        { provider = p, owner = o, repo = r } = {
+          provider: p,
+          owner: o,
+          repo: r,
+        }
+      ) => `/${provider}/${owner}/${repo}/flags`,
+      isExternalLink: false,
+      text: 'Flags',
+    },
+    branches: {
+      path: (
+        { provider = p, owner = o, repo = r } = {
+          provider: p,
+          owner: o,
+          repo: r,
+        }
+      ) => `/${provider}/${owner}/${repo}/branches`,
+      isExternalLink: gazeboRepoTabs,
+      text: 'Branches',
+    },
+    pulls: {
+      path: (
+        { provider = p, owner = o, repo = r } = {
+          provider: p,
+          owner: o,
+          repo: r,
+        }
+      ) => `/${provider}/${owner}/${repo}/pulls`,
+      isExternalLink: gazeboPullsTab,
+      text: 'Pulls',
+    },
+    pullDetail: {
+      path: (
+        { provider = p, owner = o, repo = r, pullId = pi } = {
+          provider: p,
+          owner: o,
+          repo: r,
+          pullId: pi,
+        }
+      ) => `/${provider}/${owner}/${repo}/pull/${pullId}`,
+      isExternalLink: gazeboPullRequestPage,
+      text: 'Pull',
+    },
+    settings: {
+      path: (
+        { provider = p, owner = o, repo = r } = {
+          provider: p,
+          owner: o,
+          repo: r,
+        }
+      ) => `/${provider}/${owner}/${repo}/settings`,
+      isExternalLink: gazeboSettingsTab,
+      text: 'Settings',
+    },
+    settingsGeneral: {
+      path: (
+        { provider = p, owner = o, repo = r } = {
+          provider: p,
+          owner: o,
+          repo: r,
+        }
+      ) => `/${provider}/${owner}/${repo}/settings`,
+      isExternalLink: gazeboSettingsTab,
+      text: 'General',
+    },
+    settingsYaml: {
+      path: (
+        { provider = p, owner = o, repo = r } = {
+          provider: p,
+          owner: o,
+          repo: r,
+        }
+      ) => `/${provider}/${owner}/${repo}/settings/yaml`,
+      isExternalLink: gazeboSettingsTab,
+      text: 'Yaml',
+    },
+    settingsBadge: {
+      path: (
+        { provider = p, owner = o, repo = r } = {
+          provider: p,
+          owner: o,
+
+          repo: r,
+        }
+      ) => `/${provider}/${owner}/${repo}/settings/badge`,
+      isExternalLink: gazeboSettingsTab,
+      text: 'Badges & Graphs',
+    },
+    feedback: {
+      text: 'Feedback',
+      path: ({ provider = p, ref } = { provider: p, ref: null }) => {
+        if (ref) {
+          return `/${provider}/feedback?ref=${encodeURIComponent(ref)}`
+        }
+        return `/${provider}/feedback`
+      },
+      isExternalLink: false,
+    },
+    prevLink: {
+      text: 'Back',
+      path: ({ provider = p, ref } = { provider: p, ref: null }) => {
+        if (ref) {
+          return decodeURIComponent(ref)
+        }
+        return `/${provider}`
+      },
+      isExternalLink: false,
+    },
+    access: {
+      text: 'Access',
+      path: ({ provider = p } = { provider: p }) => {
+        return `/admin/${provider}/access`
+      },
+      isExternalLink: false,
+    },
+    users: {
+      text: 'Users',
+      path: ({ provider = p } = { provider: p }) => {
+        return `/admin/${provider}/users`
+      },
+      isExternalLink: false,
+    },
+    profile: {
+      text: 'Profile',
+      path: ({ provider = p, owner = o } = { provider: p, owner: o }) =>
+        `/account/${provider}/${owner}`,
+      isExternalLink: false,
+    },
+  }
+}
