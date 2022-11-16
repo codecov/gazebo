bc4db8f1f435c899912d00510238df1b3bae5c12
Terry <87824812+terry-codecov@users.noreply.github.com>
2022-11-16 15:40:34 -0400

    Revert "CODE-2167: Update Member list to table component (#1667)"
    This reverts commit 31d4cfa5e1899ae9ed8c0f954a1d52dbac222804.

---
+++ src/pages/MembersPage/MembersList/FormControls/enums.js
@@ -0,0 +1,20 @@
+import { ApiFilterEnum } from 'services/navigation'
+
+export const AdminItems = [
+  { label: 'Everyone', value: ApiFilterEnum.none },
+  { label: 'Admins', value: ApiFilterEnum.true },
+  { label: 'Collaborators', value: ApiFilterEnum.false },
+]
+
+export const ActivatedItems = [
+  { label: 'All users', value: ApiFilterEnum.none },
+  { label: 'Active users', value: ApiFilterEnum.true },
+  { label: 'Inactive users', value: ApiFilterEnum.false },
+]
+
+export const OrderItems = [
+  { value: 'last_pull_timestamp', label: 'Oldest PR' },
+  { value: '-last_pull_timestamp', label: 'Newest PR' },
+  { value: 'name', label: 'Name Z-A' },
+  { value: '-name', label: 'Name A-Z' },
+]
