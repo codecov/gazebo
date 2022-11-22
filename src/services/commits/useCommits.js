c74d639df7027d8909384544d8e9485b1b1b5b0f
nicholas-codecov <nicholas.deschenes@codecov.io>
2022-11-22 07:47:09 -0400

    update useCommits to pass along signal

--- src/services/commits/useCommits.js
+++ src/services/commits/useCommits.js
@@ -3,7 +3,7 @@ import { useInfiniteQuery } from '@tanstack/react-query'
 import Api from 'shared/api'
 import { mapEdges } from 'shared/utils/graphql'
 
-function fetchRepoCommits({ provider, owner, repo, variables, after }) {
+function fetchRepoCommits({ provider, owner, repo, variables, after, signal }) {
   const CommitFragment = `
    fragment CommitFragment on Commit {
         ciPassed
@@ -54,6 +54,7 @@ function fetchRepoCommits({ provider, owner, repo, variables, after }) {
     provider,
     repo,
     query,
+    signal,
     variables: {
       owner,
       repo,
@@ -76,13 +77,14 @@ export function useCommits({ provider, owner, repo, filters }) {
   }
   const { data, ...rest } = useInfiniteQuery(
     ['commits', provider, owner, repo, variables],
-    ({ pageParam }) =>
+    ({ pageParam, signal }) =>
       fetchRepoCommits({
         provider,
         owner,
         repo,
         variables,
         after: pageParam,
+        signal,
       }),
     {
       getNextPageParam: (data) =>
