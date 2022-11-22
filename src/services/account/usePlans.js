6bade34e98a4de82ff9906cb4be03a4021292560
nicholas-codecov <nicholas.deschenes@codecov.io>
2022-11-22 07:43:30 -0400

    update usePlans to pass along signal

--- src/services/account/usePlans.js
+++ src/services/account/usePlans.js
@@ -2,14 +2,14 @@ import { useQuery } from '@tanstack/react-query'
 
 import Api from 'shared/api'
 
-function fetchPlan(provider) {
+function fetchPlan({ provider, signal }) {
   const path = `/plans`
-  return Api.get({ path, provider })
+  return Api.get({ path, provider, signal })
 }
 
 export function usePlans(provider) {
   // the plans are very static data
-  return useQuery(['plans'], () => fetchPlan(provider), {
+  return useQuery(['plans'], ({ signal }) => fetchPlan({ provider, signal }), {
     // I dunno here if staleTime should be infinity. Plans do change every now and then :surprised-face:
     cacheTime: Infinity,
     staleTime: Infinity,
