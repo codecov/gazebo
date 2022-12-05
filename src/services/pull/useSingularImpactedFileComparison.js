5bd0732ff52740b4eda20bedfacd7e6b9887b6ce
nicholas-codecov <nicholas.deschenes@codecov.io>
2022-12-05 13:55:52 -0400

    clean/refactor useSingularImpactedFileComp to allow us to properly prefetch the data

--- src/services/pull/useSingularImpactedFileComparison.js
+++ src/services/pull/useSingularImpactedFileComparison.js
@@ -2,14 +2,7 @@ import { useQuery } from '@tanstack/react-query'
 
 import Api from 'shared/api'
 
-export function useSingularImpactedFileComparison({
-  provider,
-  owner,
-  repo,
-  pullId,
-  path,
-}) {
-  const query = `
+const query = `
     query ImpactedFileComparison($owner: String!, $repo: String!, $pullId: Int!, $path: String!) {
       owner(username: $owner) {
         repository(name: $repo) {
@@ -49,52 +42,56 @@ export function useSingularImpactedFileComparison({
       }
     }
     `
+function setFileLabel({ isNewFile, isRenamedFile, isDeletedFile }) {
+  if (isNewFile) return 'New'
+  if (isRenamedFile) return 'Renamed'
+  if (isDeletedFile) return 'Deleted'
+  return null
+}
 
-  const fetchSingularImpactedFile = ({ signal }) => {
-    return Api.graphql({
-      provider,
-      query,
-      signal,
-      variables: {
-        provider,
-        owner,
-        repo,
-        pullId: parseInt(pullId, 10),
-        path,
-      },
-    })
+function transformImpactedFileData(impactedFile) {
+  const fileLabel = setFileLabel({
+    isNewFile: impactedFile?.isNewFile,
+    isRenamedFile: impactedFile?.isRenamedFile,
+    isDeletedFile: impactedFile?.isDeletedFile,
+  })
+
+  return {
+    fileLabel,
+    headName: impactedFile?.headName,
+    isCriticalFile: impactedFile?.isCriticalFile,
+    segments: impactedFile?.segments,
   }
+}
 
-  function setFileLabel({ isNewFile, isRenamedFile, isDeletedFile }) {
-    if (isNewFile) return 'New'
-    if (isRenamedFile) return 'Renamed'
-    if (isDeletedFile) return 'Deleted'
-    return null
-  }
-
-  function transformImpactedFileData(impactedFile) {
-    const fileLabel = setFileLabel({
-      isNewFile: impactedFile?.isNewFile,
-      isRenamedFile: impactedFile?.isRenamedFile,
-      isDeletedFile: impactedFile?.isDeletedFile,
-    })
-
-    return {
-      fileLabel,
-      headName: impactedFile?.headName,
-      isCriticalFile: impactedFile?.isCriticalFile,
-      segments: impactedFile?.segments,
-    }
-  }
-
+export function useSingularImpactedFileComparison({
+  provider,
+  owner,
+  repo,
+  pullId,
+  path,
+}) {
   return useQuery(
     ['ImpactedFileComparison', provider, owner, repo, pullId, path],
-    ({ signal }) => fetchSingularImpactedFile({ signal }),
-    {
-      select: ({ data }) =>
+    ({ signal }) =>
+      Api.graphql({
+        provider,
+        query,
+        signal,
+        variables: {
+          provider,
+          owner,
+          repo,
+          pullId: parseInt(pullId, 10),
+          path,
+        },
+      }).then((res) =>
         transformImpactedFileData(
-          data?.owner?.repository?.pull?.compareWithBase?.impactedFile
-        ),
+          res?.data?.owner?.repository?.pull?.compareWithBase?.impactedFile
+        )
+      ),
+    {
+      suspense: false,
     }
   )
 }
