951aae659f61b7ef4afe68264ed201814136994f
nicholas-codecov <nicholas.deschenes@sentry.io>
2022-12-15 10:52:55 -0400

    clean up upload alert banners

--- src/pages/OwnerPage/Header/HeaderBanners/ExceededUploadsAlert/ExceededUploadsAlert.jsx
+++ src/pages/OwnerPage/Header/HeaderBanners/ExceededUploadsAlert/ExceededUploadsAlert.jsx
@@ -7,15 +7,13 @@ import Button from 'ui/Button'
 const ExceededUploadsAlert = () => (
   <Banner>
     <BannerHeading>
-      <div className="flex justify-center gap-2">
-        <h2>Upload limit has been reached</h2>
-      </div>
+      <h2 className="font-semibold">Upload limit has been reached</h2>
     </BannerHeading>
     <BannerContent>
-      <p className="text-ds-gray-quinary">
+      <p>
         This org is currently on the free plan; which includes 250 free uploads
-        on a rolling monthly basis. This limit has been reached and the reports
-        will not generate. To resolve this,{' '}
+        monthly. This month&apos;s period has been reached and the reports will
+        not generate. To resolve this,{' '}
         <A to={{ pageName: 'upgradeOrgPlan' }}>upgrade plan</A> and you&apos;ll
         have unlimited uploads.
       </p>
@@ -24,12 +22,9 @@ const ExceededUploadsAlert = () => (
           Upgrade plan
         </Button>
       </div>
-      <p className="text-ds-gray-quinary">
-        <span className="font-semibold">
-          Do you have questions or need help?
-        </span>{' '}
-        Connect with our sales team today at{' '}
-        <A to={{ pageName: 'sales' }}>sales@codecov.io</A>
+      <p>
+        <span className="font-semibold">Need help?</span> Connect with our sales
+        team today at <A to={{ pageName: 'sales' }}>sales@codecov.io</A>
       </p>
     </BannerContent>
   </Banner>
--- src/pages/OwnerPage/Header/HeaderBanners/ReachingUploadLimit/ReachingUploadLimit.jsx
+++ src/pages/OwnerPage/Header/HeaderBanners/ReachingUploadLimit/ReachingUploadLimit.jsx
@@ -7,15 +7,13 @@ import Button from 'ui/Button'
 const ExceededUploadsAlert = () => (
   <Banner>
     <BannerHeading>
-      <div className="flex justify-center gap-2">
-        <h2>Upload limit almost reached</h2>
-      </div>
+      <h2 className="font-semibold">Upload limit almost reached</h2>
     </BannerHeading>
     <BannerContent>
-      <p className="text-ds-gray-quinary">
+      <p>
         This org is currently on the free plan; which includes 250 free uploads
-        on a rolling monthly basis. This limit has been reached and the reports
-        will not generate. To resolve this,{' '}
+        monthly. This month&apos;s period is nearly reached and the reports will
+        not generate. To resolve this,{' '}
         <A to={{ pageName: 'upgradeOrgPlan' }}>upgrade plan</A> and you&apos;ll
         have unlimited uploads.
       </p>
@@ -24,7 +22,7 @@ const ExceededUploadsAlert = () => (
           Upgrade plan
         </Button>
       </div>
-      <p className="text-ds-gray-quinary">
+      <p>
         <span className="font-semibold">Need help?</span> Connect with our sales
         team today at <A to={{ pageName: 'sales' }}>sales@codecov.io</A>
       </p>
