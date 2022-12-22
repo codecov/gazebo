3d4c54e765c87cca3aba7028c68d804915a3eaec
nicholas-codecov <nicholas.deschenes@sentry.io>
2022-12-22 15:38:08 -0400

    setup new tab navigation component for the commit page

---
+++ src/pages/CommitPage/CommitPageTabs/CommitPageTabs.jsx
@@ -0,0 +1,54 @@
+import PropTypes from 'prop-types'
+import { useLocation, useParams } from 'react-router-dom'
+
+import {
+  commitFileviewString,
+  commitTreeviewString,
+} from 'pages/RepoPage/utils'
+import TabNavigation from 'ui/TabNavigation'
+
+// eslint-disable-next-line max-statements
+function CommitPageTabs({ commitSHA }) {
+  const location = useLocation()
+  const { provider, owner, repo } = useParams()
+
+  const blobPath = location.pathname.includes(
+    `/${provider}/${commitFileviewString({ owner, repo, commitSHA })}`
+  )
+
+  const filePath = location.pathname.includes(
+    `/${provider}/${commitTreeviewString({ owner, repo, commitSHA })}`
+  )
+
+  let customLocation = undefined
+  if (blobPath || filePath) {
+    customLocation = {
+      pathname: `/${provider}/${owner}/${repo}/commit/${commitSHA}/tree`,
+    }
+  }
+
+  return (
+    <TabNavigation
+      tabs={[
+        {
+          pageName: 'commit',
+          children: 'Impacted Files',
+          options: { commit: commitSHA },
+          exact: true,
+        },
+        {
+          pageName: 'commitTreeView',
+          children: 'Files',
+          options: { commitSha: commitSHA },
+          location: customLocation,
+        },
+      ]}
+    />
+  )
+}
+
+CommitPageTabs.propTypes = {
+  commitSHA: PropTypes.string,
+}
+
+export default CommitPageTabs
