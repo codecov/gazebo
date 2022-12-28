3f0a09bbc080a5bc6064f9b6aa4f86c20f24ac68
Rula Abu Hasna <rula@codecov.io>
2022-12-28 17:52:55 +0200

    more test sfor no repos block

--- src/shared/ListRepo/ReposTable/NoReposBlock.spec.jsx
+++ src/shared/ListRepo/ReposTable/NoReposBlock.spec.jsx
@@ -2,15 +2,28 @@ import { render, screen } from 'custom-testing-library'
 
 import { MemoryRouter, Route } from 'react-router-dom'
 
+import { useLocationParams } from 'services/navigation'
 import { ActiveContext } from 'shared/context'
 
 import NoReposBlock from './NoReposBlock'
 
+jest.mock('services/navigation', () => ({
+  ...jest.requireActual('services/navigation'),
+  useLocationParams: jest.fn(),
+}))
+
+const mockUpdateParams = jest.fn()
+
 describe('NoReposBlock', () => {
-  function setup({ owner, repoDisplay }) {
+  function setup({ owner, repoDisplay, urlParams = '' }) {
     const props = {
       owner,
     }
+    useLocationParams.mockReturnValue({
+      updateParams: mockUpdateParams,
+      params: urlParams,
+    })
+
     render(
       <MemoryRouter>
         <Route>
@@ -35,9 +48,27 @@ describe('NoReposBlock', () => {
       expect(p).toBeInTheDocument()
     })
 
+    it('updates the params when clicking on select the repo text', () => {
+      const p = screen.getByText('Select the repo')
+      p.click()
+
+      expect(mockUpdateParams).toBeCalledWith({
+        repoDisplay: 'Inactive',
+      })
+    })
+
     it('renders the button the set up link', () => {
       const buttons = screen.getAllByText(/No repos setup yet/)
       expect(buttons.length).toBe(1)
+    })
+
+    it('updates the params when clicking on View repos for setup button', () => {
+      const btn = screen.getByText('View repos for setup')
+      btn.click()
+
+      expect(mockUpdateParams).toBeCalledWith({
+        repoDisplay: 'Inactive',
+      })
     })
   })
 
