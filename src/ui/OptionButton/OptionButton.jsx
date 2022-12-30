b5599927fb07ceff3d227b4099ee05303ae181b5
Terry Smith <terry@codecov.io>
2022-12-30 14:56:10 -0400

    Fix up the org control table again

--- src/ui/OptionButton/OptionButton.jsx
+++ src/ui/OptionButton/OptionButton.jsx
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types'
 
 function OptionButton({ active, options, onChange }) {
   return (
-    <div className="rounded border max-w-max">
+    <div className="flex flex-wrap rounded border divide-x">
       {options.map((o, index) => {
         return (
           <button
-            className={cs('py-1 px-2 text-sm cursor-pointer', {
+            className={cs('flex-1 py-1 px-2 text-sm cursor-pointer', {
               'bg-ds-blue-darker text-white font-semibold': active === o.text,
               'rounded-l': index === 0,
               'rounded-r': index === options.length - 1,
