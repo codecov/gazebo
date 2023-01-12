74336bda0c6174e5e77037a1fcc5ab4e15d66131
Terry <87824812+terry-codecov@users.noreply.github.com>
2023-01-12 11:34:04 -0400

    Mobile css tweak on org select and coverage tab (#1790)
    * Fix up mobile ord members tab
    
    * Improve billing mobile styles
    
    * Mobile styles in the repo settings page
    
    * Disable graph zoom in coverage tab
    
    * cleanup typo
    
    * Fix up the org control table again
    
    * Remove dead class code
    
    * Improve the options button styles on mobile

--- src/shared/ContentsTable/ContentsTableHeader/ContentsTableHeader.jsx
+++ src/shared/ContentsTable/ContentsTableHeader/ContentsTableHeader.jsx
@@ -1,5 +1,9 @@
 function ContentsTableHeader({ children }) {
-  return <div className="flex justify-between">{children}</div>
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 2xl:grid-cols-8 grid-flow-row gap-4">
+      {children}
+    </div>
+  )
 }
 
 export default ContentsTableHeader
