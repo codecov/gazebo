4426b590e54e088becb6b01495cf65cd04755512
Terry Smith <terry@codecov.io>
2023-01-12 11:15:42 -0400

    Improve the options button styles on mobile

--- src/ui/OptionButton/OptionButton.jsx
+++ src/ui/OptionButton/OptionButton.jsx
@@ -7,11 +7,14 @@ function OptionButton({ active, options, onChange }) {
       {options.map((o, index) => {
         return (
           <button
-            className={cs('flex-1 py-1 px-2 text-sm cursor-pointer', {
-              'bg-ds-blue-darker text-white font-semibold': active === o.text,
-              'rounded-l': index === 0,
-              'rounded-r': index === options.length - 1,
-            })}
+            className={cs(
+              'flex-1 py-1 px-2 text-sm cursor-pointer whitespace-nowrap',
+              {
+                'bg-ds-blue-darker text-white font-semibold': active === o.text,
+                'rounded-l': index === 0,
+                'rounded-r': index === options.length - 1,
+              }
+            )}
             onClick={() => {
               onChange(o)
             }}
