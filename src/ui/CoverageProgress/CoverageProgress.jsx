0662a22993ff358041f9def97712971d9e491ed9
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-01-18 11:43:07 -0400

    remove width from CoverageProgress

--- src/ui/CoverageProgress/CoverageProgress.jsx
+++ src/ui/CoverageProgress/CoverageProgress.jsx
@@ -4,11 +4,7 @@ import Progress from 'ui/Progress'
 
 const CoverageProgress = ({ color, totals }) => {
   if (typeof totals?.coverage === 'number') {
-    return (
-      <span className="w-64">
-        <Progress amount={totals?.coverage} label color={color} />
-      </span>
-    )
+    return <Progress amount={totals?.coverage} label color={color} />
   }
 
   return <p className="text-ds-gray-quinary text-sm">No report uploaded yet</p>
