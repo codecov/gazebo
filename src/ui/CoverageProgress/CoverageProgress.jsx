75ac22c78290c6066d89721cc4c4268cf620e773
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-01-18 11:04:14 -0400

    CODE-2290: Create Shared Coverage Progress Bar (#1804)
    * create new coverage progress bar that can be shared across different pages
    
    * add missing export
    
    * switch from span to p

---
+++ src/ui/CoverageProgress/CoverageProgress.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types'
+
+import Progress from 'ui/Progress'
+
+const CoverageProgress = ({ color, totals }) => {
+  if (typeof totals?.coverage === 'number') {
+    return (
+      <span className="w-64">
+        <Progress amount={totals?.coverage} label color={color} />
+      </span>
+    )
+  }
+
+  return <p className="text-ds-gray-quinary text-sm">No report uploaded yet</p>
+}
+
+CoverageProgress.propTypes = {
+  totals: PropTypes.shape({
+    coverage: PropTypes.number,
+  }),
+  color: PropTypes.oneOf(['default', 'danger', 'warning']),
+}
+
+export default CoverageProgress
