f341c3001c640732bf990ce8b22907cdfa45824a
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-01-20 10:27:20 -0400

    update useRepoCommitContentsTable to use new useTableDefaultSort hook

--- src/shared/ContentsTable/useRepoCommitContentsTable/useRepoCommitContentsTable.spec.js
+++ src/shared/ContentsTable/useRepoCommitContentsTable/useRepoCommitContentsTable.spec.js
@@ -257,18 +257,18 @@ describe('useRepoCommitContentsTable', () => {
       await waitFor(() => !result.current.isLoading)
 
       act(() => {
-        result.current.handleSort([{ desc: false, id: 'name' }])
+        result.current.handleSort([{ desc: true, id: 'name' }])
       })
 
       await waitFor(() => result.current.isLoading)
       await waitFor(() => !result.current.isLoading)
 
-      expect(calledCommitContents).toHaveBeenCalledTimes(2)
-      expect(calledCommitContents).toHaveBeenNthCalledWith(2, {
+      expect(calledCommitContents).toHaveBeenCalledTimes(3)
+      expect(calledCommitContents).toHaveBeenNthCalledWith(3, {
         commit: 'sha256',
         filters: {
           ordering: {
-            direction: 'ASC',
+            direction: 'DESC',
             parameter: 'NAME',
           },
         },
