1ebafdf9c8c58d25671a3d90eaebb51fc8375ecb
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-01-24 10:13:03 -0400

    quick fix to link to the correct page

--- src/pages/CommitPage/subroute/ImpactedFiles/CommitsTable.jsx
+++ src/pages/CommitPage/subroute/ImpactedFiles/CommitsTable.jsx
@@ -102,8 +102,8 @@ function createTable({ tableData }) {
         <div className="flex flex-col break-all">
           <A
             to={{
-              pageName: 'commitFile',
-              options: { commit, path: headName },
+              pageName: 'commitFileView',
+              options: { commit, tree: headName },
             }}
           >
             {headName}
