3bb655b173ea5250c15dc53a93240ece63eea7f7
Rula Abu Hasna <rula@codecov.io>
2023-01-24 18:16:01 +0200

    use waitForNextUpdate() to solve timeout issue

--- src/pages/PullRequestPage/hooks/useTabsCounts.spec.js
+++ src/pages/PullRequestPage/hooks/useTabsCounts.spec.js
@@ -1,41 +1,74 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { renderHook } from '@testing-library/react-hooks'
+import { graphql } from 'msw'
+import { setupServer } from 'msw/node'
 import { MemoryRouter, Route } from 'react-router-dom'
 
-import { useCommits } from 'services/commits'
-
-import { usePullPageData } from './usePullPageData'
 import { useTabsCounts } from './useTabsCounts'
-
-jest.mock('services/commits')
-jest.mock('./usePullPageData')
-
-const mockPullData = {
-  pull: {
-    pullId: 1,
-    compareWithBase: {
-      impactedFilesCount: 2,
-      indirectChangedFilesCount: 3,
-      flagComparisonsCount: 1,
-      __typename: 'Comparison',
-    },
-  },
-}
 
 const queryClient = new QueryClient()
 
 const wrapper = ({ children }) => (
-  <MemoryRouter initialEntries={['/bb/critical-role/bells-hells']}>
-    <Route path="/:provider/:owner/:repo">
+  <MemoryRouter initialEntries={['/bb/critical-role/bells-hells/pull/9']}>
+    <Route path="/:provider/:owner/:repo/pull/:pullId">
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </Route>
   </MemoryRouter>
 )
 
+const mockCommits = {
+  owner: {
+    repository: {
+      commits: {
+        totalCount: 11,
+      },
+    },
+  },
+}
+
+const mockPullData = {
+  owner: {
+    isCurrentUserPartOfOrg: true,
+    repository: {
+      private: true,
+      pull: {
+        pullId: 1,
+        compareWithBase: {
+          impactedFilesCount: 4,
+          indirectChangedFilesCount: 0,
+          flagComparisonsCount: 1,
+          __typename: 'Comparison',
+        },
+      },
+    },
+  },
+}
+
+const server = setupServer()
+
+beforeAll(() => {
+  server.listen()
+})
+
+afterEach(() => {
+  queryClient.clear()
+  server.resetHandlers()
+})
+
+afterAll(() => {
+  server.close()
+})
+
 describe('useTabsCount', () => {
   function setup() {
-    useCommits.mockReturnValue({ data: { commitsCount: 4 } })
-    usePullPageData.mockReturnValue({ data: mockPullData })
+    server.use(
+      graphql.query('PullPageData', (req, res, ctx) =>
+        res(ctx.status(200), ctx.data(mockPullData))
+      ),
+      graphql.query('GetCommits', (req, res, ctx) =>
+        res(ctx.status(200), ctx.data(mockCommits))
+      )
+    )
   }
 
   describe('calling hook', () => {
@@ -44,15 +77,17 @@ describe('useTabsCount', () => {
     })
 
     it('returns the correct data', async () => {
-      const { result } = renderHook(() => useTabsCounts(), {
+      const { result, waitForNextUpdate } = renderHook(() => useTabsCounts(), {
         wrapper,
       })
 
+      await waitForNextUpdate()
+
       expect(result.current).toStrictEqual({
         flagsCount: 1,
-        impactedFilesCount: 2,
-        indirectChangesCount: 3,
-        commitsCount: 4,
+        impactedFilesCount: 4,
+        indirectChangesCount: 0,
+        commitsCount: 11,
       })
     })
   })
