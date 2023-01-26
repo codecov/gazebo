91043ea08c9b5cf184f20e28a0fb20a1ad6e8c00
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-01-26 10:03:04 -0400

    refactor tests

--- src/shared/ContentsTable/TableEntries/BranchEntries/BranchDirEntry.spec.jsx
+++ src/shared/ContentsTable/TableEntries/BranchEntries/BranchDirEntry.spec.jsx
@@ -1,43 +1,64 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { render, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { MemoryRouter, Route, useParams } from 'react-router-dom'
+import { graphql } from 'msw'
+import { setupServer } from 'msw/node'
+import { MemoryRouter, Route } from 'react-router-dom'
 
 import BranchDirEntry from './BranchDirEntry'
-import { usePrefetchBranchDirEntry } from './hooks/usePrefetchBranchDirEntry'
 
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
-  useParams: jest.fn(() => {}),
-}))
+const mockData = {
+  owner: {
+    username: 'codecov',
+    repository: {
+      branch: {
+        head: {
+          pathContents: {
+            results: [
+              {
+                __typename: 'PathContentDir',
+                name: 'src',
+                path: null,
+                percentCovered: 0.0,
+                hits: 4,
+                misses: 2,
+                lines: 7,
+                partials: 1,
+              },
+            ],
+          },
+        },
+      },
+    },
+  },
+}
 
-jest.mock('./hooks/usePrefetchBranchDirEntry')
+const queryClient = new QueryClient()
+const server = setupServer()
+
+const wrapper = ({ children }) => (
+  <QueryClientProvider client={queryClient}>
+    <MemoryRouter initialEntries={['/gh/codecov/test-repo/tree/main/src']}>
+      <Route path="/:provider/:owner/:repo/tree/:branch/:path+">
+        {children}
+      </Route>
+    </MemoryRouter>
+  </QueryClientProvider>
+)
+
+beforeAll(() => server.listen())
+afterEach(() => {
+  queryClient.clear()
+  server.resetHandlers()
+})
+afterAll(() => server.close())
 
 describe('BranchDirEntry', () => {
-  const runPrefetchMock = jest.fn()
-
   function setup() {
-    useParams.mockReturnValue({
-      owner: 'codecov',
-      provider: 'gh',
-      repo: 'test-repo',
-      branch: 'main',
-      path: '',
-    })
-
-    usePrefetchBranchDirEntry.mockReturnValue({
-      runPrefetch: runPrefetchMock,
-    })
-
-    render(
-      <MemoryRouter initialEntries={['/gh/codecov/test-repo']}>
-        <Route path="/:provider/:owner/:repo/">
-          <BranchDirEntry
-            branch="branch"
-            name="dir"
-            urlPath="path/to/directory"
-          />
-        </Route>
-      </MemoryRouter>
+    server.use(
+      graphql.query('BranchContents', (req, res, ctx) =>
+        res(ctx.status(200), ctx.data(mockData))
+      )
     )
   }
 
@@ -45,12 +66,23 @@ describe('BranchDirEntry', () => {
     setup()
   })
 
-  it('displays the directory name', () => {
-    expect(screen.getByText('dir')).toBeInTheDocument()
+  it('displays the directory name', async () => {
+    render(
+      <BranchDirEntry branch="branch" name="dir" urlPath="path/to/directory" />,
+      { wrapper }
+    )
+
+    const dir = await screen.findByText('dir')
+    expect(dir).toBeInTheDocument()
   })
 
-  it('sets the correct href', () => {
-    const a = screen.getByRole('link')
+  it('sets the correct href', async () => {
+    render(
+      <BranchDirEntry branch="branch" name="dir" urlPath="path/to/directory" />,
+      { wrapper }
+    )
+
+    const a = await screen.findByRole('link')
     expect(a).toHaveAttribute(
       'href',
       '/gh/codecov/test-repo/tree/branch/path/to/directory/dir'
@@ -58,8 +90,31 @@ describe('BranchDirEntry', () => {
   })
 
   it('fires the prefetch function on hover', async () => {
+    render(
+      <BranchDirEntry branch="branch" name="dir" urlPath="path/to/directory" />,
+      { wrapper }
+    )
+
     userEvent.hover(screen.getByText('dir'))
 
-    await waitFor(() => expect(runPrefetchMock).toHaveBeenCalled())
+    await waitFor(() => queryClient.getQueryState().isFetching)
+    await waitFor(() => !queryClient.getQueryState().isFetching)
+
+    await waitFor(() =>
+      expect(queryClient.getQueryState().data).toStrictEqual({
+        results: [
+          {
+            __typename: 'PathContentDir',
+            name: 'src',
+            path: null,
+            percentCovered: 0,
+            hits: 4,
+            misses: 2,
+            lines: 7,
+            partials: 1,
+          },
+        ],
+      })
+    )
   })
 })
