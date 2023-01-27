6f0514d842b51d60b0470b40a9c9e53ba71b62c9
Rula Abu Hasna <rula@codecov.io>
2023-01-27 19:04:43 +0200

    some tests refactoring

--- src/pages/RepoPage/SettingsTab/tabs/BadgesAndGraphsTab/Badges/Badges.spec.jsx
+++ src/pages/RepoPage/SettingsTab/tabs/BadgesAndGraphsTab/Badges/Badges.spec.jsx
@@ -10,33 +10,30 @@ jest.mock('config')
 
 const queryClient = new QueryClient()
 
+const wrapper = ({ children }) => (
+  <MemoryRouter initialEntries={['/gh/codecov/codecov-client/settings/badge']}>
+    <QueryClientProvider client={queryClient}>
+      <Route path="/:provider/:owner/:repo/settings/badge">{children}</Route>
+    </QueryClientProvider>
+  </MemoryRouter>
+)
+
 describe('Badges', () => {
-  function setup() {
-    render(
-      <MemoryRouter
-        initialEntries={['/gh/codecov/codecov-client/settings/badge']}
-      >
-        <QueryClientProvider client={queryClient}>
-          <Route path="/:provider/:owner/:repo/settings/badge">
-            <Badges graphToken="WIO9JXFGE" defaultBranch="master" />
-          </Route>
-        </QueryClientProvider>
-      </MemoryRouter>
-    )
-
-    config.BASE_URL = 'https://stage-codecov.io'
-  }
-
-  describe('renders Badges componenet', () => {
-    beforeEach(() => {
-      setup()
-    })
+  describe('renders', () => {
     it('renders title', () => {
+      render(<Badges graphToken="WIO9JXFGE" defaultBranch="master" />, {
+        wrapper,
+      })
+
       const title = screen.getByText(/Codecov badge/)
       expect(title).toBeInTheDocument()
     })
 
     it('renders body', () => {
+      render(<Badges graphToken="WIO9JXFGE" defaultBranch="master" />, {
+        wrapper,
+      })
+
       const p = screen.getByText(
         /A live icon that you can embed in code, such as in a README.md, to provide quick insight into your project's code coverage percentage./
       )
@@ -44,13 +41,22 @@ describe('Badges', () => {
     })
 
     it('renders with expected base url', () => {
+      render(<Badges graphToken="WIO9JXFGE" defaultBranch="master" />, {
+        wrapper,
+      })
+      config.BASE_URL = 'https://stage-codecov.io'
+
       const baseUrl = screen.getByText(
-        '[![codecov](https://stage-codecov.io/gh/codecov/codecov-client/branch/master/graph/badge.svg?token=WIO9JXFGE)](https://stage-codecov.io/gh/codecov/codecov-client)'
+        '[![codecov](https://stage-web.codecov.dev/gh/codecov/codecov-client/branch/master/graph/badge.svg?token=WIO9JXFGE)](https://stage-web.codecov.dev/gh/codecov/codecov-client)'
       )
       expect(baseUrl).toBeInTheDocument()
     })
 
     it('renders tokens', () => {
+      render(<Badges graphToken="WIO9JXFGE" defaultBranch="master" />, {
+        wrapper,
+      })
+
       expect(screen.getByText('Markdown')).toBeInTheDocument()
       expect(screen.getByText('HTML')).toBeInTheDocument()
       expect(screen.getByText('RST')).toBeInTheDocument()
