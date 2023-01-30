a28c0c48a90637e1f09e45bc7eb53d89a2f6b28e
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-01-30 13:56:42 -0400

    refactor coverage progress ... again

--- src/ui/CoverageProgress/CoverageProgress.spec.jsx
+++ src/ui/CoverageProgress/CoverageProgress.spec.jsx
@@ -5,7 +5,7 @@ import CoverageProgress from './CoverageProgress'
 describe('CoverageProgress', () => {
   describe('when rendered', () => {
     it('renders commit coverage', () => {
-      render(<CoverageProgress commitid="123456789" coverage={45} />)
+      render(<CoverageProgress commitid="123456789" amount={45} />)
 
       const coverage = screen.getByText(/45.00%/)
       expect(coverage).toBeInTheDocument()
@@ -14,7 +14,7 @@ describe('CoverageProgress', () => {
 
   describe('when rendered with no coverage', () => {
     it('renders no report text', () => {
-      render(<CoverageProgress commitid="123456789" coverage={null} />)
+      render(<CoverageProgress commitid="123456789" amount={null} />)
 
       const text = screen.getByText(/No report uploaded/)
       expect(text).toBeInTheDocument()
@@ -24,7 +24,7 @@ describe('CoverageProgress', () => {
   describe('when rendered as tall variant', () => {
     it('light is not applied to totals number', () => {
       render(
-        <CoverageProgress commitid="123456789" coverage={45} variant="tall" />
+        <CoverageProgress commitid="123456789" amount={45} variant="tall" />
       )
 
       const totalsNumber = screen.getByTestId('coverage-value')
