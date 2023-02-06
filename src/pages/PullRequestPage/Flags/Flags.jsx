9348e2605979b74f684c31c9348e5dcf26f0bade
Rula Abu Hasna <91732700+RulaKhaled@users.noreply.github.com>
2023-02-06 12:11:45 +0200

    Remove the use of feature flag (#1857)

--- src/pages/PullRequestPage/Flags/Flags.jsx
+++ src/pages/PullRequestPage/Flags/Flags.jsx
@@ -1,14 +1,9 @@
-import PropTypes from 'prop-types'
-import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 
-import FlagsNotConfigured from 'pages/RepoPage/FlagsTab/FlagsNotConfigured'
 import { usePull } from 'services/pull'
-import { useFlags } from 'shared/featureFlags'
+import FlagsNotConfigured from 'shared/FlagsNotConfigured'
 import Table from 'ui/Table'
 import TotalsNumber from 'ui/TotalsNumber'
-
-import Card from '../Card'
 
 const tableColumns = [
   {
@@ -41,8 +36,6 @@ const tableColumns = [
     cell: (info) => info.getValue(),
   },
 ]
-
-const localStorageKey = 'gz-dismissFlagsCard'
 
 function getTableData(data) {
   return (
@@ -77,102 +70,19 @@ function getTableData(data) {
   )
 }
 
-function handleOnDismiss(setIsCardDismissed) {
-  setIsCardDismissed(true)
-  localStorage.setItem(localStorageKey, 'true')
-}
-
-function getTableInfo({ tableData, isTableDataEmpty, setIsCardDismissed }) {
-  const cardInfo = isTableDataEmpty ? 'showMarketingInfo' : 'showFlagsData'
-  return {
-    showFlagsData: {
-      // TODO: Add carryforward flag title here. This endpoint doesn't surface if a flag is CFF or not, so this would be a feature for the GQL implementation here
-      title: 'Flags',
-      value: <Table data={tableData} columns={tableColumns} />,
-    },
-    showMarketingInfo: {
-      title: (
-        <div className="flex justify-between w-full">
-          <span>Flags</span>
-          <button
-            className="text-ds-blue"
-            onClick={() => handleOnDismiss(setIsCardDismissed)}
-          >
-            Dismiss
-          </button>
-        </div>
-      ),
-      value: <FlagsNotConfigured />,
-    },
-  }[cardInfo]
-}
-
-function NewFlagsTab({ isTableDataEmpty, tableData }) {
-  if (isTableDataEmpty) {
-    return <FlagsNotConfigured />
-  }
-  return <Table data={tableData} columns={tableColumns} />
-}
-
-NewFlagsTab.propTypes = {
-  isTableDataEmpty: PropTypes.bool.isRequired,
-  tableData: PropTypes.arrayOf(
-    PropTypes.shape({
-      changeCoverage: PropTypes.object,
-      headCoverage: PropTypes.object,
-      name: PropTypes.object,
-      patchCoverage: PropTypes.object,
-    })
-  ),
-}
-
-function OldFlagsSection({ isTableDataEmpty, tableData }) {
-  const [isCardDismissed, setIsCardDismissed] = useState(
-    !!JSON.parse(localStorage.getItem(localStorageKey))
-  )
-  const { title, value } = getTableInfo({
-    tableData,
-    isTableDataEmpty,
-    setIsCardDismissed,
-  })
-
-  return (
-    !(isTableDataEmpty && isCardDismissed) && <Card title={title}>{value}</Card>
-  )
-}
-OldFlagsSection.propTypes = {
-  isTableDataEmpty: PropTypes.bool.isRequired,
-  tableData: PropTypes.arrayOf(
-    PropTypes.shape({
-      changeCoverage: PropTypes.object,
-      headCoverage: PropTypes.object,
-      name: PropTypes.object,
-      patchCoverage: PropTypes.object,
-    })
-  ),
-}
-
 function Flags() {
   const { owner, repo, pullId, provider } = useParams()
   const { data } = usePull({ provider, owner, repo, pullId })
   const flagComparison = data?.pull?.compareWithBase?.flagComparisons || []
-  const { pullPageTabs } = useFlags({ pullPageTabs: true })
 
   const tableData = getTableData(flagComparison)
   const isTableDataEmpty = tableData && tableData?.length <= 0
 
-  if (pullPageTabs) {
-    return (
-      <NewFlagsTab isTableDataEmpty={isTableDataEmpty} tableData={tableData} />
-    )
+  if (isTableDataEmpty) {
+    return <FlagsNotConfigured />
   }
 
-  return (
-    <OldFlagsSection
-      tableData={tableData}
-      isTableDataEmpty={isTableDataEmpty}
-    />
-  )
+  return <Table data={tableData} columns={tableColumns} />
 }
 
 export default Flags
