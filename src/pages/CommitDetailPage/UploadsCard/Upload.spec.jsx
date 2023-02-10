54e38bc660eeffade6532ca974bafdbcc03f33b7
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-02-10 14:22:04 -0400

    CODE-2644: Upload is Displaying Error Message Multiple Times (#1873)
    * combine errors into single message with a count
    
    * update tests
    
    * use the object like a smart person

--- src/pages/CommitDetailPage/UploadsCard/Upload.spec.jsx
+++ src/pages/CommitDetailPage/UploadsCard/Upload.spec.jsx
@@ -5,96 +5,121 @@ import { formatTimeToNow } from 'shared/utils/dates'
 import Upload from './Upload'
 
 describe('UploadsCard', () => {
-  function setup(props) {
-    render(<Upload {...props} />)
-  }
-
   describe('renders', () => {
+    let props
     beforeEach(() => {
-      setup({
+      props = {
         ciUrl: 'ciUrl.com',
         createdAt: '2020-08-25T16:36:19.559474+00:00',
         downloadUrl: 'download.com',
         buildCode: '1234',
         uploadType: 'CARRIEDFORWARD',
-      })
+      }
     })
 
     it('renders the build code', () => {
-      expect(screen.getByText(/1234/)).toBeInTheDocument()
+      render(<Upload {...props} />)
+
+      const buildCode = screen.getByText(/1234/)
+      expect(buildCode).toBeInTheDocument()
     })
+
     it('link to the build', () => {
-      expect(screen.getByRole('link', { name: /1234/ })).toHaveAttribute(
-        'href',
-        'ciUrl.com'
-      )
+      render(<Upload {...props} />)
+
+      const buildLink = screen.getByRole('link', { name: /1234/ })
+      expect(buildLink).toHaveAttribute('href', 'ciUrl.com')
     })
+
     it('created at dates', () => {
+      render(<Upload {...props} />)
+
       // If we dont use date-fns this test will break over time
-      const createDate = formatTimeToNow('2020-08-25T16:36:19.559474+00:00')
-      expect(screen.getByText(createDate)).toBeInTheDocument()
-    })
-    it('renders a download link', () => {
-      expect(screen.getByRole('link', { name: /Download/ })).toHaveAttribute(
-        'href',
-        'download.com'
+      const createDate = screen.getByText(
+        formatTimeToNow('2020-08-25T16:36:19.559474+00:00')
       )
+      expect(createDate).toBeInTheDocument()
+    })
+
+    it('renders a download link', () => {
+      render(<Upload {...props} />)
+
+      const downloadLink = screen.getByRole('link', { name: /Download/ })
+      expect(downloadLink).toHaveAttribute('href', 'download.com')
     })
 
     it('renders carry-forward text', () => {
-      expect(screen.getByText('carry-forward')).toBeInTheDocument()
+      render(<Upload {...props} />)
+
+      const carryForward = screen.getByText('carry-forward')
+      expect(carryForward).toBeInTheDocument()
     })
   })
 
   describe('build without build link', () => {
-    beforeEach(() => {
-      setup({ buildCode: '1234' })
-    })
     it('renders a the build code', () => {
-      expect(screen.getByText(/1234/)).toBeInTheDocument()
-    })
-    it('does not link to the build if no url provided', () => {
-      expect(
-        screen.queryByRole('link', { name: /1234/ })
-      ).not.toBeInTheDocument()
-    })
-  })
-  describe('missinng data renders', () => {
-    beforeEach(() => {
-      setup({})
+      render(<Upload buildCode="1234" />)
+
+      const buildCode = screen.getByText(/1234/)
+      expect(buildCode).toBeInTheDocument()
     })
 
-    it('renders a default build code if no code was provided', () => {
-      expect(screen.getByText(/build code not found/)).toBeInTheDocument()
-    })
     it('does not link to the build if no url provided', () => {
-      expect(
-        screen.queryByRole('link', /build code not found/)
-      ).not.toBeInTheDocument()
-    })
-    it('Does not show a download link if there is no availble download', () => {
-      expect(
-        screen.queryByRole('link', { name: /Download/ })
-      ).not.toBeInTheDocument()
+      render(<Upload buildCode="1234" />)
+
+      const buildLink = screen.queryByRole('link', { name: /1234/ })
+      expect(buildLink).not.toBeInTheDocument()
     })
   })
+
+  describe('missing data renders', () => {
+    it('renders a default build code if no code was provided', () => {
+      render(<Upload />)
+
+      const noBuildCode = screen.getByText(/build code not found/)
+      expect(noBuildCode).toBeInTheDocument()
+    })
+
+    it('does not link to the build if no url provided', () => {
+      render(<Upload />)
+
+      const noBuildLink = screen.queryByRole('link', /build code not found/)
+      expect(noBuildLink).not.toBeInTheDocument()
+    })
+
+    it('Does not show a download link if there is no available download', () => {
+      render(<Upload />)
+
+      const noDownloadLink = screen.queryByRole('link', { name: /Download/ })
+      expect(noDownloadLink).not.toBeInTheDocument()
+    })
+  })
+
   describe('rendering flags', () => {
     it('one flag', () => {
-      setup({
-        flags: ['flag1'],
-      })
-      expect(screen.getByText(/flag1/)).toBeInTheDocument()
+      render(<Upload flags={['flag1']} />)
+
+      const flag1 = screen.getByText(/flag1/)
+      expect(flag1).toBeInTheDocument()
     })
+
     it('multiple flags', () => {
-      setup({
-        flags: ['flag1', 'flag2', 'flag3', 'flag4'],
-      })
-      expect(screen.getByText(/flag1/)).toBeInTheDocument()
-      expect(screen.getByText(/flag2/)).toBeInTheDocument()
-      expect(screen.getByText(/flag3/)).toBeInTheDocument()
-      expect(screen.getByText(/flag4/)).toBeInTheDocument()
+      render(<Upload flags={['flag1', 'flag2', 'flag3', 'flag4']} />)
+
+      const flag1 = screen.getByText(/flag1/)
+      expect(flag1).toBeInTheDocument()
+
+      const flag2 = screen.getByText(/flag2/)
+      expect(flag2).toBeInTheDocument()
+
+      const flag3 = screen.getByText(/flag3/)
+      expect(flag3).toBeInTheDocument()
+
+      const flag4 = screen.getByText(/flag4/)
+      expect(flag4).toBeInTheDocument()
     })
   })
+
   describe('rendering errors', () => {
     beforeEach(() => {
       // Supress proptype warnings.
@@ -102,83 +127,124 @@ describe('UploadsCard', () => {
     })
 
     it('fileNotFoundInStorage error', () => {
-      setup({
-        errors: [{ errorCode: 'FILE_NOT_IN_STORAGE' }],
-      })
-      expect(screen.getByText(/processing failed/)).toBeInTheDocument()
+      render(<Upload errors={[{ errorCode: 'FILE_NOT_IN_STORAGE' }]} />)
+
+      const processingFailed = screen.getByText(/processing failed/)
+      expect(processingFailed).toBeInTheDocument()
     })
+
     it('reportExpired error', () => {
-      setup({
-        errors: [{ errorCode: 'REPORT_EXPIRED' }],
-      })
-      expect(screen.getByText(/upload expired/)).toBeInTheDocument()
+      render(<Upload errors={[{ errorCode: 'REPORT_EXPIRED' }]} />)
+
+      const uploadExpired = screen.getByText(/upload expired/)
+      expect(uploadExpired).toBeInTheDocument()
     })
+
     it('reportEmpty error', () => {
-      setup({
-        errors: [{ errorCode: 'REPORT_EMPTY' }],
-      })
-      expect(screen.getByText(/upload is empty/)).toBeInTheDocument()
+      render(<Upload errors={[{ errorCode: 'REPORT_EMPTY' }]} />)
+
+      const uploadIsEmpty = screen.getByText(/unusable report/)
+      expect(uploadIsEmpty).toBeInTheDocument()
     })
+
     it('all errors', () => {
-      setup({
-        errors: [
-          { errorCode: 'FILE_NOT_IN_STORAGE' },
-          { errorCode: 'REPORT_EXPIRED' },
-          { errorCode: 'REPORT_EMPTY' },
-          { errorCode: 'SOME_NEW_ERROR' },
-        ],
-      })
-      expect(screen.getByText(/processing failed/)).toBeInTheDocument()
-      expect(screen.getByText(/upload expired/)).toBeInTheDocument()
-      expect(screen.getByText(/upload is empty/)).toBeInTheDocument()
+      render(
+        <Upload
+          errors={[
+            { errorCode: 'FILE_NOT_IN_STORAGE' },
+            { errorCode: 'REPORT_EXPIRED' },
+            { errorCode: 'REPORT_EMPTY' },
+            { errorCode: 'SOME_NEW_ERROR' },
+          ]}
+        />
+      )
+
+      const processingFailed = screen.getByText(/processing failed/)
+      expect(processingFailed).toBeInTheDocument()
+
+      const uploadExpired = screen.getByText(/upload expired/)
+      expect(uploadExpired).toBeInTheDocument()
+
+      const emptyUpload = screen.getByText(/unusable report/)
+      expect(emptyUpload).toBeInTheDocument()
     })
-    it('handles new errors the front end doesnt know how to handle', () => {
-      setup({
-        errors: [{ errorCode: 'SOME_NEW_ERROR' }],
-      })
-      expect(screen.getByText(/unknown error/)).toBeInTheDocument()
+
+    it('handles new errors the front end does not know how to handle', () => {
+      render(<Upload errors={[{ errorCode: 'SOME_NEW_ERROR' }]} />)
+
+      const unknownError = screen.getByText(/unknown error/)
+      expect(unknownError).toBeInTheDocument()
     })
+
     it('handles an unexpected error type', () => {
-      setup({
-        errors: [{ errorCode: { error: 'bad config or something' } }],
-      })
-      expect(screen.getByText(/unknown error/)).toBeInTheDocument()
+      render(
+        <Upload
+          errors={[{ errorCode: { error: 'bad config or something' } }]}
+        />
+      )
+
+      const unknownError = screen.getByText(/unknown error/)
+      expect(unknownError).toBeInTheDocument()
     })
+
     it('handles upload state error but no error code resolved as an known error', () => {
-      setup({
-        state: 'ERROR',
-      })
-      expect(screen.getByText(/unknown error/)).toBeInTheDocument()
+      render(<Upload state="ERROR" />)
+
+      const unknownError = screen.getByText(/unknown error/)
+      expect(unknownError).toBeInTheDocument()
     })
+
     it('handles upload state error but no errors returned', () => {
-      setup({
-        state: 'ERROR',
-        errors: [],
-      })
-      expect(screen.getByText(/unknown error/)).toBeInTheDocument()
+      render(<Upload errors={[]} state="ERROR" />)
+
+      const unknownError = screen.getByText(/unknown error/)
+      expect(unknownError).toBeInTheDocument()
     })
+
     it('If no state is provided and no errors received do not show an error', () => {
-      setup({
-        error: [],
-      })
-      expect(screen.queryByText(/unknown error/)).not.toBeInTheDocument()
+      render(<Upload errors={[]} />)
+
+      const unknownError = screen.queryByText(/unknown error/)
+      expect(unknownError).not.toBeInTheDocument()
+    })
+
+    it('removes duplicate errors', () => {
+      render(
+        <Upload
+          errors={[
+            { errorCode: 'REPORT_EMPTY' },
+            { errorCode: 'REPORT_EMPTY' },
+            { errorCode: { error: 'bad config or something' } },
+            { errorCode: { error: 'bad config or something' } },
+            { errorCode: 'SOME_NEW_ERROR' },
+            { errorCode: 'SOME_NEW_ERROR' },
+          ]}
+        />
+      )
+
+      const erroredUpload = screen.getByText(/unusable report \(2\)/)
+      expect(erroredUpload).toBeInTheDocument()
+
+      const unknownError = screen.getByText(/unknown error \(4\)/)
+      expect(unknownError).toBeInTheDocument()
     })
   })
 
   describe('rendering uploaded type of uploads', () => {
-    setup({ uploadType: 'UPLOADED' })
     it('does not render carry-forward text', () => {
-      expect(screen.queryByText('carry-forward')).not.toBeInTheDocument()
+      render(<Upload uploadType="UPLOADED" />)
+
+      const carryForward = screen.queryByText('carry-forward')
+      expect(carryForward).not.toBeInTheDocument()
     })
   })
 
   describe('Upload reference when upload name exists', () => {
-    beforeEach(() => {
-      setup({ name: 'upload name', ciUrl: 'ciUrl.com' })
-    })
-
     it('renders upload name instead of buildCode', () => {
-      expect(screen.getByText('upload name')).toBeInTheDocument()
+      render(<Upload name="upload name" ciUrl="ciUrl.com" />)
+
+      const name = screen.getByText('upload name')
+      expect(name).toBeInTheDocument()
     })
   })
 })
