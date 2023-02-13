dc32a2de3a869bc3db29c39b9d1b25a186fe009c
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-02-13 11:53:04 -0400

    rework cancel plan page

--- src/pages/PlanPage/subRoutes/CancelPlanPage/CancelPlanPage.jsx
+++ src/pages/PlanPage/subRoutes/CancelPlanPage/CancelPlanPage.jsx
@@ -1,98 +1,36 @@
-import difference from 'lodash/difference'
-import { useLayoutEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { lazy, Suspense } from 'react'
+import { Redirect, Switch } from 'react-router-dom'
 
-import { useAccountDetails, usePlans } from 'services/account'
-import { isFreePlan } from 'shared/utils/billing'
-import Card from 'ui/Card'
+import { SentryRoute } from 'sentry'
 
-import CancelCard from './CancelButton'
-import { useProPlanMonth } from './hooks'
-import umbrellaImg from './umbrella.svg'
+import Spinner from 'ui/Spinner'
 
-import { useSetCrumbs } from '../../context'
-import BenefitList from '../../shared/BenefitList'
+import SpecialOffer from './subRoutes/SpecialOffer'
+
+const DowngradePlan = lazy(() => import('./subRoutes/DowngradePlan'))
+
+const Loader = () => (
+  <div className="flex flex-1 justify-center">
+    <Spinner size={60} />
+  </div>
+)
 
 function CancelPlanPage() {
-  const { provider, owner } = useParams()
-  const { data: accountDetails } = useAccountDetails({ provider, owner })
-  const { data: plans } = usePlans(provider)
-  const setCrumbs = useSetCrumbs()
-
-  useLayoutEffect(() => {
-    setCrumbs([
-      {
-        pageName: 'cancelOrgPlan',
-        text: 'Cancel Plan',
-      },
-    ])
-  }, [setCrumbs])
-
-  const { proPlanMonth } = useProPlanMonth({ plans })
-  const freePlan = plans.find((plan) => isFreePlan(plan?.value))
-
-  const unavailableBenefits = difference(
-    proPlanMonth?.benefits,
-    freePlan?.benefits
-  )
-
   return (
-    <>
-      <div className="flex w-4/5 gap-8">
-        <div className="flex basis-3/5">
-          <Card variant="cancel">
-            <div className="flex flex-col gap-4 text-ds-gray-quinary">
-              <h2 className="text-2xl text-codecov-red">Downgrading to Free</h2>
-              <hr />
-              <p>
-                Note that, when downgrading to free the following features will
-                become unavailable:
-              </p>
-              <BenefitList
-                benefits={unavailableBenefits}
-                iconColor="text-codecov-red"
-                iconName="times"
-              />
-              <hr />
-              <p>
-                You currently have {accountDetails?.activatedUserCount} active
-                users. On downgrade, all users will be automatically
-                deactivated. You will need to manually reactivate up to five
-                users or ensure auto activate is enabled in your plan settings.
-              </p>
-              {/* This is a weird component that is both a button and a modal, hence why it's imported this way. Defs not a good practice but I feel the overhaul of this component will be for another time */}
-              <CancelCard
-                customerId={accountDetails?.subscriptionDetail?.customer?.id}
-                planCost={accountDetails?.plan?.value}
-                upComingCancelation={
-                  accountDetails?.subscriptionDetail?.cancelAtPeriodEnd
-                }
-                currentPeriodEnd={
-                  accountDetails?.subscriptionDetail?.currentPeriodEnd
-                }
-              />
-            </div>
-          </Card>
-        </div>
-        {/* TODO: Do we need this for the final iteration */}
-        <div className="flex basis-2/5">
-          <Card variant="large">
-            <div className="-mt-16 mb-4">
-              <img src={umbrellaImg} alt="closed umbrella illustration" />
-            </div>
-            <h3 className="text-2xl text-ds-pink-quinary">
-              {freePlan?.marketingName}
-            </h3>
-            <h2 className="mb-8 text-5xl">Free</h2>
-            <BenefitList
-              benefits={freePlan?.benefits}
-              iconName="check"
-              iconColor="text-ds-pink-quinary"
-            />
-          </Card>
-        </div>
-      </div>
-    </>
+    <Switch>
+      <SentryRoute path="/plan/:provider/:owner/cancel/downgrade" exact>
+        <Suspense fallback={<Loader />}>
+          <DowngradePlan />
+        </Suspense>
+      </SentryRoute>
+      <SentryRoute path="/plan/:provider/:owner/cancel" exact>
+        <SpecialOffer />
+      </SentryRoute>
+      <Redirect
+        from="/plan/:provider/:owner/cancel/*"
+        to="/plan/:provider/:owner/cancel"
+      />
+    </Switch>
   )
 }
 
--- src/pages/PlanPage/subRoutes/CancelPlanPage/CancelPlanPage.spec.jsx
+++ src/pages/PlanPage/subRoutes/CancelPlanPage/CancelPlanPage.spec.jsx
@@ -1,166 +1,63 @@
-import { render, screen } from 'custom-testing-library'
-
+import { render, screen } from '@testing-library/react'
 import { MemoryRouter, Route } from 'react-router-dom'
-
-import { useAccountDetails, useCancelPlan, usePlans } from 'services/account'
-import { useAddNotification } from 'services/toastNotification'
 
 import CancelPlanPage from './CancelPlanPage'
 
-jest.mock('services/account')
-jest.mock('services/toastNotification')
-jest.mock('./CancelButton', () => () => 'Cancel Card')
+jest.mock('./subRoutes/CancelPlan', () => () => 'CancelPlan')
+jest.mock('./subRoutes/DowngradePlan', () => () => 'DowngradePlan')
 
-const provider = 'gh'
-const owner = 'codecov'
-
-const proPlan = {
-  marketingName: 'Pro Team',
-  baseUnitPrice: 12,
-  benefits: ['Configurable # of users', 'Unlimited repos'],
-  quantity: 5,
-  value: 'users-inappm',
-}
-
-describe('CancelPlanPage', () => {
-  const mutate = jest.fn()
-  const addNotification = jest.fn()
-
-  function setup(currentPlan = proPlan) {
-    useAddNotification.mockReturnValue(addNotification)
-    useAccountDetails.mockReturnValue({
-      data: {
-        plan: currentPlan,
-        activatedUserCount: 2,
-        inactiveUserCount: 1,
-        subscriptionDetail: {
-          currentPeriodEnd: 1638614662,
-        },
-      },
-    })
-    usePlans.mockReturnValue({
-      data: getPlans(),
-    })
-    useCancelPlan.mockReturnValue({ mutate, isLoading: false })
-    const { unmount } = render(
-      <MemoryRouter initialEntries={['/my/initial/route']}>
-        <CancelPlanPage provider={provider} owner={owner} />
+let testLocation
+const wrapper =
+  (initialEntries = '') =>
+  ({ children }) =>
+    (
+      <MemoryRouter initialEntries={[initialEntries]}>
+        <Route path="/plan/:provider/:owner">{children}</Route>
         <Route
           path="*"
           render={({ location }) => {
+            testLocation = location
             return null
           }}
         />
       </MemoryRouter>
     )
 
-    return { unmount }
-  }
+describe('CancelPlanPage', () => {
+  describe('testing routes', () => {
+    describe('on root cancel path', () => {
+      it('renders cancel plan', async () => {
+        render(<CancelPlanPage />, {
+          wrapper: wrapper('/plan/gh/codecov/cancel'),
+        })
 
-  describe('when rendered', () => {
-    beforeEach(() => {
-      setup()
+        const cancelPlan = await screen.findByText('CancelPlan')
+        expect(cancelPlan).toBeInTheDocument()
+      })
     })
 
-    it('renders the basic plan title', () => {
-      const tab = screen.getByText(/Basic/)
-      expect(tab).toBeInTheDocument()
+    describe('on downgrade path', () => {
+      it('renders downgrade plan', async () => {
+        render(<CancelPlanPage />, {
+          wrapper: wrapper('/plan/gh/codecov/cancel/downgrade'),
+        })
+
+        const downgrade = await screen.findByText('DowngradePlan')
+        expect(downgrade).toBeInTheDocument()
+      })
     })
 
-    it('renders the downgrade button component', () => {
-      expect(screen.getByText(/Cancel Card/)).toBeInTheDocument()
-    })
+    describe('on random path', () => {
+      it('redirects to root cancel plan', async () => {
+        render(<CancelPlanPage />, {
+          wrapper: wrapper('/plan/gh/codecov/cancel/blah'),
+        })
 
-    it('renders the title', () => {
-      expect(screen.getByText(/Downgrading to Free/)).toBeInTheDocument()
-    })
+        expect(testLocation.pathname).toBe('/plan/gh/codecov/cancel')
 
-    it('renders cancelation text', () => {
-      expect(
-        screen.getByText(
-          /On downgrade, all users will be automatically deactivated./
-        )
-      ).toBeInTheDocument()
-    })
-
-    it('renders unavailable benefits', () => {
-      expect(screen.getByText(/Configurable # of users/)).toBeInTheDocument()
-      expect(screen.getByText(/Priority Support/)).toBeInTheDocument()
-    })
-
-    it('renders basic plan benefits', () => {
-      expect(screen.getByText(/Basic/)).toBeInTheDocument()
-      expect(screen.getByText(/Up to 5 users/)).toBeInTheDocument()
-      expect(
-        screen.getByText(/Unlimited public repositories/)
-      ).toBeInTheDocument()
-      expect(
-        screen.getByText(/Unlimited private repositories/)
-      ).toBeInTheDocument()
+        const cancelPlan = await screen.findByText('CancelPlan')
+        expect(cancelPlan).toBeInTheDocument()
+      })
     })
   })
 })
-
-function getPlans() {
-  return [
-    {
-      marketingName: 'Basic',
-      value: 'users-free',
-      billingRate: null,
-      baseUnitprice: 0,
-      benefits: [
-        'Up to 5 users',
-        'Unlimited public repositories',
-        'Unlimited private repositories',
-      ],
-    },
-    {
-      marketingName: 'Pro Team',
-      value: 'users-pr-inappm',
-      billingRate: 'monthly',
-      baseUnitPrice: 12,
-      benefits: [
-        'Configurable # of users',
-        'Unlimited public repositories',
-        'Unlimited private repositories',
-        'Priority Support',
-      ],
-    },
-    {
-      marketingName: 'Pro Team',
-      value: 'users-pr-inappy',
-      billingRate: 'annually',
-      baseUnitPrice: 10,
-      benefits: [
-        'Configurable # of users',
-        'Unlimited public repositories',
-        'Unlimited private repositories',
-        'Priority Support',
-      ],
-    },
-    {
-      marketingName: 'Pro Team',
-      value: 'users-enterprisem',
-      billingRate: 'monthly',
-      baseUnitPrice: 12,
-      benefits: [
-        'Configurable # of users',
-        'Unlimited public repositories',
-        'Unlimited private repositories',
-        'Priority Support',
-      ],
-    },
-    {
-      marketingName: 'Pro Team',
-      value: 'users-enterprisey',
-      billingRate: 'annually',
-      baseUnitPrice: 10,
-      benefits: [
-        'Configurable # of users',
-        'Unlimited public repositories',
-        'Unlimited private repositories',
-        'Priority Support',
-      ],
-    },
-  ]
-}
