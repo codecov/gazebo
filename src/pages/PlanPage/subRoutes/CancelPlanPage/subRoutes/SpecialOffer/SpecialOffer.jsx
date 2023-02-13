e09166158417426845c44e5561f7b43d0bf34471
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-02-13 13:12:04 -0400

    rename cancel plan to special offer

--- src/pages/PlanPage/subRoutes/CancelPlanPage/subRoutes/SpecialOffer/SpecialOffer.jsx
+++ src/pages/PlanPage/subRoutes/CancelPlanPage/subRoutes/SpecialOffer/SpecialOffer.jsx
@@ -1,8 +1,33 @@
+import { useLayoutEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+
+import { useNavLinks } from 'services/navigation'
+
+import { useSetCrumbs } from '../../../../context'
+
 const { default: A } = require('ui/A')
 const { default: Button } = require('ui/Button')
 const { default: Icon } = require('ui/Icon')
 
-function CancelPlan() {
+function SpecialOffer() {
+  const history = useHistory()
+  const { owner } = useNavLinks()
+
+  const setCrumbs = useSetCrumbs()
+
+  useLayoutEffect(() => {
+    setCrumbs([
+      {
+        pageName: 'cancelOrgPlan',
+        text: 'Special offer',
+      },
+    ])
+  }, [setCrumbs])
+
+  const handleDiscount = () => {
+    history.push(owner?.path())
+  }
+
   return (
     <div className="flex flex-col gap-8 w-5/12">
       <div className="">
@@ -18,7 +43,11 @@ function CancelPlan() {
         Get 30% off Codecov for 6 months! &#127881;
       </p>
       <div className="flex gap-4">
-        <Button variant="primary" hook="apply-30-discount">
+        <Button
+          variant="primary"
+          hook="apply-30-discount"
+          onClick={() => handleDiscount()}
+        >
           Yes, I&apos;d like 6 months with 30% discount
         </Button>
         <A variant="blueSeptenary" to={{ pageName: 'downgradePlanPage' }}>
@@ -33,4 +62,4 @@ function CancelPlan() {
   )
 }
 
-export default CancelPlan
+export default SpecialOffer
