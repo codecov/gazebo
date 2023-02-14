60faf6042b4babcddc37513e07ff39f0b997c5df
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-02-14 11:20:38 -0400

    add redirect logic for when users have had discount applied to their account

--- src/pages/PlanPage/subRoutes/CancelPlanPage/CancelPlanPage.jsx
+++ src/pages/PlanPage/subRoutes/CancelPlanPage/CancelPlanPage.jsx
@@ -1,8 +1,9 @@
 import { lazy, Suspense } from 'react'
-import { Redirect, Switch } from 'react-router-dom'
+import { Redirect, Switch, useParams } from 'react-router-dom'
 
 import { SentryRoute } from 'sentry'
 
+import { useAccountDetails } from 'services/account'
 import Spinner from 'ui/Spinner'
 
 import SpecialOffer from './subRoutes/SpecialOffer'
@@ -16,6 +17,16 @@ const Loader = () => (
 )
 
 function CancelPlanPage() {
+  const { provider, owner } = useParams()
+  const { data: accountDetailsData } = useAccountDetails({ provider, owner })
+
+  const discountNotApplied = !accountDetailsData?.subscriptionDetail?.discount
+
+  let redirectTo = '/plan/:provider/:owner/cancel/downgrade'
+  if (discountNotApplied) {
+    redirectTo = '/plan/:provider/:owner/cancel'
+  }
+
   return (
     <Switch>
       <SentryRoute path="/plan/:provider/:owner/cancel/downgrade" exact>
@@ -23,13 +34,12 @@ function CancelPlanPage() {
           <DowngradePlan />
         </Suspense>
       </SentryRoute>
-      <SentryRoute path="/plan/:provider/:owner/cancel" exact>
-        <SpecialOffer />
-      </SentryRoute>
-      <Redirect
-        from="/plan/:provider/:owner/cancel/*"
-        to="/plan/:provider/:owner/cancel"
-      />
+      {discountNotApplied && (
+        <SentryRoute path="/plan/:provider/:owner/cancel" exact>
+          <SpecialOffer />
+        </SentryRoute>
+      )}
+      <Redirect from="/plan/:provider/:owner/cancel" to={redirectTo} />
     </Switch>
   )
 }
--- src/pages/PlanPage/subRoutes/CancelPlanPage/CancelPlanPage.spec.jsx
+++ src/pages/PlanPage/subRoutes/CancelPlanPage/CancelPlanPage.spec.jsx
@@ -1,4 +1,8 @@
-import { render, screen } from '@testing-library/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { render, screen, waitFor } from '@testing-library/react'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+import { Suspense } from 'react'
 import { MemoryRouter, Route } from 'react-router-dom'
 
 import CancelPlanPage from './CancelPlanPage'
@@ -6,57 +10,170 @@ import CancelPlanPage from './CancelPlanPage'
 jest.mock('./subRoutes/SpecialOffer', () => () => 'SpecialOffer')
 jest.mock('./subRoutes/DowngradePlan', () => () => 'DowngradePlan')
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+    },
+  },
+})
+const server = setupServer()
+
 let testLocation
 const wrapper =
   (initialEntries = '') =>
   ({ children }) =>
     (
-      <MemoryRouter initialEntries={[initialEntries]}>
-        <Route path="/plan/:provider/:owner">{children}</Route>
-        <Route
-          path="*"
-          render={({ location }) => {
-            testLocation = location
-            return null
-          }}
-        />
-      </MemoryRouter>
+      <QueryClientProvider client={queryClient}>
+        <Suspense fallback={null}>
+          <MemoryRouter initialEntries={[initialEntries]}>
+            <Route path="/plan/:provider/:owner">{children}</Route>
+            <Route
+              path="*"
+              render={({ location }) => {
+                testLocation = location
+                return null
+              }}
+            />
+          </MemoryRouter>
+        </Suspense>
+      </QueryClientProvider>
     )
 
+beforeAll(() => {
+  server.listen()
+})
+afterEach(() => {
+  queryClient.clear()
+  server.resetHandlers()
+})
+afterAll(() => {
+  server.close()
+})
+
 describe('CancelPlanPage', () => {
-  describe('testing routes', () => {
-    describe('on root cancel path', () => {
-      it('renders cancel plan', async () => {
-        render(<CancelPlanPage />, {
-          wrapper: wrapper('/plan/gh/codecov/cancel'),
-        })
+  function setup({ hasDiscount = false } = { hasDiscount: false }) {
+    server.use(
+      rest.get('internal/gh/codecov/account-details/', (req, res, ctx) =>
+        res(
+          ctx.status(200),
+          ctx.json({
+            subscriptionDetail: {
+              discount: hasDiscount,
+            },
+          })
+        )
+      )
+    )
+  }
 
-        const specialOffer = await screen.findByText('SpecialOffer')
-        expect(specialOffer).toBeInTheDocument()
+  describe('user has not applied discount', () => {
+    beforeEach(() => setup())
+
+    describe('testing routes', () => {
+      describe('on root cancel path', () => {
+        it('renders cancel plan', async () => {
+          render(<CancelPlanPage />, {
+            wrapper: wrapper('/plan/gh/codecov/cancel'),
+          })
+
+          const specialOffer = await screen.findByText('SpecialOffer')
+          expect(specialOffer).toBeInTheDocument()
+        })
+      })
+
+      describe('on downgrade path', () => {
+        it('renders downgrade plan', async () => {
+          render(<CancelPlanPage />, {
+            wrapper: wrapper('/plan/gh/codecov/cancel/downgrade'),
+          })
+
+          const downgrade = await screen.findByText('DowngradePlan')
+          expect(downgrade).toBeInTheDocument()
+        })
+      })
+
+      describe('on random path', () => {
+        it('redirects to root cancel plan', async () => {
+          render(<CancelPlanPage />, {
+            wrapper: wrapper('/plan/gh/codecov/cancel/blah'),
+          })
+
+          await waitFor(() =>
+            expect(testLocation.pathname).toBe('/plan/gh/codecov/cancel')
+          )
+
+          const specialOffer = await screen.findByText('SpecialOffer')
+          expect(specialOffer).toBeInTheDocument()
+        })
       })
     })
+  })
 
-    describe('on downgrade path', () => {
-      it('renders downgrade plan', async () => {
-        render(<CancelPlanPage />, {
-          wrapper: wrapper('/plan/gh/codecov/cancel/downgrade'),
+  describe('user has discount applied', () => {
+    beforeEach(() => setup({ hasDiscount: true }))
+
+    describe('testing routes', () => {
+      describe('on root cancel path', () => {
+        it('redirects to downgrade plan', async () => {
+          render(<CancelPlanPage />, {
+            wrapper: wrapper('/plan/gh/codecov/cancel'),
+          })
+
+          await waitFor(() =>
+            expect(testLocation.pathname).toBe(
+              '/plan/gh/codecov/cancel/downgrade'
+            )
+          )
+
+          const downgrade = await screen.findByText('DowngradePlan')
+          expect(downgrade).toBeInTheDocument()
         })
-
-        const downgrade = await screen.findByText('DowngradePlan')
-        expect(downgrade).toBeInTheDocument()
       })
-    })
 
-    describe('on random path', () => {
-      it('redirects to root cancel plan', async () => {
-        render(<CancelPlanPage />, {
-          wrapper: wrapper('/plan/gh/codecov/cancel/blah'),
+      describe('on downgrade path', () => {
+        it('renders downgrade plan', async () => {
+          render(<CancelPlanPage />, {
+            wrapper: wrapper('/plan/gh/codecov/cancel/downgrade'),
+          })
+
+          const downgrade = await screen.findByText('DowngradePlan')
+          expect(downgrade).toBeInTheDocument()
         })
+      })
 
-        expect(testLocation.pathname).toBe('/plan/gh/codecov/cancel')
+      describe('on random cancel path', () => {
+        it('redirects to root downgrade plan', async () => {
+          render(<CancelPlanPage />, {
+            wrapper: wrapper('/plan/gh/codecov/cancel/blah'),
+          })
 
-        const specialOffer = await screen.findByText('SpecialOffer')
-        expect(specialOffer).toBeInTheDocument()
+          await waitFor(() =>
+            expect(testLocation.pathname).toBe(
+              '/plan/gh/codecov/cancel/downgrade'
+            )
+          )
+
+          const downgrade = await screen.findByText('DowngradePlan')
+          expect(downgrade).toBeInTheDocument()
+        })
+      })
+
+      describe('on random downgrade path', () => {
+        it('redirects to root downgrade plan', async () => {
+          render(<CancelPlanPage />, {
+            wrapper: wrapper('/plan/gh/codecov/cancel/downgrade/blah'),
+          })
+
+          await waitFor(() =>
+            expect(testLocation.pathname).toBe(
+              '/plan/gh/codecov/cancel/downgrade'
+            )
+          )
+
+          const downgrade = await screen.findByText('DowngradePlan')
+          expect(downgrade).toBeInTheDocument()
+        })
       })
     })
   })
