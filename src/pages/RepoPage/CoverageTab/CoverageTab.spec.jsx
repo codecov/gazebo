f5adc9f0a142f561ca536903fef1111e193241e0
Terry <87824812+terry-codecov@users.noreply.github.com>
2023-02-14 12:08:22 -0400

    Flagged sunburst chart on coverage tab round 2 (#1871)
    * Revert "Revert "Flagged sunburst chart on coverage tab (#1836)" (#1870)"
    
    This reverts commit 7ccb4e1e4559921c7d12666674fd500210ab03ee.
    
    * Rename coverage chart to CoverageChart, update sunburst to mount on layout effect, fix coverage chart on single column view
    
    * fix failing test from rename
    
    * update toggle element tests + coverage tab tests
    
    * refactor more tests and increase coverage
    
    * Fix incorrect test names (copy paste mistake)

--- src/pages/RepoPage/CoverageTab/CoverageTab.spec.jsx
+++ src/pages/RepoPage/CoverageTab/CoverageTab.spec.jsx
@@ -1,30 +1,14 @@
-import { render, screen, waitFor } from 'custom-testing-library'
-
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { MemoryRouter, Route, useParams } from 'react-router-dom'
+import { render, screen } from '@testing-library/react'
+import { graphql, rest } from 'msw'
+import { setupServer } from 'msw/node'
+import { MemoryRouter, Route } from 'react-router-dom'
 
-import { useLocationParams } from 'services/navigation'
-import { useRepo } from 'services/repo'
+import { useFlags } from 'shared/featureFlags'
 
 import CoverageTab from './CoverageTab'
 
-jest.mock('./subroute/Fileviewer', () => () => 'Fileviewer Component')
-jest.mock('./subroute/RepoContents', () => () => 'RepoContents Component')
-jest.mock('./Summary', () => () => 'Summary Component')
-jest.mock('./Chart/ToggleChart', () => () => 'ToggleChart Component')
-jest.mock('./DeactivatedRepo', () => () => 'Disabled Repo Component')
-jest.mock('services/repo')
-jest.mock('services/user')
-jest.mock('services/commits')
-
-jest.mock('services/navigation', () => ({
-  ...jest.requireActual('services/navigation'),
-  useLocationParams: jest.fn(),
-}))
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
-  useParams: jest.fn(() => {}),
-}))
+jest.mock('shared/featureFlags')
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -33,123 +17,201 @@ const queryClient = new QueryClient({
     },
   },
 })
+const server = setupServer()
+
+const wrapper =
+  (initialEntries = ['/gh/codecov/cool-repo/tree/main']) =>
+  ({ children }) =>
+    (
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter initialEntries={initialEntries}>{children}</MemoryRouter>
+      </QueryClientProvider>
+    )
+
+beforeAll(() => {
+  server.listen({ onUnhandledRequest: 'warn' })
+})
+afterEach(() => {
+  queryClient.clear()
+  server.resetHandlers()
+})
+afterAll(() => {
+  server.close()
+})
+
+const mockRepo = {
+  owner: {
+    repository: {
+      defaultBranch: 'main',
+    },
+  },
+}
+const repoConfigMock = {
+  owner: {
+    repository: {
+      repositoryConfig: {
+        indicationRange: { upperRange: 80, lowerRange: 60 },
+      },
+    },
+  },
+}
+const treeMock = { name: 'repoName', children: [] }
+const overviewMock = {
+  owner: { repository: { private: false, defaultBranch: 'main' } },
+}
+const branchesMock = {
+  owner: {
+    repository: {
+      branches: {
+        edges: [
+          {
+            node: {
+              name: 'main',
+              head: {
+                commitid: '1',
+              },
+            },
+          },
+          {
+            node: {
+              name: 'dummy',
+              head: {
+                commitid: '2',
+              },
+            },
+          },
+          {
+            node: {
+              name: 'dummy2',
+              head: {
+                commitid: '3',
+              },
+            },
+          },
+        ],
+        pageInfo: {
+          hasNextPage: false,
+          endCursor: 'someEndCursor',
+        },
+      },
+    },
+  },
+}
+const branchMock = {
+  branch: {
+    name: 'main',
+    head: {
+      commitid: '321fdsa',
+    },
+  },
+}
+const branchesContentsMock = {
+  owner: {
+    username: 'Rabee-AbuBaker',
+    repository: {
+      repositoryConfig: {
+        indicationRange: {
+          upperRange: 80,
+          lowerRange: 60,
+        },
+      },
+      branch: {
+        head: {
+          pathContents: {
+            results: [
+              {
+                name: 'flag1',
+                filePath: null,
+                percentCovered: 100.0,
+                type: 'dir',
+              },
+            ],
+          },
+        },
+      },
+    },
+  },
+}
 
 describe('Coverage Tab', () => {
-  const mockUpdateParams = jest.fn()
-  let originalLocation
-
-  beforeAll(() => {
-    originalLocation = global.window.location
-    delete global.window.location
-    global.window.location = {
-      replace: jest.fn(),
-    }
-  })
-
-  afterAll(() => {
-    jest.resetAllMocks()
-    window.location = originalLocation
-  })
-
-  function setup({ initialEntries, repoActivated = true }) {
-    useParams.mockReturnValue({
-      owner: 'test-org',
-      provider: 'gh',
-      repo: 'test-repo',
-    })
-
-    useRepo.mockReturnValue({
-      data: { repository: { activated: repoActivated } },
-    })
-
-    useLocationParams.mockReturnValue({
-      params: { search: '' },
-      updateParams: mockUpdateParams,
-    })
-
-    render(
-      <QueryClientProvider client={queryClient}>
-        <MemoryRouter initialEntries={initialEntries}>
-          <Route path="/:provider/:owner/:repo/tree/:branch/:path+" exact>
-            <CoverageTab />
-          </Route>
-          <Route path="/:provider/:owner/:repo/tree/:branch" exact>
-            <CoverageTab />
-          </Route>
-          <Route path="/:provider/:owner/:repo/blob/:ref/:path+">
-            <CoverageTab />
-          </Route>
-          <Route path="/:provider/:owner/:repo" exact={true}>
-            <CoverageTab />
-          </Route>
-        </MemoryRouter>
-      </QueryClientProvider>
+  function setup({ repoData = mockRepo } = { repoData: mockRepo }) {
+    server.use(
+      graphql.query('GetRepo', (req, res, ctx) =>
+        res(ctx.status(200), ctx.data(repoData))
+      ),
+      graphql.query('GetBranches', (req, res, ctx) =>
+        res(ctx.status(200), ctx.data(branchesMock))
+      ),
+      graphql.query('GetBranch', (req, res, ctx) =>
+        res(ctx.status(200), ctx.data({ owner: { repository: branchMock } }))
+      ),
+      graphql.query('BranchContents', (req, res, ctx) =>
+        res(ctx.status(200), ctx.data(branchesContentsMock))
+      ),
+      graphql.query('RepoConfig', (req, res, ctx) =>
+        res(ctx.status(200), ctx.data(repoConfigMock))
+      ),
+      graphql.query('GetRepoOverview', (req, res, ctx) => {
+        return res(ctx.status(200), ctx.data(overviewMock))
+      }),
+      rest.get(
+        '/internal/:provider/:owner/:repo/coverage/tree',
+        (req, res, ctx) => {
+          return res(ctx.status(200), ctx.json({ data: treeMock }))
+        }
+      )
     )
   }
 
-  describe('when rendered with default route', () => {
+  describe('sunburst flag enabled', () => {
     beforeEach(() => {
-      setup({ initialEntries: ['/gh/test-org/test-repo/'] })
-    })
+      useFlags.mockReturnValue({ coverageSunburstChart: true })
 
-    it('renders summary, and root tree component', () => {
-      expect(screen.getByText(/Summary Component/)).toBeInTheDocument()
-      expect(screen.queryByText(/Fileviewer Component/)).not.toBeInTheDocument()
+      setup()
     })
-  })
+    afterEach(() => jest.resetAllMocks)
 
-  describe('when rendered with tree+branch on default route returns the root of the project', () => {
-    beforeEach(() => {
-      setup({ initialEntries: ['/gh/test-org/test-repo/tree/some-branch'] })
-    })
-
-    it('renders summary and root tree component', () => {
-      expect(screen.getByText(/Summary Component/)).toBeInTheDocument()
-      expect(screen.getByText(/RepoContents Component/)).toBeInTheDocument()
-      expect(screen.queryByText(/Fileviewer Component/)).not.toBeInTheDocument()
-    })
-  })
-
-  describe('when rendered with tree+branch route returns the root of that branch', () => {
-    beforeEach(() => {
-      setup({ initialEntries: ['/gh/test-org/test-repo/tree/main'] })
-    })
-
-    it('renders summary and root tree component', () => {
-      expect(screen.getByText(/Summary Component/)).toBeInTheDocument()
-      expect(screen.getByText(/RepoContents Component/)).toBeInTheDocument()
-      expect(screen.queryByText(/Fileviewer Component/)).not.toBeInTheDocument()
-    })
-  })
-
-  describe('when rendered with tree+branch route on a sub folder', () => {
-    beforeEach(() => {
-      setup({ initialEntries: ['/gh/test-org/test-repo/tree/master/src'] })
-    })
-
-    it('renders summary and root tree component', () => {
-      expect(screen.getByText(/Summary Component/)).toBeInTheDocument()
-      expect(screen.getByText(/RepoContents Component/)).toBeInTheDocument()
-      expect(screen.queryByText(/Fileviewer Component/)).not.toBeInTheDocument()
-    })
-  })
-
-  describe('when rendered with blob route', () => {
-    beforeEach(async () => {
-      setup({
-        initialEntries: ['/gh/test-org/test-repo/blob/main/path/to/file.js'],
-      })
-      await waitFor(() =>
-        expect(screen.queryByTestId('spinner')).not.toBeInTheDocument()
+    it('renders the sunburst chart', async () => {
+      render(
+        <Route path="/:provider/:owner/:repo" exact={true}>
+          <CoverageTab />
+        </Route>,
+        { wrapper: wrapper(['/gh/test-org/test-repo']) }
       )
-    })
 
-    it('renders summary and root tree component', () => {
-      expect(screen.getByText(/Summary Component/)).toBeInTheDocument()
-      expect(screen.getByText(/Fileviewer Component/)).toBeInTheDocument()
-      expect(
-        screen.queryByText(/RepoContents Component/)
-      ).not.toBeInTheDocument()
+      const hideChart = await screen.findByText(/Hide Chart/)
+
+      expect(hideChart).toBeInTheDocument()
+
+      const toggleContents = screen.getByTestId('toggle-element-contents')
+
+      expect(toggleContents.childElementCount).toBe(2)
+    })
+  })
+
+  describe('sunburst flag disabled', () => {
+    beforeEach(() => {
+      useFlags.mockReturnValue({ coverageSunburstChart: false })
+
+      setup()
+    })
+    afterEach(() => jest.resetAllMocks)
+
+    it('renders the sunburst chart', async () => {
+      render(
+        <Route path="/:provider/:owner/:repo" exact={true}>
+          <CoverageTab />
+        </Route>,
+        { wrapper: wrapper(['/gh/test-org/test-repo']) }
+      )
+
+      const hideChart = await screen.findByText(/Hide Chart/)
+
+      expect(hideChart).toBeInTheDocument()
+
+      const toggleContents = screen.getByTestId('toggle-element-contents')
+
+      expect(toggleContents.childElementCount).toBe(1)
     })
   })
 })
