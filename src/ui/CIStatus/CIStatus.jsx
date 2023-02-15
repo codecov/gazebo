edb1aa63e00845b5ee8f94be9f80aa598e434129
Adrian <adrian@codecov.io>
2023-02-15 09:33:33 -0800

    Early return to CIStatus component if cipassed is null

--- src/ui/CIStatus/CIStatus.jsx
+++ src/ui/CIStatus/CIStatus.jsx
@@ -5,28 +5,28 @@ import PropTypes from 'prop-types'
 import Icon from 'ui/Icon'
 
 export default function CIStatusLabel({ ciPassed }) {
+  if (isNil(ciPassed)) {
+    return (
+      <span className="flex items-center text-xs flex-none gap-1">
+        <span>
+          <Icon size="sm" name="statusRunning" variant="developer" />
+        </span>
+        Processing
+      </span>
+    )
+  }
+
   return (
     <span className="flex items-center text-xs flex-none gap-1">
-      {isNil(ciPassed) ? (
-        <>
-          <span>
-            <Icon size="sm" name="statusRunning" variant="developer" />
-          </span>
-          Processing
-        </>
-      ) : (
-        <>
-          <span
-            className={cs({
-              'text-ds-primary-green': ciPassed,
-              'text-ds-primary-red': !ciPassed,
-            })}
-          >
-            <Icon size="sm" name={ciPassed ? 'check' : 'x'} />
-          </span>
-          CI {ciPassed ? 'Passed' : 'Failed'}
-        </>
-      )}
+      <span
+        className={cs({
+          'text-ds-primary-green': ciPassed,
+          'text-ds-primary-red': !ciPassed,
+        })}
+      >
+        <Icon size="sm" name={ciPassed ? 'check' : 'x'} />
+      </span>
+      CI {ciPassed ? 'Passed' : 'Failed'}
     </span>
   )
 }
