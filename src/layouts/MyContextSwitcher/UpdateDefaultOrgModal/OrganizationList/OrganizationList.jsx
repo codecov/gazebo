b09ec963b9bf82d355330eddc74bb89aabb3d7ca
Adrian <adrian@codecov.io>
2023-02-15 18:38:40 -0800

    Final little check

--- src/layouts/MyContextSwitcher/UpdateDefaultOrgModal/OrganizationList/OrganizationList.jsx
+++ src/layouts/MyContextSwitcher/UpdateDefaultOrgModal/OrganizationList/OrganizationList.jsx
@@ -12,47 +12,49 @@ function OrganizationList({ selectedOrgUsername, setSelectedOrgUsername }) {
   const currentUser = data?.currentUser
 
   return (
-    <div className="flex flex-col gap-4">
-      <ul className="text-ds-gray-octonary divide-y border border-ds-gray-secondary">
-        {organizations?.map((organization) => {
-          const currentOrgUsername = organization?.username
-          return (
-            <li
-              key={currentOrgUsername}
-              className={cs(
-                'flex p-4 hover:bg-ds-gray-primary transition duration-150 cursor-pointer',
-                {
-                  'hover:bg-ds-blue-selected bg-ds-blue-selected':
-                    currentOrgUsername === selectedOrgUsername,
-                }
-              )}
-              onClick={() => setSelectedOrgUsername(currentOrgUsername)}
-            >
-              <button className="flex items-center gap-3">
-                <Avatar user={organization} bordered />
-                <span>{currentOrgUsername}</span>
-                {currentOrgUsername === currentUser?.defaultOrgUsername && (
-                  <span className="text-ds-gray-quaternary font-medium">
-                    Current default org
-                  </span>
+    data?.isSuccess && (
+      <div className="flex flex-col gap-4">
+        <ul className="text-ds-gray-octonary divide-y border border-ds-gray-secondary">
+          {organizations?.map((organization) => {
+            const currentOrgUsername = organization?.username
+            return (
+              <li
+                key={currentOrgUsername}
+                className={cs(
+                  'flex p-4 hover:bg-ds-gray-primary transition duration-150 cursor-pointer',
+                  {
+                    'hover:bg-ds-blue-selected bg-ds-blue-selected':
+                      currentOrgUsername === selectedOrgUsername,
+                  }
                 )}
-              </button>
-            </li>
-          )
-        })}
-      </ul>
-      {data?.hasNextPage && (
-        <div className="w-full flex justify-center">
-          <Button
-            hook="load-more"
-            isLoading={data?.isFetching}
-            onClick={() => data?.fetchNextPage()}
-          >
-            Load More
-          </Button>
-        </div>
-      )}
-    </div>
+                onClick={() => setSelectedOrgUsername(currentOrgUsername)}
+              >
+                <button className="flex items-center gap-3">
+                  <Avatar user={organization} bordered />
+                  <span>{currentOrgUsername}</span>
+                  {currentOrgUsername === currentUser?.defaultOrgUsername && (
+                    <span className="text-ds-gray-quaternary font-medium">
+                      Current default org
+                    </span>
+                  )}
+                </button>
+              </li>
+            )
+          })}
+        </ul>
+        {data?.hasNextPage && (
+          <div className="w-full flex justify-center">
+            <Button
+              hook="load-more"
+              isLoading={data?.isFetching}
+              onClick={() => data?.fetchNextPage()}
+            >
+              Load More
+            </Button>
+          </div>
+        )}
+      </div>
+    )
   )
 }
 
--- src/layouts/MyContextSwitcher/UpdateDefaultOrgModal/OrganizationList/hooks/useOrganizations.js
+++ src/layouts/MyContextSwitcher/UpdateDefaultOrgModal/OrganizationList/hooks/useOrganizations.js
@@ -18,6 +18,7 @@ export function useOrganizations() {
         })),
       ],
       currentUser,
+      isSuccess,
       ...rest,
     }
   }
