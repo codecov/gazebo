a57803a960a72b5700411c76f6513aabe7a05151
Rula Abu Hasna <rula@codecov.io>
2023-02-17 20:07:36 +0200

    refactor tests

--- src/pages/RepoPage/SettingsTab/tabs/GeneralTab/DangerZone/RepoState/DeactivateRepoModal/DeactivateRepoModal.spec.jsx
+++ src/pages/RepoPage/SettingsTab/tabs/GeneralTab/DangerZone/RepoState/DeactivateRepoModal/DeactivateRepoModal.spec.jsx
@@ -8,40 +8,99 @@ describe('DeactivateRepoModal component', () => {
   const closeModalMock = jest.fn()
   const deactivateRepoMock = jest.fn()
 
-  it('renders correctly when repo is activated', () => {
-    render(
-      <DeactivateRepoModal
-        closeModal={closeModalMock}
-        deactivateRepo={deactivateRepoMock}
-        isLoading={false}
-        showModal={true}
-        activated
-      />
-    )
-
-    const modalTitle = screen.getByText(
-      /Are you sure you want to deactivate the repo\?/
-    )
-    expect(modalTitle).toBeInTheDocument()
-
-    const modalBody = screen.getByText(
-      /Deactivate repo will deactivate a repo and prevent the upload of coverage information to that repo going forward./
-    )
-    expect(modalBody).toBeInTheDocument()
-
-    const cancelButton = screen.getByRole('button', { name: /Cancel/ })
-    expect(cancelButton).toBeInTheDocument()
-
-    const deactivateButton = screen.getByRole('button', {
-      name: 'Deactivate repo',
+  describe('renders the component correctly', () => {
+    it('renders modal title', () => {
+      render(
+        <DeactivateRepoModal
+          closeModal={closeModalMock}
+          deactivateRepo={deactivateRepoMock}
+          isLoading={false}
+          showModal={true}
+          activated
+        />
+      )
+      const modalTitle = screen.getByText(
+        /Are you sure you want to deactivate the repo\?/
+      )
+      expect(modalTitle).toBeInTheDocument()
     })
-    expect(deactivateButton).toBeInTheDocument()
+    it('renders modal body', () => {
+      render(
+        <DeactivateRepoModal
+          closeModal={closeModalMock}
+          deactivateRepo={deactivateRepoMock}
+          isLoading={false}
+          showModal={true}
+          activated
+        />
+      )
+      const modalBody = screen.getByText(
+        /Deactivate repo will deactivate a repo and prevent the upload of coverage information to that repo going forward./
+      )
+      expect(modalBody).toBeInTheDocument()
+    })
 
-    userEvent.click(cancelButton)
-    expect(closeModalMock).toHaveBeenCalled()
+    it('renders cancel button', () => {
+      render(
+        <DeactivateRepoModal
+          closeModal={closeModalMock}
+          deactivateRepo={deactivateRepoMock}
+          isLoading={false}
+          showModal={true}
+          activated
+        />
+      )
+      const cancelButton = screen.getByRole('button', { name: /Cancel/ })
+      expect(cancelButton).toBeInTheDocument()
+    })
+    it('renders deactivate button', () => {
+      render(
+        <DeactivateRepoModal
+          closeModal={closeModalMock}
+          deactivateRepo={deactivateRepoMock}
+          isLoading={false}
+          showModal={true}
+          activated
+        />
+      )
+      const deactivateButton = screen.getByRole('button', {
+        name: 'Deactivate repo',
+      })
+      expect(deactivateButton).toBeInTheDocument()
+    })
 
-    userEvent.click(deactivateButton)
-    expect(deactivateRepoMock).toHaveBeenCalledWith(true)
+    it('closes modal when cancel is clicked', () => {
+      render(
+        <DeactivateRepoModal
+          closeModal={closeModalMock}
+          deactivateRepo={deactivateRepoMock}
+          isLoading={false}
+          showModal={true}
+          activated
+        />
+      )
+      const cancelButton = screen.getByRole('button', { name: /Cancel/ })
+
+      userEvent.click(cancelButton)
+      expect(closeModalMock).toHaveBeenCalled()
+    })
+
+    it('calls deactivate when deactivate button is clicked', () => {
+      render(
+        <DeactivateRepoModal
+          closeModal={closeModalMock}
+          deactivateRepo={deactivateRepoMock}
+          isLoading={false}
+          showModal={true}
+          activated
+        />
+      )
+      const deactivateButton = screen.getByRole('button', {
+        name: 'Deactivate repo',
+      })
+      userEvent.click(deactivateButton)
+      expect(deactivateRepoMock).toHaveBeenCalledWith(true)
+    })
   })
 
   it('renders correctly when repo is deactivated', () => {
