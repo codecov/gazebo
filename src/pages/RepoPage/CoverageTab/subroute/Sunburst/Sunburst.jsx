191eaefade4d2fdfb2092d4b297eede4a48d9085
Terry Smith <terry@codecov.io>
2023-02-21 17:09:27 -0400

    update useTreePaths to fallback to the default repo, change ux to onclicks instead of hover for the breadcrumb, no tests yet

--- src/pages/RepoPage/CoverageTab/subroute/Sunburst/Sunburst.jsx
+++ src/pages/RepoPage/CoverageTab/subroute/Sunburst/Sunburst.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 import { useRepoConfig } from 'services/repo/useRepoConfig'
+import { useTreePaths } from 'shared/treePaths'
+import Breadcrumb from 'ui/Breadcrumb'
 import SunburstChart from 'ui/SunburstChart'
 
 import useSunburstChart from './hooks/useSunburstChart'
@@ -13,11 +15,25 @@ const Placeholder = () => (
   />
 )
 
+function useConvertD3ToBreadcrumbs(path) {
+  const { repo } = useParams()
+  const { treePaths } = useTreePaths(path)
+
+  if (path.length === 0) {
+    return [{ pageName: 'repo', text: repo }]
+  }
+
+  // Reversed for the left truncating trick
+  return treePaths.reverse()
+}
+
 function Sunburst() {
   const { provider, owner, repo } = useParams()
   const [currentPath, setCurrentPath] = useState('')
   const { data, isFetching, isError, isLoading } = useSunburstChart()
   const { data: config } = useRepoConfig({ provider, owner, repo })
+
+  const breadcrumbPaths = useConvertD3ToBreadcrumbs(currentPath)
 
   if (isFetching || isLoading) {
     return <Placeholder />
@@ -34,12 +50,12 @@ function Sunburst() {
         svgFontSize="24px"
         svgRenderSize={930}
         selector={(data) => data?.coverage}
-        onHover={(path) => setCurrentPath(`${path}`)}
+        onClick={(path) => setCurrentPath(`${path}`)}
         colorDomainMin={config?.indicationRange?.lowerRange}
         colorDomainMax={config?.indicationRange?.upperRange}
       />
       <span dir="rtl" className="truncate text-left">
-        {currentPath}
+        <Breadcrumb paths={breadcrumbPaths} />
       </span>
     </>
   )
