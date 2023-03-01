38da8f684d2e5f60fdb4e05719df876cb79cbcb1
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-03-01 13:31:00 -0400

    point pages to the plan tab

--- src/pages/AllOrgsPlanPage/Header/Header.jsx
+++ src/pages/AllOrgsPlanPage/Header/Header.jsx
@@ -1,6 +1,6 @@
 import MyContextSwitcher from 'layouts/MyContextSwitcher'
 
 function Header() {
-  return <MyContextSwitcher pageName="owner" activeContext={null} />
+  return <MyContextSwitcher pageName="planTab" activeContext={null} />
 }
 export default Header
