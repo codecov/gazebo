ec7d84ecd71ace1e78d54b6d2fbd255a12befb6b
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-03-06 11:03:08 -0400

    update PullsTab tests

--- src/pages/RepoPage/PullsTab/PullsTab.spec.jsx
+++ src/pages/RepoPage/PullsTab/PullsTab.spec.jsx
@@ -11,91 +11,120 @@ describe('Pulls Pab', () => {
     jest.resetAllMocks()
   })
 
-  function setup() {
-    repoPageRender({
-      initialEntries: ['/gh/codecov/gazebo/pulls'],
-      renderPulls: () => <PullsTab />,
-    })
-  }
-
   describe('when rendered', () => {
-    beforeEach(() => {
-      setup()
-    })
-
     it('renders select by updatestamp label', () => {
+      repoPageRender({
+        initialEntries: ['/gh/codecov/gazebo/pulls'],
+        renderPulls: () => <PullsTab />,
+      })
+
       const label = screen.getByText(/Sort by:/)
       expect(label).toBeInTheDocument()
     })
 
     it('renders view by state label', () => {
+      repoPageRender({
+        initialEntries: ['/gh/codecov/gazebo/pulls'],
+        renderPulls: () => <PullsTab />,
+      })
+
       const label = screen.getByText(/View:/)
       expect(label).toBeInTheDocument()
     })
 
     it('renders default of select by updatestamp', () => {
+      repoPageRender({
+        initialEntries: ['/gh/codecov/gazebo/pulls'],
+        renderPulls: () => <PullsTab />,
+      })
+
       const label = screen.getByText(/All/)
       expect(label).toBeInTheDocument()
     })
 
     it('renders default of select by state', () => {
+      repoPageRender({
+        initialEntries: ['/gh/codecov/gazebo/pulls'],
+        renderPulls: () => <PullsTab />,
+      })
+
       const label = screen.getByText(/Newest/)
       expect(label).toBeInTheDocument()
     })
   })
 
   describe('view by state', () => {
-    beforeEach(() => {
-      setup({ hasNextPage: true })
+    it('renders all options', () => {
+      repoPageRender({
+        initialEntries: ['/gh/codecov/gazebo/pulls'],
+        renderPulls: () => <PullsTab />,
+      })
+
       const select = screen.getByText('All')
       userEvent.click(select)
-    })
 
-    it('renders all options', () => {
-      expect(screen.getByText('Open')).toBeInTheDocument()
-      expect(screen.getByText('Merged')).toBeInTheDocument()
-      expect(screen.getByText('Closed')).toBeInTheDocument()
+      const openOption = screen.getByText('Open')
+      expect(openOption).toBeInTheDocument()
+
+      const mergedOption = screen.getByText('Merged')
+      expect(mergedOption).toBeInTheDocument()
+
+      const closedOption = screen.getByText('Closed')
+      expect(closedOption).toBeInTheDocument()
     })
   })
 
   describe('order by updatestamp', () => {
-    beforeEach(() => {
-      setup({ hasNextPage: true })
+    it('renders all options', () => {
+      repoPageRender({
+        initialEntries: ['/gh/codecov/gazebo/pulls'],
+        renderPulls: () => <PullsTab />,
+      })
+
       const select = screen.getByText('Newest')
       userEvent.click(select)
-    })
 
-    it('renders all options', () => {
-      expect(screen.getByText('Oldest')).toBeInTheDocument()
+      const oldest = screen.getByText('Oldest')
+      expect(oldest).toBeInTheDocument()
     })
   })
 
   describe('order by Oldest', () => {
-    beforeEach(() => {
-      setup({ hasNextPage: true })
+    it('renders the selected option', async () => {
+      repoPageRender({
+        initialEntries: ['/gh/codecov/gazebo/pulls'],
+        renderPulls: () => <PullsTab />,
+      })
+
       const select = screen.getByText('Newest')
       userEvent.click(select)
-      const state = screen.getAllByRole('option')[1]
-      userEvent.click(state)
-    })
 
-    it('renders the selected option', () => {
-      expect(screen.getByText('Oldest')).toBeInTheDocument()
-      expect(screen.queryByText('Newest')).not.toBeInTheDocument()
+      const state = screen.getByRole('option', { name: 'Oldest' })
+      userEvent.click(state)
+
+      const oldestOption = await screen.findByText('Oldest')
+      expect(oldestOption).toBeInTheDocument()
+
+      const newestOption = screen.queryByText('Newest')
+      expect(newestOption).not.toBeInTheDocument()
     })
   })
 
   describe('view by Merged', () => {
-    beforeEach(() => {
-      setup({ hasNextPage: false })
+    it('renders the number of selected options', async () => {
+      repoPageRender({
+        initialEntries: ['/gh/codecov/gazebo/pulls'],
+        renderPulls: () => <PullsTab />,
+      })
+
       const select = screen.getByText('All')
       userEvent.click(select)
+
       const state = screen.getAllByRole('option')[2]
       userEvent.click(state)
-    })
 
-    it('renders the number of selected options', () => {
-      expect(screen.getByText(/1 selected/)).toBeInTheDocument()
+      const itemSelected = await screen.findByText(/1 selected/)
+      expect(itemSelected).toBeInTheDocument()
     })
   })
 })
