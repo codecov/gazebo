0408d733aa52ba5156b565363a05f2c2b1541ef3
Rula Abu Hasna <rula@codecov.io>
2023-03-07 15:05:07 +0200

    fix width

--- src/pages/RepoPage/UnauthorizedAccess/UnauthorizedAccess.jsx
+++ src/pages/RepoPage/UnauthorizedAccess/UnauthorizedAccess.jsx
@@ -4,7 +4,7 @@ import A from 'ui/A'
 const UnauthorizedAccess = () => {
   return (
     <div className="flex h-full flex-col items-center justify-center gap-4">
-      <img alt="illustration error" src={img403} width="400px" />
+      <img alt="illustration error" src={img403} width="200px" />
       <h1 className="text-2xl">Unauthorized Access</h1>
       <div className="flex flex-col items-center justify-center gap-2">
         <p>
