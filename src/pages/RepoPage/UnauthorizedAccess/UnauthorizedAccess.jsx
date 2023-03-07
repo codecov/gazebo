685a8d9cdfddaeeedd0cf52a4aa84ee90e828e6e
Rula Abu Hasna <rula@codecov.io>
2023-03-07 15:05:44 +0200

    final ui polish

--- src/pages/RepoPage/UnauthorizedAccess/UnauthorizedAccess.jsx
+++ src/pages/RepoPage/UnauthorizedAccess/UnauthorizedAccess.jsx
@@ -6,11 +6,11 @@ const UnauthorizedAccess = () => {
     <div className="flex h-full flex-col items-center justify-center gap-4">
       <img alt="illustration error" src={img403} width="200px" />
       <h1 className="text-2xl">Unauthorized Access</h1>
-      <div className="flex flex-col items-center justify-center gap-2">
+      <div className="flex flex-col items-center justify-center gap-4">
         <p>
           Activation is required to view this repo, please{' '}
           <A to={{ pageName: 'membersTab' }}>click here </A> to activate your
-          account
+          account.
         </p>
         <strong>Error 403</strong>
       </div>
