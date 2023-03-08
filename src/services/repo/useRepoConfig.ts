5e907db0c6eccc4a48d6c24490643404624f095b
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-03-08 14:26:20 -0400

    clean up useRepoConfig a little tiny bit

--- src/services/repo/useRepoConfig.ts
+++ src/services/repo/useRepoConfig.ts
@@ -9,16 +9,12 @@ export interface UseRepoConfigArgs {
   repo: string
 }
 
-const RepoConfig = z.object({
-  indicationRange: z
-    .object({
-      lowerRange: z.number(),
-      upperRange: z.number(),
-    })
-    .nullable(),
+export const RepoConfig = z.object({
+  indicationRange: z.object({
+    lowerRange: z.number(),
+    upperRange: z.number(),
+  }),
 })
-
-export type UseRepoConfig = z.infer<typeof RepoConfig>
 
 const query = `
   query RepoConfig($owner: String!, $repo: String!) {
@@ -38,7 +34,7 @@ const query = `
 export const useRepoConfig = ({ provider, owner, repo }: UseRepoConfigArgs) =>
   useQuery({
     queryKey: ['RepoConfig', provider, owner, repo, query],
-    queryFn: ({ signal }): Promise<UseRepoConfig | {}> =>
+    queryFn: ({ signal }) =>
       Api.graphql({
         provider,
         query,
@@ -47,15 +43,7 @@ export const useRepoConfig = ({ provider, owner, repo }: UseRepoConfigArgs) =>
           owner,
           repo,
         },
-      }).then((res) => {
-        const zRes = RepoConfig.safeParse(
-          res?.data?.owner?.repository?.repositoryConfig
-        )
-
-        if (!zRes.success) {
-          return {}
-        }
-
-        return zRes.data
-      }),
+      }).then((res) =>
+        RepoConfig.parse(res?.data?.owner?.repository?.repositoryConfig)
+      ),
   })
