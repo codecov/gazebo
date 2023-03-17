c154bf79c73f798fbd2c3e5c8e01b1e96ba77eea
Terry Smith <terry@codecov.io>
2023-03-17 17:49:08 -0300

    Style polish + fix a lable htmlFor

--- src/pages/TermsOfService/TermsOfService.jsx
+++ src/pages/TermsOfService/TermsOfService.jsx
@@ -27,11 +27,11 @@ export default function TermsOfService() {
   }
 
   return (
-    <div className="mx-auto w-full max-w-[30rem] text-sm text-ds-gray-octonary">
-      <h1 className="py-4 text-2xl font-semibold">Welcome to Codecov</h1>
+    <div className="mx-auto w-full max-w-[38rem] text-sm text-ds-gray-octonary">
+      <h1 className="pt-20 pb-3 text-2xl font-semibold">Welcome to Codecov</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="mb-4 border-y border-ds-gray-tertiary py-4">
-          <p>Select organization</p>
+        <div className="mb-4 border-y border-ds-gray-tertiary pt-6">
+          <p className="font-semibold">Select organization</p>
           <div className="max-w-[15rem] py-1">
             <Controller
               name="select"
@@ -50,48 +50,56 @@ export default function TermsOfService() {
               )}
             />
           </div>
-          <p className="mb-4 text-xs text-ds-gray-quaternary">
+          <p className="text-xs text-ds-gray-quaternary">
             Don&apos;t see your org?{' '}
             <A href="#" hook="help finding an org">
               Help finding org
               <Icon name="chevronRight" size="sm" variant="solid" />
             </A>
           </p>
-          <div className="flex gap-2">
-            <input
-              {...register('marketingEmail')}
-              type="checkbox"
-              id="marketingEmail"
-              aria-label="I would like to receive updates via email"
-            />
-            <label className="cursor-pointer" htmlFor="marketingEmail">
-              I would like to receive updates via email
-            </label>
-          </div>
-          <div className="flex gap-2">
-            <input
-              {...register('tos', { required: true })}
-              type="checkbox"
-              aria-label="I accept the terms of service and privacy policy"
-            />
-            <label
-              className="cursor-pointer font-semibold"
-              htmlFor="tos"
-              aria-label="I agree to the TOS and privacy policy"
-            >
-              I agree to{' '}
-              <A
-                href="https://about.codecov.io/terms-of-service"
-                hook="terms of service"
+          <div className="my-6 flex flex-col gap-2">
+            <div className="flex gap-2">
+              <input
+                {...register('marketingEmail')}
+                type="checkbox"
+                id="marketingEmail"
+                aria-label="I would like to receive updates via email"
+              />
+              <label className="cursor-pointer" htmlFor="marketingEmail">
+                I would like to receive updates via email
+              </label>
+            </div>
+            <div className="flex gap-2">
+              <input
+                {...register('tos', { required: true })}
+                type="checkbox"
+                id="tos"
+                aria-label="I accept the terms of service and privacy policy"
+              />
+              <label
+                className="cursor-pointer"
+                htmlFor="tos"
+                aria-label="I agree to the TOS and privacy policy"
               >
-                terms of services
-              </A>{' '}
-              and the{' '}
-              <A href="https://about.codecov.io/privacy" hook="privacy policy">
-                privacy policy
-              </A>{' '}
-              required
-            </label>
+                <span className="font-semibold">
+                  I agree to{' '}
+                  <A
+                    href="https://about.codecov.io/terms-of-service"
+                    hook="terms of service"
+                  >
+                    terms of services
+                  </A>{' '}
+                  and the{' '}
+                  <A
+                    href="https://about.codecov.io/privacy"
+                    hook="privacy policy"
+                  >
+                    privacy policy
+                  </A>
+                </span>{' '}
+                required
+              </label>
+            </div>
           </div>
         </div>
         <Button
