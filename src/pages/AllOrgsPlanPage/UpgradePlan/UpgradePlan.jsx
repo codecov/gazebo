1a05146d5e05d4e1d3a66e91e64012004d607ab8
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-03-20 11:21:50 -0300

    finish up work on AllOrgsPlanPage

--- src/pages/AllOrgsPlanPage/UpgradePlan/UpgradePlan.jsx
+++ src/pages/AllOrgsPlanPage/UpgradePlan/UpgradePlan.jsx
@@ -2,8 +2,6 @@ import PropTypes from 'prop-types'
 import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 
-import parasolImg from 'assets/plan/parasol.png'
-import sentryCodecov from 'assets/plan/sentry_codecov.svg'
 import {
   accountDetailsPropType,
   planPropType,
@@ -15,28 +13,15 @@ import {
   canApplySentryUpgrade,
   findSentryPlans,
   isEnterprisePlan,
-  isFreePlan,
   useProPlans,
 } from 'shared/utils/billing'
 import A from 'ui/A'
 import Card from 'ui/Card'
-import Icon from 'ui/Icon'
 import Select from 'ui/Select'
 
 import SentryUpgradeForm from './SentryUpgradeForm'
+import UpgradeDetails from './UpgradeDetails'
 import UpgradeForm from './UpgradeForm'
-
-import BenefitList from '../BenefitList'
-
-function shouldRenderCancelLink(accountDetails, plan) {
-  // cant cancel a free plan
-  if (isFreePlan(plan?.value) || isEnterprisePlan(plan?.value)) return false
-
-  // plan is already set for cancellation
-  if (accountDetails?.subscriptionDetail?.cancelAtPeriodEnd) return false
-
-  return true
-}
 
 const mergeOrgs = ({ contexts }) => [
   contexts?.currentUser,
@@ -121,88 +106,19 @@ function UpgradePlan() {
   const organizations = mergeOrgs({ contexts })
   const plan = determinePlan({ accountDetails })
 
-  let details = {
-    img: (
-      <div className="-mt-16">
-        <img src={parasolImg} alt="parasol" />
-      </div>
-    ),
-    marketingName: proPlanYear?.marketingName,
-    baseUnitPrice: (
-      <>
-        ${proPlanYear?.baseUnitPrice}*
-        <span className="text-base">/monthly</span>
-      </>
-    ),
-    priceDisclaimer: (
-      <p className="text-ds-gray-quaternary">
-        *${proPlanMonth?.baseUnitPrice} per user / month if paid monthly
-      </p>
-    ),
-    benefits: plan?.benefits ?? proPlanYear?.benefits,
-  }
-
-  if (isEnterprisePlan(plan?.value)) {
-    details = {
-      img: (
-        <div className="-mt-16">
-          <img src={parasolImg} alt="parasol" />
-        </div>
-      ),
-      marketingName: plan?.marketingName,
-      baseUnitPrice: 'Custom Pricing',
-      benefits: plan?.benefits ?? proPlanYear?.benefits,
-    }
-  } else if (canApplySentryUpgrade({ plans })) {
-    details = {
-      img: (
-        <div>
-          <img src={sentryCodecov} alt="sentry codecov logos" />
-        </div>
-      ),
-      marketingName: sentryPlanYear?.marketingName,
-      baseUnitPrice: (
-        <>
-          $29.99<span className="text-base">/monthly</span>
-        </>
-      ),
-      priceDisclaimer: (
-        <p className="text-ds-gray-quaternary">
-          *${sentryPlanMonth?.baseUnitPrice} per user / month if paid monthly
-        </p>
-      ),
-      benefits: plan?.benefits ?? sentryPlanYear?.benefits,
-    }
-  }
-
   return (
     <div className="mt-6 flex flex-col gap-8 md:w-11/12 md:flex-row lg:w-10/12">
       <Card variant="large">
-        <div className="flex flex-col gap-4">
-          {details?.img}
-          <h3 className="text-2xl font-semibold text-ds-pink-quinary">
-            {details?.marketingName}
-          </h3>
-          <h2 className="text-4xl">{details?.baseUnitPrice}</h2>
-          <BenefitList
-            iconName="check"
-            iconColor="text-ds-pink-quinary"
-            benefits={details?.benefits}
-          />
-          {details?.priceDisclaimer}
-          {organizationName && shouldRenderCancelLink(accountDetails, plan) && (
-            <A
-              to={{
-                pageName: 'cancelOrgPlan',
-                options: { owner: organizationName },
-              }}
-              variant="grayQuinary"
-              hook="cancel-plan"
-            >
-              Cancel plan <Icon name="chevronRight" size="sm" variant="solid" />
-            </A>
-          )}
-        </div>
+        <UpgradeDetails
+          accountDetails={accountDetails}
+          organizationName={organizationName}
+          plan={plan}
+          plans={plans}
+          proPlanMonth={proPlanMonth}
+          proPlanYear={proPlanYear}
+          sentryPlanYear={sentryPlanYear}
+          sentryPlanMonth={sentryPlanMonth}
+        />
       </Card>
       <div className="flex flex-col gap-4 md:w-2/3">
         <Card variant="upgradeForm">
