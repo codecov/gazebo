07ba79e0647ea84259b8ad8c4c71fa8b2d7dabdb
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-03-22 10:46:03 -0300

    add some memory routers and routes to provide the provider

--- src/pages/AdminSettings/AdminMembers/ActivationInfo/ActivationCount/ActivationCount.spec.jsx
+++ src/pages/AdminSettings/AdminMembers/ActivationInfo/ActivationCount/ActivationCount.spec.jsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { render, screen } from '@testing-library/react'
 import { rest } from 'msw'
 import { setupServer } from 'msw/node'
+import { MemoryRouter, Route } from 'react-router-dom'
 
 import ActivationCount from './ActivationCount'
 
@@ -16,7 +17,13 @@ const mockResponse = {
 const wrapper =
   () =>
   ({ children }) =>
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    (
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter initialEntries={['/gh']}>
+          <Route path="/:provider">{children}</Route>
+        </MemoryRouter>
+      </QueryClientProvider>
+    )
 
 beforeAll(() => server.listen())
 beforeEach(() => {
