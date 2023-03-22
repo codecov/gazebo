718214436e4c88a46309f90fc1b3cc7514e25f4f
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-03-22 10:44:52 -0300

    pass along the provider

--- src/services/selfHosted/useSelfHostedSettings.js
+++ src/services/selfHosted/useSelfHostedSettings.js
@@ -1,9 +1,13 @@
 import { useQuery } from '@tanstack/react-query'
+import { useParams } from 'react-router-dom'
 
 import Api from 'shared/api'
 
-export const useSelfHostedSettings = () =>
-  useQuery({
-    queryKey: ['SelfHostedSettings'],
-    queryFn: ({ signal }) => Api.get({ path: '/settings', signal }),
+export const useSelfHostedSettings = () => {
+  const { provider } = useParams()
+
+  return useQuery({
+    queryKey: ['SelfHostedSettings', provider],
+    queryFn: ({ signal }) => Api.get({ provider, path: '/settings', signal }),
   })
+}
