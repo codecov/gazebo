e8fa715e8fd30c24c93849e5b3ab0e6124362f96
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-03-28 08:38:12 -0300

    add user count to UpgradeForm for plan page upgrade

---
+++ src/pages/PlanPage/subRoutes/UpgradePlanPage/UpgradeForm/UserCount/UserCount.tsx
@@ -0,0 +1,59 @@
+import isNumber from 'lodash/isNumber'
+import PropTypes from 'prop-types'
+
+function renderStudentText(activatedStudents: number) {
+  if (activatedStudents < 1) {
+    return null
+  }
+
+  if (activatedStudents === 1) {
+    return (
+      <p className="mb-4 text-xs text-ds-gray-quinary">
+        *You have {activatedStudents} active student that does not count towards
+        the number of active users.
+      </p>
+    )
+  }
+
+  return (
+    <p className="mb-4 text-xs text-ds-gray-quinary">
+      *You have {activatedStudents} active students that do not count towards
+      the number of active users.
+    </p>
+  )
+}
+
+interface StudentTextProps {
+  activatedUserCount?: number
+  inactiveUserCount?: number
+  activatedStudentCount?: number
+}
+
+const StudentText: React.FC<StudentTextProps> = ({
+  activatedUserCount,
+  inactiveUserCount,
+  activatedStudentCount,
+}) => {
+  if (!isNumber(activatedUserCount) || !isNumber(inactiveUserCount)) {
+    return null
+  }
+
+  return (
+    <div className="border-l-2 pl-2">
+      <p>
+        Currently {activatedUserCount} users activated out of{' '}
+        {activatedUserCount + inactiveUserCount} users.
+      </p>
+      {isNumber(activatedStudentCount) &&
+        renderStudentText(activatedStudentCount)}
+    </div>
+  )
+}
+
+StudentText.propTypes = {
+  activatedUserCount: PropTypes.number,
+  inactiveUserCount: PropTypes.number,
+  activatedStudentCount: PropTypes.number,
+}
+
+export default StudentText
