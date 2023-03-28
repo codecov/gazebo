6e0574ab038513852ca1a77e3b52b4c633c7e0bf
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-03-28 08:45:30 -0300

    move useUpgradeForm hook into file where its only used

--- src/pages/AllOrgsPlanPage/UpgradePlan/UpgradeForm/UpgradeForm.jsx
+++ src/pages/AllOrgsPlanPage/UpgradePlan/UpgradeForm/UpgradeForm.jsx
@@ -1,22 +1,30 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import PropTypes from 'prop-types'
 import { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import { useHistory, useParams } from 'react-router-dom'
 
 import {
   accountDetailsPropType,
   planPropType,
   usePlans,
+  useUpgradePlan,
 } from 'services/account'
+import { useAddNotification } from 'services/toastNotification'
 import {
   canApplySentryUpgrade,
   getNextBillingDate,
+  isAnnualPlan,
   Plans,
 } from 'shared/utils/billing'
-import { getInitialDataForm } from 'shared/utils/upgradeForm'
+import {
+  calculatePrice,
+  getInitialDataForm,
+  getSchema,
+} from 'shared/utils/upgradeForm'
 import RadioInput from 'ui/RadioInput/RadioInput'
 import TextInput from 'ui/TextInput'
 
-import { useUpgradeForm } from './hooks'
 import TotalBanner from './TotalBanner'
 import UpdateButton from './UpdateButton'
 import UserCount from './UserCount'
@@ -24,6 +32,104 @@ import UserCount from './UserCount'
 const MIN_NB_SEATS = 2
 const MIN_SENTRY_SEATS = 5
 const SENTRY_PRICE = 29
+
+const useUpgradeForm = ({
+  proPlanYear,
+  proPlanMonth,
+  accountDetails,
+  minSeats,
+  sentryPrice,
+  sentryPlanYear,
+  sentryPlanMonth,
+  isSentryUpgrade,
+  organizationName,
+}) => {
+  const { provider } = useParams()
+  const history = useHistory()
+  const addToast = useAddNotification()
+
+  const { mutate, ...rest } = useUpgradePlan({
+    provider,
+    owner: organizationName,
+  })
+
+  const upgradePlan = ({ seats, newPlan }) =>
+    mutate(
+      {
+        seats,
+        newPlan,
+      },
+      {
+        onSuccess: () => {
+          addToast({
+            type: 'success',
+            text: 'Plan successfully upgraded',
+          })
+          history.push(`/plan/${provider}/${organizationName}`)
+        },
+        onError: (error) => {
+          addToast({
+            type: 'error',
+            text: error?.data?.detail || 'Something went wrong',
+          })
+        },
+      }
+    )
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState,
+    setValue,
+    getValues,
+    reset,
+  } = useForm({
+    defaultValues: getInitialDataForm({
+      accountDetails,
+      proPlanYear,
+      isSentryUpgrade,
+      minSeats,
+      sentryPlanYear,
+    }),
+    resolver: zodResolver(getSchema({ accountDetails, minSeats })),
+    mode: 'onChange',
+  })
+
+  const perYearPrice = calculatePrice({
+    seats: watch('seats'),
+    baseUnitPrice: isSentryUpgrade
+      ? sentryPlanYear?.baseUnitPrice
+      : proPlanYear?.baseUnitPrice,
+    isSentryUpgrade,
+    sentryPrice,
+  })
+
+  const perMonthPrice = calculatePrice({
+    seats: watch('seats'),
+    baseUnitPrice: isSentryUpgrade
+      ? sentryPlanMonth?.baseUnitPrice
+      : proPlanMonth?.baseUnitPrice,
+    isSentryUpgrade,
+    sentryPrice,
+  })
+
+  const isPerYear = isAnnualPlan(watch('newPlan'))
+
+  return {
+    perYearPrice,
+    perMonthPrice,
+    register,
+    handleSubmit,
+    isPerYear,
+    formState,
+    setValue,
+    getValues,
+    reset,
+    upgradePlan,
+    ...rest,
+  }
+}
 
 const PlanDetails = ({ isSentryUpgrade }) => {
   if (!isSentryUpgrade) {
