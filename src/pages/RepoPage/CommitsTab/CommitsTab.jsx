4fd607292e6ee8a3715a85befadcb98e96b143d2
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-04-04 16:02:47 -0300

    CODE-1502: Search for a Commit Based on Message or (Short)Sha (#1987)
    * update CommitsTable to support searching
    
    * add search box to commits tab
    
    * fix styling on firefox

--- src/pages/RepoPage/CommitsTab/CommitsTab.jsx
+++ src/pages/RepoPage/CommitsTab/CommitsTab.jsx
@@ -5,6 +5,7 @@ import { useLocationParams } from 'services/navigation'
 import { useRepoOverview } from 'services/repo'
 import Icon from 'ui/Icon'
 import MultiSelect from 'ui/MultiSelect'
+import SearchField from 'ui/SearchField'
 import Select from 'ui/Select'
 
 import CommitsTable from './CommitsTable'
@@ -17,10 +18,11 @@ const useControlParams = ({ defaultBranch }) => {
   const defaultParams = {
     branch: defaultBranch,
     states: [],
+    search: '',
   }
 
   const { params, updateParams } = useLocationParams(defaultParams)
-  const { branch: selectedBranch, states } = params
+  const { branch: selectedBranch, states, search } = params
 
   const paramStatesNames = states.map((filter) => statusNames[filter])
 
@@ -38,6 +40,7 @@ const useControlParams = ({ defaultBranch }) => {
     updateParams,
     selectedStates,
     setSelectedStates,
+    search,
   }
 }
 
@@ -56,6 +59,7 @@ function CommitsTab() {
     updateParams,
     selectedStates,
     setSelectedStates,
+    search,
   } = useControlParams({ defaultBranch: overview?.defaultBranch })
 
   const {
@@ -101,53 +105,65 @@ function CommitsTab() {
 
   return (
     <div className="flex flex-1 flex-col gap-4">
-      <div className="flex gap-2 px-2 sm:px-0">
-        <div className="flex flex-col gap-1">
-          <h2 className="flex flex-initial items-center gap-1 font-semibold">
-            <span className="text-ds-gray-quinary">
-              <Icon name="branch" variant="developer" size="sm" />
-            </span>
-            Branch Context
-          </h2>
-          <div className="min-w-[16rem]">
-            <Select
-              {...branchSelectorProps}
-              dataMarketing="branch-selector-commits-page"
-              ariaName="Select branch"
-              variant="gray"
-              resourceName="branch"
-              isLoading={branchListIsFetching}
-              onChange={(branch) => {
-                updateParams({ branch: branch })
-              }}
-              onLoadMore={() => {
-                if (branchListHasNextPage) {
-                  branchesFetchNextPage()
-                  branchListFetchNextPage()
-                }
-              }}
-              onSearch={(term) => setBranchSearchTerm(term)}
-              items={newBranches}
-            />
+      <div className="grid grid-cols-1 justify-between gap-4 md:grid-cols-2 md:items-end md:gap-0">
+        <div className="flex flex-col gap-2 px-2 sm:px-0 md:flex-row">
+          <div className="flex flex-col gap-1">
+            <h2 className="flex flex-initial items-center gap-1 font-semibold">
+              <span className="text-ds-gray-quinary">
+                <Icon name="branch" variant="developer" size="sm" />
+              </span>
+              Branch Context
+            </h2>
+            <div className="min-w-[13rem] lg:min-w-[16rem]">
+              <Select
+                {...branchSelectorProps}
+                dataMarketing="branch-selector-commits-page"
+                ariaName="Select branch"
+                variant="gray"
+                resourceName="branch"
+                isLoading={branchListIsFetching}
+                onChange={(branch) => {
+                  updateParams({ branch: branch })
+                }}
+                onLoadMore={() => {
+                  if (branchListHasNextPage) {
+                    branchesFetchNextPage()
+                    branchListFetchNextPage()
+                  }
+                }}
+                onSearch={(term) => setBranchSearchTerm(term)}
+                items={newBranches}
+              />
+            </div>
+          </div>
+          <div className="flex flex-col gap-1">
+            <h2 className="font-semibold">CI status</h2>
+            <div className="min-w-[13rem] lg:min-w-[16rem]">
+              <MultiSelect
+                dataMarketing="commits-filter-by-status"
+                ariaName="Filter by CI states"
+                value={selectedStates}
+                items={filterItems}
+                resourceName="CI States"
+                onChange={handleStatusChange}
+              />
+            </div>
           </div>
         </div>
-        <div className="flex flex-col gap-1">
-          <h2 className="font-semibold">CI status</h2>
-          <div className="min-w-[16rem]">
-            <MultiSelect
-              dataMarketing="commits-filter-by-status"
-              ariaName="Filter by CI states"
-              value={selectedStates}
-              items={filterItems}
-              resourceName="CI States"
-              onChange={handleStatusChange}
-            />
-          </div>
+        <div className="flex px-2 sm:px-0 md:flex-row-reverse">
+          <SearchField
+            dataMarketing="commits-tab-search"
+            placeholder="Search commits"
+            searchValue={search || ''}
+            setSearchValue={(search) => updateParams({ search })}
+            data-testid="search-input-members"
+          />
         </div>
       </div>
       <CommitsTable
         branch={branch}
         states={selectedStates?.map((state) => state?.toUpperCase())}
+        search={search}
       />
     </div>
   )
