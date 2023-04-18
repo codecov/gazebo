3190365a40adc09a2a2725e291dac1d56621b3af
Rula Abu Hasna <91732700+RulaKhaled@users.noreply.github.com>
2023-04-18 23:39:58 +0300

    Add new static token -Analysis token- 2/2 (#1993)
    * refactor impact anaylysis token
    
    * refactor the rest of files
    
    * refactor the settings page
    
    * we don't need this here
    
    * refactor the test spec
    
    * impact analysis increase coverage
    
    * smol polish
    
    * move to tokens section
    
    * add new static analysis token
    
    * missing a space between link and copy
    
    * this was a bit flaky too
    
    * add new link
    
    * move to modal

---
+++ src/pages/RepoPage/SettingsTab/tabs/GeneralTab/Tokens/StaticAnalysisToken/RegenerateStaticTokenModal.jsx
@@ -0,0 +1,56 @@
+import PropTypes from 'prop-types'
+
+import { useRegenerateRepositoryToken } from 'services/repositoryToken'
+import Button from 'ui/Button'
+import Modal from 'ui/Modal'
+
+import { TokenType } from '../enums'
+
+const RegenerateStaticTokenModal = ({ showModal, closeModal }) => {
+  const { mutate, isLoading } = useRegenerateRepositoryToken({
+    tokenType: TokenType.STATIC_ANALYSIS,
+  })
+
+  return (
+    <Modal
+      isOpen={showModal}
+      onClose={closeModal}
+      title="New static analysis token"
+      body={
+        <div className="flex  flex-col gap-4">
+          <h2 className="font-semibold">Static Analysis</h2>
+          <p>If you save the new token, make sure to update your CI yml</p>
+        </div>
+      }
+      footer={
+        <div className="flex gap-2">
+          <div>
+            <Button hook="close-modal" onClick={closeModal}>
+              Cancel
+            </Button>
+          </div>
+          <div>
+            <Button
+              isLoading={isLoading}
+              hook="generate-token"
+              variant="primary"
+              onClick={async () => {
+                await mutate()
+                closeModal()
+              }}
+            >
+              Generate New Token
+            </Button>
+          </div>
+        </div>
+      }
+    />
+  )
+}
+
+RegenerateStaticTokenModal.propTypes = {
+  showModal: PropTypes.bool.isRequired,
+  closeModal: PropTypes.func.isRequired,
+}
+
+export default RegenerateStaticTokenModal
