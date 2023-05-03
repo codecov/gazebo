cae15523e9864157052c764adee997d300180259
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-05-03 15:37:12 -0300

    CODE-3262: Add "Start trial" Variation to Plan Page Button (#2037)
    * add new field to subscriptionDetailType
    
    * switch trialEnd prop to just a number because that allows nulls
    
    * update single org upgrade page to have a start trial button
    
    * update all orgs plan page update button to support start trial
    
    * if subscription detail is null and is sentry upgrade then display start trial
    
    * disable button when switching orgs
    
    * remove w-min class from wrapping update button in org upgrade page
    
    * bring div back and add w-fit for width sizing

--- src/pages/PlanPage/subRoutes/UpgradePlanPage/UpgradeForm/UpdateButton/UpdateButton.tsx
+++ src/pages/PlanPage/subRoutes/UpgradePlanPage/UpgradeForm/UpdateButton/UpdateButton.tsx
@@ -1,23 +1,40 @@
-import PropTypes from 'prop-types'
+import isNull from 'lodash/isNull'
+import PropTypes, { type InferProps } from 'prop-types'
 
+import { accountDetailsPropType } from 'services/account'
 import Button from 'ui/Button'
 
-interface UpdateButtonProps {
-  isValid: boolean
-  getValues: () => { newPlan: string; seats: number }
-  value: string | undefined
-  quantity: number | undefined
-}
-
-const UpdateButton: React.FC<UpdateButtonProps> = ({
+// eslint-disable-next-line complexity
+function UpdateButton({
   isValid,
   getValues,
   value,
   quantity,
-}) => {
+  accountDetails,
+  isSentryUpgrade,
+}: InferProps<typeof UpdateButton.propTypes>) {
   const isSamePlan = getValues()?.newPlan === value
   const noChangeInSeats = getValues()?.seats === quantity
   const disabled = !isValid || (isSamePlan && noChangeInSeats)
+  const trialEndTimestamp = accountDetails?.subscriptionDetail?.trialEnd ?? null
+
+  if (isSentryUpgrade && isNull(trialEndTimestamp)) {
+    return (
+      <div className="flex items-center gap-2">
+        <Button
+          data-cy="all-orgs-plan-update"
+          disabled={false}
+          type="submit"
+          variant="primary"
+          hook="submit-upgrade"
+          to={undefined}
+        >
+          Start trial
+        </Button>
+        <p>No credit card required!</p>
+      </div>
+    )
+  }
 
   return (
     <Button
@@ -38,6 +55,8 @@ UpdateButton.propTypes = {
   getValues: PropTypes.func.isRequired,
   value: PropTypes.string,
   quantity: PropTypes.number,
+  accountDetails: accountDetailsPropType,
+  isSentryUpgrade: PropTypes.bool.isRequired,
 }
 
 export default UpdateButton
