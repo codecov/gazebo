103db9c5bd47bfce8ea89bd2e4f6c1919aa465fe
Rula Abu Hasna <91732700+RulaKhaled@users.noreply.github.com>
2023-05-09 19:56:55 +0300

    Code 2649 sort by default icon not visible without initial click (#1999)
    * separate the two tables
    
    * break down file explorer
    
    * try using act
    
    * use act
    
    * update classes
    
    * wrap test with act

--- src/pages/RepoPage/CoverageTab/subroute/FileExplorer/FileExplorer.jsx
+++ src/pages/RepoPage/CoverageTab/subroute/FileExplorer/FileExplorer.jsx
@@ -1,61 +1,21 @@
-import PropType from 'prop-types'
-
 import { useLocationParams } from 'services/navigation'
 import DisplayTypeButton from 'shared/ContentsTable/DisplayTypeButton'
 import FileBreadcrumb from 'shared/ContentsTable/FileBreadcrumb'
-import MissingFileData from 'shared/ContentsTable/MissingFileData'
 import SearchField from 'ui/SearchField'
-import Spinner from 'ui/Spinner'
-import Table from 'ui/Table'
 
+import CodeTreeTable from './CodeTreeTable'
+import FileListTable from './FileListTable'
 import { useRepoBranchContentsTable } from './hooks'
-
-const Loader = ({ isLoading }) => {
-  return (
-    isLoading && (
-      <div className="flex flex-1 justify-center">
-        <Spinner size={60} />
-      </div>
-    )
-  )
-}
-
-Loader.propTypes = {
-  isLoading: PropType.bool,
-}
-
-function RepoContentsResult({ isSearching, isMissingHeadReport }) {
-  if (isMissingHeadReport) {
-    return (
-      <p className="flex flex-1 justify-center">
-        No coverage report uploaded for this branch head commit
-      </p>
-    )
-  }
-
-  return <MissingFileData isSearching={isSearching} />
-}
-
-RepoContentsResult.propTypes = {
-  isSearching: PropType.bool,
-  isMissingHeadReport: PropType.bool,
-}
 
 const defaultQueryParams = {
   search: '',
 }
 
 function FileExplorer() {
-  const {
-    data,
-    headers,
-    handleSort,
-    isSearching,
-    isMissingHeadReport,
-    isLoading,
-  } = useRepoBranchContentsTable()
+  const { data, isLoading } = useRepoBranchContentsTable()
 
   const { params, updateParams } = useLocationParams(defaultQueryParams)
+  const isFileListDisplay = params?.displayType === 'list'
 
   return (
     <>
@@ -71,24 +31,8 @@ function FileExplorer() {
           setSearchValue={(search) => updateParams({ search })}
         />
       </div>
-      <div className=" grid flex-1 grid-cols-12 gap-8">
-        <div className="col-span-12 flex flex-col md:col-span-12">
-          <Table
-            data={data}
-            columns={headers}
-            onSort={handleSort}
-            enableHover
-          />
-          <div className="mt-4">
-            <Loader isLoading={isLoading} />
-            {data?.length === 0 && !isLoading && (
-              <RepoContentsResult
-                isSearching={isSearching}
-                isMissingHeadReport={isMissingHeadReport}
-              />
-            )}
-          </div>
-        </div>
+      <div className="flex flex-col gap-8">
+        {isFileListDisplay ? <FileListTable /> : <CodeTreeTable />}
       </div>
     </>
   )
