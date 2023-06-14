4f0cde858da05fa7d6ed061dd4a9f1f2101da39a
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-06-14 08:48:32 -0300

    CODE-3317: Update Admin Table in Org Settings (#2077)
    * remove admin list component
    
    * refactor and move AddAdmins into it's own dir
    
    * create new admin table component that uses the ui table
    
    * update ManageAdminCard with new components
    
    * use params instead of props for passing owner and provider
    
    * add user when hovering then hitting enter

--- src/pages/AccountSettings/tabs/Admin/ManageAdminCard/ManageAdminCard.jsx
+++ src/pages/AccountSettings/tabs/Admin/ManageAdminCard/ManageAdminCard.jsx
@@ -1,28 +1,41 @@
-import { Suspense } from 'react'
+import { useParams } from 'react-router-dom'
 
-import Card from 'ui/Card'
-import Spinner from 'ui/Spinner'
+import { ApiFilterEnum } from 'services/navigation'
+import { useUpdateUser } from 'services/users'
 
-import AdminList from './AdminList'
-
-const Loader = () => (
-  <div className="flex items-center justify-center py-16">
-    <Spinner />
-  </div>
-)
+import AddAdmins from './AddAdmins'
+import AdminTable from './AdminTable'
 
 function ManageAdminCard() {
+  const { provider, owner } = useParams()
+
+  const { mutate } = useUpdateUser({
+    provider,
+    owner,
+    params: { isAdmin: ApiFilterEnum.true },
+  })
+
+  function setAdminStatus(user, isAdmin) {
+    mutate({ targetUserOwnerid: user.ownerid, isAdmin: isAdmin })
+  }
+
   return (
-    <Card className="flex flex-col gap-2 p-8">
-      <h2 className="text-lg font-semibold">Account administrators</h2>
-      <p>
-        Admins are able to: Add other admins, activate deactivate other users,
-        view billing and modify the team yaml.
-      </p>
-      <Suspense fallback={<Loader />}>
-        <AdminList />
-      </Suspense>
-    </Card>
+    <div className="flex flex-col gap-4">
+      <div>
+        <h2 className="text-lg font-semibold">Account administrators</h2>
+        <p>
+          Admins are able to: Add other admins, activate deactivate other users,
+          view billing and modify the team yaml.
+        </p>
+      </div>
+      <hr />
+      <div className="my-4">
+        <AddAdmins setAdminStatus={setAdminStatus} />
+      </div>
+      <div className="max-h-56 overflow-y-auto">
+        <AdminTable />
+      </div>
+    </div>
   )
 }
 
