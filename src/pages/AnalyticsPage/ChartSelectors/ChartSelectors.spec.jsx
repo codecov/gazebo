ac32b68b299a4ef418d700b60dacbbad2dc31451
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-07-04 08:24:07 -0300

    CODE-3469: Fix Repo Remaining Selected on Analytics Page When Switching Contexts (#2102)
    * allow the selected items to be overriden in the multi select
    
    * override the selected items with some local state and reset them if the context is changed
    
    * refactor the chart selectors
    
    * remove passing the owner and just grab it from where its needed

--- src/pages/AnalyticsPage/ChartSelectors/ChartSelectors.spec.jsx
+++ src/pages/AnalyticsPage/ChartSelectors/ChartSelectors.spec.jsx
@@ -70,7 +70,6 @@ describe('ChartSelectors', () => {
     it('renders date picker', async () => {
       render(
         <ChartSelectors
-          owner="bob"
           active={true}
           sortItem={{
             ordering: 'NAME',
@@ -89,7 +88,6 @@ describe('ChartSelectors', () => {
     it('renders multiselect', async () => {
       render(
         <ChartSelectors
-          owner="bob"
           active={true}
           sortItem={{
             ordering: 'NAME',
@@ -108,7 +106,6 @@ describe('ChartSelectors', () => {
     it('renders clear filters', async () => {
       render(
         <ChartSelectors
-          owner="bob"
           active={true}
           sortItem={{
             ordering: 'NAME',
@@ -130,7 +127,6 @@ describe('ChartSelectors', () => {
       const { user } = setup()
       render(
         <ChartSelectors
-          owner="bob"
           active={true}
           sortItem={{
             ordering: 'NAME',
@@ -159,7 +155,6 @@ describe('ChartSelectors', () => {
       const updateParams = jest.fn()
       render(
         <ChartSelectors
-          owner="bob"
           active={true}
           sortItem={{
             ordering: 'NAME',
@@ -191,7 +186,6 @@ describe('ChartSelectors', () => {
       const { user } = setup()
       render(
         <ChartSelectors
-          owner="bob"
           active={true}
           sortItem={{
             ordering: 'NAME',
@@ -218,25 +212,24 @@ describe('ChartSelectors', () => {
         const { user } = setup()
         render(
           <ChartSelectors
-            owner="bob"
             active={true}
             sortItem={{
               ordering: 'NAME',
               direction: 'ASC',
             }}
-            params={{ search: 'Repo name 1', repositories: [] }}
+            params={{ search: 'Repo name 1', repositories: ['Repo name 1'] }}
             updateParams={jest.fn()}
           />,
           { wrapper }
         )
 
-        const multiselect = screen.getByText('All Repos')
+        const multiselect = screen.getByText('1 Repo selected')
         await user.click(multiselect)
 
-        const repo1 = screen.getByText('Repo name 1')
+        const repo1 = screen.getByText('Repo name 3')
         await user.click(repo1)
 
-        const multiSelectUpdated = await screen.findByText('1 Repo selected')
+        const multiSelectUpdated = screen.getByText('2 Repos selected')
         expect(multiSelectUpdated).toBeInTheDocument()
       })
 
@@ -245,7 +238,6 @@ describe('ChartSelectors', () => {
         const updateParams = jest.fn()
         render(
           <ChartSelectors
-            owner="bob"
             active={true}
             sortItem={{
               ordering: 'NAME',
@@ -274,7 +266,6 @@ describe('ChartSelectors', () => {
         const { user } = setup()
         render(
           <ChartSelectors
-            owner="bob"
             active={true}
             sortItem={{
               ordering: 'NAME',
@@ -297,7 +288,6 @@ describe('ChartSelectors', () => {
         const { user } = setup()
         render(
           <ChartSelectors
-            owner="bob"
             active={true}
             sortItem={{
               ordering: 'NAME',
@@ -322,7 +312,7 @@ describe('ChartSelectors', () => {
           expect(useRepos).toBeCalledWith({
             active: true,
             first: Infinity,
-            owner: 'bob',
+            owner: 'codecov',
             sortItem: {
               direction: 'ASC',
               ordering: 'NAME',
@@ -344,7 +334,6 @@ describe('ChartSelectors', () => {
 
           render(
             <ChartSelectors
-              owner="bob"
               active={true}
               sortItem={{
                 ordering: 'NAME',
@@ -369,7 +358,6 @@ describe('ChartSelectors', () => {
 
           render(
             <ChartSelectors
-              owner="bob"
               active={true}
               sortItem={{
                 ordering: 'NAME',
@@ -396,7 +384,6 @@ describe('ChartSelectors', () => {
       const updateParams = jest.fn()
       render(
         <ChartSelectors
-          owner="bob"
           active={true}
           sortItem={{
             ordering: 'NAME',
