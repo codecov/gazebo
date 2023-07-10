61329f6dc514b55d0efcd349371b77a7d38e5a21
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-07-10 14:08:23 -0300

    add disclaimer to ts-expect-error

--- src/pages/PullRequestPage/Header/PendoLink.tsx
+++ src/pages/PullRequestPage/Header/PendoLink.tsx
@@ -12,6 +12,7 @@ const PendoLink: React.FC = () => {
     <div className="flex items-end">
       {/* Need this rule here as the href will be handled by pendo */}
       {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+      {/* This ts expect error is required because of weird types on the A */}
       {/* @ts-expect-error */}
       <A
         id="pendo-modal-anchor-link"
