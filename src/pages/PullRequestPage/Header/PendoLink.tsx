760e027a48ad3ed15b28a1d0616c293c7d7cde0f
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-07-12 15:56:04 -0300

    CODE-3597: Add Anchor Link to PR Page Header for Pendo Modal (#2126)
    * create pendo link component for the PR page header
    
    * add pendo link to PR Page header
    
    * mock out flag for pendo link
    
    * add disclaimer to ts-expect-error

---
+++ src/pages/PullRequestPage/Header/PendoLink.tsx
@@ -0,0 +1,29 @@
+import { useFlags } from 'shared/featureFlags'
+import A from 'ui/A'
+
+const PendoLink: React.FC = () => {
+  const { pendoModalPrPage } = useFlags({ pendoModalPrPage: false })
+
+  if (!pendoModalPrPage) {
+    return null
+  }
+
+  return (
+    <div className="flex items-end">
+      {/* Need this rule here as the href will be handled by pendo */}
+      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+      {/* This ts expect error is required because of weird types on the A */}
+      {/* @ts-expect-error */}
+      <A
+        id="pendo-modal-anchor-link"
+        hook="pendo-pr-page"
+        href=""
+        variant="black"
+      >
+        Does this report look accurate?
+      </A>
+    </div>
+  )
+}
+
+export default PendoLink
