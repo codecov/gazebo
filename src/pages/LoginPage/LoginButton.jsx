1cfa8df117abbd4ec658a05d1833339444a3379f
Rula Abu Hasna <rula@codecov.io>
2023-07-17 13:34:12 +0300

    Remove public vs private login

--- src/pages/LoginPage/LoginButton.jsx
+++ src/pages/LoginPage/LoginButton.jsx
@@ -1,31 +1,14 @@
-import cs from 'classnames'
 import PropTypes from 'prop-types'
-import { useRef, useState } from 'react'
-import useClickAway from 'react-use/lib/useClickAway'
 
 import { useNavLinks } from 'services/navigation'
 import { providerImage, providerToName } from 'shared/utils/provider'
-import Icon from 'ui/Icon'
-
-function useCloseOnLooseFocus({ setToggle }) {
-  const ref = useRef(null)
-  useClickAway(ref, () => setToggle((toggle) => (!toggle ? toggle : false)))
-
-  return ref
-}
 
 function LoginButton({ provider }) {
-  const [toggle, setToggle] = useState(false)
   const { signIn } = useNavLinks()
   const to = `${window.location.protocol}//${window.location.host}/${provider}`
 
-  const ref = useCloseOnLooseFocus({ setToggle })
-
   return (
-    <div
-      ref={ref}
-      className="flex h-14 items-center rounded-sm border border-ds-gray-quaternary bg-ds-gray-primary text-left shadow"
-    >
+    <div className="flex h-14 items-center rounded-sm border border-ds-gray-quaternary bg-ds-gray-primary text-left shadow">
       <a
         className="flex h-full grow items-center font-semibold hover:bg-ds-gray-secondary"
         href={signIn.path({ to, provider, privateScope: true })}
@@ -38,41 +21,6 @@ function LoginButton({ provider }) {
         />
         Login with {providerToName(provider)}
       </a>
-      {provider === 'gh' && (
-        <div id="scope-dropdown" className="relative h-full">
-          <button
-            className="flex h-full w-12 items-center justify-center border-l border-ds-gray-quaternary hover:bg-ds-gray-secondary"
-            aria-haspopup="listbox"
-            aria-expanded={toggle}
-            onClick={() => setToggle((toggle) => !toggle)}
-          >
-            <Icon name="chevron-down" />
-          </button>
-          <ul
-            className={cs(
-              ' min-w-[10rem] right-0 bg-ds-gray-primary border border-ds-gray-quaternary rounded-sm absolute z-10  shadow-md ring-1 ring-black ring-opacity-5 focus:outline-none',
-              { hidden: !toggle }
-            )}
-          >
-            <li>
-              <a
-                className="block px-4 pb-1 pt-2 hover:bg-ds-gray-secondary hover:text-ds-gray-octonary"
-                href={signIn.path({ to, provider, privateScope: true })}
-              >
-                All repos
-              </a>
-            </li>
-            <li>
-              <a
-                className="block px-4 pb-2 pt-1 hover:bg-ds-gray-secondary hover:text-ds-gray-octonary"
-                href={signIn.path({ to, provider })}
-              >
-                Public repos only
-              </a>
-            </li>
-          </ul>
-        </div>
-      )}
     </div>
   )
 }
--- src/pages/LoginPage/LoginButton.spec.jsx
+++ src/pages/LoginPage/LoginButton.spec.jsx
@@ -1,5 +1,4 @@
-import { act, render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import { render, screen } from '@testing-library/react'
 import { MemoryRouter, Route, Switch } from 'react-router-dom'
 
 import LoginButton from './LoginButton'
@@ -26,38 +25,10 @@ describe('LoginButton', () => {
     expect(bitbucket).toBeInTheDocument()
   })
   it('GitHub: renders the login button', async () => {
-    const user = userEvent.setup()
     render(<LoginButton provider="gh" />, { wrapper: wrapper() })
 
     const github = screen.getByText(/Login with GitHub/i)
     expect(github).toBeInTheDocument()
-
-    let closedMenu = screen.getByRole('button', { expanded: false })
-    expect(closedMenu).toBeInTheDocument()
-
-    await user.click(closedMenu)
-
-    const openMenu = screen.getByRole('button', { expanded: true })
-    expect(openMenu).toBeInTheDocument()
-
-    const allRepos = screen.getByText('All repos')
-    expect(allRepos).toBeInTheDocument()
-    const publicReposOnly = screen.getByText('Public repos only')
-    expect(publicReposOnly).toBeInTheDocument()
-
-    const clickAway = screen.getByText('Click away')
-    expect(clickAway).toBeInTheDocument()
-
-    await act(async () => await user.click(clickAway))
-
-    closedMenu = screen.getByRole('button', { expanded: false })
-    expect(openMenu).toBeInTheDocument()
-
-    // Click away only works when open
-    await act(async () => await user.click(clickAway))
-
-    closedMenu = screen.getByRole('button', { expanded: false })
-    expect(openMenu).toBeInTheDocument()
   })
   it('GitLab: renders the login button', () => {
     render(<LoginButton provider="gl" />, { wrapper: wrapper() })
