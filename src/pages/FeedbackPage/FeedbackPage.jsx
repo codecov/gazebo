ccd3a82ad39a68662cea1f30e2cebd645046fe82
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-07-21 08:51:38 -0300

    revert: Remediation of Leaked Secrets in Gazebo (#2145)
    This reverts commit f330f7bf67856fa88e68554e191825b2388c562f.
    
    reverts:  build: Remediation of Leaked Secrets in Gazebo (#2113)

--- src/pages/FeedbackPage/FeedbackPage.jsx
+++ src/pages/FeedbackPage/FeedbackPage.jsx
@@ -1,13 +1,11 @@
 import { useParams } from 'react-router-dom'
 
-import config from 'config'
-
 import ErrorBoundary from 'layouts/shared/ErrorBoundary'
 import { useUser } from 'services/user'
 
 import CannyWidget from './Canny/CannyWidget'
 
-const BOARD_TOKEN = config.CANNY_BOARD_TOKEN
+const BOARD_TOKEN = '0acac6b2-1912-bfa8-cc54-0dac794f7375'
 
 function FeedbackPage() {
   const { data: user } = useUser()
