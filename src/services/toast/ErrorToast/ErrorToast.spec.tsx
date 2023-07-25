cb0ba671c15ff2b27fbb76c98b5fc17010b7bf77
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-07-25 15:46:32 -0300

    feat: Create Mutation to Handle Trial Start (#2149)
    Add in a new service that triggers a mutation that will start a users trial and invalidate plan related query caches.
    
    Closes CODE-3625

---
+++ src/services/toast/ErrorToast/ErrorToast.spec.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react'
+
+import ErrorToast from './ErrorToast'
+
+describe('ErrorToast', () => {
+  it('renders the title', () => {
+    render(<ErrorToast title="Error Title" content="Error Content" />)
+
+    const title = screen.getByRole('heading', { name: /Error Title/ })
+    expect(title).toBeInTheDocument()
+    expect(title).toHaveClass('font-semibold')
+  })
+
+  it('renders the content', () => {
+    render(<ErrorToast title="Error Title" content="Error Content" />)
+
+    const content = screen.getByText(/Error Content/)
+    expect(content).toBeInTheDocument()
+  })
+})
---
+++ src/services/trial/useStartTrial.ts
@@ -0,0 +1,102 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useParams } from 'react-router-dom'
+import { z } from 'zod'
+
+import { renderToast } from 'services/toast'
+import Api from 'shared/api'
+
+const StartTrialSchema = z.object({
+  startTrial: z
+    .object({
+      error: z
+        .object({
+          message: z.string(),
+        })
+        .nullable(),
+    })
+    .nullable(),
+})
+
+const mutationQuery = `
+  mutation startTrial(
+    $input: StartTrialInput!
+  ) {
+    startTrial(
+      input: $input
+    ) {
+      error {
+        ... on UnauthenticatedError {
+          message
+        }  
+        ... on ValidationError {
+          message
+        }
+      }
+    }
+  }
+`
+
+interface Params {
+  provider: string
+}
+
+interface UseStartTrialArgs {
+  owner: string
+}
+
+// need to take owner as an arg here because
+// onboarding won't have it in the url
+export const useStartTrial = ({ owner }: UseStartTrialArgs) => {
+  const { provider } = useParams<Params>()
+  const queryClient = useQueryClient()
+
+  const mutation = useMutation({
+    mutationFn: () => {
+      const variables = {
+        input: { orgUsername: owner },
+      }
+
+      return Api.graphqlMutation({
+        provider,
+        query: mutationQuery,
+        variables,
+        mutationPath: 'startTrial',
+      })
+    },
+    onSuccess: ({ data }) => {
+      const parsedData = StartTrialSchema.parse(data)
+
+      const error = parsedData.startTrial?.error
+
+      if (error) {
+        throw new Error(error.message)
+      }
+
+      queryClient.invalidateQueries(['accountDetails'])
+      queryClient.invalidateQueries(['GetPlanData'])
+
+      renderToast({
+        type: 'generic',
+        title: '14 day trial has started',
+        content: '',
+        options: {
+          duration: 5000,
+          position: 'bottom-left',
+        },
+      })
+    },
+    onError: () => {
+      renderToast({
+        type: 'error',
+        title: 'Error starting trial',
+        content:
+          'Please try again. If the error persists please contact support',
+        options: {
+          duration: 10000,
+        },
+      })
+    },
+  })
+
+  return mutation
+}
