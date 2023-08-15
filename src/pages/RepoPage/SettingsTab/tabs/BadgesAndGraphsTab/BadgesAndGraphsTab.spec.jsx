b190950d81b58293e96c5ccd73dd8b067b02f9b4
Rula Abu Hasna <rula@codecov.io>
2023-08-15 15:48:50 +0300

    update badges and graphs spec

--- src/pages/RepoPage/SettingsTab/tabs/BadgesAndGraphsTab/BadgesAndGraphsTab.spec.jsx
+++ src/pages/RepoPage/SettingsTab/tabs/BadgesAndGraphsTab/BadgesAndGraphsTab.spec.jsx
@@ -1,62 +1,84 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { render, screen } from '@testing-library/react'
+import { graphql } from 'msw'
+import { setupServer } from 'msw/node'
 import { MemoryRouter, Route } from 'react-router-dom'
-
-import { useRepoSettings } from 'services/repo'
 
 import BadgesAndGraphsTab from './BadgesAndGraphsTab'
 
-jest.mock('services/repo')
 const queryClient = new QueryClient({
   defaultOptions: { queries: { retry: false } },
 })
 
+const server = setupServer()
+
+const wrapper = ({ children }) => (
+  <MemoryRouter initialEntries={['/gh/codecov/codecov-client/settings']}>
+    <QueryClientProvider client={queryClient}>
+      <Route path="/:provider/:owner/:repo/settings">{children}</Route>
+    </QueryClientProvider>
+  </MemoryRouter>
+)
+
+beforeAll(() => {
+  server.listen()
+  console.error = () => {}
+})
+afterEach(() => {
+  queryClient.clear()
+  server.resetHandlers()
+})
+afterAll(() => server.close())
+
 describe('BadgesAndGraphsTab', () => {
   function setup({ graphToken }) {
-    useRepoSettings.mockReturnValue({
-      data: {
-        repository: { graphToken },
-      },
-    })
-
-    render(
-      <MemoryRouter initialEntries={['/gh/codecov/codecov-client/settings']}>
-        <QueryClientProvider client={queryClient}>
-          <Route path="/:provider/:owner/:repo/settings">
-            <BadgesAndGraphsTab />
-          </Route>
-        </QueryClientProvider>
-      </MemoryRouter>
+    server.use(
+      graphql.query('GetRepoSettings', (req, res, ctx) => {
+        return res(
+          ctx.status(200),
+          ctx.data({
+            owner: {
+              repository: {
+                graphToken: graphToken,
+              },
+            },
+          })
+        )
+      })
     )
   }
 
   describe('when rendered with graphToken', () => {
-    beforeEach(() => {
+    it('renders badges component', async () => {
       setup({ graphToken: 'WIO9JXFGE3' })
-    })
+      render(<BadgesAndGraphsTab />, { wrapper })
 
-    it('renders badges component', () => {
-      const title = screen.getByText(/Codecov badge/)
+      const title = await screen.findByText(/Codecov badge/)
       expect(title).toBeInTheDocument()
     })
 
-    it('renders graphs component', () => {
-      const title = screen.getByText(/Graphs/)
+    it('renders graphs component', async () => {
+      setup({ graphToken: 'WIO9JXFGE3' })
+      render(<BadgesAndGraphsTab />, { wrapper })
+
+      const title = await screen.findByText(/Graphs/)
       expect(title).toBeInTheDocument()
     })
   })
 
   describe('when rendered with no graphToken', () => {
-    beforeEach(() => {
-      setup({ graphToken: null })
-    })
-
     it('does not render badges component', () => {
+      setup({ graphToken: null })
+      render(<BadgesAndGraphsTab />, { wrapper })
+
       const title = screen.queryByText(/Codecov badge/)
       expect(title).not.toBeInTheDocument()
     })
 
     it('does not render graphs component', () => {
+      setup({ graphToken: null })
+      render(<BadgesAndGraphsTab />, { wrapper })
+
       const title = screen.queryByText(/Graphs/)
       expect(title).not.toBeInTheDocument()
     })
