5d7bc0d3ff558d9858f8512f4dc02d2ab1ff908c
Rola Abuhasna <91732700+RulaKhaled@users.noreply.github.com>
2023-09-25 19:54:52 +0200

    Delete src/services/comparison/schemas/FirstPullRequst.ts

--- src/services/comparison/schemas/FirstPullRequst.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod'
-
-export const FirstPullRequestSchema = z.object({
-  __typename: z.literal('FirstPullRequest'),
-  message: z.string(),
-})
-
-export type FirstPullRequest = z.infer<typeof FirstPullRequestSchema>
