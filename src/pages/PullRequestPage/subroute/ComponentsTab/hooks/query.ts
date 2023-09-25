c391e1f920e6388015fc9d235c2ce3a0c186fab7
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-09-25 08:34:11 -0300

    update useComponentComparison to handle first pull request schema

--- src/pages/PullRequestPage/subroute/ComponentsTab/hooks/query.ts
+++ src/pages/PullRequestPage/subroute/ComponentsTab/hooks/query.ts
@@ -36,6 +36,9 @@ export const query = `
               ... on MissingHeadReport {
                 message
               }
+              ... on FirstPullRequest {
+                message
+              }
             }
           }
         }
