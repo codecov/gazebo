f37c37199091cfb4683708ddcf9c912e0cfb75d7
Adrian <adrian@codecov.io>
2023-09-28 11:59:07 -0700

    fix: fix nullish for nullable

--- src/services/tier/useTier.ts
+++ src/services/tier/useTier.ts
@@ -18,7 +18,7 @@ export const TierSchema = z
           .object({
             tierName: z.nativeEnum(TierNames),
           })
-          .nullish(),
+          .nullable(),
       })
       .nullable(),
   })
