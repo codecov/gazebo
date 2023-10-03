f52983d40f03d8cbbeb680f844e6d539b24c5dfd
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-10-03 09:51:59 -0300

    update useNavLinks to support updated commit page links

--- src/services/navigation/useNavLinks/useNavLinks.js
+++ src/services/navigation/useNavLinks/useNavLinks.js
@@ -155,12 +155,20 @@ export function useNavLinks() {
     },
     commit: {
       path: (
-        { provider = p, owner = o, repo = r, commit } = {
+        { provider = p, owner = o, repo = r, commit, ...queryParams } = {
           provider: p,
           owner: o,
           repo: r,
+          queryParams: {},
         }
-      ) => `/${provider}/${owner}/${repo}/commit/${commit}`,
+      ) => {
+        let query = ''
+        if (Object.keys(queryParams).length > 0) {
+          query = qs.stringify(queryParams, { addQueryPrefix: true })
+        }
+
+        return `/${provider}/${owner}/${repo}/commit/${commit}${query}`
+      },
       isExternalLink: false,
       text: 'Commit',
     },
@@ -229,29 +237,41 @@ export function useNavLinks() {
     },
     commitTreeView: {
       path: (
-        { provider = p, owner = o, repo = r, tree, commit } = {
+        { provider = p, owner = o, repo = r, tree, commit, ...queryParams } = {
           provider: p,
           owner: o,
           repo: r,
+          queryParams: {},
         }
       ) => {
-        if (tree) {
-          return `/${provider}/${owner}/${repo}/commit/${commit}/tree/${tree}`
+        let query = ''
+        if (Object.keys(queryParams).length > 0) {
+          query = qs.stringify(queryParams, { addQueryPrefix: true })
         }
-        return `/${provider}/${owner}/${repo}/commit/${commit}/tree`
+
+        if (tree) {
+          return `/${provider}/${owner}/${repo}/commit/${commit}/tree/${tree}${query}`
+        }
+        return `/${provider}/${owner}/${repo}/commit/${commit}/tree${query}`
       },
       isExternalLink: false,
       text: 'Commit Tree View',
     },
     commitFileDiff: {
       path: (
-        { provider = p, owner = o, repo = r, tree, commit } = {
+        { provider = p, owner = o, repo = r, tree, commit, ...queryParams } = {
           provider: p,
           owner: o,
           repo: r,
+          queryParams: {},
         }
       ) => {
-        return `/${provider}/${owner}/${repo}/commit/${commit}/blob/${tree}`
+        let query = ''
+        if (Object.keys(queryParams).length > 0) {
+          query = qs.stringify(queryParams, { addQueryPrefix: true })
+        }
+
+        return `/${provider}/${owner}/${repo}/commit/${commit}/blob/${tree}${query}`
       },
       isExternalLink: false,
       text: 'Commit File View',
@@ -430,12 +450,20 @@ export function useNavLinks() {
     commitIndirectChanges: {
       text: 'Indirect changes',
       path: (
-        { provider = p, owner = o, repo = r, commit } = {
+        { provider = p, owner = o, repo = r, commit, ...queryParams } = {
           provider: p,
           owner: o,
           repo: r,
+          queryParams: {},
         }
-      ) => `/${provider}/${owner}/${repo}/commit/${commit}/indirect-changes`,
+      ) => {
+        let query = ''
+        if (Object.keys(queryParams).length > 0) {
+          query = qs.stringify(queryParams, { addQueryPrefix: true })
+        }
+
+        return `/${provider}/${owner}/${repo}/commit/${commit}/indirect-changes${query}`
+      },
       isExternalLink: false,
     },
     pullCommits: {
