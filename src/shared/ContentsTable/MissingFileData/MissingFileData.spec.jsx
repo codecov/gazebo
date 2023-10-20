2901403cce9ed73b7ab8e97cb7e403d74315681d
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-10-20 13:09:03 -0300

    feat: Update Commit Detail Page File Explorer Tab to Grab Selected Flags (#2326)
    Update the the file explorer on the commit detail page to fetch the flags from params and pass along to the api request.
    
    GH codecov/engineering-team#345

--- src/shared/ContentsTable/MissingFileData/MissingFileData.spec.jsx
+++ src/shared/ContentsTable/MissingFileData/MissingFileData.spec.jsx
@@ -3,6 +3,13 @@ import { render, screen } from '@testing-library/react'
 import MissingFileData from './MissingFileData'
 
 describe('MissingFileData', () => {
+  it('displays problem fetching contents', () => {
+    render(<MissingFileData />)
+
+    const problem = screen.getByText(/problem getting repo contents/)
+    expect(problem).toBeInTheDocument()
+  })
+
   describe('when isSearching is true', () => {
     it('displays no results found', () => {
       render(<MissingFileData isSearching={true} />)
@@ -12,12 +19,14 @@ describe('MissingFileData', () => {
     })
   })
 
-  describe('when isSearching is false', () => {
-    it('displays problem fetching contents', () => {
-      render(<MissingFileData isSearching={false} />)
+  describe('when hasFlagsSelected is true', () => {
+    it('displays no results with selected flags found', () => {
+      render(<MissingFileData hasFlagsSelected={true} />)
 
-      const problem = screen.getByText(/problem getting repo contents/)
-      expect(problem).toBeInTheDocument()
+      const noResults = screen.getByText(
+        "No coverage report uploaded for the selected flags in this branch's head commit"
+      )
+      expect(noResults).toBeInTheDocument()
     })
   })
 })
