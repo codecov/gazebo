b9464ca5f04527d2ae29b7a0b93bbede8b9f337e
Rohit <rohit.vinnakota@sentry.io>
2023-10-23 15:09:10 -0400

    Migrate textinput to TS

---
+++ src/ui/TextInput/TextInput.stories.tsx
@@ -0,0 +1,37 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import TextInput from './TextInput'
+
+export default {
+  title: 'Components/TextInput',
+  component: TextInput,
+} as Meta
+
+type Story = StoryObj<typeof TextInput>
+
+export const TextInputWithLabel: Story = {
+  args: {
+    label: 'Name',
+    placeholder: 'Write your name',
+  },
+  render: (args) => {
+    return <TextInput {...args} />
+  },
+}
+
+export const TextInputWithPlaceholder: Story = {
+  args: {
+    placeholder: 'Type your age',
+    type: 'number',
+  },
+  render: (args) => {
+    return <TextInput {...args} />
+  },
+}
+
+export const TextInputWithIcon: Story = {
+  args: {
+    icon: 'search',
+    placeholder: 'Search',
+  },
+}
