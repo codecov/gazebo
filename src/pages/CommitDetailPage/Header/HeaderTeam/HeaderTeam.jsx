c671f02700b792061a37d6ce2df1dfbec16fb419
Adrian <adrian@codecov.io>
2023-10-24 10:52:11 -0700

    feat: add patch coverage section to commit detail page for team tier customer

---
+++ src/pages/CommitDetailPage/Header/HeaderTeam/HeaderTeam.jsx
@@ -0,0 +1,103 @@
+import { useParams } from 'react-router-dom'
+
+import { getProviderCommitURL, getProviderPullURL } from 'shared/utils'
+import { formatTimeToNow } from 'shared/utils/dates'
+import A from 'ui/A'
+import CIStatusLabel from 'ui/CIStatus'
+import Icon from 'ui/Icon'
+import TotalsNumber from 'ui/TotalsNumber'
+import TruncatedMessage from 'ui/TruncatedMessage/TruncatedMessage'
+
+import { useCommitHeaderDataTeam } from './hooks'
+
+import PullLabel from '../PullLabel'
+
+function HeaderDefault() {
+  const { provider, owner, repo, commit: commitSha } = useParams()
+  const shortSHA = commitSha?.slice(0, 7)
+
+  const { data: headerData } = useCommitHeaderDataTeam({
+    provider,
+    owner,
+    repo,
+    commitId: commitSha,
+  })
+  const commit = headerData?.commit
+
+  const providerPullUrl = getProviderPullURL({
+    provider,
+    owner,
+    repo,
+    pullId: commit?.pullId,
+  })
+
+  const providerCommitUrl = getProviderCommitURL({
+    provider,
+    owner,
+    repo,
+    commit: commitSha,
+  })
+
+  return (
+    <div className="flex flex-col justify-between gap-2 border-b border-ds-gray-secondary pb-2 text-xs md:flex-row">
+      <div className="flex flex-row flex-wrap items-center gap-6 divide-x divide-ds-gray-secondary">
+        <div>
+          {commit?.message && (
+            <TruncatedMessage>{commit?.message}</TruncatedMessage>
+          )}
+          <div className="flex items-center gap-2 text-ds-gray-quinary">
+            <div>
+              {commit?.createdAt && (
+                <span className="font-light">
+                  {formatTimeToNow(commit?.createdAt)}
+                </span>
+              )}{' '}
+              {commit?.author?.username && (
+                <A
+                  to={{
+                    pageName: 'owner',
+                    options: { owner: commit?.author?.username },
+                  }}
+                >
+                  {commit?.author?.username}
+                </A>
+              )}{' '}
+              <span className="font-light">authored commit</span>{' '}
+              <A
+                variant="code"
+                href={providerCommitUrl}
+                hook="provider commit url"
+                isExternal={true}
+              >
+                {shortSHA}
+              </A>
+            </div>
+            <CIStatusLabel ciPassed={commit?.ciPassed} />
+            <span className="flex flex-none items-center">
+              <Icon name="branch" variant="developer" size="sm" />
+              {commit?.branchName}
+            </span>
+            <PullLabel
+              pullId={commit?.pullId}
+              provider={provider}
+              providerPullUrl={providerPullUrl}
+            />
+          </div>
+        </div>
+        <div className="flex flex-col justify-center gap-2 px-6">
+          <h4 className="gap-2 font-mono text-xs text-ds-gray-quinary">
+            Patch Coverage
+          </h4>
+          <TotalsNumber
+            value={commit?.compareWithParent?.patchTotals?.percentCovered}
+            plain
+            large
+          />
+        </div>
+      </div>
+      <hr />
+    </div>
+  )
+}
+
+export default HeaderDefault
