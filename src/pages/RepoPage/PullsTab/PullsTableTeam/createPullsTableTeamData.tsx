99d82d6aa16eb3d191983f55ba27819d6d8b4ff3
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-10-30 10:34:43 -0300

    create function to compile table data

---
+++ src/pages/RepoPage/PullsTab/PullsTableTeam/createPullsTableTeamData.tsx
@@ -0,0 +1,62 @@
+import isArray from 'lodash/isArray'
+import isEmpty from 'lodash/isEmpty'
+
+import { Pull } from 'services/pulls/usePullsTeam'
+import TotalsNumber from 'ui/TotalsNumber'
+
+import Title from '../shared/Title'
+
+export const createPullsTableTeamData = ({
+  pages,
+}: {
+  pages?: Array<{ pulls: Array<Pull> }>
+}) => {
+  // only map through if there's actual pulls to map
+  let pulls: Array<Pull> = []
+  if (isArray(pages) && !isEmpty(pages)) {
+    pulls = pages?.map((pull) => pull?.pulls).flat()
+  }
+
+  if (!isEmpty(pulls)) {
+    return pulls.filter(Boolean).map((pull) => {
+      let patch = <p className="text-right">No report uploaded</p>
+      if (pull?.compareWithBase?.__typename === 'Comparison') {
+        const patchPercentage =
+          pull?.compareWithBase?.patchTotals?.percentCovered ?? 0
+        patch = (
+          <div className="text-right">
+            <TotalsNumber
+              plain={true}
+              large={false}
+              light={false}
+              value={patchPercentage}
+              showChange={false}
+            />
+          </div>
+        )
+      }
+
+      const updatestamp = pull?.updatestamp ?? undefined
+      const title = pull?.title ?? 'Pull Request'
+      const pullId = pull?.pullId ?? NaN
+
+      return {
+        title: (
+          <Title
+            author={{
+              username: pull?.author?.username,
+              avatarUrl: pull?.author?.avatarUrl,
+            }}
+            pullId={pullId}
+            title={title}
+            updatestamp={updatestamp}
+            compareWithBaseType={pull?.compareWithBase?.__typename}
+          />
+        ),
+        patch,
+      }
+    })
+  }
+
+  return []
+}
