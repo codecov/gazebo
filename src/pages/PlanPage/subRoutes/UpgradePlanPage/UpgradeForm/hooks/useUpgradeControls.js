4ac7fd67d8523998c1b609a8cf267f84301592a4
Adrian <adrian@codecov.io>
2023-11-04 21:31:18 -0700

    delete unecessary comments

--- src/pages/PlanPage/subRoutes/UpgradePlanPage/UpgradeForm/hooks/useUpgradeControls.js
+++ src/pages/PlanPage/subRoutes/UpgradePlanPage/UpgradeForm/hooks/useUpgradeControls.js
@@ -36,65 +36,6 @@ export const useUpgradeControls = () => {
   }
 
   return {
-    // perYearPrice,
-    // perMonthPrice,
-    // register,
-    // handleSubmit,
-    // // isPerYear,
-    // // formState,
-    // setValue,
-    // getValues,
-    // // reset,
-    // watch,
     upgradePlan,
-    // ...rest,
   }
-
-  //   const {
-  //     register,
-  //     handleSubmit,
-  //     watch,
-  //     formState,
-  //     setValue,
-  //     getValues,
-  //     reset,
-  //   } = useForm({
-  //     defaultValues: getInitialDataForm({
-  //       accountDetails,
-  //       proPlanYear,
-  //       // isSentryUpgrade,
-  //       // sentryPlanYear,
-  //       // trialStatus: planData?.plan?.trialStatus,
-  //     }),
-  //     resolver: zodResolver(
-  //       getSchema({
-  //         accountDetails,
-  //         minSeats: MIN_NB_SEATS_PRO,
-  //         // trialStatus: planData?.plan?.trialStatus,
-  //       })
-  //     ),
-  //     mode: 'onChange',
-  //   })
-
-  // const perYearPrice = calculatePrice({
-  //   seats: watch('seats'),
-  //   baseUnitPrice: proPlanYear?.baseUnitPrice,
-  //   // baseUnitPrice: isSentryUpgrade
-  //   //   ? sentryPlanYear?.baseUnitPrice
-  //   //   : proPlanYear?.baseUnitPrice,
-
-  //   // isSentryUpgrade,
-  //   // sentryPrice,
-  // })
-
-  // const perMonthPrice = calculatePrice({
-  //   seats: watch('seats'),
-  //   baseUnitPrice: proPlanMonth?.baseUnitPrice,
-  //   // baseUnitPrice: isSentryUpgrade
-  //   //   ? sentryPlanYear?.baseUnitPrice
-  //   //   : proPlanMonth?.baseUnitPrice,
-
-  //   // isSentryUpgrade,
-  //   // sentryPrice,
-  // })
 }
