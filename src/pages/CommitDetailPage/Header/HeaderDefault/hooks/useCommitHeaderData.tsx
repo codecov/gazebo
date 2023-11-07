4b6bda421129d9c95a8c38b96a0081037a1790d7
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-11-07 09:57:12 -0400

    update zod schema for useCommitHeaderData

--- src/pages/CommitDetailPage/Header/HeaderDefault/hooks/useCommitHeaderData.tsx
+++ src/pages/CommitDetailPage/Header/HeaderDefault/hooks/useCommitHeaderData.tsx
@@ -19,10 +19,10 @@ const RepositorySchema = z.object({
         .nullable(),
       branchName: z.string().nullable(),
       ciPassed: z.boolean().nullable(),
-      commitid: z.string().nullable(),
-      createdAt: z.string().nullable(),
+      commitid: z.string(),
+      createdAt: z.string(),
       message: z.string().nullable(),
-      pullId: z.number().nullable(),
+      pullId: z.number(),
     })
     .nullable(),
 })
