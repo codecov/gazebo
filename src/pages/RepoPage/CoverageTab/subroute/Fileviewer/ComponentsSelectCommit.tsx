4a24df2dd4560b9b09f103bfef208f7b41820fd2
Rohit <rohit.vinnakota@sentry.io>
2023-12-12 12:33:48 -0500

    update tests

--- src/pages/RepoPage/CoverageTab/subroute/Fileviewer/ComponentsSelectCommit.tsx
+++ src/pages/RepoPage/CoverageTab/subroute/Fileviewer/ComponentsSelectCommit.tsx
@@ -32,7 +32,7 @@ export function ComponentsSelectCommit() {
     params?.components
   )
   const { componentsSelect: componentsSelectFlag } = useFlags({
-    componentsSelect: true,
+    componentsSelect: false,
   })
 
   const { data, isLoading } = useBranchComponents({
