44942c3f2d932270a27819ad7ff9b8a8f3b855e4
Rohit <rohit.vinnakota@sentry.io>
2024-01-02 17:45:27 -0500

    remove banner from global banners

--- src/shared/GlobalTopBanners/GlobalTopBanners.tsx
+++ src/shared/GlobalTopBanners/GlobalTopBanners.tsx
@@ -4,14 +4,12 @@ import SilentNetworkErrorWrapper from 'layouts/shared/SilentNetworkErrorWrapper'
 
 const RequestInstallBanner = lazy(() => import('./RequestInstallBanner'))
 const TrialBanner = lazy(() => import('./TrialBanner'))
-const AppInstallBanner = lazy(() => import('./AppInstallBanner'))
 
 const GlobalTopBanners: React.FC = () => {
   return (
     <SilentNetworkErrorWrapper>
       <RequestInstallBanner />
       <TrialBanner />
-      <AppInstallBanner />
     </SilentNetworkErrorWrapper>
   )
 }
