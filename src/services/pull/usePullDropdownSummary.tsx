a3132b973441624302697aa80add25d922f2e4fc
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-01-08 15:38:14 -0400

    create new usePullDropdownSummary hook

---
+++ src/services/pull/usePullDropdownSummary.tsx
@@ -0,0 +1,167 @@
+import { useQuery } from '@tanstack/react-query'
+import { z } from 'zod'
+
+import {
+  FirstPullRequestSchema,
+  MissingBaseCommitSchema,
+  MissingBaseReportSchema,
+  MissingComparisonSchema,
+  MissingHeadCommitSchema,
+  MissingHeadReportSchema,
+} from 'services/comparison/schemas'
+import {
+  RepoNotFoundErrorSchema,
+  RepoOwnerNotActivatedErrorSchema,
+} from 'services/repo/schemas'
+import Api from 'shared/api'
+import A from 'ui/A'
+
+const ComparisonSchema = z.object({
+  __typename: z.literal('Comparison'),
+  patchTotals: z
+    .object({
+      missesCount: z.number().nullable(),
+      partialsCount: z.number().nullable(),
+    })
+    .nullable(),
+})
+
+const CompareWithBaseSchema = z
+  .discriminatedUnion('__typename', [
+    ComparisonSchema,
+    FirstPullRequestSchema,
+    MissingBaseCommitSchema,
+    MissingBaseReportSchema,
+    MissingComparisonSchema,
+    MissingHeadCommitSchema,
+    MissingHeadReportSchema,
+  ])
+  .nullable()
+
+const RepositorySchema = z.object({
+  __typename: z.literal('Repository'),
+  pull: z
+    .object({
+      compareWithBase: CompareWithBaseSchema.nullable(),
+    })
+    .nullable(),
+})
+
+const RequestSchema = z.object({
+  owner: z
+    .object({
+      repository: z.discriminatedUnion('__typename', [
+        RepositorySchema,
+        RepoNotFoundErrorSchema,
+        RepoOwnerNotActivatedErrorSchema,
+      ]),
+    })
+    .nullable(),
+})
+
+const query = `
+query PullDropdownSummary($owner: String!, $repo: String!, $pullId: Int!) {
+  owner(username: $owner) {
+    repository(name: $repo) {
+      __typename
+      ... on Repository {
+        pull(id: $pullId) {
+          compareWithBase {
+            __typename
+            ... on Comparison {
+              patchTotals {
+                missesCount
+                partialsCount
+              }
+            }
+            ... on FirstPullRequest {
+              message
+            }
+            ... on MissingBaseCommit {
+              message
+            }
+            ... on MissingHeadCommit {
+              message
+            }
+            ... on MissingComparison {
+              message
+            }
+            ... on MissingBaseReport {
+              message
+            }
+            ... on MissingHeadReport {
+              message
+            }
+          }
+        }
+      }
+      ... on NotFoundError {
+        message
+      }
+      ... on OwnerNotActivatedError {
+        message
+      }
+    }
+  }
+}`
+
+interface UsePullDropdownSummaryArgs {
+  provider: string
+  owner: string
+  repo: string
+  pullId: number
+}
+
+export function usePullDropdownSummary({
+  provider,
+  owner,
+  repo,
+  pullId,
+}: UsePullDropdownSummaryArgs) {
+  return useQuery({
+    queryKey: ['PullDropdownSummary', provider, owner, repo, pullId],
+    queryFn: ({ signal }) =>
+      Api.graphql({
+        provider,
+        query,
+        signal,
+        variables: { owner, repo, pullId },
+      }).then((res) => {
+        const parsedRes = RequestSchema.safeParse(res?.data)
+
+        if (!parsedRes.success) {
+          return Promise.reject({
+            status: 404,
+            data: null,
+          })
+        }
+
+        const data = parsedRes.data
+
+        if (data?.owner?.repository?.__typename === 'NotFoundError') {
+          return Promise.reject({
+            status: 404,
+            data: {},
+          })
+        }
+
+        if (data?.owner?.repository?.__typename === 'OwnerNotActivatedError') {
+          return Promise.reject({
+            status: 403,
+            data: {
+              detail: (
+                <p>
+                  Activation is required to view this repo, please{' '}
+                  {/* @ts-expect-error */}
+                  <A to={{ pageName: 'membersTab' }}>click here </A> to activate
+                  your account.
+                </p>
+              ),
+            },
+          })
+        }
+
+        return data
+      }),
+  })
+}
