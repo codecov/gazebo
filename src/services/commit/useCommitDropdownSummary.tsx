91cab97103c89099c66e0cd92bbd77aca2383e91
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-01-08 11:12:47 -0400

    add hook to fetch summary info for a given commit for an upcoming dropdown component

---
+++ src/services/commit/useCommitDropdownSummary.tsx
@@ -0,0 +1,175 @@
+import { useQuery } from '@tanstack/react-query'
+import { z } from 'zod'
+
+import {
+  FirstPullRequestSchema,
+  MissingBaseCommitSchema,
+  MissingBaseReportSchema,
+  MissingComparisonSchema,
+  MissingHeadCommitSchema,
+  MissingHeadReportSchema,
+} from 'services/comparison/schemas'
+import {
+  RepoNotFoundErrorSchema,
+  RepoOwnerNotActivatedErrorSchema,
+} from 'services/repo/schemas'
+import Api from 'shared/api'
+import A from 'ui/A'
+
+const ComparisonSchema = z.object({
+  __typename: z.literal('Comparison'),
+  patchTotals: z
+    .object({
+      missesCount: z.number().nullable(),
+      partialsCount: z.number().nullable(),
+    })
+    .nullable(),
+})
+
+const CompareWithParentSchema = z
+  .discriminatedUnion('__typename', [
+    ComparisonSchema,
+    FirstPullRequestSchema,
+    MissingBaseCommitSchema,
+    MissingBaseReportSchema,
+    MissingComparisonSchema,
+    MissingHeadCommitSchema,
+    MissingHeadReportSchema,
+  ])
+  .nullable()
+
+const RepositorySchema = z.object({
+  __typename: z.literal('Repository'),
+  commit: z
+    .object({
+      compareWithParent: CompareWithParentSchema.nullable(),
+    })
+    .nullable(),
+})
+
+const RequestSchema = z.object({
+  owner: z
+    .object({
+      repository: z.discriminatedUnion('__typename', [
+        RepositorySchema,
+        RepoNotFoundErrorSchema,
+        RepoOwnerNotActivatedErrorSchema,
+      ]),
+    })
+    .nullable(),
+})
+
+const query = `
+query CommitDropdownSummary(
+  $owner: String!
+  $repo: String!
+  $commitid: String!
+) {
+  owner(username: $owner) {
+    repository(name: $repo) {
+      __typename
+      ... on Repository {
+        commit(id: $commitid) {
+          compareWithParent {
+            __typename
+            ... on Comparison {
+              patchTotals {
+                missesCount
+                partialsCount
+              }
+            }
+            ... on FirstPullRequest {
+              message
+            }
+            ... on MissingBaseCommit {
+              message
+            }
+            ... on MissingHeadCommit {
+              message
+            }
+            ... on MissingComparison {
+              message
+            }
+            ... on MissingBaseReport {
+              message
+            }
+            ... on MissingHeadReport {
+              message
+            }
+          }
+        }
+      }
+      ... on NotFoundError {
+        message
+      }
+      ... on OwnerNotActivatedError {
+        message
+      }
+    }
+  }
+}`
+
+interface UseCommitDropdownSummaryArgs {
+  provider: string
+  owner: string
+  repo: string
+  commitid: string
+}
+
+export function useCommitDropdownSummary({
+  provider,
+  owner,
+  repo,
+  commitid,
+}: UseCommitDropdownSummaryArgs) {
+  return useQuery({
+    queryKey: ['CommitSummary', provider, owner, repo, commitid],
+    queryFn: ({ signal }) =>
+      Api.graphql({
+        provider,
+        query,
+        signal,
+        variables: {
+          owner,
+          repo,
+          commitid,
+        },
+      }).then((res) => {
+        const parsedRes = RequestSchema.safeParse(res?.data)
+
+        if (!parsedRes.success) {
+          return Promise.reject({
+            status: 404,
+            data: null,
+          })
+        }
+
+        const data = parsedRes.data
+
+        if (data?.owner?.repository?.__typename === 'NotFoundError') {
+          return Promise.reject({
+            status: 404,
+            data: {},
+          })
+        }
+
+        if (data?.owner?.repository?.__typename === 'OwnerNotActivatedError') {
+          return Promise.reject({
+            status: 403,
+            data: {
+              detail: (
+                <p>
+                  Activation is required to view this repo, please{' '}
+                  {/* @ts-expect-error */}
+                  <A to={{ pageName: 'membersTab' }}>click here </A> to activate
+                  your account.
+                </p>
+              ),
+            },
+          })
+        }
+
+        return data
+      }),
+  })
+}
