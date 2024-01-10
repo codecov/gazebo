c25582c97818aab459995519ef55a5b471a6c87c
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-01-10 07:29:58 -0400

    feat: Create hook to fetch a list of bundles for a commit (#2495)
    Create a hook that can fetch a list of bundles for a given commit.
    
    GH codecov/engineering-team#993

---
+++ src/services/commit/useCommitBundleList.tsx
@@ -0,0 +1,184 @@
+import { useQuery } from '@tanstack/react-query'
+import { z } from 'zod'
+
+import {
+  FirstPullRequestSchema,
+  MissingBaseCommitSchema,
+  MissingBaseReportSchema,
+  MissingComparisonSchema,
+  MissingHeadCommitSchema,
+  MissingHeadReportSchema,
+} from 'services/comparison/schemas'
+import {
+  RepoNotFoundErrorSchema,
+  RepoOwnerNotActivatedErrorSchema,
+} from 'services/repo/schemas'
+import Api from 'shared/api'
+import A from 'ui/A'
+
+const BundleComparisonSchema = z.object({
+  name: z.string(),
+  changeType: z.string(),
+  sizeDelta: z.number(),
+  sizeTotal: z.number(),
+  loadTimeDelta: z.number(),
+  loadTimeTotal: z.number(),
+})
+
+const BAComparisonSchema = z.object({
+  __typename: z.literal('BundleAnalysisComparison'),
+  bundles: z.array(BundleComparisonSchema),
+})
+
+const BundleAnalysisCompareWithParentSchema = z
+  .discriminatedUnion('__typename', [
+    BAComparisonSchema,
+    FirstPullRequestSchema,
+    MissingBaseCommitSchema,
+    MissingBaseReportSchema,
+    MissingComparisonSchema,
+    MissingHeadCommitSchema,
+    MissingHeadReportSchema,
+  ])
+  .nullable()
+
+const RepositorySchema = z.object({
+  __typename: z.literal('Repository'),
+  commit: z
+    .object({
+      bundleAnalysisCompareWithParent:
+        BundleAnalysisCompareWithParentSchema.nullable(),
+    })
+    .nullable(),
+})
+
+const RequestSchema = z.object({
+  owner: z
+    .object({
+      repository: z.discriminatedUnion('__typename', [
+        RepositorySchema,
+        RepoNotFoundErrorSchema,
+        RepoOwnerNotActivatedErrorSchema,
+      ]),
+    })
+    .nullable(),
+})
+
+const query = `
+query CommitBundleList(
+  $owner: String!
+  $repo: String!
+  $commitid: String!
+) {
+  owner(username: $owner) {
+    repository(name: $repo) {
+      __typename
+      ... on Repository {
+        commit(id: $commitid) {
+          bundleAnalysisCompareWithParent {
+            __typename
+            ... on BundleAnalysisComparison {
+              bundles {
+                name
+                changeType
+                sizeDelta
+                sizeTotal
+                loadTimeDelta
+                loadTimeTotal
+              }
+            }
+            ... on FirstPullRequest {
+              message
+            }
+            ... on MissingBaseCommit {
+              message
+            }
+            ... on MissingHeadCommit {
+              message
+            }
+            ... on MissingComparison {
+              message
+            }
+            ... on MissingBaseReport {
+              message
+            }
+            ... on MissingHeadReport {
+              message
+            }
+          }
+        }
+      }
+      ... on NotFoundError {
+        message
+      }
+      ... on OwnerNotActivatedError {
+        message
+      }
+    }
+  }
+}`
+
+interface UseCommitBundleListArgs {
+  provider: string
+  owner: string
+  repo: string
+  commitid: string
+}
+
+export function useCommitBundleList({
+  provider,
+  owner,
+  repo,
+  commitid,
+}: UseCommitBundleListArgs) {
+  return useQuery({
+    queryKey: ['CommitBundleList', provider, owner, repo, commitid],
+    queryFn: ({ signal }) =>
+      Api.graphql({
+        provider,
+        query,
+        signal,
+        variables: {
+          owner,
+          repo,
+          commitid,
+        },
+      }).then((res) => {
+        const parsedRes = RequestSchema.safeParse(res?.data)
+
+        if (!parsedRes.success) {
+          return Promise.reject({
+            status: 404,
+            data: null,
+          })
+        }
+
+        const data = parsedRes.data
+
+        if (data?.owner?.repository?.__typename === 'NotFoundError') {
+          return Promise.reject({
+            status: 404,
+            data: {},
+          })
+        }
+
+        if (data?.owner?.repository?.__typename === 'OwnerNotActivatedError') {
+          return Promise.reject({
+            status: 403,
+            data: {
+              detail: (
+                <p>
+                  Activation is required to view this repo, please{' '}
+                  {/* @ts-expect-error */}
+                  <A to={{ pageName: 'membersTab' }}>click here </A> to activate
+                  your account.
+                </p>
+              ),
+            },
+          })
+        }
+
+        return data
+      }),
+  })
+}
