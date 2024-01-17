64bc272d43aca6b96017204aefc054c1d2abae73
Adrian <adrian@codecov.io>
2024-01-17 12:44:18 -0800

    Create email mutation (#2519)
    * feat: create email update mutation

---
+++ src/services/account/useUpdateBillingEmail.ts
@@ -0,0 +1,30 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+
+import Api from 'shared/api'
+
+interface UsePlanDataArgs {
+  provider: string
+  owner: string
+}
+
+interface FormDataArgs {
+  newEmail: string
+}
+
+export function useUpdateBillingEmail({ provider, owner }: UsePlanDataArgs) {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: (formData: FormDataArgs) => {
+      const path = `/${provider}/${owner}/account-details/update_email`
+      const body = {
+        /* eslint-disable camelcase */
+        new_email: formData?.newEmail,
+      }
+      return Api.patch({ path, provider, body })
+    },
+    onSuccess: (data) => {
+      queryClient.invalidateQueries(['accountDetails'])
+    },
+  })
+}
