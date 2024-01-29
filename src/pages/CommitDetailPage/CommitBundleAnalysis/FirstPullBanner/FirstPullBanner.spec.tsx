b636039987a82b5da55709e39873a98b15d9f572
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-01-29 14:06:17 -0400

    add first pull banner for commit bundle analysis

---
+++ src/pages/CommitDetailPage/CommitBundleAnalysis/FirstPullBanner/FirstPullBanner.spec.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react'
+
+import FirstPullBanner from './FirstPullBanner'
+
+describe('FirstPullBanner', () => {
+  describe('When render with first pull request result', () => {
+    it('should render heading', async () => {
+      render(<FirstPullBanner />)
+
+      const header = await screen.findByRole('heading', {
+        name: /Welcome to bundle analysis/,
+      })
+      expect(header).toBeInTheDocument()
+    })
+
+    it('should render content', async () => {
+      render(<FirstPullBanner />)
+
+      const content = await screen.findByText(
+        'Once merged to your default branch, Codecov will compare your bundle reports and display the results on pull request and commits.'
+      )
+      expect(content).toBeInTheDocument()
+    })
+  })
+})
