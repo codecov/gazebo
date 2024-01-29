ca3c88a9ac47023ec6a4eea630e48a8194e91dae
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-01-29 11:38:37 -0400

    remove condtionally render logic from FirstPullBanner

--- src/pages/CommitDetailPage/CommitCoverage/FirstPullBanner/FirstPullBanner.tsx
+++ src/pages/CommitDetailPage/CommitCoverage/FirstPullBanner/FirstPullBanner.tsx
@@ -1,32 +1,8 @@
-import { useParams } from 'react-router-dom'
-
-import { useCommitPageData } from 'pages/CommitDetailPage/hooks'
-import { ComparisonReturnType } from 'shared/utils/comparison'
 import Banner from 'ui/Banner'
 import BannerContent from 'ui/Banner/BannerContent'
 import BannerHeading from 'ui/Banner/BannerHeading'
 
-interface URLParams {
-  provider: string
-  owner: string
-  repo: string
-  commit: string
-}
-
 const FirstPullBanner: React.FC = () => {
-  const { provider, owner, repo, commit } = useParams<URLParams>()
-  const { data } = useCommitPageData({
-    provider,
-    owner,
-    repo,
-    commitId: commit,
-  })
-
-  const resultType = data?.commit?.compareWithParent?.__typename
-  if (resultType !== ComparisonReturnType.FIRST_PULL_REQUEST) {
-    return null
-  }
-
   return (
     <Banner>
       <BannerHeading>
