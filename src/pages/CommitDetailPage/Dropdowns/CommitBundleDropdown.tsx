77123b8ed911bbbc47f60b4fec293226ac845ae6
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-01-29 15:44:46 -0400

    update bundle dropdown to render error message

--- src/pages/CommitDetailPage/Dropdowns/CommitBundleDropdown.tsx
+++ src/pages/CommitDetailPage/Dropdowns/CommitBundleDropdown.tsx
@@ -1,3 +1,4 @@
+import { isNumber } from 'lodash'
 import { useParams } from 'react-router-dom'
 
 import { useCommitBADropdownSummary } from 'services/commit/useCommitBADropdownSummary'
@@ -12,30 +13,44 @@ interface URLParams {
 }
 
 interface BundleMessageProps {
-  sizeDelta: number
+  sizeDelta?: number
+  errorType?: string
+  errorMsg?: string
 }
 
-const BundleMessage: React.FC<BundleMessageProps> = ({ sizeDelta }) => {
-  const positiveSize = Math.abs(sizeDelta)
-  if (sizeDelta < 0) {
-    return (
-      <>
-        changes will decrease total bundle size by{' '}
-        {formatSizeToString(positiveSize)} &#x2139;
-      </>
-    )
+const BundleMessage: React.FC<BundleMessageProps> = ({
+  sizeDelta,
+  errorType,
+  errorMsg,
+}) => {
+  if (errorType && errorMsg) {
+    return <>{errorMsg.toLowerCase()} &#x26A0;</>
   }
 
-  if (sizeDelta > 0) {
-    return (
-      <>
-        changes will increase total bundle size by{' '}
-        {formatSizeToString(positiveSize)} &#x2139;
-      </>
-    )
+  if (isNumber(sizeDelta)) {
+    const positiveSize = Math.abs(sizeDelta)
+    if (sizeDelta < 0) {
+      return (
+        <>
+          changes will decrease total bundle size by{' '}
+          {formatSizeToString(positiveSize)} &#x2139;
+        </>
+      )
+    }
+
+    if (sizeDelta > 0) {
+      return (
+        <>
+          changes will increase total bundle size by{' '}
+          {formatSizeToString(positiveSize)} &#x2139;
+        </>
+      )
+    }
+
+    return <>bundle size has no change &#x2705;</>
   }
 
-  return <>bundle size has no change &#x2705;</>
+  return <>an unknown error occurred</>
 }
 
 const CommitBundleDropdown: React.FC<React.PropsWithChildren> = ({
@@ -49,22 +64,34 @@ const CommitBundleDropdown: React.FC<React.PropsWithChildren> = ({
     commitid: commitSha,
   })
 
+  let sizeDelta: number | undefined
   if (
-    !data ||
-    data?.commit?.bundleAnalysisCompareWithParent?.__typename !==
-      'BundleAnalysisComparison'
+    data?.commit?.bundleAnalysisCompareWithParent?.__typename ===
+    'BundleAnalysisComparison'
   ) {
-    return null
+    sizeDelta = data?.commit?.bundleAnalysisCompareWithParent?.sizeDelta
   }
 
-  const sizeDelta = data?.commit?.bundleAnalysisCompareWithParent?.sizeDelta
+  let errorMsg: string | undefined = undefined
+  let errorType: string | undefined = undefined
+  if (
+    data?.commit?.bundleAnalysisCompareWithParent?.__typename !==
+    'BundleAnalysisComparison'
+  ) {
+    errorType = data?.commit?.bundleAnalysisCompareWithParent?.__typename
+    errorMsg = data?.commit?.bundleAnalysisCompareWithParent?.message
+  }
 
   return (
     <SummaryDropdown.Item value="bundle-analysis">
       <SummaryDropdown.Trigger>
         <p className="flex w-full flex-col sm:flex-row sm:gap-1">
           <span className="font-semibold">Bundle Report: </span>
-          <BundleMessage sizeDelta={sizeDelta} />
+          <BundleMessage
+            sizeDelta={sizeDelta}
+            errorType={errorType}
+            errorMsg={errorMsg}
+          />
         </p>
       </SummaryDropdown.Trigger>
       <SummaryDropdown.Content className="py-2">
