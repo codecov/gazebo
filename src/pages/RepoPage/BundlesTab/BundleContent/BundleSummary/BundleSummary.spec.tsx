daea865040112d7cea2a1c267a0136266309a9f0
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-02-02 07:37:02 -0400

    create new BundleSummary component

---
+++ src/pages/RepoPage/BundlesTab/BundleContent/BundleSummary/BundleSummary.spec.tsx
@@ -0,0 +1,197 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { render, screen } from '@testing-library/react'
+import { graphql } from 'msw'
+import { setupServer } from 'msw/node'
+import { Suspense } from 'react'
+import { MemoryRouter, Route } from 'react-router-dom'
+
+import BundleSummary from './BundleSummary'
+
+const mockRepoOverview = {
+  owner: {
+    repository: {
+      private: false,
+      defaultBranch: 'main',
+      oldestCommitAt: '2022-10-10T11:59:59',
+    },
+  },
+}
+
+const mockUnknownError = {
+  owner: {
+    repository: {
+      __typename: 'Repository',
+      branch: null,
+    },
+  },
+}
+
+const mockBranchBundles = (
+  commitid: string | null = '543a5268dce725d85be7747c0f9b61e9a68dea57'
+) => ({
+  owner: {
+    repository: {
+      __typename: 'Repository',
+      branch: {
+        head: {
+          commitid,
+          bundleAnalysisReport: {
+            __typename: 'BundleAnalysisReport',
+            sizeTotal: 100,
+            loadTimeTotal: 200,
+            bundles: [{ name: 'bundle1', sizeTotal: 50, loadTimeTotal: 100 }],
+          },
+        },
+      },
+    },
+  },
+})
+
+const mockBranchBundlesError = {
+  owner: {
+    repository: {
+      __typename: 'Repository',
+      branch: {
+        head: {
+          commitid: '543a5268dce725d85be7747c0f9b61e9a68dea57',
+          bundleAnalysisReport: {
+            __typename: 'MissingHeadReport',
+            message: 'Missing head report',
+          },
+        },
+      },
+    },
+  },
+}
+
+const server = setupServer()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      suspense: true,
+    },
+  },
+})
+
+const wrapper: React.FC<React.PropsWithChildren> = ({ children }) => (
+  <QueryClientProvider client={queryClient}>
+    <MemoryRouter initialEntries={['/gh/codecov/test-repo/bundles']}>
+      <Route path="/:provider/:owner/:repo/bundles">
+        <Suspense fallback={<p>Loading</p>}>{children}</Suspense>
+      </Route>
+    </MemoryRouter>
+  </QueryClientProvider>
+)
+
+beforeAll(() => {
+  server.listen()
+})
+
+afterEach(() => {
+  queryClient.clear()
+  server.resetHandlers()
+})
+
+interface SetupArgs {
+  hasBranchBundleError?: boolean
+  hasCommitId?: boolean
+  isUnknownError?: boolean
+}
+
+describe('BundleSummary', () => {
+  function setup({
+    hasBranchBundleError = false,
+    hasCommitId = true,
+    isUnknownError = false,
+  }: SetupArgs) {
+    server.use(
+      graphql.query('BranchBundleSummaryData', (req, res, ctx) => {
+        if (hasBranchBundleError) {
+          return res(ctx.status(200), ctx.data(mockBranchBundlesError))
+        } else if (isUnknownError) {
+          return res(ctx.status(200), ctx.data(mockUnknownError))
+        }
+
+        let data = mockBranchBundles()
+        if (!hasCommitId) {
+          data = mockBranchBundles(null)
+        }
+
+        return res(ctx.status(200), ctx.data(data))
+      }),
+      graphql.query('GetRepoOverview', (req, res, ctx) => {
+        return res(ctx.status(200), ctx.data(mockRepoOverview))
+      })
+    )
+  }
+
+  describe('there is a bundle report', () => {
+    it('renders the bundle summary message', async () => {
+      setup({})
+      render(<BundleSummary />, { wrapper })
+
+      const report = await screen.findByText(/Report:/)
+      expect(report).toBeInTheDocument()
+
+      const totalSize = await screen.findByText(
+        /total combined bundle size 100B/
+      )
+      expect(totalSize).toBeInTheDocument()
+    })
+
+    it('renders link to the commit page', async () => {
+      setup({})
+      render(<BundleSummary />, { wrapper })
+
+      const commitLink = await screen.findByRole('link', {
+        name: '543a526',
+      })
+      expect(commitLink).toBeInTheDocument()
+      expect(commitLink).toHaveAttribute(
+        'href',
+        '/gh/codecov/test-repo/commit/543a5268dce725d85be7747c0f9b61e9a68dea57'
+      )
+    })
+  })
+
+  describe('there is a known error', () => {
+    it('renders the known error message', async () => {
+      setup({ hasBranchBundleError: true })
+      render(<BundleSummary />, { wrapper })
+
+      const report = await screen.findByText(/Report:/)
+      expect(report).toBeInTheDocument()
+
+      const message = await screen.findByText(/missing head report/)
+      expect(message).toBeInTheDocument()
+    })
+
+    it('renders the link to the commit page', async () => {
+      setup({ hasBranchBundleError: true })
+      render(<BundleSummary />, { wrapper })
+
+      const commitLink = await screen.findByRole('link', {
+        name: '543a526',
+      })
+      expect(commitLink).toBeInTheDocument()
+      expect(commitLink).toHaveAttribute(
+        'href',
+        '/gh/codecov/test-repo/commit/543a5268dce725d85be7747c0f9b61e9a68dea57'
+      )
+    })
+  })
+
+  describe('there is an unknown error', () => {
+    it('renders the unknown error message', async () => {
+      setup({ isUnknownError: true })
+      render(<BundleSummary />, { wrapper })
+
+      const report = await screen.findByText(/Report:/)
+      expect(report).toBeInTheDocument()
+
+      const message = await screen.findByText(/an unknown error has occurred/)
+      expect(message).toBeInTheDocument()
+    })
+  })
+})
---
+++ src/pages/RepoPage/BundlesTab/BundleContent/BundleSummary/BundleSummary.tsx
@@ -0,0 +1,83 @@
+import isString from 'lodash/isString'
+import { useParams } from 'react-router-dom'
+
+import { useBranchBundleSummary } from 'services/branches'
+import { formatSizeToString } from 'shared/utils/bundleAnalysis'
+import A from 'ui/A'
+
+interface URLParams {
+  provider: string
+  owner: string
+  repo: string
+}
+
+const BundleSummary: React.FC = () => {
+  const { provider, owner, repo } = useParams<URLParams>()
+  const { data: branchBundle } = useBranchBundleSummary({
+    provider,
+    owner,
+    repo,
+  })
+
+  const branchHead = branchBundle?.branch?.head
+
+  if (branchHead?.bundleAnalysisReport?.__typename === 'BundleAnalysisReport') {
+    const shortSha = branchHead?.commitid?.slice(0, 7)
+    const sizeTotal = branchHead?.bundleAnalysisReport?.sizeTotal
+
+    return (
+      <div className="bg-ds-gray-primary p-4">
+        <p className="w-full text-base">
+          <span className="font-semibold">Report: </span>total combined bundle
+          size {formatSizeToString(sizeTotal)} &#x2139;
+        </p>
+        <p className="pt-2 text-sm">
+          <span className="font-semibold">Source</span> commit:{' '}
+          <A
+            hook="bundles-tab-to-commit"
+            isExternal={false}
+            to={{
+              pageName: 'commit',
+              options: { commit: branchHead.commitid },
+            }}
+          >
+            <span className="font-mono">{shortSha}</span>
+          </A>
+        </p>
+      </div>
+    )
+  }
+
+  let message = 'an unknown error has occurred'
+  if (isString(branchHead?.bundleAnalysisReport?.message)) {
+    message = branchHead?.bundleAnalysisReport?.message.toLowerCase()
+  }
+
+  return (
+    <div className="bg-ds-gray-primary p-4">
+      <p className="w-full text-base">
+        <span className="font-semibold">Report: </span>
+        {message} &#x26A0;
+      </p>
+      {isString(branchHead?.commitid) ? (
+        <p className="pt-2 text-sm">
+          <span className="font-semibold">Source</span> commit:{' '}
+          <A
+            hook="bundles-tab-to-commit"
+            isExternal={false}
+            to={{
+              pageName: 'commit',
+              options: { commit: branchHead?.commitid },
+            }}
+          >
+            <span className="font-mono">
+              {branchHead?.commitid?.slice(0, 7)}
+            </span>
+          </A>
+        </p>
+      ) : null}
+    </div>
+  )
+}
+
+export default BundleSummary
