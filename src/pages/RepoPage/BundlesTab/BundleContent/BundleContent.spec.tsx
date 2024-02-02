48735877ce01d07bd292b543332b527d2e752f93
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-02-02 07:37:34 -0400

    create new bundle content component

---
+++ src/pages/RepoPage/BundlesTab/BundleContent/BundleContent.spec.tsx
@@ -0,0 +1,86 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { render, screen } from '@testing-library/react'
+import { graphql } from 'msw'
+import { setupServer } from 'msw/node'
+import { Suspense } from 'react'
+import { MemoryRouter, Route } from 'react-router-dom'
+
+import BundleContent from './BundleContent'
+
+const mockRepoOverview = {
+  owner: {
+    repository: {
+      private: false,
+      defaultBranch: 'main',
+      oldestCommitAt: '2022-10-10T11:59:59',
+    },
+  },
+}
+
+const mockBranchBundles = {
+  owner: {
+    repository: {
+      __typename: 'Repository',
+      branch: {
+        head: {
+          commitid: '543a5268dce725d85be7747c0f9b61e9a68dea57',
+          bundleAnalysisReport: {
+            __typename: 'BundleAnalysisReport',
+            sizeTotal: 100,
+            loadTimeTotal: 200,
+            bundles: [{ name: 'bundle1', sizeTotal: 50, loadTimeTotal: 100 }],
+          },
+        },
+      },
+    },
+  },
+}
+
+const server = setupServer()
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { retry: false, suspense: true } },
+})
+
+const wrapper: React.FC<React.PropsWithChildren> = ({ children }) => (
+  <QueryClientProvider client={queryClient}>
+    <MemoryRouter initialEntries={['/gh/codecov/test-repo/bundles']}>
+      <Route path="/:provider/:owner/:repo/bundles">
+        <Suspense fallback={<p>Loading</p>}>{children}</Suspense>
+      </Route>
+    </MemoryRouter>
+  </QueryClientProvider>
+)
+
+beforeAll(() => {
+  server.listen()
+})
+
+afterEach(() => {
+  queryClient.clear()
+  server.resetHandlers()
+})
+
+afterAll(() => {
+  server.close()
+})
+
+describe('BundleContent', () => {
+  function setup() {
+    server.use(
+      graphql.query('BranchBundleSummaryData', (req, res, ctx) => {
+        return res(ctx.status(200), ctx.data(mockBranchBundles))
+      }),
+      graphql.query('GetRepoOverview', (req, res, ctx) => {
+        return res(ctx.status(200), ctx.data(mockRepoOverview))
+      })
+    )
+  }
+
+  it('renders the bundle summary', async () => {
+    setup()
+    render(<BundleContent />, { wrapper })
+
+    const report = await screen.findByText(/Report:/)
+    expect(report).toBeInTheDocument()
+  })
+})
