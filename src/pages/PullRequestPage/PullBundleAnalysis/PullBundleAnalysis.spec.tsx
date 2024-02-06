ab6bec5ab8285cfaa87a639191b1058d7645de71
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-02-06 13:52:09 -0400

    update pull bundle analysis to conditionally render table vs table + message depending on what products have been setup in the repo

--- src/pages/PullRequestPage/PullBundleAnalysis/PullBundleAnalysis.spec.tsx
+++ src/pages/PullRequestPage/PullBundleAnalysis/PullBundleAnalysis.spec.tsx
@@ -1,4 +1,16 @@
-import { render, screen } from '@testing-library/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react'
+import { graphql } from 'msw'
+import { setupServer } from 'msw/node'
+import { Suspense } from 'react'
+import { MemoryRouter, Route } from 'react-router-dom'
+
+import { TierNames } from 'services/tier'
+import { useFlags } from 'shared/featureFlags'
 
 import PullBundleAnalysis from './PullBundleAnalysis'
 
@@ -6,11 +18,152 @@ jest.mock('./PullBundleAnalysisTable', () => () => (
   <div>PullBundleAnalysisTable</div>
 ))
 
-describe('PullBundleAnalysis', () => {
-  it('displays the PullBundleAnalysisTable', async () => {
-    render(<PullBundleAnalysis />)
+jest.mock('shared/featureFlags')
+const mockedUseFlags = useFlags as jest.Mock<{
+  multipleTiers: boolean
+}>
 
-    const table = await screen.findByText('PullBundleAnalysisTable')
-    expect(table).toBeInTheDocument()
+const mockPullPageData = {
+  pullId: 1,
+  head: {
+    commitid: '123',
+  },
+  compareWithBase: {
+    __typename: 'Comparison',
+    impactedFilesCount: 4,
+    indirectChangedFilesCount: 0,
+    flagComparisonsCount: 1,
+    componentComparisonsCount: 6,
+    directChangedFilesCount: 0,
+  },
+}
+
+const mockSummaryData = {
+  owner: {
+    repository: {
+      __typename: 'Repository',
+      pull: {
+        bundleAnalysisCompareWithBase: {
+          __typename: 'BundleAnalysisComparison',
+          sizeDelta: 10,
+          loadTimeDelta: 0,
+        },
+      },
+    },
+  },
+}
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { retry: false, suspense: true } },
+})
+const server = setupServer()
+
+const wrapper: React.FC<React.PropsWithChildren> = ({ children }) => (
+  <QueryClientProvider client={queryClient}>
+    <MemoryRouter initialEntries={['/gh/test-org/test-repo/pull/12']}>
+      <Route path="/:provider/:owner/:repo/pull/:pullId">
+        <Suspense fallback={<p>Loading</p>}>{children}</Suspense>
+      </Route>
+    </MemoryRouter>
+  </QueryClientProvider>
+)
+
+beforeAll(() => {
+  server.listen()
+})
+beforeEach(() => {
+  server.resetHandlers()
+  queryClient.clear()
+})
+afterAll(() => {
+  server.close()
+})
+
+interface SetupArgs {
+  coverageEnabled?: boolean
+  bundleAnalysisEnabled?: boolean
+}
+
+describe('PullBundleAnalysis', () => {
+  function setup({ coverageEnabled, bundleAnalysisEnabled }: SetupArgs) {
+    mockedUseFlags.mockReturnValue({
+      multipleTiers: true,
+    })
+
+    server.use(
+      graphql.query('PullPageData', (req, res, ctx) => {
+        return res(
+          ctx.status(200),
+          ctx.data({
+            owner: {
+              repository: {
+                __typename: 'Repository',
+                coverageEnabled: coverageEnabled,
+                bundleAnalysisEnabled: bundleAnalysisEnabled,
+                pull: mockPullPageData,
+              },
+            },
+          })
+        )
+      }),
+      graphql.query('GetRepoSettings', (req, res, ctx) => {
+        return res(
+          ctx.status(200),
+          ctx.data({
+            owner: { repository: { private: false } },
+          })
+        )
+      }),
+      graphql.query('OwnerTier', (req, res, ctx) => {
+        return res(
+          ctx.status(200),
+          ctx.data({
+            owner: { plan: { tierName: TierNames.PRO } },
+          })
+        )
+      }),
+      graphql.query('PullBADropdownSummary', (req, res, ctx) => {
+        return res(ctx.status(200), ctx.data(mockSummaryData))
+      })
+    )
+  }
+
+  describe('coverage is enabled and bundles are enabled', () => {
+    it('does not render bundle summary', async () => {
+      setup({ coverageEnabled: true, bundleAnalysisEnabled: true })
+      render(<PullBundleAnalysis />, { wrapper })
+
+      const loader = await screen.findByText('Loading')
+      await waitForElementToBeRemoved(loader)
+
+      const message = screen.queryByText(/Bundle report:/)
+      expect(message).not.toBeInTheDocument()
+    })
+
+    it('displays the PullBundleAnalysisTable', async () => {
+      setup({ coverageEnabled: true, bundleAnalysisEnabled: true })
+      render(<PullBundleAnalysis />, { wrapper })
+
+      const table = await screen.findByText('PullBundleAnalysisTable')
+      expect(table).toBeInTheDocument()
+    })
+  })
+
+  describe('coverage is disabled and bundles are enabled', () => {
+    it('renders bundle summary', async () => {
+      setup({ coverageEnabled: false, bundleAnalysisEnabled: true })
+      render(<PullBundleAnalysis />, { wrapper })
+
+      const message = await screen.findByText(/Bundle report:/)
+      expect(message).toBeInTheDocument()
+    })
+
+    it('displays the PullBundleAnalysisTable', async () => {
+      setup({ coverageEnabled: false, bundleAnalysisEnabled: true })
+      render(<PullBundleAnalysis />, { wrapper })
+
+      const table = await screen.findByText('PullBundleAnalysisTable')
+      expect(table).toBeInTheDocument()
+    })
   })
 })
--- src/pages/PullRequestPage/PullBundleAnalysis/PullBundleAnalysis.tsx
+++ src/pages/PullRequestPage/PullBundleAnalysis/PullBundleAnalysis.tsx
@@ -1,8 +1,23 @@
 import { lazy, Suspense } from 'react'
+import { useParams } from 'react-router-dom'
 
+import { useRepoSettings } from 'services/repo'
+import { TierNames, useTier } from 'services/tier'
+import { useFlags } from 'shared/featureFlags'
 import Spinner from 'ui/Spinner'
 
+import BundleMessage from './BundleMessage'
+
+import { usePullPageData } from '../hooks'
+
 const PullBundleAnalysisTable = lazy(() => import('./PullBundleAnalysisTable'))
+
+interface URLParams {
+  provider: string
+  owner: string
+  repo: string
+  pullId: string
+}
 
 const Loader = () => (
   <div className="flex flex-1 justify-center">
@@ -11,10 +26,44 @@ const Loader = () => (
 )
 
 const PullBundleAnalysis: React.FC = () => {
+  const { provider, owner, repo, pullId } = useParams<URLParams>()
+  const { data: settings } = useRepoSettings()
+  const { data: tierData } = useTier({ provider, owner })
+
+  const { multipleTiers } = useFlags({
+    multipleTiers: false,
+  })
+
+  const isTeamPlan =
+    multipleTiers &&
+    tierData === TierNames.TEAM &&
+    settings?.repository?.private
+
+  const { data } = usePullPageData({
+    provider,
+    owner,
+    repo,
+    pullId,
+    isTeamPlan,
+  })
+
+  if (data?.coverageEnabled && data?.bundleAnalysisEnabled) {
+    return (
+      <Suspense fallback={<Loader />}>
+        <PullBundleAnalysisTable />
+      </Suspense>
+    )
+  }
+
   return (
-    <Suspense fallback={<Loader />}>
-      <PullBundleAnalysisTable />
-    </Suspense>
+    <>
+      <p className="flex w-full items-center gap-2 bg-ds-gray-primary px-2 py-4">
+        <BundleMessage />
+      </p>
+      <Suspense fallback={<Loader />}>
+        <PullBundleAnalysisTable />
+      </Suspense>
+    </>
   )
 }
 
