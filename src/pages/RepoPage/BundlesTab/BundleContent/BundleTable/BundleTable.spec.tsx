fd6cf6f13c5c97364806faf6c3b0bfb48781ee2e
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-02-06 11:28:46 -0400

    fix up a few tests

--- src/pages/RepoPage/BundlesTab/BundleContent/BundleTable/BundleTable.spec.tsx
+++ src/pages/RepoPage/BundlesTab/BundleContent/BundleTable/BundleTable.spec.tsx
@@ -11,9 +11,13 @@ import BundleTable, { useTableData } from './BundleTable'
 const mockRepoOverview = {
   owner: {
     repository: {
+      __typename: 'Repository',
       private: false,
       defaultBranch: 'main',
       oldestCommitAt: '2022-10-10T11:59:59',
+      coverageEnabled: true,
+      bundleAnalysisEnabled: true,
+      languages: ['javascript'],
     },
   },
 }
