2fda34edac60d0f0d79e6f55ea185dd331f6f951
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-02-13 13:49:58 -0400

    fix up pull bundle text size and warning icons

--- src/pages/PullRequestPage/PullBundleAnalysis/PullBundleAnalysis.tsx
+++ src/pages/PullRequestPage/PullBundleAnalysis/PullBundleAnalysis.tsx
@@ -34,7 +34,7 @@ const PullBundleAnalysis: React.FC = () => {
 
   return (
     <>
-      <p className="flex w-full items-center gap-2 bg-ds-gray-primary px-2 py-4">
+      <p className="flex w-full items-center gap-2 bg-ds-gray-primary px-2 py-4 text-base">
         <BundleMessage />
       </p>
       <Suspense fallback={<Loader />}>
