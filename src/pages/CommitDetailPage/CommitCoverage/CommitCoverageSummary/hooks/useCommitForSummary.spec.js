7a643b38e037d2b5914c83627168f5d1b4dfcbfc
Spencer Murray <159931558+spalmurray-codecov@users.noreply.github.com>
2024-03-04 15:44:06 -0500

    feat: Add deleted file status to files changed tables (#2639)
    Adds a deleted file status indicator to the files changed tables for both pull and commit.
    
    GH codecov/engineering-team#1087

--- src/pages/CommitDetailPage/CommitCoverage/CommitCoverageSummary/hooks/useCommitForSummary.spec.js
+++ src/pages/CommitDetailPage/CommitCoverage/CommitCoverageSummary/hooks/useCommitForSummary.spec.js
@@ -94,8 +94,9 @@ const data = {
         __typename: 'ImpactedFiles',
         results: [
           {
-            patchCoverage: { coverage: 75 },
             headName: 'flag1/mafs.js',
+            isCriticalFile: false,
+            patchCoverage: { coverage: 75 },
             baseCoverage: { coverage: 100 },
             headCoverage: { coverage: 90.9090909090909 },
           },
