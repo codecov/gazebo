8290649643c1923c72874c2d45f60f9df5f7179a
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-03-05 11:03:09 -0400

    update useUploads to use useRepoOverview over useRepoSettings

--- src/pages/CommitDetailPage/CommitCoverage/UploadsCard/useUploads/useUploads.js
+++ src/pages/CommitDetailPage/CommitCoverage/UploadsCard/useUploads/useUploads.js
@@ -1,14 +1,14 @@
 import { useParams } from 'react-router-dom'
 
 import { useCommit } from 'services/commit'
-import { useRepoSettings } from 'services/repo'
+import { useRepoOverview } from 'services/repo'
 import { TierNames, useTier } from 'services/tier'
 import { useFlags } from 'shared/featureFlags'
 import { extractUploads } from 'shared/utils/extractUploads'
 
 export function useUploads() {
   const { provider, owner, repo, commit } = useParams()
-  const { data: settings } = useRepoSettings()
+  const { data: overview } = useRepoOverview({ provider, owner, repo })
   const { multipleTiers } = useFlags({
     multipleTiers: false,
   })
@@ -16,9 +16,7 @@ export function useUploads() {
 
   // TODO: We need backend functionality to properly manage access to carryforward flags for team tier members
   const isTeamPlan =
-    multipleTiers &&
-    tierData === TierNames.TEAM &&
-    settings?.repository?.private
+    multipleTiers && tierData === TierNames.TEAM && overview?.private
 
   const { data } = useCommit({
     provider,
