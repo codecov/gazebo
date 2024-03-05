c2c9e4be803ed8976ad2d9447c48eec49b456c1c
Rohit <rohit.vinnakota@sentry.io>
2024-03-05 15:38:39 -0500

    remove extra styles

--- src/ui/FileViewer/ToggleHeader/ToggleHeader.jsx
+++ src/ui/FileViewer/ToggleHeader/ToggleHeader.jsx
@@ -12,20 +12,16 @@ function ToggleHeader({
   showComponentsSelect = false,
 }) {
   return (
-    <Title title={title} sticky={sticky}>
-      <div className="flex flex-col items-start space-x-0 lg:flex-row lg:items-center lg:space-x-2">
-        <div className="flex flex-row items-center space-x-2">
-          <TitleCoverage coverage={LINE_STATE.UNCOVERED} />
-          <TitleCoverage coverage={LINE_STATE.PARTIAL} />
-          <TitleCoverage coverage={LINE_STATE.COVERED} />
-          <TitleHitCount showHitCount={showHitCount} />
-        </div>
-        <div className="mt-2 flex flex-col items-center space-y-2 lg:mt-0 lg:flex-row lg:space-x-2 lg:space-y-0">
-          {showFlagsSelect ? <TitleFlags /> : null}
-          {showComponentsSelect ? <ComponentsMultiSelect /> : null}
-        </div>
-      </div>
-    </Title>
+    <div>
+      <Title title={title} sticky={sticky}>
+        <TitleCoverage coverage={LINE_STATE.UNCOVERED} />
+        <TitleCoverage coverage={LINE_STATE.PARTIAL} />
+        <TitleCoverage coverage={LINE_STATE.COVERED} />
+        <TitleHitCount showHitCount={showHitCount} />
+        {showFlagsSelect ? <TitleFlags /> : null}
+        {showComponentsSelect ? <ComponentsMultiSelect /> : null}
+      </Title>
+    </div>
   )
 }
 
