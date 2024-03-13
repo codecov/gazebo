14c57909106e5180f6b33657a307e1cbf03da250
Spencer Murray <spencer.murray@sentry.io>
2024-03-13 14:38:18 -0400

    Use React.FC in test

--- src/shared/ContentsTable/useTableDefaultSort/useTableDefaultSort.spec.tsx
+++ src/shared/ContentsTable/useTableDefaultSort/useTableDefaultSort.spec.tsx
@@ -1,6 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { renderHook } from '@testing-library/react'
-import { ReactNode } from 'react'
 import { MemoryRouter, Route } from 'react-router-dom'
 
 import { useLocationParams } from 'services/navigation'
@@ -18,7 +17,7 @@ const queryClient = new QueryClient({
   defaultOptions: { queries: { retry: false } },
 })
 
-const wrapper = ({ children }: { children: ReactNode }) => (
+const wrapper: React.FC<React.PropsWithChildren> = ({ children }) => (
   <MemoryRouter initialEntries={['/gh/codecov/test-repo']}>
     <Route path="/:provider/:owner/:repo">
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
