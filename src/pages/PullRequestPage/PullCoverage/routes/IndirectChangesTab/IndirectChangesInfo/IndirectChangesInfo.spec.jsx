e63db33a4a2ffda018da3d56b0345f852de50208
Rohit <rohit.vinnakota@sentry.io>
2024-03-13 12:30:52 -0400

    undo spec changes

--- src/pages/PullRequestPage/PullCoverage/routes/IndirectChangesTab/IndirectChangesInfo/IndirectChangesInfo.spec.jsx
+++ src/pages/PullRequestPage/PullCoverage/routes/IndirectChangesTab/IndirectChangesInfo/IndirectChangesInfo.spec.jsx
@@ -1,44 +1,24 @@
 import { render, screen } from 'custom-testing-library'
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { setupServer } from 'msw/node'
 import { MemoryRouter, Route } from 'react-router-dom'
 
 import IndirectChangesInfo from './IndirectChangesInfo'
 
 jest.mock('../../ComponentsSelector', () => () => 'ComponentsSelector')
 
-const server = setupServer()
-const queryClient = new QueryClient()
-
-const wrapper = ({ children }) => (
-  <QueryClientProvider client={queryClient}>
-    <MemoryRouter
-      initialEntries={['/provider/owner/repo/pull/pullId/indirectChangesInfo']}
-    >
-      <Route path="/:provider/:owner/:repo/pull/:pullId/indirectChangesInfo">
-        {children}
-      </Route>
-    </MemoryRouter>
-  </QueryClientProvider>
-)
-
-beforeAll(() => {
-  server.listen()
-})
-
-afterEach(() => {
-  queryClient.clear()
-  server.resetHandlers()
-})
-
-afterAll(() => {
-  server.close()
-})
-
-describe('Indirect changes Info', () => {
+describe('Indiret changes Info', () => {
   it('renders the expected copy', () => {
-    render(<IndirectChangesInfo />, { wrapper })
+    render(
+      <MemoryRouter
+        initialEntries={[
+          '/provider/owner/repo/pull/pullId/indirectChangesInfo',
+        ]}
+      >
+        <Route path="/:provider/:owner/:repo/pull/:pullId/indirectChangesInfo">
+          <IndirectChangesInfo />
+        </Route>
+      </MemoryRouter>
+    )
 
     expect(
       screen.getByText(
@@ -48,7 +28,17 @@ describe('Indirect changes Info', () => {
   })
 
   it('renders the expected link', () => {
-    render(<IndirectChangesInfo />, { wrapper })
+    render(
+      <MemoryRouter
+        initialEntries={[
+          '/provider/owner/repo/pull/pullId/indirectChangesInfo',
+        ]}
+      >
+        <Route path="/:provider/:owner/:repo/pull/:pullId/indirectChangesInfo">
+          <IndirectChangesInfo />
+        </Route>
+      </MemoryRouter>
+    )
 
     expect(
       screen.getByRole('link', {
@@ -58,7 +48,17 @@ describe('Indirect changes Info', () => {
   })
 
   it('renders the expected components selector', () => {
-    render(<IndirectChangesInfo />, { wrapper })
+    render(
+      <MemoryRouter
+        initialEntries={[
+          '/provider/owner/repo/pull/pullId/indirectChangesInfo',
+        ]}
+      >
+        <Route path="/:provider/:owner/:repo/pull/:pullId/indirectChangesInfo">
+          <IndirectChangesInfo />
+        </Route>
+      </MemoryRouter>
+    )
 
     expect(screen.getByText('ComponentsSelector')).toBeInTheDocument()
   })
