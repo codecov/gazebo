b6b242b6693d5c113d0f1d0ca6028af13c696387
Rohit <rohit.vinnakota@sentry.io>
2024-03-13 12:32:58 -0400

    update test

--- src/pages/PullRequestPage/PullCoverage/routes/IndirectChangesTab/IndirectChangesInfo/IndirectChangesInfo.jsx
+++ src/pages/PullRequestPage/PullCoverage/routes/IndirectChangesTab/IndirectChangesInfo/IndirectChangesInfo.jsx
@@ -1,21 +1,17 @@
-import ToggleHeader from 'pages/PullRequestPage/Header/ToggleHeader/ToggleHeader'
 import A from 'ui/A'
 import Icon from 'ui/Icon'
 
 function IndirectChangesInfo() {
   return (
-    <div>
-      <div className="flex justify-between bg-ds-gray-primary p-2">
-        <div className="flex items-center gap-1">
-          <Icon name="information-circle" size="sm" />
-          <p>
-            These are files that didn&apos;t have author revisions, but contain
-            unexpected coverage changes{' '}
-            <A to={{ pageName: 'unexpectedChanges' }}>learn more.</A>
-          </p>
-        </div>
+    <div className="flex justify-between bg-ds-gray-primary p-2">
+      <div className="flex items-center gap-1">
+        <Icon name="information-circle" size="sm" />
+        <p>
+          These are files that didn&apos;t have author revisions, but contain
+          unexpected coverage changes{' '}
+          <A to={{ pageName: 'unexpectedChanges' }}>learn more.</A>
+        </p>
       </div>
-      <ToggleHeader />
     </div>
   )
 }
