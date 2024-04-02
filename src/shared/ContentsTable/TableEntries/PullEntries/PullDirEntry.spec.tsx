059d8276ff8526e02df91945631e19615296d8a2
Spencer Murray <spencer.murray@sentry.io>
2024-04-02 09:47:36 -0400

    fix import

--- src/shared/ContentsTable/TableEntries/PullEntries/PullDirEntry.spec.tsx
+++ src/shared/ContentsTable/TableEntries/PullEntries/PullDirEntry.spec.tsx
@@ -3,7 +3,6 @@ import { render, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { graphql } from 'msw'
 import { setupServer } from 'msw/node'
-import { PropsWithChildren } from 'react'
 import { MemoryRouter, Route } from 'react-router-dom'
 
 import PullDirEntry from './PullDirEntry'
