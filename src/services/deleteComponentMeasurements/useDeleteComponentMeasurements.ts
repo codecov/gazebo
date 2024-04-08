6988fc0c8130b93bbb6f2b2174e6a6612ac0bd3c
JerrySentry <142266253+JerrySentry@users.noreply.github.com>
2024-04-08 18:44:07 -0400

    Add delete component measurements hook (#2749)
    A hook to be called when clicking on the trash can on the coverage over time for components tab.

---
+++ src/services/deleteComponentMeasurements/useDeleteComponentMeasurements.ts
@@ -0,0 +1,64 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useParams } from 'react-router-dom'
+
+import { useAddNotification } from 'services/toastNotification'
+import Api from 'shared/api'
+
+interface URLParams {
+  provider: string
+  owner: string
+  repo: string
+}
+
+interface StarTrialMutationArgs {
+  componentId: string
+}
+
+export function useDeleteComponentMeasurements() {
+  const { provider, owner, repo } = useParams<URLParams>()
+  const queryClient = useQueryClient()
+  const addToast = useAddNotification()
+
+  return useMutation({
+    mutationFn: ({ componentId }: StarTrialMutationArgs) => {
+      const query = `
+        mutation deleteComponentMeasurements(
+          $input: DeleteComponentMeasurementsInput!
+        ) {
+          deleteComponentMeasurements(input: $input) {
+            error {
+              __typename
+            }
+          }
+        }
+      `
+      const variables = {
+        input: { ownerUsername: owner, repoName: repo, componentId },
+      }
+      return Api.graphqlMutation({
+        provider,
+        query,
+        variables,
+        mutationPath: 'deleteComponentMeasurements',
+      })
+    },
+    onSuccess: ({ data }) => {
+      const error = data?.deleteComponentMeasurements?.error?.__typename
+      if (error) {
+        // TODO: adjust backend to provide a message so we can tailor the message here
+        addToast({
+          type: 'error',
+          text: 'There was an error deleting your component measurements',
+        })
+      } else {
+        queryClient.invalidateQueries(['RepoFlags'])
+      }
+    },
+    onError: (e) => {
+      addToast({
+        type: 'error',
+        text: 'There was an error deleting your component measurements',
+      })
+    },
+  })
+}
