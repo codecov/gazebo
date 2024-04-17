50beb945771b736621fb9102e5f51c31ec1a50f9
Ajay Singh <ajay.singh@sentry.io>
2024-04-17 15:48:17 -0700

    fix a couple typos and convert an easy hook to TS

--- src/services/defaultOrganization/useUpdateDefaultOrganization.ts
+++ src/services/defaultOrganization/useUpdateDefaultOrganization.ts
@@ -4,25 +4,27 @@ import { useParams } from 'react-router-dom'
 import { useAddNotification } from 'services/toastNotification'
 import Api from 'shared/api'
 
+const query = `
+mutation updateDefaultOrganization(
+  $input: UpdateDefaultOrganizationInput!
+) {
+  updateDefaultOrganization(input: $input) {
+    error {
+      __typename
+    }
+    username
+  }
+}
+`
+
 export function useUpdateDefaultOrganization() {
+  // @ts-expect-error  useParams needs to be converted
   const { provider } = useParams()
   const queryClient = useQueryClient()
   const addToast = useAddNotification()
 
   return useMutation({
-    mutationFn: ({ username = null }) => {
-      const query = `
-        mutation updateDefaultOrganization(
-          $input: UpdateDefaultOrganizationInput!
-        ) {
-          updateDefaultOrganization(input: $input) {
-            error {
-              __typename
-            }
-            username
-          }
-        }
-      `
+    mutationFn: ({ username = null }: { username: string | null }) => {
       const variables = { input: { username } }
       return Api.graphqlMutation({
         provider,
@@ -38,7 +40,7 @@ export function useUpdateDefaultOrganization() {
           'Organization does not belong in the current users organization list'
         )
       } else {
-        queryClient.invalidateQueries('DetailOwner')
+        queryClient.invalidateQueries(['DetailOwner'])
       }
     },
     onError: (e) => {
