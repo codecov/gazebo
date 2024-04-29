ea4d427bae053644c8107311b17bedb385a01020
Rohit Vinnakota <148245014+rohitvinnakota-codecov@users.noreply.github.com>
2024-04-29 13:47:40 -0400

    [ref] Refactor commit detail file explorer table (#2802)

---
+++ src/shared/ContentsTable/TableEntries/CommitEntries/CommitDirEntry.tsx
@@ -0,0 +1,45 @@
+import { usePrefetchCommitDirEntry } from 'services/pathContents/commit/dir'
+import { PathContentsFilters } from 'services/pathContents/constants'
+
+import DirEntry from '../BaseEntries/DirEntry'
+
+interface CommitDirEntryProps {
+  commitSha: string
+  name: string
+  urlPath?: string
+  filters?: PathContentsFilters
+}
+
+function CommitDirEntry({
+  commitSha,
+  urlPath,
+  name,
+  filters,
+}: CommitDirEntryProps) {
+  const flags =
+    filters?.flags && filters?.flags?.length > 0 ? filters?.flags : []
+
+  const components =
+    filters?.components && filters?.components?.length > 0
+      ? filters?.components
+      : []
+
+  const { runPrefetch } = usePrefetchCommitDirEntry({
+    commit: { commitSha },
+    path: name,
+    filters,
+  })
+
+  return (
+    <DirEntry
+      name={name}
+      urlPath={urlPath}
+      runPrefetch={runPrefetch}
+      pageName="commitTreeView"
+      commitSha={commitSha}
+      queryParams={{ flags, components }}
+    />
+  )
+}
+
+export default CommitDirEntry
