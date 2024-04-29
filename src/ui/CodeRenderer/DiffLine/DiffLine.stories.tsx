971801fb0e317f1de906addb8384e61752897ac3
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-04-29 11:03:44 -0300

    update DiffLine to TS and accept stickyPadding prop

---
+++ src/ui/CodeRenderer/DiffLine/DiffLine.stories.tsx
@@ -0,0 +1,53 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { MemoryRouter } from 'react-router-dom'
+
+import DiffLineComponent from './DiffLine'
+
+const line = [
+  { types: ['keyword'], content: 'function' },
+  { types: ['plain'], content: ' ' },
+  { types: ['function'], content: 'add' },
+  { types: ['punctuation'], content: '(' },
+  { types: ['parameter'], content: 'a' },
+  { types: ['parameter', 'punctuation'], content: ',' },
+  { types: ['parameter'], content: ' b' },
+  { types: ['punctuation'], content: ')' },
+  { types: ['plain'], content: ' ' },
+  { types: ['punctuation'], content: '{' },
+  { types: ['plain'], content: '' },
+]
+
+const meta: Meta<typeof DiffLineComponent> = {
+  title: 'Components/CodeRenderer/DiffLine',
+  component: DiffLineComponent,
+  argTypes: {
+    baseCoverage: {
+      options: ['H', 'P', 'M'],
+      control: 'radio',
+    },
+    headCoverage: {
+      options: ['H', 'P', 'M'],
+      control: 'radio',
+    },
+  },
+}
+
+export default meta
+
+type Story = StoryObj<typeof DiffLineComponent>
+
+export const DiffLine: Story = {
+  render: (args) => {
+    return (
+      <MemoryRouter initialEntries={['/']}>
+        <DiffLineComponent
+          {...args}
+          lineContent={line}
+          getTokenProps={({ token, key }) => ({
+            children: token.content,
+          })}
+        />
+      </MemoryRouter>
+    )
+  },
+}
