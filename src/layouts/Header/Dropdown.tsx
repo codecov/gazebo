c39915911f7e7c45b3ad5c5b1601ea1bdb890d39
ajay-sentry <159853603+ajay-sentry@users.noreply.github.com>
2024-04-30 16:06:10 -0700

    ref: 1548 Part 1: Convert all Header files to TS (#2821)
    * ref all header files to TS
    
    * remove prop types and rebase

--- src/layouts/Header/Dropdown.tsx
+++ src/layouts/Header/Dropdown.tsx
@@ -1,6 +1,5 @@
 import cs from 'classnames'
 import { useSelect } from 'downshift'
-import PropTypes from 'prop-types'
 import { useParams } from 'react-router-dom'
 
 import config from 'config'
@@ -12,8 +11,30 @@ import Icon from 'ui/Icon'
 
 const LOCAL_STORAGE_SESSION_TRACKING_KEY = 'tracking-session-expiry'
 
-function Dropdown({ currentUser }) {
-  const { provider } = useParams()
+interface URLParams {
+  provider: string
+}
+
+type CurrentUser = {
+  user: {
+    avatarUrl: string
+    username: string
+  }
+}
+
+type itemProps = {
+  to: toProps
+  onClick?: () => void
+}
+
+type toProps = {
+  pageName: string
+  options?: object
+}
+
+// TODO: get types for free after converting useUser hook
+function Dropdown({ currentUser }: { currentUser: CurrentUser }) {
+  const { provider } = useParams<URLParams>()
   const isGh = providerToName(provider) === 'Github'
 
   const to = `${window.location.protocol}//${window.location.host}/login`
@@ -22,7 +43,7 @@ function Dropdown({ currentUser }) {
     !config.IS_SELF_HOSTED && isGh
       ? [
           {
-            props: { to: { pageName: 'codecovAppInstallation' } },
+            props: { to: { pageName: 'codecovAppInstallation' } } as itemProps,
             children: 'Install Codecov app',
           },
         ]
@@ -84,7 +105,7 @@ function Dropdown({ currentUser }) {
             'rotate-0': !isOpen,
           })}
         >
-          <Icon variant="solid" name="chevron-down" />
+          <Icon variant="solid" name="chevronDown" />
         </span>
       </button>
       <ul
@@ -102,6 +123,7 @@ function Dropdown({ currentUser }) {
               className="grid cursor-pointer text-sm first:pt-2 last:pb-2 hover:bg-ds-gray-secondary"
               {...getItemProps({ item, index })}
             >
+              {/* @ts-expect-error props might be overloaded with stuff */}
               <Button variant="listbox" {...item.props}>
                 {item.children}
               </Button>
@@ -110,15 +132,6 @@ function Dropdown({ currentUser }) {
       </ul>
     </div>
   )
-}
-
-Dropdown.propTypes = {
-  currentUser: PropTypes.shape({
-    user: PropTypes.shape({
-      avatarUrl: PropTypes.string.isRequired,
-      username: PropTypes.string.isRequired,
-    }).isRequired,
-  }),
 }
 
 export default Dropdown
--- src/layouts/Header/Header.spec.tsx
+++ src/layouts/Header/Header.spec.tsx
@@ -1,12 +1,10 @@
 import { render, screen } from '@testing-library/react'
 import { MemoryRouter, Route, Switch } from 'react-router-dom'
 
-import { useImage } from 'services/image'
 import { useUser } from 'services/user'
 
 import Header from './Header'
 
-jest.mock('services/image')
 jest.mock('services/user')
 
 const loggedInUser = {
@@ -16,24 +14,30 @@ const loggedInUser = {
   },
 }
 
-describe('Header', () => {
-  function setup({ provider }) {
-    useUser.mockReturnValue({ data: loggedInUser })
-    useImage.mockReturnValue({ src: 'imageUrl', isLoading: false, error: null })
-
-    render(
-      <MemoryRouter initialEntries={[`/${provider}`]}>
+const wrapper: (initialEntries?: string) => React.FC<React.PropsWithChildren> =
+  (initialEntries = '/gh') =>
+  ({ children }) =>
+    (
+      <MemoryRouter initialEntries={[initialEntries]}>
         <Switch>
           <Route path="/:provider" exact>
-            <Header />
+            {children}
           </Route>
         </Switch>
       </MemoryRouter>
     )
+
+describe('Header', () => {
+  function setup() {
+    const mockedUseUser = useUser as jest.Mock
+
+    mockedUseUser.mockReturnValue({ data: loggedInUser })
   }
 
   it('renders the DesktopMenu', () => {
-    setup({ provider: 'gh' })
+    setup()
+
+    render(<Header />, { wrapper: wrapper() })
     const menu = screen.getByTestId('desktop-menu')
     expect(menu).toBeInTheDocument()
   })
