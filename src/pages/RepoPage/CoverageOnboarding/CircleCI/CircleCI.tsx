e755f930b7707ccd8ad79d341165d626ba0090d9
Spencer Murray <159931558+spalmurray-codecov@users.noreply.github.com>
2024-05-01 16:31:17 -0400

    Update repo onboarding steps with new Card component (#2819)
    GH https://github.com/codecov/engineering-team/issues/1665

--- src/pages/RepoPage/CoverageOnboarding/CircleCI/CircleCI.tsx
+++ src/pages/RepoPage/CoverageOnboarding/CircleCI/CircleCI.tsx
@@ -1,11 +1,11 @@
 import { useParams } from 'react-router-dom'
 
-import patchAndProject from 'assets/repoConfig/patch-and-project.svg'
 import { useOrgUploadToken } from 'services/orgUploadToken'
 import { useRepo } from 'services/repo'
 import { useFlags } from 'shared/featureFlags'
 import { providerToInternalProvider } from 'shared/utils/provider'
 import A from 'ui/A'
+import { Card } from 'ui/Card'
 import CopyClipboard from 'ui/CopyClipboard'
 
 import ExampleBlurb from '../ExampleBlurb'
@@ -38,110 +38,142 @@ function CircleCI() {
     enabled: showOrgToken,
   })
 
-  const uploadToken = orgUploadToken ?? data?.repository?.uploadToken
+  const uploadToken = orgUploadToken ?? data?.repository?.uploadToken ?? ''
   const tokenCopy = orgUploadToken ? 'global' : 'repository'
 
   return (
     <div className="flex flex-col gap-6">
-      <div className="flex flex-col gap-3">
-        <div>
-          <h2 className="text-base font-semibold">
-            Step 1: add {tokenCopy} token to{' '}
-            <A
-              hook="circleCIEnvVarsLink"
-              isExternal
-              to={{
-                pageName: 'circleCIEnvVars',
-                options: { provider: providerName },
-              }}
-            >
-              environment variables
-            </A>
-          </h2>
-          <p className="text-base">
-            Environment variables in CircleCI can be found in project&apos;s
-            settings.
-          </p>
-        </div>
-        <pre className="flex items-center gap-2 overflow-auto rounded-md border-2 border-ds-gray-secondary bg-ds-gray-primary px-4 py-2 font-mono">
-          CODECOV_TOKEN={uploadToken}
-          <CopyClipboard string={uploadToken ?? ''} />
-        </pre>
-      </div>
-      <div className="flex flex-col gap-3">
-        <div className="text-base">
-          <h2 className="font-semibold">
-            Step 2: add Codecov orb to CircleCI{' '}
-            <A
-              hook="circleCIyamlLink"
-              isExternal
-              to={{
-                pageName: 'circleCIyaml',
-                options: { branch: data?.repository?.defaultBranch },
-              }}
-            >
-              config.yml
-            </A>
-          </h2>
-          <p>
-            Add the following to your .circleci/config.yaml and push changes to
-            repository.
-          </p>
-        </div>
-        <div className="flex items-start justify-between overflow-auto whitespace-pre-line rounded-md border-2 border-ds-gray-secondary bg-ds-gray-primary px-4 py-2 font-mono">
-          <pre className="whitespace-pre">{orbsString}</pre>
-          <CopyClipboard string={orbsString} />
-        </div>
-        <small>
-          For more, see Codecov specific{' '}
-          <A
-            to={{ pageName: 'circleCIOrbs' }}
-            isExternal
-            hook="circleCIOrbsLink"
-          >
-            CircleCI Documentation
-          </A>
-        </small>
-      </div>
-      <ExampleBlurb />
-      <div>
-        <p>
-          After you committed your changes and ran the repo&apos;s CI/CD
-          pipeline. In your pull request, you should see two status checks and
-          PR comment.
-        </p>
-        <img
-          alt="codecov patch and project"
-          src={patchAndProject.toString()}
-          className="my-3 md:px-5"
-          loading="lazy"
-        />
-        <p>
-          Once merged to the default branch, subsequent pull requests will have
-          checks and report comment. Additionally, you&apos;ll find your repo
-          coverage dashboard here.
-        </p>
-        <p className="mt-6">
-          Visit our guide to{' '}
-          <A to={{ pageName: 'quickStart' }} isExternal hook="quick-start-link">
-            learn more
-          </A>{' '}
-          about integrating Codecov into your CI/CD workflow.
-        </p>
-        <p className="mt-6 border-l-2 border-ds-gray-secondary pl-4">
-          <span className="font-semibold">How was your setup experience?</span>{' '}
-          Let us know in{' '}
-          <A
-            to={{ pageName: 'repoConfigFeedback' }}
-            isExternal
-            hook="repoConfigFeedbackLink"
-          >
-            this issue
-          </A>
-        </p>
-      </div>
+      <Step1
+        tokenCopy={tokenCopy}
+        uploadToken={uploadToken}
+        providerName={providerName}
+      />
+      <Step2 defaultBranch={data?.repository?.defaultBranch ?? ''} />
+      <Step3 />
+      <FeedbackCTA />
     </div>
   )
 }
 
 export default CircleCI
+
+interface Step1Props {
+  tokenCopy: string
+  uploadToken: string
+  providerName: string
+}
+
+function Step1({ tokenCopy, uploadToken, providerName }: Step1Props) {
+  return (
+    <Card>
+      <Card.Header>
+        <Card.Title size="base">
+          Step 1: add {tokenCopy} token to{' '}
+          <A
+            hook="circleCIEnvVarsLink"
+            isExternal
+            to={{
+              pageName: 'circleCIEnvVars',
+              options: { provider: providerName },
+            }}
+          >
+            environment variables
+          </A>
+        </Card.Title>
+      </Card.Header>
+      <Card.Content className="flex flex-col gap-4">
+        <p>
+          Environment variables in CircleCI can be found in project settings.
+        </p>
+        <div className="flex gap-4">
+          <pre className="flex basis-1/3 items-center justify-between gap-2 rounded-md border border-ds-gray-secondary bg-ds-gray-primary p-4 font-mono">
+            <div className="w-0 flex-1 overflow-hidden" data-testid="token-key">
+              CODECOV_TOKEN
+            </div>
+            <CopyClipboard string="CODECOV_TOKEN" />
+          </pre>
+          <pre className="flex basis-2/3 items-center justify-between gap-2 rounded-md border border-ds-gray-secondary bg-ds-gray-primary p-4 font-mono">
+            <div className="w-0 flex-1 overflow-hidden">{uploadToken}</div>
+            <CopyClipboard string={uploadToken ?? ''} />
+          </pre>
+        </div>
+      </Card.Content>
+    </Card>
+  )
+}
+
+interface Step2Props {
+  defaultBranch: string
+}
+
+function Step2({ defaultBranch }: Step2Props) {
+  return (
+    <Card>
+      <Card.Header>
+        <Card.Title size="base">
+          Step 2: add Codecov orb to CircleCI{' '}
+          <A
+            hook="circleCIyamlLink"
+            isExternal
+            to={{
+              pageName: 'circleCIyaml',
+              options: { branch: defaultBranch },
+            }}
+          >
+            config.yml
+          </A>
+        </Card.Title>
+      </Card.Header>
+      <Card.Content className="flex flex-col gap-4">
+        <p>
+          Add the following to your .circleci/config.yaml and push changes to
+          repository.
+        </p>
+        <div className="flex items-start justify-between overflow-auto whitespace-pre-line rounded-md border border-ds-gray-secondary bg-ds-gray-primary p-4 font-mono">
+          <pre className="whitespace-pre">{orbsString}</pre>
+          <CopyClipboard string={orbsString} />
+        </div>
+        <ExampleBlurb />
+      </Card.Content>
+    </Card>
+  )
+}
+
+function Step3() {
+  return (
+    <Card>
+      <Card.Header>
+        <Card.Title size="base">
+          Step 3: merge to main or your preferred feature branch
+        </Card.Title>
+      </Card.Header>
+      <Card.Content className="flex flex-col gap-4">
+        <p>
+          Once merged to your default branch, subsequent pull requests will have
+          Codecov checks and comments. Additionally, you’ll find your repo
+          coverage dashboard here. If you have merged try reloading the page.
+        </p>
+      </Card.Content>
+    </Card>
+  )
+}
+
+function FeedbackCTA() {
+  return (
+    <Card>
+      <Card.Content>
+        <p>
+          <span className="font-semibold">How was your setup experience?</span>{' '}
+          Let us know in{' '}
+          <A
+            to={{ pageName: 'repoConfigFeedback' }}
+            isExternal
+            hook="repo-config-feedback"
+          >
+            this issue
+          </A>
+        </p>
+      </Card.Content>
+    </Card>
+  )
+}
