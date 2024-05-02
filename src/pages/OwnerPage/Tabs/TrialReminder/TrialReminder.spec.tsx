cbfc013db01ef69f8c672d87c6337cf84da1e53a
Rola Abuhasna <91732700+RulaKhaled@users.noreply.github.com>
2024-05-02 19:40:16 +0200

    feat: Add `hasSeatsLeft` to plan query (#2827)
    * feat: Add hasSeatsLeft to plan query
    
    * update to use hasSeatsLeft

--- src/pages/OwnerPage/Tabs/TrialReminder/TrialReminder.spec.tsx
+++ src/pages/OwnerPage/Tabs/TrialReminder/TrialReminder.spec.tsx
@@ -39,6 +39,7 @@ const mockResponse = {
   trialTotalDays: 0,
   pretrialUsersCount: 0,
   planUserCount: 1,
+  hasSeatsLeft: true,
 }
 
 const wrapper: React.FC<React.PropsWithChildren> = ({ children }) => {
