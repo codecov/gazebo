4a402dbc90acda368001b1c36c37d104f8264f18
Spencer Murray <159931558+spalmurray-codecov@users.noreply.github.com>
2024-05-03 13:39:46 -0400

    feat: Implement RadioTileGroup component (#2826)
    * First pass at RadioTileGroup
    
    * Add test for selection behaviour
    
    * Use correct blue color
    
    * Remove unnecessary styles
    
    * Remove space
    
    * Add Title and Description subcomponents
    
    * Fix tests
    
    * Await user interactions in tests
    
    * Add controlled input story
    
    * Fix test
    
    * Make item label a proper input label
    
    * Use cn util for styles

---
+++ src/ui/RadioTileGroup/RadioTileGroup.tsx
@@ -0,0 +1,143 @@
+import * as LabelPrimitive from '@radix-ui/react-label'
+import * as RadioGroupPrimitive from '@radix-ui/react-radio-group'
+import { cva, VariantProps } from 'cva'
+import React, { createContext, useContext, useId } from 'react'
+
+import { cn } from 'shared/utils/cn'
+
+const group = cva(['flex', 'gap-4'], {
+  variants: {
+    direction: {
+      row: 'flex-row',
+      col: 'flex-col',
+    },
+  },
+  defaultVariants: {
+    direction: 'row',
+  },
+})
+interface GroupProps
+  extends React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Root>,
+    VariantProps<typeof group> {}
+
+const Group = React.forwardRef<
+  React.ElementRef<typeof RadioGroupPrimitive.Root>,
+  GroupProps
+>(({ className, direction, ...props }, ref) => {
+  return (
+    <RadioGroupPrimitive.Root
+      className={cn(group({ className, direction }))}
+      {...props}
+      ref={ref}
+    />
+  )
+})
+Group.displayName = 'RadioTileGroup'
+
+const item = cva(['relative'], {
+  variants: {
+    flex: {
+      1: 'flex-1',
+      none: 'flex-none',
+    },
+  },
+  defaultVariants: {
+    flex: 1,
+  },
+})
+interface ItemProps
+  extends React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item>,
+    VariantProps<typeof item> {}
+const ItemContext = createContext<string | null>(null)
+
+const Item = React.forwardRef<
+  React.ElementRef<typeof RadioGroupPrimitive.Item>,
+  ItemProps
+>(({ children, className, flex, ...props }, ref) => {
+  const itemId = useId()
+  return (
+    <RadioGroupPrimitive.Item
+      id={itemId}
+      ref={ref}
+      className={cn(item({ className, flex }))}
+      {...props}
+    >
+      <ItemContext.Provider value={itemId}>
+        <div className="flex h-full flex-col gap-2 rounded-md border border-ds-gray-quaternary p-4">
+          {children}
+        </div>
+        <RadioGroupPrimitive.Indicator className="absolute right-0 top-0 h-full w-full">
+          <div className="absolute h-full w-full rounded-md border-2 border-ds-blue-darker" />
+          <div className="h-full w-full rounded-md border border-ds-blue-darker p-4">
+            <div className="flex h-5 w-full items-center justify-end">
+              <RadioButtonCircle selected />
+            </div>
+          </div>
+        </RadioGroupPrimitive.Indicator>
+      </ItemContext.Provider>
+    </RadioGroupPrimitive.Item>
+  )
+})
+Item.displayName = 'RadioTileGroup.Item'
+
+const label = cva(['font-medium'])
+interface LabelProps
+  extends React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>,
+    VariantProps<typeof label> {}
+
+const Label = React.forwardRef<
+  React.ElementRef<typeof LabelPrimitive.Root>,
+  LabelProps
+>(({ className, ...props }, ref) => {
+  const itemId = useContext(ItemContext)
+  return (
+    <div className="flex items-center justify-between gap-4">
+      <LabelPrimitive.Root
+        {...{ htmlFor: itemId ?? undefined }}
+        ref={ref}
+        className={cn(label({ className }))}
+        {...props}
+      />
+      <RadioButtonCircle />
+    </div>
+  )
+})
+Label.displayName = 'RadioTileGroup.Label'
+
+const description = cva(['text-left text-ds-gray-quinary'])
+interface DescriptionProps
+  extends React.HTMLAttributes<HTMLParagraphElement>,
+    VariantProps<typeof description> {}
+
+const Description = React.forwardRef<HTMLParagraphElement, DescriptionProps>(
+  ({ children, className, ...props }, ref) => {
+    return (
+      <p ref={ref} className={cn(description({ className }))} {...props}>
+        {children}
+      </p>
+    )
+  }
+)
+Description.displayName = 'RadioTileGroup.Description'
+
+function RadioButtonCircle({ selected = false }: { selected?: boolean }) {
+  return selected ? (
+    <div className="flex h-4 w-4 items-center justify-center rounded-full bg-ds-blue-darker">
+      <div
+        className="h-1 w-1 rounded-full bg-white"
+        data-testid="radio-button-circle-selected"
+      />
+    </div>
+  ) : (
+    <div
+      className="h-4 w-4 rounded-full border border-ds-gray-quaternary"
+      data-testid="radio-button-circle-unselected"
+    />
+  )
+}
+
+export const RadioTileGroup = Object.assign(Group, {
+  Item,
+  Label,
+  Description,
+})
