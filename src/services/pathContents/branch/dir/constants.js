7aa791c1fcb4abd1cd5053c8ab62e0bcdb47d568
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-05-06 15:22:59 -0400

    update query off of repositoryDeprecated

--- src/services/pathContents/branch/dir/constants.js
+++ src/services/pathContents/branch/dir/constants.js
@@ -1,48 +1,57 @@
 export const query = `
-    query BranchContents(
-      $name: String!
-      $repo: String!
-      $branch: String!
-      $path: String!
-      $filters: PathContentsFilters!
-    ) {
-      owner(username: $name) {
-        username
-        repository: repositoryDeprecated(name: $repo) {
-          repositoryConfig {
-            indicationRange {
-              upperRange
-              lowerRange
-            }
+query BranchContents(
+  $name: String!
+  $repo: String!
+  $branch: String!
+  $path: String!
+  $filters: PathContentsFilters!
+) {
+  owner(username: $name) {
+    username
+    repository(name: $repo) {
+      __typename
+      ... on Repository {
+        repositoryConfig {
+          indicationRange {
+            upperRange
+            lowerRange
           }
-          branch(name: $branch) {
-            head {
-              pathContents(path: $path, filters: $filters) {
-                ... on PathContents {
-                  results {
-                    __typename
-                    hits
-                    misses
-                    partials
-                    lines
-                    name
-                    path
-                    percentCovered
-                    ... on PathContentFile {
-                      isCriticalFile
-                    }
+        }
+        branch(name: $branch) {
+          head {
+            pathContents(path: $path, filters: $filters) {
+              __typename
+              ... on PathContents {
+                results {
+                  __typename
+                  hits
+                  misses
+                  partials
+                  lines
+                  name
+                  path
+                  percentCovered
+                  ... on PathContentFile {
+                    isCriticalFile
                   }
                 }
-                ... on UnknownPath {
-                  message
-                }
-                ... on MissingCoverage {
-                  message
-                }
-                __typename
+              }
+              ... on UnknownPath {
+                message
+              }
+              ... on MissingCoverage {
+                message
               }
             }
           }
         }
       }
-    }`
+      ... on NotFoundError {
+        message
+      }
+      ... on OwnerNotActivatedError {
+        message
+      }
+    }
+  }
+}`
