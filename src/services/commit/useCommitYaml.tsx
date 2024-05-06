2eab514bc1cc29e0fc763cdcfdc8d65b12b14af5
Spencer Murray <spencer.murray@sentry.io>
2024-05-06 15:10:19 -0400

    Convert UseCommitYaml to Repository from RepositoryDeprecated

---
+++ src/services/commit/useCommitYaml.tsx
@@ -0,0 +1,123 @@
+import { useQuery } from '@tanstack/react-query'
+import { z } from 'zod'
+
+import {
+  RepoNotFoundErrorSchema,
+  RepoOwnerNotActivatedErrorSchema,
+} from 'services/repo'
+import Api from 'shared/api'
+import { NetworkErrorObject } from 'shared/api/helpers'
+import A from 'ui/A'
+
+const RepositorySchema = z.object({
+  __typename: z.literal('Repository'),
+  commit: z
+    .object({
+      commitid: z.string(),
+      yaml: z.string().nullable(),
+    })
+    .nullable(),
+})
+
+const RequestSchema = z.object({
+  owner: z
+    .object({
+      repository: z
+        .discriminatedUnion('__typename', [
+          RepositorySchema,
+          RepoNotFoundErrorSchema,
+          RepoOwnerNotActivatedErrorSchema,
+        ])
+        .nullable(),
+    })
+    .nullable(),
+})
+
+const query = `
+query CommitYaml($owner: String!, $repo: String!, $commitid: String!) {
+  owner(username: $owner) {
+    repository: repository(name: $repo) {
+      __typename
+      ... on Repository {
+        commit(id: $commitid) {
+          commitid
+          yaml
+        }
+      }
+      ... on NotFoundError {
+        message
+      }
+      ... on OwnerNotActivatedError {
+        message
+      }
+    }
+  }
+}`
+
+interface UseCommitYamlArgs {
+  provider: string
+  owner: string
+  repo: string
+  commitid: string
+}
+
+export function useCommitYaml({
+  provider,
+  owner,
+  repo,
+  commitid,
+}: UseCommitYamlArgs) {
+  return useQuery({
+    queryKey: ['commit-yaml', provider, owner, repo, commitid],
+    queryFn: ({ signal }) =>
+      Api.graphql({
+        provider,
+        query,
+        signal,
+        variables: {
+          owner,
+          repo,
+          commitid,
+        },
+      }).then((res) => {
+        const parsedData = RequestSchema.safeParse(res?.data)
+
+        if (!parsedData.success) {
+          return Promise.reject({
+            status: 404,
+            data: {},
+            dev: 'useCommitYaml - 404 Failed to parse data',
+          } satisfies NetworkErrorObject)
+        }
+
+        const data = parsedData.data
+
+        if (data?.owner?.repository?.__typename === 'NotFoundError') {
+          return Promise.reject({
+            status: 404,
+            data: {},
+            dev: 'useCommitYaml - 404 Not found error',
+          } satisfies NetworkErrorObject)
+        }
+
+        if (data?.owner?.repository?.__typename === 'OwnerNotActivatedError') {
+          return Promise.reject({
+            status: 403,
+            data: {
+              detail: (
+                <p>
+                  Activation is required to view this repo, please{' '}
+                  {/* @ts-expect-error */}
+                  <A to={{ pageName: 'membersTab' }}>click here </A> to activate
+                  your account.
+                </p>
+              ),
+            },
+            dev: 'useCommitYaml - 403 Owner not activated',
+          } satisfies NetworkErrorObject)
+        }
+
+        return data.owner?.repository?.commit?.yaml
+      }),
+  })
+}
