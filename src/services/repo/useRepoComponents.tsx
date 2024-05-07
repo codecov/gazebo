b15fa496047e797dd68c543b948cef88abfcacb6
JerrySentry <jerry.feng@sentry.io>
2024-05-07 12:51:35 -0400

    components over time display name instead of id

--- src/services/repo/useRepoComponents.tsx
+++ src/services/repo/useRepoComponents.tsx
@@ -33,6 +33,7 @@ query ComponentMeasurements(
           before: $before
           branch: $branch
         ) {
+          componentId
           name
           percentCovered
           percentChange
@@ -48,6 +49,7 @@ query ComponentMeasurements(
 `
 
 export const ComponentEdgeSchema = z.object({
+  componentId: z.string(),
   name: z.string(),
   percentCovered: z.number().nullable(),
   percentChange: z.number().nullable(),
