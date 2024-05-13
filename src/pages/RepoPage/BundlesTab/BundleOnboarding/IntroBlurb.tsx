9b643e039b73ba3f958b3fbeab3101f8507188a3
Spencer Murray <spencer.murray@sentry.io>
2024-05-13 16:14:33 -0400

    Modify BA intro blurb copy

--- src/pages/RepoPage/BundlesTab/BundleOnboarding/IntroBlurb.tsx
+++ src/pages/RepoPage/BundlesTab/BundleOnboarding/IntroBlurb.tsx
@@ -8,12 +8,8 @@ const IntroBlurb = () => {
       <div className="flex flex-col gap-2" data-testid="ba-intro-blurb">
         <h2 className="text-base font-semibold">Configure Bundle Analysis</h2>
         <p>
-          Bundle Analysis helps improve your application&apos;s performance,
-          bandwidth usage, and load times by letting you know if what you’re
-          about to merge will cause any performance regressions. It will allow
-          you to explore all of the modules in your bundle and determine where
-          you might be able to streamline your bundle size as well as find areas
-          of concern.
+          Before integrating with Codecov, ensure that your project already uses
+          one of the bundlers below as we use them to generate our analysis.
         </p>
         <A
           to={{ pageName: 'bundleAnalysisDocs' }}
