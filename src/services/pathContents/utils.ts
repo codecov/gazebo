da4a048f816a77ea1f64ee0f9b8aa46547420059
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-05-13 15:52:57 -0300

    small TS refactors

--- src/services/pathContents/utils.ts
+++ src/services/pathContents/utils.ts
@@ -1,22 +1,31 @@
 import keyBy from 'lodash/keyBy'
 import mapValues from 'lodash/mapValues'
 
-export function extractCoverageFromResponse(res) {
-  const commit = res?.data?.owner?.repository?.commit
-  const branch = res?.data?.owner?.repository?.branch?.head
+import { PathContentsRepositorySchema } from './constants'
+
+export function extractCoverageFromResponse(
+  repository: PathContentsRepositorySchema | undefined | null
+) {
+  if (!repository) return null
+  const commit = repository?.commit
+  const branch = repository?.branch?.head
   const coverageSource = commit || branch
   const coverageFile = coverageSource?.coverageFile
   if (!coverageFile) return null
   const lineWithCoverage = keyBy(coverageFile?.coverage, 'line')
   const fileCoverage = mapValues(lineWithCoverage, 'coverage')
-  const coverageTotal = coverageFile?.totals?.coverage
+  const coverageTotal = coverageFile?.totals?.percentCovered
   const hashedPath = coverageFile?.hashedPath
 
   return {
     content: coverageFile?.content,
     coverage: fileCoverage,
-    totals: isNaN(coverageTotal) ? 0 : coverageTotal,
+    totals:
+      typeof coverageTotal !== 'number' || isNaN(coverageTotal)
+        ? 0
+        : coverageTotal,
     flagNames: coverageSource?.flagNames ?? [],
+    componentNames: coverageSource?.components?.map(({ name }) => name) ?? [],
     isCriticalFile: !!coverageFile?.isCriticalFile,
     ...(hashedPath && { hashedPath }),
   }
