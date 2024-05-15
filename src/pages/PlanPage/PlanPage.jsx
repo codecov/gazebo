059a9bcd7de37806a18e7a9648972e01b24fd559
ajay-sentry <159853603+ajay-sentry@users.noreply.github.com>
2024-05-15 14:29:21 -0700

    dev: Upgrade stripe components and pin stripe version to V8 (#2879)

--- src/pages/PlanPage/PlanPage.jsx
+++ src/pages/PlanPage/PlanPage.jsx
@@ -21,7 +21,9 @@ const InvoicesPage = lazy(() => import('./subRoutes/InvoicesPage'))
 const InvoiceDetailsPage = lazy(() => import('./subRoutes/InvoiceDetailsPage'))
 const UpgradePlanPage = lazy(() => import('./subRoutes/UpgradePlanPage'))
 
-const stripePromise = loadStripe(config.STRIPE_KEY)
+const stripePromise = loadStripe(config.STRIPE_KEY, {
+  apiVersion: '2023-10-16',
+})
 const path = '/plan/:provider/:owner'
 
 const Loader = () => (
