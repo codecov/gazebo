31af7e70d174d168bd47e248aa1d50a492e72acc
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-06-04 11:37:49 -0300

    fix: Small fix to remove jumping tab text (#2916)

--- src/pages/RepoPage/FailedTestsTab/GitHubActions/FrameworkTabs/FrameworkTabs.tsx
+++ src/pages/RepoPage/FailedTestsTab/GitHubActions/FrameworkTabs/FrameworkTabs.tsx
@@ -1,8 +1,7 @@
-import cs from 'classnames'
 import { useState } from 'react'
 
+import { cn } from 'shared/utils/cn'
 import { CodeSnippet } from 'ui/CodeSnippet'
-import { CopyClipboard } from 'ui/CopyClipboard'
 
 const Frameworks = {
   PYTEST: 'Pytest',
@@ -14,10 +13,11 @@ const Frameworks = {
 type FrameworkType = (typeof Frameworks)[keyof typeof Frameworks]
 
 const FrameworkCopy = {
-  Pytest: 'pytest --cov --junitxml=junit.xml',
-  Vitest: 'vitest --reporter=junit',
-  Jest: 'npm i --save-dev jest-junit \njest --reporters=jest-junit',
-  PHPunit: './vendor/bin/phpunit --log-junit junit.xml',
+  [Frameworks.PYTEST]: 'pytest --cov --junitxml=junit.xml',
+  [Frameworks.VITEST]: 'vitest --reporter=junit',
+  [Frameworks.JEST]:
+    'npm i --save-dev jest-junit \njest --reporters=jest-junit',
+  [Frameworks.PHP_UNIT]: './vendor/bin/phpunit --log-junit junit.xml',
 } as const
 
 export function FrameworkTabs() {
@@ -27,27 +27,22 @@ export function FrameworkTabs() {
 
   return (
     <div>
-      <div className="flex justify-between">
-        <div className="flex gap-1">
-          {Object.keys(FrameworkCopy).map((f) => (
-            <button
-              key={f}
-              className={cs(
-                'px-4 py-2',
-                selectedFramework === f && 'border-b-2 border-ds-gray-octonary'
-              )}
-              onClick={() => setSelectedFramework(f as FrameworkType)}
-            >
-              {f}
-            </button>
-          ))}
-        </div>
+      <div className="flex gap-1">
+        {Object.values(Frameworks).map((framework) => (
+          <button
+            key={framework}
+            className={cn(
+              'px-4 py-2 border-b-2 border-transparent',
+              selectedFramework === framework && 'border-ds-gray-octonary'
+            )}
+            onClick={() => setSelectedFramework(framework)}
+          >
+            {framework}
+          </button>
+        ))}
       </div>
-      <CodeSnippet>
-        <div className="flex justify-between">
-          {FrameworkCopy[selectedFramework]}
-          <CopyClipboard value={FrameworkCopy[selectedFramework]} />
-        </div>
+      <CodeSnippet clipboard={FrameworkCopy[selectedFramework]}>
+        {FrameworkCopy[selectedFramework]}
       </CodeSnippet>
     </div>
   )
