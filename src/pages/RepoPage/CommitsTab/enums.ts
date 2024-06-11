bc3383d970bc4ceae0cbe096011a9edf6c8bfaa3
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-06-11 07:58:58 -0300

    remove skipped and add completed to commitstab enum

--- src/pages/RepoPage/CommitsTab/enums.ts
+++ src/pages/RepoPage/CommitsTab/enums.ts
@@ -1,20 +1,17 @@
 export const statusEnum = {
-  Complete: { status: 'COMPLETE', name: 'Complete' },
+  Completed: { status: 'COMPLETED', name: 'Completed' },
   Pending: { status: 'PENDING', name: 'Pending' },
   Error: { status: 'ERROR', name: 'Error' },
-  Skipped: { status: 'SKIPPED', name: 'Skipped' },
 } as const
 
 export const filterItems = [
-  statusEnum.Complete.name,
+  statusEnum.Completed.name,
   statusEnum.Pending.name,
   statusEnum.Error.name,
-  statusEnum.Skipped.name,
 ] as const
 
 export const statusNames = {
-  COMPLETE: statusEnum.Complete.name,
+  COMPLETED: statusEnum.Completed.name,
   PENDING: statusEnum.Pending.name,
   ERROR: statusEnum.Error.name,
-  SKIPPED: statusEnum.Skipped.name,
 } as const
