8a6ad8dd4679204d39d7271a53edccce63011148
Calvin Yau <calvin.yau@sentry.io>
2024-06-18 16:23:32 -0700

    chore: add a comma

--- src/pages/RepoPage/CoverageOnboarding/OtherCI/OtherCI.tsx
+++ src/pages/RepoPage/CoverageOnboarding/OtherCI/OtherCI.tsx
@@ -132,7 +132,7 @@ function Step4() {
         <p>
           Once merged to your default branch, subsequent pull requests will have
           Codecov checks and comments. Additionally, you’ll find your repo
-          coverage dashboard here. If you have merged try reloading the page.
+          coverage dashboard here. If you have merged, try reloading the page.
         </p>
       </Card.Content>
     </Card>
