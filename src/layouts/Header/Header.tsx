bb2f4b4184fc3ea644c4567a955da9f114e7b9e2
le.minh.hoang <le.minh.hoang@sun-asterisk.com>
2024-06-24 16:05:11 +0700

    feat: self-hosted FE 24.6.12

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -9,17 +9,7 @@ function Header() {
   })
 
   return (
-<<<<<<< HEAD
-    <header
-      className={cs('text-white', {
-        'bg-white  border-b-2': !isImpersonating && !currentUser,
-        'bg-ds-primary-base': !isImpersonating && !!currentUser,
-        'bg-ds-pink-tertiary': isImpersonating,
-      })}
-    >
-=======
     <header className="border-b-2 bg-ds-primary-base text-white">
->>>>>>> feat: codecov 24.5.1-rc2 for apply athena service
       <nav className="container mx-auto flex flex-wrap items-center justify-between gap-2 px-3 py-4 sm:px-0">
         {currentUser ? <DesktopMenu /> : <GuestHeader />}
       </nav>
