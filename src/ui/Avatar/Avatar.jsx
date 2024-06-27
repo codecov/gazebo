effc2e7fc4aee27318c1b2729494cc6c8159c601
Spencer Murray <spencer.murray@sentry.io>
2024-06-27 14:50:27 -0400

    Update Avatar to have light and dark border option

--- src/ui/Avatar/Avatar.jsx
+++ src/ui/Avatar/Avatar.jsx
@@ -6,10 +6,15 @@ import { useImage } from 'services/image'
 import AvatarSVG from './AvatarSVG'
 
 let baseClasses = 'rounded-full h-6 w-6 flex items-center justify-center'
-let borderedClasses = 'border-ds-grey-secondary border-2'
+let lightBorderedClasses = 'border-ds-grey-secondary border-2'
+let darkBorderedClasses = 'border-ds-grey-octonary border-2'
 
-function Avatar({ user, bordered, ariaLabel }) {
-  const classes = cs(baseClasses, bordered ? borderedClasses : '')
+function Avatar({ user, lightBordered, darkBordered, ariaLabel }) {
+  const classes = cs(
+    baseClasses,
+    lightBordered ? lightBorderedClasses : '',
+    darkBordered ? darkBorderedClasses : ''
+  )
 
   const { src, error, isLoading } = useImage({
     src: user?.avatarUrl,
@@ -36,7 +41,8 @@ Avatar.propTypes = {
     username: PropTypes.string.isRequired,
     avatarUrl: PropTypes.string,
   }),
-  bordered: PropTypes.bool,
+  lightBordered: PropTypes.bool,
+  darkBordered: PropTypes.bool,
   ariaLabel: PropTypes.string,
 }
 
