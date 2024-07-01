f84c189da2ee46ab49ef3462970b79ee5034f711
Rula Abu Hasna <rula@codecov.io>
2024-07-01 13:46:02 +0300

    feat: Create new okta banners

---
+++ src/shared/GlobalTopBanners/OktaEnabledBanner/OktaEnforcedBanner/OktaEnabledBanner.spec.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/react'
+import { MemoryRouter, Route } from 'react-router-dom'
+
+import OktaEnabledBanner from './OktaEnabledBanner'
+
+const wrapper: React.FC<React.PropsWithChildren> = ({ children }) => (
+  <MemoryRouter initialEntries={['/gh/codecov']}>
+    <Route path="/:provider/:owner">{children}</Route>
+  </MemoryRouter>
+)
+
+describe('OktaEnabledBanner', () => {
+  it('should render content', () => {
+    render(<OktaEnabledBanner />, { wrapper })
+
+    const content = screen.getByText(
+      / this will be the only way to access private repositories for this organization./
+    )
+    expect(content).toBeInTheDocument()
+  })
+
+  it('should render link', () => {
+    render(<OktaEnabledBanner />, { wrapper })
+
+    const link = screen.getByRole('link', { name: /Authenticate/ })
+    expect(link).toBeInTheDocument()
+    expect(link).toHaveAttribute('href', '/login/okta')
+  })
+})
---
+++ src/shared/GlobalTopBanners/OktaEnabledBanner/OktaEnforcedBanner/OktaEnabledBanner.tsx
@@ -0,0 +1,35 @@
+import Button from 'ui/Button'
+import Icon from 'ui/Icon'
+import TopBanner from 'ui/TopBanner'
+
+const OktaEnabledBanner = () => {
+  return (
+    <TopBanner>
+      <TopBanner.Start>
+        <p className="items-center gap-1 md:flex">
+          <span className="flex items-center gap-1 font-semibold">
+            <Icon name="informationCircle" />
+            Single sign-on has been enabled for Turing-Corp.
+          </span>
+          In the future, this will be the only way to access private
+          repositories for this organization.
+        </p>
+      </TopBanner.Start>
+      <TopBanner.End>
+        <Button
+          to={{
+            pageName: 'signIn',
+            options: { provider: 'okta' },
+          }}
+          hook=""
+          disabled={false}
+          variant="primary"
+        >
+          Authenticate
+        </Button>
+      </TopBanner.End>
+    </TopBanner>
+  )
+}
+
+export default OktaEnabledBanner
