40e208e2843d9b8c5198f6628503dc3f1ef900e5
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-07-05 14:22:21 -0300

    refactor timeseries utils to TS

--- src/shared/utils/timeseriesCharts.spec.ts
+++ src/shared/utils/timeseriesCharts.spec.ts
@@ -1,8 +1,8 @@
 import {
   analyticsQuery,
   calculateDayDifference,
+  createTimeSeriesQueryVars,
   getTrendEnum,
-  timeseriesRepoCoverageQuery,
   Trend,
 } from './timeseriesCharts'
 
@@ -46,36 +46,48 @@ describe.each`
   })
 })
 
-describe('chartQuery', () => {
+describe('analyticsQuery', () => {
   // Craft the query to be sent to graphql
   describe('interval', () => {
     describe('when the day difference is greater then one', () => {
       describe('day difference is less then day threshold', () => {
         it('returns single day interval', () => {
-          const startDate = '2020-01-01'
-          const endDate = '2020-01-03'
+          const startDate = new Date('2020-01-01')
+          const endDate = new Date('2020-01-03')
 
-          const { interval } = analyticsQuery({ startDate, endDate })
+          const { interval } = analyticsQuery({
+            startDate,
+            endDate,
+            repositories: [],
+          })
           expect(interval).toBe('INTERVAL_1_DAY')
         })
       })
 
       describe('day difference is between day and month threshold', () => {
         it('returns week interval', () => {
-          const startDate = '2020-01-01'
-          const endDate = '2020-07-19'
+          const startDate = new Date('2020-01-01')
+          const endDate = new Date('2020-07-19')
 
-          const { interval } = analyticsQuery({ startDate, endDate })
+          const { interval } = analyticsQuery({
+            startDate,
+            endDate,
+            repositories: [],
+          })
           expect(interval).toBe('INTERVAL_7_DAY')
         })
       })
 
       describe('when day difference is greater then month threshold', () => {
         it('returns month interval', () => {
-          const startDate = '2020-01-01'
-          const endDate = '2020-12-31'
+          const startDate = new Date('2020-01-01')
+          const endDate = new Date('2020-12-31')
 
-          const { interval } = analyticsQuery({ startDate, endDate })
+          const { interval } = analyticsQuery({
+            startDate,
+            endDate,
+            repositories: [],
+          })
           expect(interval).toBe('INTERVAL_30_DAY')
         })
       })
@@ -83,28 +95,35 @@ describe('chartQuery', () => {
 
     describe('when the day difference is less than one', () => {
       it('returns week interval', () => {
-        const startDate = '2020-01-01'
-        const endDate = '2020-01-01'
+        const startDate = new Date('2020-01-01')
+        const endDate = new Date('2020-01-01')
 
-        const { interval } = analyticsQuery({ startDate, endDate })
+        const { interval } = analyticsQuery({
+          startDate,
+          endDate,
+          repositories: [],
+        })
         expect(interval).toBe('INTERVAL_7_DAY')
       })
     })
   })
+
   describe('startDate', () => {
     it('correct param is passed', () => {
-      const startDate = '2019-12-31T14'
+      const startDate = new Date('2019-12-31T14')
       expect(analyticsQuery({ startDate }).startDate).toBe(startDate)
     })
+
     it('incorrect param is passed', () => {
       expect(analyticsQuery({}).startDate).toBe(undefined)
     })
   })
   describe('endDate', () => {
     it('correct param is passed', () => {
-      const endDate = '2019-12-31T14'
+      const endDate = new Date('2019-12-31T14')
       expect(analyticsQuery({ endDate }).endDate).toBe(endDate)
     })
+
     it('incorrect param is passed', () => {
       expect(analyticsQuery({}).endDate).toBe(undefined)
     })
@@ -115,16 +134,17 @@ describe('chartQuery', () => {
       expect(analyticsQuery({ repositories }).repositories).toBe(repositories)
     })
     it('An empty array', () => {
-      const repositories = []
+      const repositories: string[] = []
       expect(analyticsQuery({ repositories }).repositories).toBe(undefined)
     })
+
     it('nothing is passed', () => {
       expect(analyticsQuery({}).repositories).toBe(undefined)
     })
   })
 })
 
-describe('timeseriesRepoCoverageQuery', () => {
+describe('createTimeSeriesQueryVars', () => {
   beforeEach(() => {
     jest.useFakeTimers()
     jest.setSystemTime(new Date('2022/01/01'))
@@ -135,107 +155,80 @@ describe('timeseriesRepoCoverageQuery', () => {
   describe('dynamic params', () => {
     it('sets the start date', () => {
       expect(
-        timeseriesRepoCoverageQuery({
-          repo: 'critical role',
-          trend: Trend.SEVEN_DAYS,
-          branch: 'main',
+        createTimeSeriesQueryVars({
           today: new Date(),
-        }).startDate
+          trend: Trend.SEVEN_DAYS,
+          oldestCommit: new Date('2010-01-01'),
+        }).after
       ).toStrictEqual(new Date('2021/12/25'))
     })
 
-    it('If today not set do not return a startDate', () => {
-      expect(
-        timeseriesRepoCoverageQuery({
-          repo: 'critical role',
-          trend: Trend.ALL_TIME,
-          branch: 'main',
-        }).startDate
-      ).toBe(undefined)
-    })
-
     describe('groupingUnit', () => {
-      it('invalid trend', () => {
-        const { interval } = timeseriesRepoCoverageQuery({
-          repo: 'critical role',
-          branch: 'main',
-          today: new Date(),
-        })
-
-        expect(interval).toBe('INTERVAL_30_DAY')
-      })
-
       it('no start', () => {
-        const { interval } = timeseriesRepoCoverageQuery({
-          repo: 'critical role',
+        const { interval } = createTimeSeriesQueryVars({
           trend: Trend.ALL_TIME,
-          branch: 'main',
+          today: new Date(),
+          oldestCommit: new Date('2010-01-01'),
         })
 
         expect(interval).toBe('INTERVAL_30_DAY')
       })
 
       it('all time', () => {
-        const { interval } = timeseriesRepoCoverageQuery({
-          repo: 'critical role',
+        const { interval } = createTimeSeriesQueryVars({
           trend: Trend.ALL_TIME,
-          branch: 'main',
           today: new Date(),
+          oldestCommit: new Date('2010-01-01'),
         })
 
         expect(interval).toBe('INTERVAL_30_DAY')
       })
 
       it('last year', () => {
-        const { interval } = timeseriesRepoCoverageQuery({
-          repo: 'critical role',
+        const { interval } = createTimeSeriesQueryVars({
           trend: Trend.TWELVE_MONTHS,
-          branch: 'main',
           today: new Date(),
+          oldestCommit: new Date('2010-01-01'),
         })
 
         expect(interval).toBe('INTERVAL_30_DAY')
       })
 
       it('seven days', () => {
-        const { interval } = timeseriesRepoCoverageQuery({
-          repo: 'critical role',
+        const { interval } = createTimeSeriesQueryVars({
           trend: Trend.SEVEN_DAYS,
-          branch: 'main',
           today: new Date(),
+          oldestCommit: new Date('2010-01-01'),
         })
 
         expect(interval).toBe('INTERVAL_1_DAY')
       })
 
       it('six months', () => {
-        const { interval } = timeseriesRepoCoverageQuery({
-          repo: 'critical role',
+        const { interval } = createTimeSeriesQueryVars({
           trend: Trend.SIX_MONTHS,
-          branch: 'main',
           today: new Date(),
+          oldestCommit: new Date('2010-01-01'),
         })
 
         expect(interval).toBe('INTERVAL_7_DAY')
       })
 
       it('30 days', () => {
-        const { interval } = timeseriesRepoCoverageQuery({
-          repo: 'critical role',
+        const { interval } = createTimeSeriesQueryVars({
           trend: Trend.THIRTY_DAYS,
-          branch: 'main',
           today: new Date(),
+          oldestCommit: new Date('2010-01-01'),
         })
 
         expect(interval).toBe('INTERVAL_1_DAY')
       })
 
       it('3 months', () => {
-        const { interval } = timeseriesRepoCoverageQuery({
-          repo: 'critical role',
+        const { interval } = createTimeSeriesQueryVars({
           trend: Trend.THREE_MONTHS,
-          branch: 'main',
           today: new Date(),
+          oldestCommit: new Date('2010-01-01'),
         })
 
         expect(interval).toBe('INTERVAL_7_DAY')
