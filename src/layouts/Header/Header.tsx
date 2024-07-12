a228e3723ccc04263ff250e8948959f030a0beb4
Spencer Murray <spencer.murray@sentry.io>
2024-07-12 14:56:20 -0400

    Add selfhosted admin breadcrumb

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -11,16 +11,16 @@ import SeatDetails from './components/SeatDetails'
 import UserDropdown from './components/UserDropdown'
 
 function Header() {
-  const { data: currentUser } = useUser()
+  const currentUser = useUser()
 
-  if (!currentUser) {
+  if (!currentUser.data) {
     return <h1>Guest header</h1>
   }
 
   return (
     <div className="container flex h-14 w-full items-center">
       <div className="flex-1">
-        <Navigator />
+        <Navigator currentUser={currentUser} />
       </div>
       <div className="flex items-center justify-end gap-4">
         {config.IS_SELF_HOSTED ? (
--- src/layouts/Header/components/Navigator/Navigator.tsx
+++ src/layouts/Header/components/Navigator/Navigator.tsx
@@ -1,18 +1,44 @@
 import { useRouteMatch } from 'react-router-dom'
 
 import { useCrumbs } from 'pages/RepoPage/context'
+import { useUser } from 'services/user'
 import Breadcrumb from 'ui/Breadcrumb'
 
 import MyContextSwitcher from './MyContextSwitcher'
 
-function Navigator() {
+interface NavigatorProps {
+  currentUser: ReturnType<typeof useUser>
+}
+
+function Navigator({ currentUser }: NavigatorProps) {
   const { path } = useRouteMatch()
   const { breadcrumbs } = useCrumbs()
 
+  // Repo page
   if (path.startsWith('/:provider/:owner/:repo')) {
     return <Breadcrumb paths={breadcrumbs} largeFont />
   }
 
+  // Selfhosted admin settings
+  if (path.startsWith('/admin/:provider')) {
+    const defaultOrg =
+      currentUser.data?.owner?.defaultOrgUsername ??
+      currentUser.data?.user?.username
+    return (
+      <Breadcrumb
+        paths={[
+          {
+            pageName: 'owner',
+            text: defaultOrg,
+            options: { owner: defaultOrg },
+          },
+          { pageName: '', readOnly: true, text: 'Admin' },
+        ]}
+      />
+    )
+  }
+
+  // Everything else uses MyContextSwitcher
   let pageName = 'owner'
   if (path.startsWith('/analytics/:provider/:owner')) {
     pageName = 'analytics'
