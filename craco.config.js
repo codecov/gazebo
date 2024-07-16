f8cdb2af0786a77ed0d655411723ed64439442cb
Ajay Singh <ajay.singh@sentry.io>
2024-07-16 13:28:30 -0700

    see if this fixes test stuff, with new babelconfig things

--- craco.config.js
+++ craco.config.js
@@ -17,6 +17,25 @@ const SentryPlugin = sentryWebpackPlugin({
 })
 
 module.exports = {
+  babel: {
+    test: /\.(js|jsx)$/,
+    exclude: /(node_modules|bower_components)/,
+    loader: 'babel-loader',
+    presets: [
+      '@babel/preset-env',
+      ['@babel/preset-react', { runtime: 'automatic' }],
+    ],
+    plugins: [
+      [
+        '@babel/plugin-transform-private-property-in-object',
+        {
+          loose: true,
+        },
+      ],
+      ['@babel/plugin-transform-class-properties', { loose: true }],
+      ['@babel/plugin-transform-private-methods', { loose: true }],
+    ],
+  },
   webpack: {
     devtool: 'source-map',
     configure: {
