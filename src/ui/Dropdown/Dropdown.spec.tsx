fcdf987d21ca244d528c95016c54e51e356389e2
Calvin Yau <calvin.yau@sentry.io>
2024-07-16 10:58:28 -0700

    feat: new ui dropdown component

---
+++ src/ui/Dropdown/Dropdown.spec.tsx
@@ -0,0 +1,107 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import { Dropdown } from './Dropdown'
+
+describe('Dropdown', () => {
+  function setup() {
+    const user = userEvent.setup()
+
+    return { user }
+  }
+
+  describe('rendering component', () => {
+    it('only renders the trigger', () => {
+      render(
+        <Dropdown>
+          <Dropdown.Trigger>My Dropdown</Dropdown.Trigger>
+          <Dropdown.Content>
+            <Dropdown.Item>First item</Dropdown.Item>
+            <Dropdown.Item>Second item</Dropdown.Item>
+            <Dropdown.Item>Third item</Dropdown.Item>
+          </Dropdown.Content>
+        </Dropdown>
+      )
+
+      const firstTitle = screen.getByText('My Dropdown')
+      expect(firstTitle).toBeInTheDocument()
+
+      const secondTitle = screen.queryByText('First item')
+      expect(secondTitle).not.toBeInTheDocument()
+    })
+
+    describe('opening a dropdown', () => {
+      it('renders the content', async () => {
+        const { user } = setup()
+        render(
+          <Dropdown>
+            <Dropdown.Trigger>My Dropdown</Dropdown.Trigger>
+            <Dropdown.Content>
+              <Dropdown.Item>First item</Dropdown.Item>
+              <Dropdown.Item>Second item</Dropdown.Item>
+              <Dropdown.Item>Third item</Dropdown.Item>
+            </Dropdown.Content>
+          </Dropdown>
+        )
+
+        const dropdownTrigger = screen.getByText('My Dropdown')
+        expect(dropdownTrigger).toBeInTheDocument()
+        await user.click(dropdownTrigger)
+
+        const firstItem = screen.getByText('First item')
+        expect(firstItem).toBeInTheDocument()
+      })
+      it('renders the label', async () => {
+        const { user } = setup()
+        render(
+          <Dropdown>
+            <Dropdown.Trigger>My Dropdown</Dropdown.Trigger>
+            <Dropdown.Content>
+              <Dropdown.Label>The Label</Dropdown.Label>
+              <Dropdown.Item>First item</Dropdown.Item>
+              <Dropdown.Item>Second item</Dropdown.Item>
+              <Dropdown.Item>Third item</Dropdown.Item>
+            </Dropdown.Content>
+          </Dropdown>
+        )
+
+        const dropdownTrigger = screen.getByText('My Dropdown')
+        expect(dropdownTrigger).toBeInTheDocument()
+        await user.click(dropdownTrigger)
+
+        // const firstItem = screen.getByText('First item')
+        // expect(firstItem).toBeInTheDocument()
+
+        const label = screen.getByText('The Label')
+        expect(label).toBeInTheDocument()
+      })
+    })
+
+    describe('closing a dropdown', () => {
+      it('hides the content', async () => {
+        const { user } = setup()
+        render(
+          <Dropdown>
+            <Dropdown.Trigger>My Dropdown</Dropdown.Trigger>
+            <Dropdown.Content>
+              <Dropdown.Item>First item</Dropdown.Item>
+              <Dropdown.Item>Second item</Dropdown.Item>
+              <Dropdown.Item>Third item</Dropdown.Item>
+            </Dropdown.Content>
+          </Dropdown>
+        )
+
+        const dropdownTrigger = screen.getByText('My Dropdown')
+        expect(dropdownTrigger).toBeInTheDocument()
+        await user.click(dropdownTrigger)
+
+        const firstItem = screen.getByText('First item')
+        expect(firstItem).toBeInTheDocument()
+        await user.click(firstItem)
+
+        const newFirstItem = screen.queryByText('First item')
+        expect(newFirstItem).not.toBeInTheDocument()
+      })
+    })
+  })
+})
