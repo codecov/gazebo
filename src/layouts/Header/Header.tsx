d020c3100392191b97a0ae455b112fd6cd835faa
Spencer Murray <spencer.murray@sentry.io>
2024-07-18 15:30:23 -0400

    Add impersonation banner and adjust Guest header layout

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -2,6 +2,7 @@ import { Suspense } from 'react'
 
 import config from 'config'
 
+import { useImpersonate } from 'services/impersonate'
 import { useUser } from 'services/user'
 
 import AdminLink from './components/AdminLink'
@@ -12,12 +13,18 @@ import SeatDetails from './components/SeatDetails'
 import UserDropdown from './components/UserDropdown'
 
 function Header() {
+  const { isImpersonating } = useImpersonate()
   const { data: currentUser } = useUser()
 
   return (
-    <nav>
+    <header>
       {!currentUser ? <GuestHeader /> : null}
-      <div className="container flex h-14 min-h-14 w-full items-center">
+      {isImpersonating ? (
+        <div className="flex justify-center bg-ds-pink-tertiary">
+          <p className="text-white">Impersonating</p>
+        </div>
+      ) : null}
+      <nav className="container flex h-14 min-h-14 w-full items-center">
         <div className="flex-1">
           <Navigator currentUser={currentUser} />
         </div>
@@ -33,8 +40,8 @@ function Header() {
             <UserDropdown />
           </div>
         )}
-      </div>
-    </nav>
+      </nav>
+    </header>
   )
 }
 
