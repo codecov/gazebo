78b9e515566979fce2245745f37e0f9c2f6a0f05
Rula Abu Hasna <rula@codecov.io>
2024-07-21 12:46:04 +0200

    Create okta banners

---
+++ src/shared/GlobalTopBanners/OktaBanners/OktaBanners.tsx
@@ -0,0 +1,35 @@
+import { lazy } from 'react'
+import { useParams } from 'react-router'
+
+import { useOktaConfig } from 'pages/AccountSettings/tabs/OktaAccess/hooks'
+
+interface URLParams {
+  provider: string
+  owner?: string
+}
+
+const OktaEnabledBanner = lazy(
+  () => import('../OktaEnabledBanner/OktaEnforcedBanner')
+)
+const OktaEnforcedBanner = lazy(
+  () => import('../OktaEnforcedBanner/OktaEnforcedBanner')
+)
+
+function OktaBanners() {
+  const { provider, owner } = useParams<URLParams>()
+
+  const { data } = useOktaConfig({
+    provider,
+    username: owner || '',
+    opts: { enabled: !!owner },
+  })
+
+  const oktaConfig = data?.owner?.account?.oktaConfig
+
+  if (!owner || !oktaConfig?.enabled || !data?.owner?.isUserOktaAuthenticated)
+    return null
+
+  return oktaConfig?.enforced ? <OktaEnforcedBanner /> : <OktaEnabledBanner />
+}
+
+export default OktaBanners
