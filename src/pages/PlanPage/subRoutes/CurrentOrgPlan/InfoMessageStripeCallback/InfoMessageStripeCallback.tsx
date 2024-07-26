4c00adff55a35996dd02464514620592efe427c6
Rohit <rohit.vinnakota@sentry.io>
2024-07-26 10:43:18 -0400

    Remove stripe sub banner

--- src/pages/PlanPage/subRoutes/CurrentOrgPlan/InfoMessageStripeCallback/InfoMessageStripeCallback.tsx
+++ src/pages/PlanPage/subRoutes/CurrentOrgPlan/InfoMessageStripeCallback/InfoMessageStripeCallback.tsx
@@ -23,19 +23,6 @@ function InfoMessageStripeCallback() {
       </div>
     )
 
-  if ('cancel' in urlParams)
-    return (
-      <div className="col-start-1 col-end-13 mb-4">
-        <Message variant="error">
-          <h2 className="text-lg">Subscription Update Failed</h2>
-          <p className="text-sm">
-            Your subscription did not update. If you believe this to be an
-            error, please contact support@codecov.io.
-          </p>
-        </Message>
-      </div>
-    )
-
   return null
 }
 
