9afdbf7d22319e096246ccb15bd1cd67e240b2ec
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-07-29 13:38:30 -0300

    add in some more tests

--- src/ui/Chart/Chart.spec.tsx
+++ src/ui/Chart/Chart.spec.tsx
@@ -1,6 +1,4 @@
-import { fireEvent, render, screen, within } from '@testing-library/react'
-// import { userEvent } from '@testing-library/user-event'
-import { format } from 'date-fns'
+import { render, screen, within } from '@testing-library/react'
 import { Area, AreaChart } from 'recharts'
 
 import {
@@ -8,7 +6,6 @@ import {
   ChartContainer,
   ChartLegend,
   ChartLegendContent,
-  ChartTooltip,
   ChartTooltipContent,
   getPayloadConfigFromPayload,
 } from './Chart'
@@ -32,12 +29,12 @@ jest.mock('recharts', () => {
 })
 
 const chartData = [
-  { month: new Date('01/01/2024').toISOString(), coverage: 10 },
-  { month: new Date('02/01/2024').toISOString(), coverage: 20 },
-  { month: new Date('03/01/2024').toISOString(), coverage: 45 },
-  { month: new Date('04/01/2024').toISOString(), coverage: 40 },
-  { month: new Date('05/01/2024').toISOString(), coverage: 50 },
-  { month: new Date('06/01/2024').toISOString(), coverage: 90 },
+  { month: new Date('01/01/2024').toISOString(), coverage: 100 },
+  { month: new Date('02/01/2024').toISOString(), coverage: 200 },
+  { month: new Date('03/01/2024').toISOString(), coverage: 450 },
+  { month: new Date('04/01/2024').toISOString(), coverage: 400 },
+  { month: new Date('05/01/2024').toISOString(), coverage: 500 },
+  { month: new Date('06/01/2024').toISOString(), coverage: 900 },
 ]
 
 const chartConfig = {
@@ -50,11 +47,6 @@ const chartConfig = {
 describe('Chart', () => {
   let resizeObserverMock: jest.Mock<any, any>
   let oldConsoleWarn = console.warn
-
-  // function setup() {
-  //   const user = userEvent.setup()
-  //   return { user }
-  // }
 
   beforeEach(() => {
     console.warn = () => null
@@ -135,72 +127,189 @@ describe('Chart', () => {
     })
   })
 
-  describe.skip('ChartTooltip', () => {
+  describe('ChartTooltipContent', () => {
     describe('there is a payload', () => {
       describe('tooltip is active', () => {
         it('renders tooltip', async () => {
-          const { container } = render(
+          render(
             <ChartContainer config={chartConfig} className="size-[400px]">
-              <AreaChart
-                data={chartData}
-                accessibilityLayer
-                width={100}
-                height={100}
-              >
-                <ChartTooltip
-                  content={
-                    <ChartTooltipContent
-                      indicator="line"
-                      labelFormatter={(date: string) =>
-                        format(new Date(date), 'MMM d, yyyy')
-                      }
-                      valueFormatter={(value: number) => `${value}%`}
-                    />
-                  }
-                />
-                <Area
-                  dataKey="coverage"
-                  type="linear"
-                  fillOpacity={0.4}
-                  fill="var(--color-coverage)"
-                  stroke="var(--color-coverage)"
-                />
-              </AreaChart>
+              <ChartTooltipContent
+                indicator="dot"
+                labelFormatter={(date: string) => date}
+                active={true}
+                payload={[
+                  {
+                    stroke: 'var(--color-coverage)',
+                    dataKey: 'coverage',
+                    name: 'coverage',
+                    color: 'var(--color-coverage)',
+                    value: 45,
+                    payload: {
+                      month: '2024-03-01T04:00:00.000Z',
+                      coverage: 45,
+                    },
+                    hide: false,
+                  },
+                ]}
+              />
             </ChartContainer>
           )
 
-          // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
-          const chartWrapper = container.querySelector('.recharts-wrapper')
-          if (!!chartWrapper) {
-            // eslint-disable-next-line testing-library/await-fire-event, testing-library/prefer-user-event
-            fireEvent.mouseOver(chartWrapper, {
-              clientX: 200,
-              clientY: 200,
-            })
-          }
-
-          expect(chartWrapper).toBeInTheDocument()
-          expect(chartWrapper).toBeVisible()
-
-          const tooltip = screen.getByTestId('chart-tooltip-content')
-          const coverage = within(tooltip).getByText('Coverage')
-
+          const datalist = screen.getByTestId('chart-tooltip-datalist')
+          const coverage = within(datalist).getByText('Coverage')
           expect(coverage).toBeInTheDocument()
         })
       })
 
       describe('tooltip is not active', () => {
-        it('does not render tooltip', () => {})
+        it('does not render tooltip', () => {
+          render(
+            <ChartContainer config={chartConfig} className="size-[400px]">
+              <ChartTooltipContent
+                indicator="dot"
+                labelFormatter={(date: string) => date}
+                active={false}
+                payload={[
+                  {
+                    stroke: 'var(--color-coverage)',
+                    dataKey: 'coverage',
+                    name: 'coverage',
+                    color: 'var(--color-coverage)',
+                    value: 45,
+                    payload: {
+                      month: '2024-03-01T04:00:00.000Z',
+                      coverage: 45,
+                    },
+                    hide: false,
+                  },
+                ]}
+              />
+            </ChartContainer>
+          )
+
+          const datalist = screen.queryByTestId('chart-tooltip-datalist')
+          expect(datalist).not.toBeInTheDocument()
+        })
       })
     })
 
     describe('there is no payload', () => {
-      describe('tooltip is active', () => {
-        it('renders tooltip', () => {})
+      describe('tooltip is not active', () => {
+        it('does not render tooltip', () => {
+          render(
+            <ChartContainer config={chartConfig} className="size-[400px]">
+              <ChartTooltipContent
+                indicator="dot"
+                labelFormatter={(date: string) => date}
+                active={false}
+                payload={[]}
+              />
+            </ChartContainer>
+          )
+
+          const datalist = screen.queryByTestId('chart-tooltip-datalist')
+          expect(datalist).not.toBeInTheDocument()
+        })
+      })
+    })
+
+    describe('tooltip label', () => {
+      describe('there is a label formatter', () => {
+        it('renders the label using the formatter', () => {
+          render(
+            <ChartContainer config={chartConfig} className="size-[400px]">
+              <ChartTooltipContent
+                indicator="dot"
+                labelFormatter={(date: string) => `Formatted ${date}`}
+                active={true}
+                payload={[
+                  {
+                    stroke: 'var(--color-coverage)',
+                    dataKey: 'coverage',
+                    name: 'coverage',
+                    color: 'var(--color-coverage)',
+                    value: 45,
+                    payload: {
+                      month: '2024-03-01T04:00:00.000Z',
+                      coverage: 45,
+                    },
+                    hide: false,
+                  },
+                ]}
+              />
+            </ChartContainer>
+          )
+
+          const formatted = screen.getByText(/Formatted/)
+          expect(formatted).toBeInTheDocument()
+        })
       })
 
-      describe('tooltip is not active', () => {
-        it('does not render tooltip', () => {})
+      describe('there is no label formatter', () => {
+        it('just renders the config label', () => {
+          render(
+            <ChartContainer config={chartConfig} className="size-[400px]">
+              <ChartTooltipContent
+                indicator="dot"
+                active={true}
+                payload={[
+                  {
+                    stroke: 'var(--color-coverage)',
+                    dataKey: 'coverage',
+                    name: 'coverage',
+                    color: 'var(--color-coverage)',
+                    value: 45,
+                    payload: {
+                      month: '2024-03-01T04:00:00.000Z',
+                      coverage: 45,
+                    },
+                    hide: false,
+                  },
+                ]}
+              />
+            </ChartContainer>
+          )
+
+          const label = screen.getByText(/45/)
+          expect(label).toBeInTheDocument()
+        })
+      })
+
+      describe('there is no value', () => {
+        it('renders the config label key', () => {
+          render(
+            <ChartContainer
+              config={{
+                coverage: {
+                  color: 'hsl(var(--chart-area-bundle-tab))',
+                },
+              }}
+              className="size-[400px]"
+            >
+              <ChartTooltipContent
+                indicator="dot"
+                active={true}
+                payload={[
+                  {
+                    stroke: 'var(--color-coverage)',
+                    dataKey: 'coverage',
+                    name: 'coverage',
+                    color: 'var(--color-coverage)',
+                    value: 45,
+                    payload: {
+                      month: '2024-03-01T04:00:00.000Z',
+                      coverage: null,
+                    },
+                    hide: false,
+                  },
+                ]}
+              />
+            </ChartContainer>
+          )
+
+          const label = screen.getByText(/coverage/i)
+          expect(label).toBeInTheDocument()
+        })
       })
     })
   })
