11c389288d3fa95b4a7220a62d0732d6e51803aa
Spencer Murray <159931558+spalmurray-codecov@users.noreply.github.com>
2024-07-30 16:14:08 -0400

    ref: Convert pull/commit details page error banners to Alert component (#3085)

--- src/pages/PullRequestPage/PullCoverage/ErrorBanner/ErrorBanner.jsx
+++ src/pages/PullRequestPage/PullCoverage/ErrorBanner/ErrorBanner.jsx
@@ -2,27 +2,28 @@ import PropTypes from 'prop-types'
 
 import { ComparisonReturnType } from 'shared/utils/comparison'
 import A from 'ui/A'
-import Banner from 'ui/Banner'
+import { Alert } from 'ui/Alert'
 
 function BannerContent({ errorType }) {
   if (errorType === ComparisonReturnType.MISSING_BASE_COMMIT) {
     return (
       <>
-        {' '}
-        <h1 className="font-semibold">Missing Base Commit</h1>
-        <div className="flex gap-1">
-          <span>
-            Unable to compare commits because the base commit of the pull
-            request is not found.
+        <Alert.Title>Missing Base Commit</Alert.Title>
+        <Alert.Description>
+          <span className="flex flex-wrap gap-1">
+            <span>
+              Unable to compare commits because the base commit of the pull
+              request is not found.
+            </span>
+            <A
+              hook="compare errors"
+              to={{ pageName: 'missingComparisonCommit' }}
+              isExternal={true}
+            >
+              Learn more here
+            </A>
           </span>
-          <A
-            hook="compare errors"
-            to={{ pageName: 'missingComparisonCommit' }}
-            isExternal={true}
-          >
-            Learn more here
-          </A>
-        </div>
+        </Alert.Description>
       </>
     )
   }
@@ -30,21 +31,22 @@ function BannerContent({ errorType }) {
   if (errorType === ComparisonReturnType.MISSING_HEAD_COMMIT) {
     return (
       <>
-        {' '}
-        <h1 className="font-semibold">Missing Head Commit</h1>
-        <div className="flex gap-1">
-          <span>
-            Unable to compare commits because the head commit of the pull
-            request is not found.
+        <Alert.Title>Missing Head Commit</Alert.Title>
+        <Alert.Description>
+          <span className="flex flex-wrap gap-1">
+            <span>
+              Unable to compare commits because the head commit of the pull
+              request is not found.
+            </span>
+            <A
+              hook="compare errors"
+              to={{ pageName: 'missingComparisonCommit' }}
+              isExternal={true}
+            >
+              Learn more here
+            </A>
           </span>
-          <A
-            hook="compare errors"
-            to={{ pageName: 'missingComparisonCommit' }}
-            isExternal={true}
-          >
-            Learn more here
-          </A>
-        </div>
+        </Alert.Description>
       </>
     )
   }
@@ -52,21 +54,22 @@ function BannerContent({ errorType }) {
   if (errorType === ComparisonReturnType.MISSING_BASE_REPORT) {
     return (
       <>
-        {' '}
-        <h1 className="font-semibold">Missing Base Report</h1>
-        <div className="flex gap-1">
-          <span>
-            Unable to compare commits because the base of the pull request did
-            not upload a coverage report.
+        <Alert.Title>Missing Base Report</Alert.Title>
+        <Alert.Description>
+          <span className="flex flex-wrap gap-1">
+            <span>
+              Unable to compare commits because the base of the pull request did
+              not upload a coverage report.
+            </span>
+            <A
+              hook="compare errors"
+              to={{ pageName: 'missingComparisonReport' }}
+              isExternal={true}
+            >
+              Learn more here
+            </A>
           </span>
-          <A
-            hook="compare errors"
-            to={{ pageName: 'missingComparisonReport' }}
-            isExternal={true}
-          >
-            Learn more here
-          </A>
-        </div>
+        </Alert.Description>
       </>
     )
   }
@@ -74,21 +77,22 @@ function BannerContent({ errorType }) {
   if (errorType === ComparisonReturnType.MISSING_HEAD_REPORT) {
     return (
       <>
-        {' '}
-        <h1 className="font-semibold">Missing Head Report</h1>
-        <div className="flex gap-1">
-          <span>
-            Unable to compare commits because the head of the pull request did
-            not upload a coverage report.
+        <Alert.Title>Missing Head Report</Alert.Title>
+        <Alert.Description>
+          <span className="flex flex-wrap gap-1">
+            <span>
+              Unable to compare commits because the head of the pull request did
+              not upload a coverage report.
+            </span>
+            <A
+              hook="compare errors"
+              to={{ pageName: 'missingComparisonReport' }}
+              isExternal={true}
+            >
+              Learn more here
+            </A>
           </span>
-          <A
-            hook="compare errors"
-            to={{ pageName: 'missingComparisonReport' }}
-            isExternal={true}
-          >
-            Learn more here
-          </A>
-        </div>
+        </Alert.Description>
       </>
     )
   }
@@ -96,21 +100,22 @@ function BannerContent({ errorType }) {
   if (errorType === ComparisonReturnType.MISSING_COMPARISON) {
     return (
       <>
-        {' '}
-        <h1 className="font-semibold">Missing Comparison</h1>
-        <div className="flex gap-1">
-          <span>
-            There was an error computing the comparison for the head and base
-            commit.
+        <Alert.Title>Missing Comparison</Alert.Title>
+        <Alert.Description>
+          <span className="flex flex-wrap gap-1">
+            <span>
+              There was an error computing the comparison for the head and base
+              commit.
+            </span>
+            <A
+              hook="compare errors"
+              to={{ pageName: 'missingComparisonReport' }}
+              isExternal={true}
+            >
+              Learn more here
+            </A>
           </span>
-          <A
-            hook="compare errors"
-            to={{ pageName: 'missingComparisonReport' }}
-            isExternal={true}
-          >
-            Learn more here
-          </A>
-        </div>
+        </Alert.Description>
       </>
     )
   }
@@ -124,11 +129,9 @@ BannerContent.propTypes = {
 
 function ErrorBanner({ errorType }) {
   return (
-    <Banner variant="warning">
-      <div className="flex flex-col gap-6 text-sm">
-        <BannerContent errorType={errorType} />
-      </div>
-    </Banner>
+    <Alert variant="warning">
+      <BannerContent errorType={errorType} />
+    </Alert>
   )
 }
 
