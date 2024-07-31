d1af2904c07677f08e0f37e7ccf7dc397816eab2
Spencer Murray <spencer.murray@sentry.io>
2024-07-31 16:36:44 -0400

    update copy for onboarding feedback as well

--- src/sentry.ts
+++ src/sentry.ts
@@ -66,6 +66,8 @@ export const SentryBugReporter = Sentry.feedbackIntegration({
   formTitle: 'Give Feedback',
   buttonLabel: 'Give Feedback',
   submitButtonLabel: 'Send Feedback',
+  messagePlaceholder:
+    'Share your experience and suggest opportunities for improvement.',
   nameLabel: 'Username',
   isEmailRequired: true,
   autoInject: false,
