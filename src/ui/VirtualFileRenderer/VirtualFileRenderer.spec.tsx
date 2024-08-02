86d7c593e2ba7e12f11f84b171defe2fbf97ff25
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-08-02 10:24:54 -0300

    remove color bar tests from virtual file renderer tests

--- src/ui/VirtualFileRenderer/VirtualFileRenderer.spec.tsx
+++ src/ui/VirtualFileRenderer/VirtualFileRenderer.spec.tsx
@@ -4,7 +4,7 @@ import { userEvent } from '@testing-library/user-event'
 import { type Dictionary } from 'lodash'
 import { MemoryRouter, Route, useLocation } from 'react-router-dom'
 
-import { ColorBar, VirtualFileRenderer } from './VirtualFileRenderer'
+import { VirtualFileRenderer } from './VirtualFileRenderer'
 
 window.requestAnimationFrame = (cb) => {
   cb(1)
@@ -356,57 +356,6 @@ describe('VirtualFileRenderer', () => {
           await waitFor(() => expect(testLocation.hash).toBe(''))
         })
       })
-    })
-  })
-})
-
-describe('ColorBar', () => {
-  describe('when there is no coverage data', () => {
-    it('does not render', () => {
-      const { container } = render(
-        <ColorBar coverage={undefined} locationHash="" lineNumber={0} />
-      )
-      expect(container).toBeEmptyDOMElement()
-    })
-  })
-
-  describe('coverage value is H', () => {
-    it('renders with covered color', () => {
-      render(<ColorBar coverage={'H'} locationHash="" lineNumber={0} />)
-
-      const bar = screen.getByTestId('covered-bar')
-      expect(bar).toBeInTheDocument()
-      expect(bar).toHaveClass('bg-ds-coverage-covered')
-    })
-  })
-
-  describe('coverage value is M', () => {
-    it('renders with uncovered color', () => {
-      render(<ColorBar coverage={'M'} locationHash="" lineNumber={0} />)
-
-      const bar = screen.getByTestId('uncovered-bar')
-      expect(bar).toBeInTheDocument()
-      expect(bar).toHaveClass('bg-ds-coverage-uncovered')
-    })
-  })
-
-  describe('coverage value is P', () => {
-    it('renders with partial color', () => {
-      render(<ColorBar coverage={'P'} locationHash="" lineNumber={0} />)
-
-      const bar = screen.getByTestId('partial-bar')
-      expect(bar).toBeInTheDocument()
-      expect(bar).toHaveClass('bg-ds-coverage-partial')
-    })
-  })
-
-  describe('highlighted line', () => {
-    it('renders with highlighted color', () => {
-      render(<ColorBar coverage={'P'} locationHash="#L18" lineNumber={18} />)
-
-      const bar = screen.getByTestId('highlighted-bar')
-      expect(bar).toBeInTheDocument()
-      expect(bar).toHaveClass('bg-ds-blue-medium')
     })
   })
 })
