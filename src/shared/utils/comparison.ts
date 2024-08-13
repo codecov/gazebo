10ea0e12c253582f5109d9470e520ce24f3fceaf
Spencer Murray <spencer.murray@sentry.io>
2024-08-13 15:18:21 -0400

    Implement generic ComparisonErrorBanner component

--- src/shared/utils/comparison.ts
+++ src/shared/utils/comparison.ts
@@ -9,3 +9,11 @@ export const ComparisonReturnType = {
   FIRST_PULL_REQUEST: 'FirstPullRequest',
   ...CommitErrorTypes,
 } as const
+
+export type TReportUploadType =
+  (typeof ReportUploadType)[keyof typeof ReportUploadType]
+
+export const ReportUploadType = {
+  BUNDLE_ANALYSIS: 'bundle analysis',
+  COVERAGE: 'coverage',
+} as const
