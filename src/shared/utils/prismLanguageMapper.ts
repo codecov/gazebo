6032eb0d9ed58c03c9994ad3385d936766eb9ce9
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-08-26 10:47:33 -0300

    feat: Even more language support (#3138)

--- src/shared/utils/prismLanguageMapper.ts
+++ src/shared/utils/prismLanguageMapper.ts
@@ -17,6 +17,9 @@ import Prism from 'prism-react-renderer/prism'
  */
 require('prismjs/components/prism-csharp')
 require('prismjs/components/prism-dart')
+require('prismjs/components/prism-cshtml')
+require('prismjs/components/prism-fortran')
+require('prismjs/components/prism-fsharp')
 require('prismjs/components/prism-java')
 require('prismjs/components/prism-julia')
 require('prismjs/components/prism-kotlin')
@@ -25,6 +28,12 @@ require('prismjs/components/prism-r')
 require('prismjs/components/prism-ruby')
 require('prismjs/components/prism-rust')
 require('prismjs/components/prism-scala')
+require('prismjs/components/prism-powershell')
+require('prismjs/components/prism-ruby')
+require('prismjs/components/prism-rust')
+require('prismjs/components/prism-solidity')
+require('prismjs/components/prism-swift')
+require('prismjs/components/prism-visual-basic')
 require('prismjs/components/prism-zig')
 
 const prismSupportedLanguages = new Map<string, string>([
@@ -37,12 +46,18 @@ const prismSupportedLanguages = new Map<string, string>([
   ['rss', 'markup'],
   ['sh', 'bash'],
   ['c', 'c'],
-  ['h', 'clike'],
+  ['h', 'c'],
   ['cc', 'cpp'],
   ['cpp', 'cpp'],
+  ['hpp', 'cpp'],
   ['cs', 'csharp'],
+  ['cshtml', 'cshtml'],
   ['css', 'css'],
   ['dart', 'dart'],
+  ['f', 'fortran'],
+  ['fpp', 'fortran'],
+  ['f90', 'fortran'],
+  ['fs', 'fsharp'],
   ['java', 'java'],
   ['jl', 'julia'],
   ['js', 'javascript'],
@@ -60,17 +75,30 @@ const prismSupportedLanguages = new Map<string, string>([
   ['ocaml', 'ocaml'],
   ['php', 'php'],
   ['py', 'python'],
-  ['R', 'r'],
   ['r', 'r'],
+  ['ps1', 'powershell'],
+  ['psm1', 'powershell'],
+  ['razor', 'cshtml'],
   ['rb', 'ruby'],
   ['reason', 'reason'],
   ['rs', 'rust'],
   ['sass', 'sass'],
   ['scala', 'scala'],
   ['scss', 'scss'],
+  ['sol', 'solidity'],
   ['sql', 'sql'],
+  ['swift', 'swift'],
+  // svelte does not have full support yet - may look into this third party
+  // https://github.com/PrismJS/prism/issues/2090
+  ['svelte', 'markup'],
   ['ts', 'typescript'],
   ['tsx', 'tsx'],
+  ['vb', 'visual-basic'],
+  ['vba', 'visual-basic'],
+  ['vbs', 'visual-basic'],
+  // vue does not have full support yet
+  // https://github.com/PrismJS/prism/issues/1665
+  ['vue', 'markup'],
   ['wasm', 'wasm'],
   ['yaml', 'yaml'],
   ['zig', 'zig'],
@@ -81,7 +109,10 @@ const DEFAULT_LANGUAGE_TYPE: Language = 'markup'
 export function prismLanguageMapper(fileName: string): Language {
   const fileExtension = fileName.split('.').pop() ?? ''
 
-  const supportedLanguage = prismSupportedLanguages.get(fileExtension)
+  // casting this to lower, as in Sentry I've seen some file extensions in uppercase
+  const supportedLanguage = prismSupportedLanguages.get(
+    fileExtension.toLowerCase()
+  )
   // we need to cast this, because we're adding in extra languages that aren't in the prism-react-renderer package
   if (supportedLanguage) return supportedLanguage as Language
 
