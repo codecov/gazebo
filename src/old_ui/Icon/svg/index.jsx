e468e4a3df9b24632622a95989a76a5f7b07a70e
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-09-12 14:35:44 -0300

    alphabetize icon list

--- src/old_ui/Icon/svg/index.jsx
+++ src/old_ui/Icon/svg/index.jsx
@@ -3,16 +3,33 @@ import { ReactComponent as angleUp } from './angleUp.svg'
 import { ReactComponent as arrowLeft } from './arrowLeft.svg'
 import { ReactComponent as ban } from './ban.svg'
 import { ReactComponent as check } from './check.svg'
+import { ReactComponent as chevronLeft } from './chevronLeft.svg'
+import { ReactComponent as chevronRight } from './chevronRight.svg'
 import { ReactComponent as creditCard } from './creditCard.svg'
 import { ReactComponent as exclamationCircle } from './exclamationCircle.svg'
-import { ReactComponent as infoCircle } from './infoCircle.svg'
-import { ReactComponent as times } from './times.svg'
-import { ReactComponent as chevronRight } from './chevronRight.svg'
-import { ReactComponent as chevronLeft } from './chevronLeft.svg'
 import { ReactComponent as fileAlt } from './fileAlt.svg'
 import { ReactComponent as hamburger } from './hamburger.svg'
+import { ReactComponent as infoCircle } from './infoCircle.svg'
+import { ReactComponent as search } from './search.svg'
 import { ReactComponent as setting } from './setting.svg'
 import { ReactComponent as signOut } from './signOut.svg'
-import { ReactComponent as search } from './search.svg'
+import { ReactComponent as times } from './times.svg'
 
-export { angleDown, angleUp, arrowLeft, ban, check, chevronLeft, chevronRight, creditCard, exclamationCircle, fileAlt, hamburger, infoCircle, search, setting, signOut, times }
+export {
+  angleDown,
+  angleUp,
+  arrowLeft,
+  ban,
+  check,
+  chevronLeft,
+  chevronRight,
+  creditCard,
+  exclamationCircle,
+  fileAlt,
+  hamburger,
+  infoCircle,
+  search,
+  setting,
+  signOut,
+  times,
+}
