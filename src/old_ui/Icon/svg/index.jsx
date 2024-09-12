0afe829b7515d4b4af350ffe2bd70aa7c718e68f
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-09-12 10:07:08 -0300

    redo old_ui icons

---
+++ src/old_ui/Icon/svg/index.jsx
@@ -0,0 +1,35 @@
+import { ReactComponent as angleDown } from './angleDown.svg'
+import { ReactComponent as angleUp } from './angleUp.svg'
+import { ReactComponent as arrowLeft } from './arrowLeft.svg'
+import { ReactComponent as ban } from './ban.svg'
+import { ReactComponent as check } from './check.svg'
+import { ReactComponent as chevronLeft } from './chevronLeft.svg'
+import { ReactComponent as chevronRight } from './chevronRight.svg'
+import { ReactComponent as creditCard } from './creditCard.svg'
+import { ReactComponent as exclamationCircle } from './exclamationCircle.svg'
+import { ReactComponent as fileAlt } from './fileAlt.svg'
+import { ReactComponent as hamburger } from './hamburger.svg'
+import { ReactComponent as infoCircle } from './infoCircle.svg'
+import { ReactComponent as search } from './search.svg'
+import { ReactComponent as setting } from './setting.svg'
+import { ReactComponent as signOut } from './signOut.svg'
+import { ReactComponent as times } from './times.svg'
+
+export {
+  angleDown,
+  angleUp,
+  arrowLeft,
+  ban,
+  check,
+  creditCard,
+  exclamationCircle,
+  infoCircle,
+  times,
+  chevronRight,
+  chevronLeft,
+  fileAlt,
+  hamburger,
+  setting,
+  signOut,
+  search,
+}
