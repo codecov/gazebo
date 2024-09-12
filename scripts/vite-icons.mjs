2a41be889d2d2099ac7c71fe534260e9540d7de2
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-09-12 10:08:34 -0300

    rename icon files from .js to .jsx

--- scripts/vite-icons.mjs
+++ scripts/vite-icons.mjs
@@ -2,52 +2,7 @@ import _ from 'lodash'
 
 import fs from 'fs'
 
-const enabledIcons = [
-  'flag',
-  'chevron-up',
-  'chevron-down',
-  'search',
-  'check',
-  'x',
-  'lock-closed',
-  'globe-alt',
-  'clipboard-copy',
-  'external-link',
-  'home',
-  'merge',
-  'pull-request-open',
-  'pull-request-closed',
-  'speakerphone',
-  'exclamation',
-  'branch',
-  'refresh',
-  'information-circle',
-  'arrow-up',
-  'arrow-down',
-  'folder',
-  'document',
-  'document-text',
-  'branch',
-  'exclamation-circle',
-  'exclamation-triangle',
-  'ban',
-  'chevron-left',
-  'download',
-  'chevron-right',
-  'printer',
-  'cog',
-  'light-bulb',
-  'no-symbol',
-  'x-circle',
-  'database',
-  'trash',
-  'book-open',
-  'check-circle',
-  'question-mark-circle',
-  'plus-circle',
-  'eye',
-  'eye-off',
-]
+import { enabledIcons, oldIcons } from './icon-list.mjs'
 
 console.log('[icon gen]: starting')
 // temp removing as we're going to be calling this a lot more
@@ -94,23 +49,6 @@ generateImports('./src/ui/Icon/svg/solid')
 generateImports('./src/ui/Icon/svg/developer')
 
 // this is the old icons and will be removed once the migration to vite is complete
-generateImports('./src/old_ui/Icon/svg', [
-  'angleDown',
-  'angleUp',
-  'arrowLeft',
-  'ban',
-  'check',
-  'creditCard',
-  'exclamationCircle',
-  'infoCircle',
-  'times',
-  'chevronRight',
-  'chevronLeft',
-  'fileAlt',
-  'hamburger',
-  'setting',
-  'signOut',
-  'search',
-])
+generateImports('./src/old_ui/Icon/svg', oldIcons)
 
 console.log('[icon gen]: done')
---
+++ src/ui/Icon/svg/developer/index.jsx
@@ -0,0 +1,14 @@
+// import { ReactComponent as branchDeleted } from './branch-deleted.svg'
+import { ReactComponent as branch } from './branch.svg'
+// import { ReactComponent as commit } from './commit.svg'
+// import { ReactComponent as comparison } from './comparison.svg'
+// import { ReactComponent as fork } from './fork.svg'
+// import { ReactComponent as gitMerge } from './git-merge.svg'
+// import { ReactComponent as hook } from './hook.svg'
+import { ReactComponent as merge } from './merge.svg'
+import { ReactComponent as pullRequestClosed } from './pull-request-closed.svg'
+import { ReactComponent as pullRequestOpen } from './pull-request-open.svg'
+// import { ReactComponent as pullRequest } from './pull-request.svg'
+// import { ReactComponent as statusRunning } from './status-running.svg'
+
+export { branch, merge, pullRequestClosed, pullRequestOpen }
