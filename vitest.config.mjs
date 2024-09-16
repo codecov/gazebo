71f0a33b36e5697f357c0e58491ea1fcdf218fed
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-09-16 14:58:33 -0300

    add in coverage stuff for vitest

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -7,10 +7,29 @@ export default defineConfig((configEnv) =>
     viteConfig(configEnv),
     defineConfig({
       test: {
+        coverage: {
+          provider: 'v8',
+          reporters: [
+            ['text'],
+            ['html', { outputFile: 'coverage/index.html' }],
+            ['junit', { outputFile: 'reports/junit/junit.xml' }],
+          ],
+          reportOnFailure: true,
+        },
         globals: true,
         environment: ['jsdom'],
         setupFiles: './src/vitest.setup.js',
-        include: ['**/*.test.*'],
+        include: ['src/**/*.test.*'],
+        exclude: [
+          // Default exclude patterns
+          '**/node_modules/**',
+          '**/dist/**',
+          '**/cypress/**',
+          '**/.{idea,git,cache,output,temp}/**',
+          '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+          // Custom exclude patterns
+          'src/**/*.spec.*'
+        ],
       },
     })
   )
