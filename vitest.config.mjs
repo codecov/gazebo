950d6acc1652f09eb75bc2f5ae155776b00e92e3
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-09-16 15:32:58 -0300

    conditionally enable junit test reporter

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,36 +1,39 @@
 import { defineConfig, mergeConfig } from 'vitest/config'
 
-import viteConfig from './vite.config.mjs'
+import ViteConfig from './vite.config.mjs'
+
+const VitestConfig = defineConfig((config) => {
+  const reporters = []
+  if (process.env.ENABLE_TEST_REPORTER) {
+    reporters.push(['junit', { outputFile: 'reports/junit/junit.xml' }])
+  }
+
+  return {
+    test: {
+      coverage: {
+        provider: 'v8',
+        reporters: [['text'], ['html', { outputFile: 'coverage/index.html' }]],
+        reportOnFailure: true,
+      },
+      globals: true,
+      environment: ['jsdom'],
+      setupFiles: './src/vitest.setup.js',
+      reporters: reporters,
+      include: ['src/**/*.test.*'],
+      exclude: [
+        // Default exclude patterns
+        '**/node_modules/**',
+        '**/dist/**',
+        '**/cypress/**',
+        '**/.{idea,git,cache,output,temp}/**',
+        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+        // Custom exclude patterns
+        'src/**/*.spec.*',
+      ],
+    },
+  }
+})
 
 export default defineConfig((configEnv) =>
-  mergeConfig(
-    viteConfig(configEnv),
-    defineConfig({
-      test: {
-        coverage: {
-          provider: 'v8',
-          reporters: [
-            ['text'],
-            ['html', { outputFile: 'coverage/index.html' }],
-          ],
-          reportOnFailure: true,
-        },
-        globals: true,
-        environment: ['jsdom'],
-        setupFiles: './src/vitest.setup.js',
-        reporters: ['junit', { outputFile: 'reports/junit/junit.xml' }],
-        include: ['src/**/*.test.*'],
-        exclude: [
-          // Default exclude patterns
-          '**/node_modules/**',
-          '**/dist/**',
-          '**/cypress/**',
-          '**/.{idea,git,cache,output,temp}/**',
-          '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
-          // Custom exclude patterns
-          'src/**/*.spec.*',
-        ],
-      },
-    })
-  )
+  mergeConfig(ViteConfig(configEnv), VitestConfig(configEnv))
 )
