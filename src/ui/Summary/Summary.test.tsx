cfef44feb7569207a9fbb43c48fe0c6d0038babf
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-09-22 10:24:01 -0300

    update Summary to TS and Vitest

--- src/ui/Summary/Summary.test.tsx
+++ src/ui/Summary/Summary.test.tsx
@@ -1,44 +1,40 @@
 import { render, screen } from '@testing-library/react'
 
-import Summary from '.'
+import Summary from './Summary'
 
 describe('Summary', () => {
-  let container
-
-  function setup({ fields }) {
-    ;({ container } = render(<Summary fields={fields} />))
-  }
-
-  it('doesnt render anything when fields array is empty', () => {
-    const fields = []
-    setup({ fields })
+  it('does not render anything when fields array is empty', () => {
+    const fields: any[] = []
+    const { container } = render(<Summary fields={fields} />)
 
     expect(container).toBeEmptyDOMElement()
   })
 
   it('renders a summary field for every field provided', () => {
     const fieldOne = {
-      name: 'firstfield',
+      name: 'firstField',
       title: 'random title',
       value: 'random value',
     }
     const fieldTwo = {
-      name: 'secondfield',
+      name: 'secondField',
       title: <span>Fancy title</span>,
       value: <span>Fancy value</span>,
     }
     const fields = [fieldOne, fieldTwo]
-    setup({ fields })
+    const { container } = render(<Summary fields={fields} />)
 
     expect(container).not.toBeEmptyDOMElement()
 
     const fieldOneTitle = screen.getByText('random title')
     expect(fieldOneTitle).toBeInTheDocument()
+
     const fieldOneValue = screen.getByText('random value')
     expect(fieldOneValue).toBeInTheDocument()
 
     const fieldTwoTitle = screen.getByText('Fancy title')
     expect(fieldTwoTitle).toBeInTheDocument()
+
     const fieldTwoValue = screen.getByText('Fancy value')
     expect(fieldTwoValue).toBeInTheDocument()
   })
