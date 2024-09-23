4e567e3adaa47f6795f42b32aba85513c9c46630
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-09-23 08:02:51 -0400

    fix issue with env not being set properly

--- src/vitest.setup.ts
+++ src/vitest.setup.ts
@@ -7,11 +7,7 @@ import '@testing-library/jest-dom/vitest'
 expect.extend(matchers)
 
 // Prevent timezone differences between local and CI/CD
-const setupTestGlobal = async () => {
-  process.env.TZ = 'UTC'
-}
-
-export default setupTestGlobal
+process.env.TZ = 'UTC'
 
 process.env.REACT_APP_ZOD_IGNORE_TESTS = 'true'
 
