9bf97c709d24679b8049722108e397be17c84656
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-09-25 08:03:52 -0300

    chore: Update ui/Checkbox tests to Vitest (#3267)

--- src/ui/Checkbox/Checkbox.test.jsx
+++ src/ui/Checkbox/Checkbox.test.jsx
@@ -6,6 +6,7 @@ describe('Checkbox', () => {
   describe('renders default radio input', () => {
     it('renders default with label', async () => {
       render(<Checkbox label={'This is the label'} />)
+
       const label = await screen.findByText('This is the label')
       expect(label).toBeInTheDocument()
     })
