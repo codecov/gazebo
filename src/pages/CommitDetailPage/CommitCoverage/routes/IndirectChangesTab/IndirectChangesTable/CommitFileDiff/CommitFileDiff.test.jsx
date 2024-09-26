639d36aadd49cb8f17eac97ce5c7bfaba59def28
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-09-26 08:48:12 -0400

    update CommitFileDiff

--- src/pages/CommitDetailPage/CommitCoverage/routes/IndirectChangesTab/IndirectChangesTable/CommitFileDiff/CommitFileDiff.test.jsx
+++ src/pages/CommitDetailPage/CommitCoverage/routes/IndirectChangesTab/IndirectChangesTable/CommitFileDiff/CommitFileDiff.test.jsx
@@ -1,14 +1,23 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { render, screen, waitFor } from '@testing-library/react'
-import { graphql } from 'msw'
-import { setupServer } from 'msw/node'
+import { graphql, HttpResponse } from 'msw2'
+import { setupServer } from 'msw2/node'
 import { MemoryRouter, Route } from 'react-router-dom'
-
-import { useScrollToLine } from 'ui/CodeRenderer/hooks/useScrollToLine'
 
 import CommitFileDiff from './CommitFileDiff'
 
-jest.mock('ui/CodeRenderer/hooks/useScrollToLine')
+const mocks = vi.hoisted(() => ({
+  useScrollToLine: vi.fn(),
+}))
+
+vi.mock('ui/CodeRenderer/hooks', () => {
+  const actual = vi.importActual('ui/CodeRenderer/hooks')
+  return {
+    ...actual,
+    useScrollToLine: mocks.useScrollToLine,
+  }
+})
+
 window.requestAnimationFrame = (cb) => cb()
 window.cancelAnimationFrame = () => {}
 
@@ -139,21 +148,18 @@ describe('CommitFileDiff', () => {
       bundleAnalysisEnabled: false,
     }
   ) {
-    useScrollToLine.mockImplementation(() => ({
+    mocks.useScrollToLine.mockImplementation(() => ({
       lineRef: () => {},
       handleClick: jest.fn(),
       targeted: false,
     }))
 
     server.use(
-      graphql.query('ImpactedFileComparedWithParent', (req, res, ctx) =>
-        res(ctx.status(200), ctx.data(baseMock(impactedFile)))
-      ),
-      graphql.query('GetRepoOverview', (req, res, ctx) => {
-        return res(
-          ctx.status(200),
-          ctx.data(mockOverview(bundleAnalysisEnabled))
-        )
+      graphql.query('ImpactedFileComparedWithParent', (info) => {
+        return HttpResponse.json({ data: baseMock(impactedFile) })
+      }),
+      graphql.query('GetRepoOverview', (info) => {
+        return HttpResponse.json({ data: mockOverview(bundleAnalysisEnabled) })
       })
     )
   }
