fb56c75ad19a83ed8a06d4c38cd542d28ea40a40
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-09-26 08:47:07 -0400

    update YAMLViewer

--- src/pages/CommitDetailPage/CommitCoverage/YamlModal/YAMLViewer.test.jsx
+++ src/pages/CommitDetailPage/CommitCoverage/YamlModal/YAMLViewer.test.jsx
@@ -1,11 +1,19 @@
 import { render, screen } from '@testing-library/react'
 import { MemoryRouter, Route } from 'react-router-dom'
 
-import { useCommitYaml } from 'services/commit'
-
 import YAMLViewer from './YAMLViewer'
 
-jest.mock('services/commit')
+const mocks = vi.hoisted(() => ({
+  useCommitYaml: vi.fn(),
+}))
+
+vi.mock('services/commit', async () => {
+  const actual = await vi.importActual('services/commit')
+  return {
+    ...actual,
+    useCommitYaml: mocks.useCommitYaml,
+  }
+})
 
 describe('YAMLViewer', () => {
   function setup() {
@@ -20,7 +28,7 @@ describe('YAMLViewer', () => {
 
   describe('renders', () => {
     beforeEach(() => {
-      useCommitYaml.mockReturnValue({
+      mocks.useCommitYaml.mockReturnValue({
         data: `codecov:\n  max_report_age: false\n  require_ci_to_pass: true\ncomment:\n  behavior: default\n  layout: reach,diff,flags,tree,reach\n  show_carryforward_flags: false\ncoverage:\n  precision: 2\n  range:\n  - 70.0\n  - 100.0\n  round: down\n  status:\n    changes: false\n    default_rules:\n      flag_coverage_not_uploaded_behavior: include\n    patch: true\n    project: true\ngithub_checks:\n  annotations: true\n`,
       })
       setup()
