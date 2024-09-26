fc962bf66563f9566bfd761e5b3bda527112b777
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-09-26 16:29:19 -0400

    update usePullPageData

--- src/pages/PullRequestPage/hooks/usePullPageData.test.tsx
+++ src/pages/PullRequestPage/hooks/usePullPageData.test.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { renderHook, waitFor } from '@testing-library/react'
-import { graphql } from 'msw'
-import { setupServer } from 'msw/node'
+import { graphql, HttpResponse } from 'msw2'
+import { setupServer } from 'msw2/node'
 
 import { usePullPageData } from './usePullPageData'
 
@@ -129,19 +129,19 @@ describe('usePullPageData', () => {
     isNullOwner = false,
   }: SetupArgs) {
     server.use(
-      graphql.query('PullPageData', (req, res, ctx) => {
+      graphql.query('PullPageData', (info) => {
         if (isNotFoundError) {
-          return res(ctx.status(200), ctx.data(mockNotFoundError))
+          return HttpResponse.json({ data: mockNotFoundError })
         } else if (isOwnerNotActivatedError) {
-          return res(ctx.status(200), ctx.data(mockOwnerNotActivatedError))
+          return HttpResponse.json({ data: mockOwnerNotActivatedError })
         } else if (isUnsuccessfulParseError) {
-          return res(ctx.status(200), ctx.data(mockUnsuccessfulParseError))
+          return HttpResponse.json({ data: mockUnsuccessfulParseError })
         } else if (isNullOwner) {
-          return res(ctx.status(200), ctx.data(mockNullOwner))
+          return HttpResponse.json({ data: mockNullOwner })
         } else {
-          return req.variables.isTeamPlan
-            ? res(ctx.status(200), ctx.data(mockPullDataTeam))
-            : res(ctx.status(200), ctx.data(mockPullData))
+          return info.variables.isTeamPlan
+            ? HttpResponse.json({ data: mockPullDataTeam })
+            : HttpResponse.json({ data: mockPullData })
         }
       })
     )
