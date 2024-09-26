93cc861f6864721fb3164d074d58d684f49d597b
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-09-26 08:46:55 -0400

    update CommitBundleDropdown

--- src/pages/CommitDetailPage/Dropdowns/CommitBundleDropdown.test.tsx
+++ src/pages/CommitDetailPage/Dropdowns/CommitBundleDropdown.test.tsx
@@ -1,8 +1,8 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { graphql } from 'msw'
-import { setupServer } from 'msw/node'
+import { graphql, HttpResponse } from 'msw2'
+import { setupServer } from 'msw2/node'
 import { Suspense } from 'react'
 import { MemoryRouter, Route } from 'react-router-dom'
 
@@ -117,16 +117,16 @@ describe('CommitBundleDropdown', () => {
     const user = userEvent.setup()
 
     server.use(
-      graphql.query('CommitBADropdownSummary', (req, res, ctx) => {
+      graphql.query('CommitBADropdownSummary', (info) => {
         if (noData) {
-          return res(ctx.status(200), ctx.data(mockNoData))
+          return HttpResponse.json({ data: mockNoData })
         } else if (firstPullRequest) {
-          return res(ctx.status(200), ctx.data(mockFirstPullRequest))
+          return HttpResponse.json({ data: mockFirstPullRequest })
         } else if (comparisonError) {
-          return res(ctx.status(200), ctx.data(mockErrorData))
+          return HttpResponse.json({ data: mockErrorData })
         }
 
-        return res(ctx.status(200), ctx.data(mockSummaryData(uncompress)))
+        return HttpResponse.json({ data: mockSummaryData(uncompress) })
       })
     )
 
