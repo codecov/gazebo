b74eb1bc570aa56d641b19356104e4eccfed4d31
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-09-27 11:45:44 -0400

    update useRepoActivation

--- src/pages/RepoPage/ConfigTab/tabs/GeneralTab/DangerZone/RepoState/hooks/useRepoActivation.test.jsx
+++ src/pages/RepoPage/ConfigTab/tabs/GeneralTab/DangerZone/RepoState/hooks/useRepoActivation.test.jsx
@@ -1,14 +1,22 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { renderHook, waitFor } from '@testing-library/react'
-import { rest } from 'msw'
-import { setupServer } from 'msw/node'
+import { http, HttpResponse } from 'msw2'
+import { setupServer } from 'msw2/node'
 import { MemoryRouter, Route } from 'react-router-dom'
-
-import { useAddNotification } from 'services/toastNotification'
 
 import { useRepoActivation } from './useRepoActivation'
 
-jest.mock('services/toastNotification')
+const mocks = vi.hoisted(() => ({
+  useAddNotification: vi.fn(),
+}))
+
+vi.mock('services/toastNotification', async () => {
+  const actual = await vi.importActual('services/toastNotification')
+  return {
+    ...actual,
+    useAddNotification: mocks.useAddNotification,
+  }
+})
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { retry: false } },
@@ -34,26 +42,23 @@ afterEach(() => {
 afterAll(() => server.close())
 
 describe('useRepoActivation', () => {
-  const mutate = jest.fn()
-  const addNotification = jest.fn()
+  const mutate = vi.fn()
+  const addNotification = vi.fn()
 
   function setup({ failMutation = false } = {}) {
     server.use(
-      rest.patch(
-        '/internal/github/codecov/repos/codecov-client/',
-        (req, res, ctx) => {
-          mutate()
+      http.patch('/internal/github/codecov/repos/codecov-client/', (info) => {
+        mutate()
 
-          if (failMutation) {
-            return res(ctx.status(500))
-          }
-
-          return res(ctx.status(200), ctx.json({}))
+        if (failMutation) {
+          return HttpResponse.error(500)
         }
-      )
+
+        return HttpResponse.json({})
+      })
     )
 
-    useAddNotification.mockReturnValue(addNotification)
+    mocks.useAddNotification.mockReturnValue(addNotification)
   }
 
   describe('when mutation is successful', () => {
