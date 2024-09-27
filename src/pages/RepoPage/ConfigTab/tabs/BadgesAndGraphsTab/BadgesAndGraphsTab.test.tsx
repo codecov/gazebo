e4930a9ff9bcf13be335a472a80e405e0c47ecdd
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-09-27 11:43:01 -0400

    update BadgesAndGraphs

--- src/pages/RepoPage/ConfigTab/tabs/BadgesAndGraphsTab/BadgesAndGraphsTab.test.tsx
+++ src/pages/RepoPage/ConfigTab/tabs/BadgesAndGraphsTab/BadgesAndGraphsTab.test.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { render, screen } from '@testing-library/react'
-import { graphql } from 'msw'
-import { setupServer } from 'msw/node'
+import { graphql, HttpResponse } from 'msw2'
+import { setupServer } from 'msw2/node'
 import { MemoryRouter, Route } from 'react-router-dom'
 
 import BadgesAndGraphsTab from './BadgesAndGraphsTab'
@@ -21,7 +21,7 @@ const wrapper: React.FC<React.PropsWithChildren> = ({ children }) => (
 )
 
 beforeAll(() => {
-  jest.clearAllMocks()
+  vi.clearAllMocks()
   server.listen()
   console.error = () => {}
 })
@@ -34,10 +34,9 @@ afterAll(() => server.close())
 describe('BadgesAndGraphsTab', () => {
   function setup({ graphToken }: { graphToken: string | null }) {
     server.use(
-      graphql.query('GetBranches', (req, res, ctx) => {
-        return res(
-          ctx.status(200),
-          ctx.data({
+      graphql.query('GetBranches', (info) => {
+        return HttpResponse.json({
+          data: {
             owner: {
               repository: {
                 branches: {
@@ -45,13 +44,12 @@ describe('BadgesAndGraphsTab', () => {
                 },
               },
             },
-          })
-        )
+          },
+        })
       }),
-      graphql.query('GetRepoSettings', (req, res, ctx) => {
-        return res(
-          ctx.status(200),
-          ctx.data({
+      graphql.query('GetRepoSettings', (info) => {
+        return HttpResponse.json({
+          data: {
             owner: {
               repository: {
                 __typename: 'Repository',
@@ -68,8 +66,8 @@ describe('BadgesAndGraphsTab', () => {
                 staticAnalysisToken: null,
               },
             },
-          })
-        )
+          },
+        })
       })
     )
   }
