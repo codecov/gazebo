d688a7c33aefb4f324a87e6fa60e35f97cd3590d
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-09-28 11:37:21 -0400

    update rest of coverage tab

---
+++ src/pages/RepoPage/CoverageTab/ComponentsTab/subroute/ComponentsTable/hooks/useRepoComponentsTable.test.tsx
@@ -0,0 +1,370 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { renderHook, waitFor } from '@testing-library/react'
+import { format, sub, subDays, subMonths } from 'date-fns'
+import { graphql, HttpResponse } from 'msw2'
+import { setupServer } from 'msw2/node'
+import React, { Suspense } from 'react'
+import { MemoryRouter, Route } from 'react-router-dom'
+
+import { TIME_OPTION_VALUES } from 'pages/RepoPage/shared/constants'
+import { useLocationParams } from 'services/navigation'
+
+import useRepoComponentsTable from './useRepoComponentsTable'
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { retry: false, suspense: true } },
+})
+
+const wrapper =
+  (initialEntries = '/gh/codecov/test'): React.FC<React.PropsWithChildren> =>
+  ({ children }) => (
+    <MemoryRouter initialEntries={[initialEntries]}>
+      <Route path="/:provider/:owner/:repo">
+        <QueryClientProvider client={queryClient}>
+          <Suspense fallback={null}>{children}</Suspense>
+        </QueryClientProvider>
+      </Route>
+    </MemoryRouter>
+  )
+
+const server = setupServer()
+
+beforeAll(() => {
+  server.listen()
+})
+
+afterEach(() => {
+  queryClient.clear()
+  server.resetHandlers()
+})
+
+afterAll(() => {
+  server.close()
+})
+
+vi.mock('services/navigation', async () => {
+  const actual = await vi.importActual('services/navigation')
+  return {
+    ...actual,
+    useLocationParams: vi.fn(),
+  }
+})
+
+const mockGetRepo = ({
+  noUploadToken = false,
+  isRepoPrivate = false,
+  isRepoActivated = true,
+  isCurrentUserPartOfOrg = true,
+  isRepoActive = true,
+  isCurrentUserActivated = true,
+}) => ({
+  owner: {
+    isAdmin: true,
+    isCurrentUserPartOfOrg,
+    isCurrentUserActivated,
+    repository: {
+      __typename: 'Repository',
+      private: isRepoPrivate,
+      uploadToken: noUploadToken
+        ? null
+        : '9e6a6189-20f1-482d-ab62-ecfaa2629295',
+      defaultBranch: 'main',
+      yaml: '',
+      activated: isRepoActivated,
+      oldestCommitAt: '2022-10-10T11:59:59',
+      active: isRepoActive,
+      isFirstPullRequest: false,
+    },
+  },
+})
+
+const mockedComponentMeasurements = {
+  owner: {
+    repository: {
+      __typename: 'Repository',
+      components: [
+        {
+          name: 'component1',
+          componentId: 'component1Id',
+          percentCovered: 93.26,
+          percentChange: 1.65,
+          lastUploaded: null,
+          measurements: [],
+        },
+        {
+          name: 'component2',
+          componentId: 'component2Id',
+          percentCovered: 91.74,
+          percentChange: 2.65,
+          lastUploaded: null,
+          measurements: [],
+        },
+      ],
+    },
+  },
+}
+
+const mockEmptyComponentMeasurements = {
+  owner: {
+    repository: {
+      __typename: 'Repository',
+      components: [],
+    },
+  },
+}
+
+const componentsData = [
+  {
+    name: 'component1',
+    componentId: 'component1Id',
+    percentCovered: 93.26,
+    percentChange: 1.65,
+    measurements: [],
+    lastUploaded: null,
+  },
+  {
+    name: 'component2',
+    componentId: 'component2Id',
+    percentCovered: 91.74,
+    percentChange: 2.65,
+    measurements: [],
+    lastUploaded: null,
+  },
+]
+
+interface useParamsValueType {
+  search?: string
+  historicalTrend?: string
+  components: string[]
+}
+
+describe('useRepoComponentsTable', () => {
+  function setup({
+    repoData = mockGetRepo,
+    noData = false,
+    useParamsValue = {
+      search: '',
+      historicalTrend: TIME_OPTION_VALUES.LAST_3_MONTHS,
+      components: [],
+    },
+  }: {
+    repoData?: any
+    noData?: boolean
+    useParamsValue?: useParamsValueType
+  }) {
+    const mockedUseLocationParams = useLocationParams as jest.Mock
+    mockedUseLocationParams.mockReturnValue({
+      params: useParamsValue,
+    })
+
+    const requestFilters = jest.fn()
+
+    server.use(
+      graphql.query('ComponentMeasurements', (info) => {
+        requestFilters(info.variables)
+        if (noData) {
+          return HttpResponse.json({ data: mockEmptyComponentMeasurements })
+        }
+        return HttpResponse.json({ data: mockedComponentMeasurements })
+      }),
+      graphql.query('GetRepo', (info) => {
+        return HttpResponse.json({ data: repoData({}) })
+      })
+    )
+
+    return { requestFilters }
+  }
+
+  it('returns data accordingly', async () => {
+    setup({ repoData: mockGetRepo })
+    const { result } = renderHook(() => useRepoComponentsTable(), {
+      wrapper: wrapper(),
+    })
+    await waitFor(() => expect(result.current.data).toEqual(componentsData))
+  })
+
+  describe('when there is no data', () => {
+    it('returns an empty array', async () => {
+      setup({ repoData: mockGetRepo, noData: true })
+      const { result } = renderHook(() => useRepoComponentsTable(), {
+        wrapper: wrapper(),
+      })
+      await waitFor(() => expect(result.current.data).toEqual([]))
+    })
+  })
+
+  describe('when there is search param', () => {
+    it('calls useRepoComponentsTable with correct filters value', async () => {
+      const { requestFilters } = setup({
+        repoData: mockGetRepo,
+        noData: true,
+        useParamsValue: {
+          search: '',
+          historicalTrend: TIME_OPTION_VALUES.LAST_3_MONTHS,
+          components: ['component1'],
+        },
+      })
+
+      const { result } = renderHook(() => useRepoComponentsTable(true), {
+        wrapper: wrapper(),
+      })
+
+      await waitFor(() => expect(result.current.isSearching).toEqual(true))
+      await waitFor(() =>
+        expect(requestFilters).toHaveBeenCalledWith({
+          after: format(sub(new Date(), { months: 3 }), 'yyyy-MM-dd'),
+          before: format(new Date(), 'yyyy-MM-dd'),
+          filters: {
+            components: ['component1'],
+          },
+          interval: 'INTERVAL_7_DAY',
+          orderingDirection: 'DESC',
+          name: 'codecov',
+          repo: 'test',
+        })
+      )
+    })
+  })
+
+  describe('historical trend', () => {
+    describe('when historical trend param is empty', () => {
+      it('calls useRepoComponentsTable with correct query params', async () => {
+        const { requestFilters } = setup({
+          repoData: mockGetRepo,
+          useParamsValue: {
+            components: [],
+            historicalTrend: '',
+          },
+        })
+
+        renderHook(() => useRepoComponentsTable(true), {
+          wrapper: wrapper(),
+        })
+
+        await waitFor(() =>
+          expect(requestFilters).toHaveBeenCalledWith({
+            after: format(sub(new Date(), { months: 3 }), 'yyyy-MM-dd'),
+            before: format(new Date(), 'yyyy-MM-dd'),
+            filters: {},
+            interval: 'INTERVAL_7_DAY',
+            orderingDirection: 'DESC',
+            name: 'codecov',
+            repo: 'test',
+          })
+        )
+      })
+    })
+
+    describe('when historical trend param is all time', () => {
+      it('calls useRepoComponentsTable with correct query params', async () => {
+        const { requestFilters } = setup({
+          repoData: mockGetRepo,
+          useParamsValue: {
+            historicalTrend: 'ALL_TIME',
+            search: '',
+            components: [],
+          },
+        })
+
+        renderHook(() => useRepoComponentsTable(true), {
+          wrapper: wrapper(),
+        })
+
+        await waitFor(() =>
+          expect(requestFilters).toHaveBeenCalledWith({
+            after: '2022-10-10',
+            before: format(new Date(), 'yyyy-MM-dd'),
+            filters: {},
+            interval: 'INTERVAL_30_DAY',
+            orderingDirection: 'DESC',
+            name: 'codecov',
+            repo: 'test',
+          })
+        )
+      })
+    })
+
+    describe('when 6 months is selected', () => {
+      it('calls useRepoComponentsTable with correct query params', async () => {
+        const { requestFilters } = setup({
+          repoData: mockGetRepo,
+          useParamsValue: {
+            historicalTrend: 'LAST_6_MONTHS',
+            search: '',
+            components: [],
+          },
+        })
+        renderHook(() => useRepoComponentsTable(true), {
+          wrapper: wrapper(),
+        })
+
+        const after = format(subMonths(new Date(), 6), 'yyyy-MM-dd')
+        await waitFor(() =>
+          expect(requestFilters).toHaveBeenCalledWith({
+            after,
+            before: format(new Date(), 'yyyy-MM-dd'),
+            filters: {},
+            interval: 'INTERVAL_7_DAY',
+            orderingDirection: 'DESC',
+            name: 'codecov',
+            repo: 'test',
+          })
+        )
+      })
+    })
+
+    describe('when last 7 days is selected', () => {
+      it('calls useRepoComponentsTable with correct query params', async () => {
+        const { requestFilters } = setup({
+          repoData: mockGetRepo,
+          useParamsValue: {
+            historicalTrend: 'LAST_7_DAYS',
+            search: '',
+            components: [],
+          },
+        })
+        renderHook(() => useRepoComponentsTable(true), {
+          wrapper: wrapper(),
+        })
+
+        const after = format(subDays(new Date(), 7), 'yyyy-MM-dd')
+        await waitFor(() =>
+          expect(requestFilters).toHaveBeenCalledWith({
+            after,
+            before: format(new Date(), 'yyyy-MM-dd'),
+            filters: {},
+            interval: 'INTERVAL_1_DAY',
+            orderingDirection: 'DESC',
+            name: 'codecov',
+            repo: 'test',
+          })
+        )
+      })
+    })
+  })
+
+  describe('when there is a components param', () => {
+    it('calls useRepoComponentsTable with correct filters values', async () => {
+      const { requestFilters } = setup({
+        repoData: mockGetRepo,
+        useParamsValue: { components: ['component1'] },
+      })
+
+      renderHook(() => useRepoComponentsTable(true), {
+        wrapper: wrapper(),
+      })
+
+      await waitFor(() =>
+        expect(requestFilters).toHaveBeenCalledWith({
+          after: format(sub(new Date(), { months: 3 }), 'yyyy-MM-dd'),
+          before: format(new Date(), 'yyyy-MM-dd'),
+          filters: { components: ['component1'] },
+          interval: 'INTERVAL_7_DAY',
+          orderingDirection: 'DESC',
+          name: 'codecov',
+          repo: 'test',
+        })
+      )
+    })
+  })
+})
---
+++ src/pages/RepoPage/CoverageTab/FlagsTab/Header/Header.test.jsx
@@ -0,0 +1,289 @@
+import { render, screen, waitFor } from 'custom-testing-library'
+
+import userEvent from '@testing-library/user-event'
+import { MemoryRouter, Route } from 'react-router-dom'
+
+import {} from 'services/repo'
+
+import Header from './Header'
+
+const mocks = vi.hoisted(() => ({
+  useIntersection: vi.fn(),
+  useLocationParams: vi.fn(),
+  useRepoBackfilled: vi.fn(),
+  useRepoFlagsSelect: vi.fn(),
+}))
+
+vi.mock('react-use', async () => {
+  const actual = await vi.importActual('react-use')
+  return {
+    ...actual,
+    useIntersection: mocks.useIntersection,
+  }
+})
+
+vi.mock('services/navigation', async () => {
+  const actual = await vi.importActual('services/navigation')
+  return {
+    ...actual,
+    useLocationParams: mocks.useLocationParams,
+  }
+})
+
+vi.mock('services/repo', async () => {
+  const actual = await vi.importActual('services/repo')
+  return {
+    ...actual,
+    useRepoBackfilled: mocks.useRepoBackfilled,
+    useRepoFlagsSelect: mocks.useRepoFlagsSelect,
+  }
+})
+
+const wrapper = ({ children }) => (
+  <MemoryRouter initialEntries={['/gh/codecov/gazebo/flags']}>
+    <Route path="/:provider/:owner/:repo/flags" exact={true}>
+      {children}
+    </Route>
+  </MemoryRouter>
+)
+
+describe('Header', () => {
+  afterEach(() => {
+    vi.clearAllMocks()
+  })
+
+  function setup(setRepoFlags = true) {
+    const user = userEvent.setup()
+    const updateLocationMock = vi.fn()
+
+    mocks.useLocationParams.mockReturnValue({
+      params: { search: '', historicalTrend: '', flags: [] },
+      updateParams: updateLocationMock,
+    })
+    mocks.useRepoBackfilled.mockReturnValue({
+      data: { flagsCount: 99 },
+    })
+    if (setRepoFlags) {
+      mocks.useRepoFlagsSelect.mockReturnValue({
+        data: [{ name: 'flag1' }],
+      })
+    }
+
+    return { user, updateLocationMock }
+  }
+
+  describe('Configured Flags', () => {
+    beforeEach(() => setup())
+
+    it('Renders the label', () => {
+      render(<Header />, { wrapper })
+
+      expect(screen.getByText(/Configured flags/)).toBeInTheDocument()
+    })
+    it('Renders the correct number of flags on the repo', () => {
+      render(<Header />, { wrapper })
+
+      expect(screen.getByText(/99/)).toBeInTheDocument()
+    })
+  })
+
+  describe('Historical Trend', () => {
+    describe('Title', () => {
+      beforeEach(() => setup())
+
+      it('Renders the label', () => {
+        render(<Header />, { wrapper })
+
+        expect(screen.getByText(/Historical trend/)).toBeInTheDocument()
+      })
+    })
+
+    describe('Select', () => {
+      beforeEach(() => setup())
+
+      it('loads the expected list', async () => {
+        const { user } = setup()
+        render(<Header />, { wrapper })
+
+        const historicalTrend = screen.getByRole('button', {
+          name: 'Select Historical Trend',
+        })
+        await user.click(historicalTrend)
+
+        expect(screen.getByText('Last 6 months')).toBeVisible()
+      })
+
+      it('updates the location params on select', async () => {
+        const { user, updateLocationMock } = setup()
+        render(<Header />, { wrapper })
+
+        const historicalTrend = screen.getByRole('button', {
+          name: 'Select Historical Trend',
+        })
+        await user.click(historicalTrend)
+
+        const item = screen.getByText('Last 7 days')
+        await user.click(item)
+
+        expect(updateLocationMock).toHaveBeenCalledWith({
+          historicalTrend: 'LAST_7_DAYS',
+        })
+      })
+    })
+  })
+
+  describe('Flags feedback link', () => {
+    beforeEach(() => setup())
+
+    it('Renders the right copy', () => {
+      render(<Header />, { wrapper })
+
+      expect(screen.getByText(/Please drop us a comment/)).toBeInTheDocument()
+    })
+
+    it('Renders the right link', () => {
+      render(<Header />, { wrapper })
+
+      const link = screen.getByRole('link', {
+        name: /here/i,
+      })
+      expect(link).toBeInTheDocument()
+      expect(link.href).toBe(
+        'https://github.com/codecov/Codecov-user-feedback/issues/27'
+      )
+    })
+  })
+
+  describe('Show by', () => {
+    describe('Title', () => {
+      beforeEach(() => setup())
+
+      it('renders the label', () => {
+        render(<Header />, { wrapper })
+
+        const showBy = screen.getByText('Show by')
+        expect(showBy).toBeInTheDocument()
+      })
+    })
+
+    describe('MultiSelect', () => {
+      describe('on page load', () => {
+        it('loads the expected list', async () => {
+          const { user } = setup()
+          render(<Header />, { wrapper })
+
+          const button = screen.getByText('All flags')
+          await user.click(button)
+
+          const flag1 = screen.getByText('flag1')
+          expect(flag1).toBeInTheDocument()
+        })
+
+        it('updates the location params on select', async () => {
+          const { user, updateLocationMock } = setup()
+          render(<Header />, { wrapper })
+
+          const button = screen.getByText('All flags')
+          await user.click(button)
+
+          const flag1 = screen.getByText('flag1')
+          await user.click(flag1)
+
+          expect(updateLocationMock).toHaveBeenCalledWith({
+            flags: ['flag1'],
+          })
+        })
+      })
+
+      describe('where onLoadMore is triggered', () => {
+        describe('when there is a next page', () => {
+          it('calls fetchNextPage', async () => {
+            const { user } = setup(false)
+            const fetchNextPage = vi.fn()
+            mocks.useRepoFlagsSelect.mockReturnValue({
+              data: [{ name: 'flag1' }],
+              fetchNextPage,
+              hasNextPage: true,
+            })
+            mocks.useIntersection.mockReturnValue({ isIntersecting: true })
+
+            render(<Header />, { wrapper })
+
+            const button = screen.getByText('All flags')
+            await user.click(button)
+
+            expect(fetchNextPage).toHaveBeenCalled()
+          })
+        })
+
+        describe('when there is no next page', () => {
+          it('does not calls fetchNextPage', async () => {
+            const { user } = setup(false)
+            const fetchNextPage = vi.fn()
+            mocks.useRepoFlagsSelect.mockReturnValue({
+              data: [{ name: 'flag1' }],
+              fetchNextPage,
+              hasNextPage: false,
+            })
+            mocks.useIntersection.mockReturnValue({ isIntersecting: true })
+
+            render(<Header />, { wrapper })
+
+            const button = screen.getByText('All flags')
+            await user.click(button)
+
+            expect(fetchNextPage).not.toHaveBeenCalled()
+          })
+        })
+      })
+
+      describe('when searching for a flag', () => {
+        it('displays the search box', async () => {
+          const { user } = setup()
+          render(<Header />, { wrapper })
+
+          const button = screen.getByText('All flags')
+          await user.click(button)
+
+          const searchBox = screen.getByPlaceholderText('Search for Flags')
+          expect(searchBox).toBeInTheDocument()
+        })
+
+        it('updates the textbox value when typing', async () => {
+          const { user } = setup()
+          render(<Header />, { wrapper })
+
+          const button = screen.getByText('All flags')
+          await user.click(button)
+
+          const searchBox = screen.getByPlaceholderText('Search for Flags')
+          await user.type(searchBox, 'flag2')
+
+          const searchBoxUpdated =
+            screen.getByPlaceholderText('Search for Flags')
+          expect(searchBoxUpdated).toHaveAttribute('value', 'flag2')
+        })
+
+        it('calls useRepoFlagsSelect with term', async () => {
+          const { user } = setup()
+          render(<Header />, { wrapper })
+
+          const button = screen.getByText('All flags')
+          await user.click(button)
+
+          const searchBox = screen.getByPlaceholderText('Search for Flags')
+          await user.type(searchBox, 'flag2')
+
+          await waitFor(
+            () =>
+              expect(mocks.useRepoFlagsSelect).toHaveBeenCalledWith({
+                filters: { term: 'flag2' },
+                options: { suspense: false },
+              }),
+            { timeout: 600 }
+          )
+        })
+      })
+    })
+  })
+})
