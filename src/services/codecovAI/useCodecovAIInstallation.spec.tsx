4519b02feac4bf1ba1916f022a832d856501ba2e
Rohit Vinnakota <148245014+rohitvinnakota-codecov@users.noreply.github.com>
2024-10-01 11:01:02 -0400

    [feat] Add configured repos table to the Codecov AI tab (#3303)

---
+++ src/services/codecovAI/useCodecovAIInstallation.spec.tsx
@@ -0,0 +1,110 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { renderHook, waitFor } from '@testing-library/react'
+import { graphql } from 'msw'
+import { setupServer } from 'msw/node'
+
+import { useCodecovAIInstallation } from './useCodecovAIInstallation'
+
+const mockAiFeaturesEnabled = {
+  owner: {
+    aiFeaturesEnabled: true,
+  },
+}
+
+const mockUnsuccessfulParseError = {
+  owner: {
+    wrong: 'schema',
+  },
+}
+
+const server = setupServer()
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { retry: false } },
+})
+
+const wrapper: React.FC<React.PropsWithChildren> = ({ children }) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+)
+
+beforeAll(() => {
+  server.listen()
+})
+
+afterEach(() => {
+  queryClient.clear()
+  server.resetHandlers()
+})
+
+afterAll(() => {
+  server.close()
+})
+
+interface SetupArgs {
+  isUnsuccessfulParseError?: boolean
+}
+
+describe('useCodecovAIInstallation', () => {
+  function setup({ isUnsuccessfulParseError = false }: SetupArgs) {
+    server.use(
+      graphql.query('GetCodecovAIAppInstallInfo', (req, res, ctx) => {
+        if (isUnsuccessfulParseError) {
+          return res(ctx.status(200), ctx.data(mockUnsuccessfulParseError))
+        }
+        return res(ctx.status(200), ctx.data(mockAiFeaturesEnabled))
+      })
+    )
+  }
+
+  describe('there is valid data', () => {
+    it('fetches the owner app installation info', async () => {
+      setup({})
+      const { result } = renderHook(
+        () =>
+          useCodecovAIInstallation({
+            owner: 'codecov',
+            provider: 'gh',
+          }),
+        { wrapper }
+      )
+
+      await waitFor(() =>
+        expect(result.current.data).toStrictEqual({
+          aiFeaturesEnabled: true,
+        })
+      )
+    })
+  })
+
+  describe('unsuccessful parse of zod schema', () => {
+    let oldConsoleError = console.error
+
+    beforeEach(() => {
+      console.error = () => null
+    })
+
+    afterEach(() => {
+      console.error = oldConsoleError
+    })
+
+    it('throws a 404', async () => {
+      setup({ isUnsuccessfulParseError: true })
+      const { result } = renderHook(
+        () =>
+          useCodecovAIInstallation({
+            owner: 'codecov',
+            provider: 'gh',
+          }),
+        { wrapper }
+      )
+
+      await waitFor(() => expect(result.current.isError).toBeTruthy())
+      await waitFor(() =>
+        expect(result.current.error).toEqual(
+          expect.objectContaining({
+            status: 404,
+          })
+        )
+      )
+    })
+  })
+})
