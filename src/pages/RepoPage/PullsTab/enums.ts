51d074b4405bce09fb58ea751e32158b4f36dfd4
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-10-01 06:48:29 -0400

    chore: Update pages/RepoPage/PullsTab tests to Vitest (#3334)

--- src/pages/RepoPage/PullsTab/enums.ts
+++ src/pages/RepoPage/PullsTab/enums.ts
@@ -2,28 +2,31 @@ export const stateEnum = {
   Merged: { state: 'MERGED', name: 'Merged' },
   Closed: { state: 'CLOSED', name: 'Closed' },
   Open: { state: 'OPEN', name: 'Open' },
-}
+} as const
 
 export const orderingEnum = {
   Oldest: { order: 'ASC', name: 'Oldest' },
   Newest: { order: 'DESC', name: 'Newest' },
-}
+} as const
 
 export const filterItems = [
   stateEnum.Open.name,
   stateEnum.Merged.name,
   stateEnum.Closed.name,
-]
+] as const
 
-export const orderItems = [orderingEnum.Newest.name, orderingEnum.Oldest.name]
+export const orderItems = [
+  orderingEnum.Newest.name,
+  orderingEnum.Oldest.name,
+] as const
 
 export const orderNames = {
   ASC: orderingEnum.Oldest.name,
   DESC: orderingEnum.Newest.name,
-}
+} as const
 
 export const stateNames = {
   MERGED: stateEnum.Merged.name,
   CLOSED: stateEnum.Closed.name,
   OPEN: stateEnum.Open.name,
-}
+} as const
