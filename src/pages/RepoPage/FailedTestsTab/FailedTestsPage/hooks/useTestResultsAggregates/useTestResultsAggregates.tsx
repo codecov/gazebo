a0869a51261d7dff03a48449382fbb0502bcd8bc
Ajay Singh <ajay.singh@sentry.io>
2024-10-08 16:26:05 -0700

    add new hook for test results aggregates

---
+++ src/pages/RepoPage/FailedTestsTab/FailedTestsPage/hooks/useTestResultsAggregates/useTestResultsAggregates.tsx
@@ -0,0 +1,132 @@
+import { useQuery } from '@tanstack/react-query'
+import { useParams } from 'react-router-dom'
+import { z } from 'zod'
+
+import {
+  RepoNotFoundErrorSchema,
+  RepoOwnerNotActivatedErrorSchema,
+} from 'services/repo'
+import Api from 'shared/api'
+import { NetworkErrorObject } from 'shared/api/helpers'
+import A from 'ui/A'
+
+const TestResultsAggregatesSchema = z.object({
+  owner: z
+    .object({
+      repository: z.discriminatedUnion('__typename', [
+        z.object({
+          __typename: z.literal('Repository'),
+          testResultsAggregates: z.object({
+            totalDuration: z.number(),
+            totalDurationPercentChange: z.number().nullable(),
+            slowestTestsDuration: z.number(),
+            slowestTestsDurationPercentChange: z.number().nullable(),
+            totalFails: z.number(),
+            totalFailsPercentChange: z.number().nullable(),
+            totalSkips: z.number(),
+            totalSkipsPercentChange: z.number().nullable(),
+          }),
+        }),
+        RepoNotFoundErrorSchema,
+        RepoOwnerNotActivatedErrorSchema,
+      ]),
+    })
+    .nullable(),
+})
+
+const query = `
+  query GetTestResultsAggregates(
+    $owner: String!
+    $repo: String!
+  ) {
+    owner(username: $owner) {
+      repository: repository(name: $repo) {
+        __typename
+        ... on Repository {
+          testResultsAggregates {
+            totalDuration
+            totalDurationPercentChange
+            slowestTestsDuration
+            slowestTestsDurationPercentChange
+            totalFails
+            totalFailsPercentChange
+            totalSkips
+            totalSkipsPercentChange
+            }
+        }
+        ... on NotFoundError {
+          message
+        }
+        ... on OwnerNotActivatedError {
+          message
+        }
+      }
+    }
+  }
+  `
+
+interface URLParams {
+  provider: string
+  owner: string
+  repo: string
+}
+
+export const useTestResultsAggregates = () => {
+  const { provider, owner, repo } = useParams<URLParams>()
+
+  return useQuery({
+    queryKey: ['GetTestResultsAggregates', provider, owner, repo],
+    queryFn: ({ signal }) =>
+      Api.graphql({
+        provider,
+        query,
+        signal,
+        variables: {
+          provider,
+          owner,
+          repo,
+        },
+      }).then((res) => {
+        const parsedData = TestResultsAggregatesSchema.safeParse(res?.data)
+
+        if (!parsedData.success) {
+          return Promise.reject({
+            status: 404,
+            data: {},
+            dev: 'useTestResultsAggregates - 404 Failed to parse data',
+          } satisfies NetworkErrorObject)
+        }
+
+        const data = parsedData.data
+
+        console.log('DATA', data.owner?.repository)
+
+        if (data?.owner?.repository?.__typename === 'NotFoundError') {
+          return Promise.reject({
+            status: 404,
+            data: {},
+            dev: 'useTestResultsAggregates - 404 Not found error',
+          } satisfies NetworkErrorObject)
+        }
+
+        if (data?.owner?.repository?.__typename === 'OwnerNotActivatedError') {
+          return Promise.reject({
+            status: 403,
+            data: {
+              detail: (
+                <p>
+                  Activation is required to view this repo, please{' '}
+                  {/* @ts-expect-error */}
+                  <A to={{ pageName: 'membersTab' }}>click here </A> to activate
+                  your account.
+                </p>
+              ),
+            },
+            dev: 'useTestResultsAggregates - 403 Owner not activated',
+          } satisfies NetworkErrorObject)
+        }
+
+        return data.owner?.repository.testResultsAggregates
+      }),
+  })
+}
