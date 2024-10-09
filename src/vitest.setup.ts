abba10b0375dff3277bd2d12c061f54f39e985a6
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-10-09 09:42:38 -0300

    adjust type exceptions in vitest setup

--- src/vitest.setup.ts
+++ src/vitest.setup.ts
@@ -30,7 +30,9 @@ vi.mock('@sentry/react', async () => {
 beforeAll(() => {
   // This is a bit of a hack to get Vitest fake timers setup to work with waitFor and findBy's
   // GH Issue: https://github.com/testing-library/react-testing-library/issues/1197#issuecomment-1693824628
+  // @ts-expect-error - we need to add this to the global object for some internal deps to work
   globalThis.jest = {
+    // @ts-expect-error - we need to add this to the global object for some internal deps to work
     ...globalThis.jest,
     /**
      * From react-intersection-observer/test-utils
@@ -41,7 +43,6 @@ beforeAll(() => {
      *   setupIntersectionMocking(vi.fn);
      */
 
-    // @ts-expect-error - because of the ordering of the checks above
     fn: vi.fn.bind(vi),
     advanceTimersByTime: vi.advanceTimersByTime.bind(vi),
   }
