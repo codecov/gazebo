38340e0f88145758535d7e65118525c794566c8e
Rohit <rohit.vinnakota@sentry.io>
2024-10-14 20:43:38 -0700

    Add email form

---
+++ src/ui/LoginForm/LoginForm.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+
+interface LoginFormProps {
+  onLogin: (username: string, password: string) => void
+}
+
+const LoginForm: React.FC<LoginFormProps> = ({ onLogin }) => {
+  const [username, setUsername] = useState<string>('')
+  const [password] = useState<string>('')
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (username) {
+      onLogin(username, password)
+      setUsername(username)
+    }
+  }
+
+  const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(username + e.target.value)
+  }
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value)
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          id="username"
+          type="text"
+          value={username}
+          onChange={handleUsernameChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          id="password"
+          type="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+      </div>
+
+      <button type="button">Login</button>
+    </form>
+  )
+}
+
+export default LoginForm
