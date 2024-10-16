1c3aee817d81b59dc2aba6316fa84f0c3e120485
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-10-16 13:28:02 -0300

    chore: Update pulls page files changed diff to new renderer (#3359)

--- src/pages/PullRequestPage/PullCoverage/routes/FilesChangedTab/FilesChanged/FileDiff/FileDiff.jsx
+++ src/pages/PullRequestPage/PullCoverage/routes/FilesChangedTab/FilesChanged/FileDiff/FileDiff.jsx
@@ -6,6 +6,7 @@ import { useParams } from 'react-router-dom'
 import { useNavLinks } from 'services/navigation'
 import { useSingularImpactedFileComparison } from 'services/pull'
 import { useRepoOverview } from 'services/repo'
+import { useFlags } from 'shared/featureFlags'
 import {
   CODE_RENDERER_TYPE,
   STICKY_PADDING_SIZES,
@@ -16,6 +17,7 @@ import CodeRendererInfoRow from 'ui/CodeRenderer/CodeRendererInfoRow'
 import CriticalFileLabel from 'ui/CodeRenderer/CriticalFileLabel'
 import DiffLine from 'ui/CodeRenderer/DiffLine'
 import Spinner from 'ui/Spinner'
+import { VirtualDiffRenderer } from 'ui/VirtualFileRenderer'
 
 const Loader = () => (
   <div className="flex items-center justify-center py-16">
@@ -27,7 +29,7 @@ function FileDiff({ path }) {
   const { pullFileView } = useNavLinks()
   const { provider, owner, repo, pullId } = useParams()
   const { data: overview } = useRepoOverview({ provider, owner, repo })
-
+  const { virtualDiffRenderer } = useFlags({ virtualDiffRenderer: false })
   const { data, isLoading } = useSingularImpactedFileComparison({
     provider,
     owner,
@@ -58,6 +60,27 @@ function FileDiff({ path }) {
       {isCriticalFile && <CriticalFileLabel variant="borderTop" />}
       {segments?.map((segment, segmentIndex) => {
         const content = segment.lines.map((line) => line.content).join('\n')
+
+        let newDiffContent = ''
+        const lineData = []
+        if (virtualDiffRenderer) {
+          segment.lines.forEach((line, lineIndex) => {
+            newDiffContent += line.content
+
+            if (lineIndex < segment.lines.length - 1) {
+              newDiffContent += '\n'
+            }
+
+            lineData.push({
+              headNumber: line?.headNumber,
+              baseNumber: line?.baseNumber,
+              headCoverage: line?.headCoverage,
+              baseCoverage: line?.baseCoverage,
+              hitCount: undefined,
+            })
+          })
+        }
+
         return (
           <Fragment key={`${headName}-${segmentIndex}`}>
             <CodeRendererInfoRow>
@@ -73,23 +96,33 @@ function FileDiff({ path }) {
                 </A>
               </div>
             </CodeRendererInfoRow>
-            <CodeRenderer
-              code={content}
-              fileName={headName}
-              rendererType={CODE_RENDERER_TYPE.DIFF}
-              LineComponent={({ i, line, ...props }) => (
-                <DiffLine
-                  // If this line one of the first 3 or last three lines of the segment
-                  key={uniqueId(i)}
-                  lineContent={line}
-                  edgeOfFile={i <= 2 || i >= segment.lines.length - 3}
-                  path={data?.hashedPath}
-                  stickyPadding={stickyPadding}
-                  {...props}
-                  {...segment.lines[i]}
-                />
-              )}
-            />
+            {virtualDiffRenderer ? (
+              <VirtualDiffRenderer
+                key={segmentIndex}
+                code={newDiffContent}
+                fileName={headName}
+                hashedPath={data?.hashedPath}
+                lineData={lineData}
+              />
+            ) : (
+              <CodeRenderer
+                code={content}
+                fileName={headName}
+                rendererType={CODE_RENDERER_TYPE.DIFF}
+                LineComponent={({ i, line, ...props }) => (
+                  <DiffLine
+                    // If this line one of the first 3 or last three lines of the segment
+                    key={uniqueId(i)}
+                    lineContent={line}
+                    edgeOfFile={i <= 2 || i >= segment.lines.length - 3}
+                    path={data?.hashedPath}
+                    stickyPadding={stickyPadding}
+                    {...props}
+                    {...segment.lines[i]}
+                  />
+                )}
+              />
+            )}
           </Fragment>
         )
       })}
