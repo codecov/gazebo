358ec63d84e1793173e2380f8b9fbc476616f26e
Spencer Murray <spencer.murray@sentry.io>
2024-10-18 14:04:31 -0400

    Add comment back

--- src/vitest.setup.ts
+++ src/vitest.setup.ts
@@ -28,13 +28,25 @@ vi.mock('@sentry/react', async () => {
 })
 
 beforeAll(() => {
-  // This is a bit of a hack to get Vitest fake timers setup to work with waitFor and findBy's
-  // GH Issue: https://github.com/testing-library/react-testing-library/issues/1197#issuecomment-1693824628
   globalThis.jest = {
     ...globalThis.jest,
-    fn: vi.fn.bind(vi),
+    // This is a bit of a hack to get Vitest fake timers setup to work with waitFor and findBy's
+    // GH Issue: https://github.com/testing-library/react-testing-library/issues/1197#issuecomment-1693824628
     // @ts-ignore-error
     advanceTimersByTime: vi.advanceTimersByTime.bind(vi),
+
+    /*
+     * Since doing this hack means we now have a jest global defined, we must also redefine jest.fn to be vi.fn,
+     * otherwise the following lines of code in react-intersection-observer/src/test-utils.ts break:
+     *
+     * // Use the exposed mock function. Currently, only supports Jest (`jest.fn`) and Vitest with globals (`vi.fn`).
+     * if (typeof jest !== "undefined") setupIntersectionMocking(jest.fn);
+     * else if (typeof vi !== "undefined") {
+     * setupIntersectionMocking(vi.fn);
+     * }
+     */
+
+    fn: vi.fn.bind(vi),
   }
 })
 
