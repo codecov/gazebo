aa1932d6208516d5d61dd1f75218939c6edfdddf
Calvin Yau <calvin.yau@sentry.io>
2024-10-21 13:22:13 -0700

    test: Fixing no testid attempt

--- src/ui/CIStatus/CIStatus.test.jsx
+++ src/ui/CIStatus/CIStatus.test.jsx
@@ -1,43 +1,65 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import { MemoryRouter, Route } from 'react-router-dom'
+import ResizeObserver from 'resize-observer-polyfill'
 
 import CIStatus from '.'
+
+global.ResizeObserver = ResizeObserver
+
+const wrapper = ({ children }) => (
+  <MemoryRouter
+    initialEntries={[
+      '/gh/codecov/test-repo/commit/803897e6ceeb6828778070208c06c5a978a48a68',
+    ]}
+  >
+    <Route path="/:provider/:owner/:repo/commit/:commit">{children}</Route>
+  </MemoryRouter>
+)
 
 describe('CIStatus', () => {
   describe('when rendered', () => {
     it('shows ci passed', () => {
-      render(<CIStatus ciPassed={true} />)
+      render(<CIStatus ciPassed={true} />, { wrapper })
 
       const passed = screen.getByText(/Passed/)
       expect(passed).toBeInTheDocument()
     })
 
     it('shows ci failed', () => {
-      render(<CIStatus ciPassed={false} />)
+      render(<CIStatus ciPassed={false} />, { wrapper })
 
       const failed = screen.getByText(/Failed/)
       expect(failed).toBeInTheDocument()
     })
 
     it('shows no status if no status is given', () => {
-      render(<CIStatus ciPassed={undefined} />)
+      render(<CIStatus ciPassed={undefined} />, { wrapper })
 
       const noStatus = screen.getByText(/No Status/)
       expect(noStatus).toBeInTheDocument()
     })
 
     it('shows tooltip on hover', async () => {
-      render(<CIStatus ciPassed={true} />)
+      render(<CIStatus ciPassed={true} />, { wrapper })
 
       const tooltipTrigger = screen.getByTestId('ci-tooltip-trigger')
       await userEvent.hover(tooltipTrigger)
 
-      const link = await screen.findByTestId('docs-link')
-      expect(link).toBeInTheDocument()
-      expect(link).toHaveAttribute(
+      const link = await screen.findAllByTestId('yml-require-ci-pass-link')
+      expect(link).toHaveLength(2)
+      expect(link[0]).toHaveAttribute(
         'href',
         'https://docs.codecov.com/docs/codecovyml-reference#codecovrequire_ci_to_pass'
       )
+    })
+
+    it('does not show tooltip when not hovered', async () => {
+      render(<CIStatus ciPassed={true} />, { wrapper })
+
+      expect(
+        screen.queryByTestId('yml-require-ci-pass-link')
+      ).not.toBeInTheDocument()
     })
   })
 })
--- src/ui/CIStatus/CIStatus.tsx
+++ src/ui/CIStatus/CIStatus.tsx
@@ -23,8 +23,8 @@ export default function CIStatusLabel({ ciPassed }: CIStatusLabelProps) {
   const iconName = ciPassed ? 'check' : 'x'
 
   return (
-    <Tooltip>
-      <Tooltip.Root delayDuration={500}>
+    <Tooltip delayDuration={0} skipDelayDuration={100}>
+      <Tooltip.Root>
         <Tooltip.Trigger>
           <span
             className="flex flex-none items-center gap-1 text-xs"
@@ -42,23 +42,22 @@ export default function CIStatusLabel({ ciPassed }: CIStatusLabelProps) {
         </Tooltip.Trigger>
         <Tooltip.Portal>
           <Tooltip.Content
-            className="inline rounded border border-ds-gray-secondary bg-ds-gray-primary px-3 py-2 text-xs"
+            className="rounded-md border border-ds-gray-secondary bg-ds-gray-primary px-3 py-2 text-xs"
             side="right"
           >
-            <div className="absolute left-[-7px] size-0 border-y-[10px] border-r-[10px] border-y-transparent border-r-ds-gray-secondary" />
-            <TooltipArrow className="fill-ds-gray-primary" />
-            <span className="text-ds-gray-octonary">
-              This CI status is based on all of your{' '}
+            <p className="text-ds-gray-octonary">
+              This CI status is based on all of your <br />
               <A
                 to={{ pageName: 'requireCIPassDocs' }}
                 hook="yml-require-ci-pass-link"
-                data-testid="docs-link"
+                data-testid="yml-require-ci-pass-link"
                 isExternal
               >
                 non-Codecov related checks
               </A>
               .
-            </span>
+            </p>
+            <TooltipArrow className="fill-ds-gray-primary" />
           </Tooltip.Content>
         </Tooltip.Portal>
       </Tooltip.Root>
