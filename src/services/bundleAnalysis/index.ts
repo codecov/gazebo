24ca8ed7dc43cefe7d749a08a0b71e6486c0e72c
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-10-23 13:36:39 -0300

    update useBranchBundleSummary

--- src/services/bundleAnalysis/index.ts
+++ src/services/bundleAnalysis/index.ts
@@ -1,7 +1,3 @@
-export {
-  useBranchBundleSummary,
-  type UseBranchBundleSummaryArgs,
-} from './useBranchBundleSummary'
 export { useBundleAssetModules } from './useBundleAssetModules'
 export { useBundleAssets } from './useBundleAssets'
 export { useBundleSummary } from './useBundleSummary'
