b7968a49394236049f425c7ffa12ce53a096f5a5
Suejung Shin <suejung.shin@sentry.io>
2024-10-29 15:06:16 -0700

    feat: Add 'viewing as visitor' label (#3445)

--- src/layouts/Header/components/Navigator/Navigator.tsx
+++ src/layouts/Header/components/Navigator/Navigator.tsx
@@ -5,6 +5,7 @@ import { useCrumbs } from 'pages/RepoPage/context'
 import { Me } from 'services/user'
 import Avatar from 'ui/Avatar'
 import Breadcrumb from 'ui/Breadcrumb'
+import Label from 'ui/Label'
 
 import MyContextSwitcher from './MyContextSwitcher'
 
@@ -18,9 +19,22 @@ function Navigator({ currentUser }: NavigatorProps) {
   const { path } = useRouteMatch()
   const { breadcrumbs } = useCrumbs()
 
+  const isCurrentUserPartOfOrg = ownerData?.isCurrentUserPartOfOrg
+
   // Repo page
   if (path.startsWith('/:provider/:owner/:repo')) {
-    return <Breadcrumb paths={breadcrumbs} largeFont />
+    return (
+      <div className="flex items-center">
+        <span className="inline-block">
+          <Breadcrumb paths={breadcrumbs} largeFont />{' '}
+        </span>
+        {isCurrentUserPartOfOrg === false ? (
+          <Label variant="plain" className="ml-2 hidden sm:block">
+            Viewing as visitor
+          </Label>
+        ) : null}
+      </div>
+    )
   }
 
   // Self-hosted admin settings
@@ -42,16 +56,21 @@ function Navigator({ currentUser }: NavigatorProps) {
     )
   }
 
-  if (ownerData && !ownerData.isCurrentUserPartOfOrg) {
+  // Fallback instead of MyContextSwitcher if not logged in
+  if (!currentUser) {
     return (
       <div className="flex items-center">
         <Avatar user={ownerData} />
-        <h2 className="mx-2 text-xl font-semibold">{ownerData.username}</h2>
+        <h2 className="mx-2 text-xl font-semibold">{ownerData?.username}</h2>
+        {isCurrentUserPartOfOrg === false ? (
+          <Label variant="plain" className="ml-2 hidden sm:block">
+            Viewing as visitor
+          </Label>
+        ) : null}
       </div>
     )
   }
 
-  // Everything else uses MyContextSwitcher
   let pageName = 'owner'
   if (path.startsWith('/analytics/:provider/:owner')) {
     pageName = 'analytics'
@@ -63,7 +82,16 @@ function Navigator({ currentUser }: NavigatorProps) {
     pageName = 'accountAdmin'
   }
 
-  return <MyContextSwitcher pageName={pageName} />
+  return (
+    <div className="flex items-center">
+      <MyContextSwitcher pageName={pageName} />
+      {isCurrentUserPartOfOrg === false ? (
+        <Label variant="plain" className="ml-2 hidden sm:block">
+          Viewing as visitor
+        </Label>
+      ) : null}
+    </div>
+  )
 }
 
 export default Navigator
