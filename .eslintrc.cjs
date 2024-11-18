ae6dbbed4506dbb96d55dcb7faf53751acac4bcc
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-11-18 07:32:30 -0400

    build out eslint config based off of react-scripts lint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,23 @@
+// The ESLint browser environment defines all browser globals as valid,
+// even though most people don't know some of them exist (e.g. `name` or `status`).
+// This is dangerous as it hides accidentally undefined variables.
+// We blacklist the globals that we deem potentially confusing.
+// To use them, explicitly reference them, e.g. `window.name` or `window.status`.
+const restrictedGlobals = require('confusing-browser-globals')
+
 module.exports = {
   extends: [
-    'react-app',
     'plugin:tailwindcss/recommended',
     'plugin:@tanstack/eslint-plugin-query/recommended',
     'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:import/recommended',
     'prettier',
     'plugin:storybook/recommended',
     'plugin:tailwindcss/recommended',
   ],
-  plugins: ['@vitest'],
+  plugins: ['@vitest', 'jsx-a11y', 'import'],
   globals: {
     vi: true,
   },
@@ -18,118 +26,100 @@ module.exports = {
     'src/old_ui/Icon/svg/*.jsx',
     'src/ui/Icon/svg/**/*.jsx',
   ],
-  rules: {
-    'react/react-in-jsx-scope': 'off',
-    'react/prefer-stateless-function': ['error'],
-    'react/prop-types': [
-      'warn',
-      {
-        ignore: ['children', 'context', 'className'],
-      },
-    ],
-    camelcase: ['error', { properties: 'always' }],
-    'sort-imports': [
-      'warn',
-      {
-        ignoreCase: true,
-        ignoreDeclarationSort: true,
-      },
-    ],
-    'import/order': [
-      'warn',
-      {
-        groups: [
-          'external',
-          'builtin',
-          'internal',
-          'sibling',
-          'parent',
-          'index',
-        ],
-        pathGroups: [
-          {
-            pattern: 'react',
-            group: 'external',
-            position: 'before',
-          },
-          {
-            pattern: 'prop-types',
-            group: 'external',
-            position: 'before',
-          },
-          {
-            pattern: 'custom-testing-library',
-            group: 'external',
-            position: 'before',
-          },
-          {
-            pattern: 'testing-library',
-            group: 'external',
-            position: 'before',
-          },
-          {
-            pattern: 'mocks',
-            group: 'internal',
-            position: 'before',
-          },
-          {
-            pattern: 'assets',
-            group: 'internal',
-            position: 'before',
-          },
-          {
-            pattern: 'layouts',
-            group: 'internal',
-          },
-          {
-            pattern: 'ui',
-            group: 'internal',
-            position: 'after',
-          },
-          {
-            pattern: 'old_ui',
-            group: 'internal',
-            position: 'after',
-          },
-          {
-            pattern: 'services',
-            group: 'internal',
-            position: 'before',
-          },
-          {
-            pattern: 'shared',
-            group: 'internal',
-            position: 'before',
-          },
-          {
-            pattern: 'pages',
-            group: 'internal',
-          },
-          {
-            pattern: 'config',
-            group: 'internal',
-            position: 'before',
-          },
-          {
-            pattern: 'sentry',
-            group: 'internal',
-            position: 'before',
-          },
-          {
-            pattern: 'types',
-            group: 'internal',
-            position: 'before',
-          },
-        ],
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
-        },
-        'newlines-between': 'always',
-      },
-    ],
-  },
   overrides: [
+    // JS Rules
+    // Copied from https://github.com/facebook/create-react-app/blob/main/packages/eslint-config-react-app/base.js
+    {
+      files: ['**/*.js?(x)', '**/*.test.js?(x)'],
+      parser: '@babel/eslint-parser',
+      plugins: ['react'],
+      env: {
+        browser: true,
+        commonjs: true,
+        es6: true,
+        jest: true,
+        node: true,
+      },
+      parserOptions: {
+        sourceType: 'module',
+        requireConfigFile: false,
+        babelOptions: {
+          presets: [require.resolve('babel-preset-react-app/prod')],
+        },
+      },
+      settings: {
+        react: {
+          version: 'detect',
+        },
+      },
+      rules: {
+        'react/jsx-uses-vars': 'warn',
+        'react/jsx-uses-react': 'warn',
+      },
+    },
+    // TypeScript rules
+    // Copied from https://github.com/facebook/create-react-app/blob/main/packages/eslint-config-react-app/index.js#L32-L92
+    {
+      files: ['**/*.ts?(x)', '**/*.test.ts?(x)'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+
+        // typescript-eslint specific options
+        warnOnUnsupportedTypeScriptVersion: true,
+      },
+      plugins: ['@typescript-eslint'],
+      rules: {
+        // No prop-types definitions required for typed React components
+        'react/prop-types': 'off',
+        // TypeScript's `noFallthroughCasesInSwitch` option is more robust (#6906)
+        'default-case': 'off',
+        // 'tsc' already handles this (https://github.com/typescript-eslint/typescript-eslint/issues/291)
+        'no-dupe-class-members': 'off',
+        // 'tsc' already handles this (https://github.com/typescript-eslint/typescript-eslint/issues/477)
+        'no-undef': 'off',
+
+        // Add TypeScript specific rules (and turn off ESLint equivalents)
+        '@typescript-eslint/consistent-type-assertions': 'warn',
+        'no-array-constructor': 'off',
+        '@typescript-eslint/no-array-constructor': 'warn',
+        'no-redeclare': 'off',
+        '@typescript-eslint/no-redeclare': 'warn',
+        'no-use-before-define': 'off',
+        '@typescript-eslint/no-use-before-define': [
+          'warn',
+          {
+            functions: false,
+            classes: false,
+            variables: false,
+            typedefs: false,
+          },
+        ],
+        'no-unused-expressions': 'off',
+        '@typescript-eslint/no-unused-expressions': [
+          'error',
+          {
+            allowShortCircuit: true,
+            allowTernary: true,
+            allowTaggedTemplates: true,
+          },
+        ],
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          {
+            args: 'none',
+            ignoreRestSiblings: true,
+          },
+        ],
+        'no-useless-constructor': 'off',
+        '@typescript-eslint/no-useless-constructor': 'warn',
+      },
+    },
     // Testing Library
     {
       extends: ['plugin:testing-library/react'],
@@ -235,7 +225,7 @@ module.exports = {
     },
     // All
     {
-      files: ['src/**/*.js', 'src/**/*.jsx', 'src/**/*.ts', 'src/**/*.tsx'],
+      files: ['**/*.js?(x)', '**/*.ts?(x)'],
       rules: {
         'no-restricted-imports': [
           'error',
@@ -254,14 +244,6 @@ module.exports = {
             patterns: ['launchdarkly-react-client-sdk'],
           },
         ],
-      },
-    },
-    // TypeScript rules
-    {
-      files: ['src/**/*.ts', 'src/**/*.tsx'],
-      rules: {
-        // No prop-types definitions required for typed React components
-        'react/prop-types': 'off',
       },
     },
     // Testing Rules
@@ -287,13 +269,25 @@ module.exports = {
     },
     // Story Rules
     {
-      files: ['*.stories.jsx'],
+      files: ['*.stories.jsx', '*.stories.tsx'],
       rules: {
         'import/no-anonymous-default-export': 'off',
+        // https://github.com/facebook/react/tree/main/packages/eslint-plugin-react-hooks
+        'react-hooks/rules-of-hooks': 'off',
       },
     },
   ],
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
   settings: {
+    react: {
+      version: 'detect',
+    },
     'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
     'import/resolver': {
       alias: {
@@ -322,5 +316,227 @@ module.exports = {
       removeDuplicates: true,
       skipClassAttribute: false,
     },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-vars': 'warn',
+    'react/jsx-uses-react': 'warn',
+    'react/prefer-stateless-function': ['error'],
+    'react/prop-types': [
+      'warn',
+      { ignore: ['children', 'context', 'className'] },
+    ],
+    camelcase: ['error', { properties: 'always' }],
+    'sort-imports': ['warn', { ignoreCase: true, ignoreDeclarationSort: true }],
+    'import/order': [
+      'warn',
+      {
+        groups: [
+          'external',
+          'builtin',
+          'internal',
+          'sibling',
+          'parent',
+          'index',
+        ],
+        pathGroups: [
+          { pattern: 'react', group: 'external', position: 'before' },
+          { pattern: 'prop-types', group: 'external', position: 'before' },
+          {
+            pattern: 'custom-testing-library',
+            group: 'external',
+            position: 'before',
+          },
+          { pattern: 'testing-library', group: 'external', position: 'before' },
+          { pattern: 'mocks', group: 'internal', position: 'before' },
+          { pattern: 'assets', group: 'internal', position: 'before' },
+          { pattern: 'layouts', group: 'internal' },
+          { pattern: 'ui', group: 'internal', position: 'after' },
+          { pattern: 'old_ui', group: 'internal', position: 'after' },
+          { pattern: 'services', group: 'internal', position: 'before' },
+          { pattern: 'shared', group: 'internal', position: 'before' },
+          { pattern: 'pages', group: 'internal' },
+          { pattern: 'config', group: 'internal', position: 'before' },
+          { pattern: 'sentry', group: 'internal', position: 'before' },
+          { pattern: 'types', group: 'internal', position: 'before' },
+        ],
+        alphabetize: { order: 'asc', caseInsensitive: true },
+        'newlines-between': 'always',
+      },
+    ],
+    // Everything below, copied from https://github.com/facebook/create-react-app/blob/main/packages/eslint-config-react-app/index.js#L97
+    // http://eslint.org/docs/rules/
+    'array-callback-return': 'warn',
+    'default-case': ['warn', { commentPattern: '^no default$' }],
+    'dot-location': ['warn', 'property'],
+    eqeqeq: ['warn', 'smart'],
+    'new-parens': 'warn',
+    'no-array-constructor': 'warn',
+    'no-caller': 'warn',
+    'no-cond-assign': ['warn', 'except-parens'],
+    'no-const-assign': 'warn',
+    'no-control-regex': 'warn',
+    'no-delete-var': 'warn',
+    'no-dupe-args': 'warn',
+    'no-dupe-class-members': 'warn',
+    'no-dupe-keys': 'warn',
+    'no-duplicate-case': 'warn',
+    'no-empty-character-class': 'warn',
+    'no-empty-pattern': 'warn',
+    'no-eval': 'warn',
+    'no-ex-assign': 'warn',
+    'no-extend-native': 'warn',
+    'no-extra-bind': 'warn',
+    'no-extra-label': 'warn',
+    'no-fallthrough': 'warn',
+    'no-func-assign': 'warn',
+    'no-implied-eval': 'warn',
+    'no-invalid-regexp': 'warn',
+    'no-iterator': 'warn',
+    'no-label-var': 'warn',
+    'no-labels': ['warn', { allowLoop: true, allowSwitch: false }],
+    'no-lone-blocks': 'warn',
+    'no-loop-func': 'warn',
+    'no-mixed-operators': [
+      'warn',
+      {
+        groups: [
+          ['&', '|', '^', '~', '<<', '>>', '>>>'],
+          ['==', '!=', '===', '!==', '>', '>=', '<', '<='],
+          ['&&', '||'],
+          ['in', 'instanceof'],
+        ],
+        allowSamePrecedence: false,
+      },
+    ],
+    'no-multi-str': 'warn',
+    'no-global-assign': 'warn',
+    'no-unsafe-negation': 'warn',
+    'no-new-func': 'warn',
+    'no-new-object': 'warn',
+    'no-new-symbol': 'warn',
+    'no-new-wrappers': 'warn',
+    'no-obj-calls': 'warn',
+    'no-octal': 'warn',
+    'no-octal-escape': 'warn',
+    'no-redeclare': 'warn',
+    'no-regex-spaces': 'warn',
+    'no-restricted-syntax': ['warn', 'WithStatement'],
+    'no-script-url': 'warn',
+    'no-self-assign': 'warn',
+    'no-self-compare': 'warn',
+    'no-sequences': 'warn',
+    'no-shadow-restricted-names': 'warn',
+    'no-sparse-arrays': 'warn',
+    'no-template-curly-in-string': 'warn',
+    'no-this-before-super': 'warn',
+    'no-throw-literal': 'warn',
+    'no-undef': 'error',
+    'no-restricted-globals': ['error'].concat(restrictedGlobals),
+    'no-unreachable': 'warn',
+    'no-unused-expressions': [
+      'error',
+      {
+        allowShortCircuit: true,
+        allowTernary: true,
+        allowTaggedTemplates: true,
+      },
+    ],
+    'no-unused-labels': 'warn',
+    'no-unused-vars': ['warn', { args: 'none', ignoreRestSiblings: true }],
+    'no-use-before-define': [
+      'warn',
+      {
+        functions: false,
+        classes: false,
+        variables: false,
+      },
+    ],
+    'no-useless-computed-key': 'warn',
+    'no-useless-concat': 'warn',
+    'no-useless-constructor': 'warn',
+    'no-useless-escape': 'warn',
+    'no-useless-rename': [
+      'warn',
+      {
+        ignoreDestructuring: false,
+        ignoreImport: false,
+        ignoreExport: false,
+      },
+    ],
+    'no-with': 'warn',
+    'no-whitespace-before-property': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+    'require-yield': 'warn',
+    'rest-spread-spacing': ['warn', 'never'],
+    strict: ['warn', 'never'],
+    'unicode-bom': ['warn', 'never'],
+    'use-isnan': 'warn',
+    'valid-typeof': 'warn',
+    'no-restricted-properties': [
+      'error',
+      {
+        object: 'require',
+        property: 'ensure',
+        message:
+          'Please use import() instead. More info: https://facebook.github.io/create-react-app/docs/code-splitting',
+      },
+      {
+        object: 'System',
+        property: 'import',
+        message:
+          'Please use import() instead. More info: https://facebook.github.io/create-react-app/docs/code-splitting',
+      },
+    ],
+    'getter-return': 'warn',
+
+    // https://github.com/benmosher/eslint-plugin-import/tree/master/docs/rules
+    'import/first': 'error',
+    'import/no-amd': 'error',
+    'import/no-anonymous-default-export': 'warn',
+    'import/no-webpack-loader-syntax': 'error',
+
+    // https://github.com/yannickcr/eslint-plugin-react/tree/master/docs/rules
+    'react/forbid-foreign-prop-types': ['warn', { allowInPropTypes: true }],
+    'react/jsx-no-comment-textnodes': 'warn',
+    'react/jsx-no-duplicate-props': 'warn',
+    'react/jsx-no-target-blank': 'warn',
+    'react/jsx-no-undef': 'error',
+    'react/jsx-pascal-case': ['warn', { allowAllCaps: true, ignore: [] }],
+    'react/no-danger-with-children': 'warn',
+    // Disabled because of undesirable warnings
+    // See https://github.com/facebook/create-react-app/issues/5204 for
+    // blockers until its re-enabled
+    // 'react/no-deprecated': 'warn',
+    'react/no-direct-mutation-state': 'warn',
+    'react/no-is-mounted': 'warn',
+    'react/no-typos': 'error',
+    'react/require-render-return': 'error',
+    'react/style-prop-object': 'warn',
+
+    // https://github.com/evcohen/eslint-plugin-jsx-a11y/tree/master/docs/rules
+    'jsx-a11y/alt-text': 'warn',
+    'jsx-a11y/anchor-has-content': 'warn',
+    'jsx-a11y/anchor-is-valid': [
+      'warn',
+      { aspects: ['noHref', 'invalidHref'] },
+    ],
+    'jsx-a11y/aria-activedescendant-has-tabindex': 'warn',
+    'jsx-a11y/aria-props': 'warn',
+    'jsx-a11y/aria-proptypes': 'warn',
+    'jsx-a11y/aria-role': ['warn', { ignoreNonDOM: true }],
+    'jsx-a11y/aria-unsupported-elements': 'warn',
+    'jsx-a11y/heading-has-content': 'warn',
+    'jsx-a11y/iframe-has-title': 'warn',
+    'jsx-a11y/img-redundant-alt': 'warn',
+    'jsx-a11y/no-access-key': 'warn',
+    'jsx-a11y/no-distracting-elements': 'warn',
+    'jsx-a11y/no-redundant-roles': 'warn',
+    'jsx-a11y/role-has-required-aria-props': 'warn',
+    'jsx-a11y/role-supports-aria-props': 'warn',
+    'jsx-a11y/scope': 'warn',
+
+    // https://github.com/facebook/react/tree/main/packages/eslint-plugin-react-hooks
+    'react-hooks/rules-of-hooks': 'error',
   },
 }
