cec828d0f3e0978e2595267ce3f7b7132d11be33
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-12-12 08:34:38 -0500

    refactor to ts and migrate usage of updated hook in ActuvationInfo

--- src/pages/AdminSettings/AdminMembers/ActivationInfo/ActivationInfo.tsx
+++ src/pages/AdminSettings/AdminMembers/ActivationInfo/ActivationInfo.tsx
@@ -2,8 +2,10 @@ import { lazy, Suspense } from 'react'
 
 import Spinner from 'ui/Spinner'
 
-const ActivationCount = lazy(() => import('./ActivationCount'))
-const AutoActivateMembers = lazy(() => import('./AutoActivateMembers'))
+const ActivationCount = lazy(() => import('./ActivationCount/ActivationCount'))
+const AutoActivateMembers = lazy(
+  () => import('./AutoActivateMembers/AutoActivateMembers')
+)
 
 const Loader = () => (
   <div className="flex items-center justify-center py-16">
