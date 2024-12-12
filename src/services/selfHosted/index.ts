43420a7be6ffaf1f8948d4a8b986df79c8de8164
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-12-12 11:02:03 -0500

    migrate useSelfHostedUserList to SelfHostedUserListQueryOpts

--- src/services/selfHosted/index.ts
+++ src/services/selfHosted/index.ts
@@ -1,5 +1,4 @@
 export * from './useSelfHostedCurrentUser'
 export * from './useSelfHostedSeatsConfig'
-export * from './useSelfHostedUserList'
 export * from './useSelfHostedHasAdmins'
 export * from './useSelfHostedSeatsAndLicense'
