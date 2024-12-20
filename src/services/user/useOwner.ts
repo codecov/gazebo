6deb723d088f586e8ed86679b3ab4df34ecef97a
Suejung Shin <suejung.shin@sentry.io>
2024-12-19 19:35:46 -0800

    add zod schema validation

--- src/services/user/useOwner.ts
+++ src/services/user/useOwner.ts
@@ -1,7 +1,23 @@
 import { useQuery } from '@tanstack/react-query'
 import { useParams } from 'react-router-dom'
+import { z } from 'zod'
 
 import Api from 'shared/api'
+import { rejectNetworkError } from 'shared/api/helpers'
+
+const OwnerSchema = z.object({
+  ownerid: z.number().nullish(),
+  username: z.string().nullish(),
+  avatarUrl: z.string().nullish(),
+  isCurrentUserPartOfOrg: z.boolean().nullish(),
+  isAdmin: z.boolean().nullish(),
+})
+
+export type Owner = z.infer<typeof OwnerSchema>
+
+const RequestSchema = z.object({
+  owner: OwnerSchema.nullish(),
+})
 
 interface URLParams {
   provider: string
@@ -40,6 +56,17 @@ export function useOwner({
     queryKey: ['owner', variables, provider, query],
     queryFn: ({ signal }) =>
       Api.graphql({ provider, query, variables, signal }).then((res) => {
+        const parsedRes = RequestSchema.safeParse(res?.data)
+
+        if (!parsedRes.success) {
+          return rejectNetworkError({
+            status: 404,
+            data: {},
+            dev: 'useOwner - 404 Failed to parse data',
+            error: parsedRes.error,
+          })
+        }
+
         return res?.data?.owner
       }),
     ...opts,
