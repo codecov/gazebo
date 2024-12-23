f30e40a446d89fbfd6cab70dd20eaa5775c39928
Suejung Shin <suejung.shin@sentry.io>
2024-12-23 11:13:34 -0800

    chore: Migrate updateProfile to ts

--- src/services/user/useUpdateProfile.ts
+++ src/services/user/useUpdateProfile.ts
@@ -1,8 +1,42 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { z } from 'zod'
 
 import config from 'config'
 
 import Api from 'shared/api'
+import { NetworkErrorObject, rejectNetworkError } from 'shared/api/helpers'
+
+const CurrentUserFragment = z.object({
+  email: z.string(),
+  privateAccess: z.boolean(),
+  onboardingCompleted: z.boolean(),
+  businessEmail: z.string(),
+  user: z.object({
+    name: z.string(),
+    username: z.string(),
+    avatarUrl: z.string(),
+    avatar: z.string(),
+    student: z.boolean(),
+    studentCreatedAt: z.string(),
+    studentUpdatedAt: z.string(),
+  }),
+  trackingMetadata: z.object({
+    service: z.string(),
+    ownerid: z.string(),
+    serviceId: z.string(),
+  }),
+})
+
+const CurrentUserFragmentSchema = z.object({
+  me: CurrentUserFragment.nullable(),
+  error: z
+    .discriminatedUnion('__typename', [
+      z.object({
+        __typename: z.literal('ValidationError'),
+      }),
+    ])
+    .nullable(),
+})
 
 const currentUserFragment = `
 fragment CurrentUserFragment on Me {
@@ -72,9 +106,18 @@ export function useUpdateProfile({ provider }: { provider: string }) {
             email,
           },
         },
-      }).then((res) => res?.data?.updateProfile?.me)
+      })
     },
-    onSuccess: (user) => {
+    onSuccess: ({ data }) => {
+      const parsedData = CurrentUserFragmentSchema.safeParse(data)
+      if (!parsedData.success) {
+        return rejectNetworkError({
+          status: 404,
+          data: {},
+          dev: 'useUpdateProfile - 404 failed to parse',
+        } satisfies NetworkErrorObject)
+      }
+
       queryClient.setQueryData(['currentUser', provider], () => user)
 
       if (config.IS_SELF_HOSTED) {
