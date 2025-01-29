28bc8f07cb3e50e0dc5e17ddbc1dacdd14d9d4c3
Spencer Murray <spencer.murray@sentry.io>
2025-01-29 13:37:41 -0500

    Only show auto activate for admins on enterprise cloud plans

--- src/pages/MembersPage/MembersActivation/MembersActivation.jsx
+++ src/pages/MembersPage/MembersActivation/MembersActivation.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { useParams } from 'react-router-dom'
 
 import { TrialStatuses, useAccountDetails, usePlanData } from 'services/account'
+import { useOwner } from 'services/user'
 
 import Activation from './Activation'
 import AutoActivate from './AutoActivate'
@@ -10,6 +11,7 @@ import AutoActivate from './AutoActivate'
 function MemberActivation() {
   const { owner, provider } = useParams()
   const { data: accountDetails } = useAccountDetails({ owner, provider })
+  const { data: ownerData } = useOwner({ username: owner })
 
   const { data: planData } = usePlanData({
     provider,
@@ -21,7 +23,8 @@ function MemberActivation() {
   const showAutoActivate =
     !isUndefined(planAutoActivate) &&
     !planData?.plan?.isTrialPlan &&
-    planData?.plan?.trialStatus !== TrialStatuses.ONGOING
+    planData?.plan?.trialStatus !== TrialStatuses.ONGOING &&
+    !(planData?.plan?.isEnterprisePlan && !ownerData.isAdmin) // only show for admins on enterprise cloud plans
 
   return (
     <div className="border-2 border-ds-gray-primary">
