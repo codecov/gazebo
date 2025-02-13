5016f6fa12e8fbdec4c03367d9f1495f5e6c1594
nicholas-codecov <nicholas.deschenes@sentry.io>
2025-02-13 13:52:03 -0500

    remove lazy from FilesChangedTable

--- src/pages/CommitDetailPage/CommitCoverage/routes/FilesChangedTab/FilesChangedTable/FilesChangedTable.tsx
+++ src/pages/CommitDetailPage/CommitCoverage/routes/FilesChangedTab/FilesChangedTable/FilesChangedTable.tsx
@@ -14,7 +14,7 @@ import isArray from 'lodash/isArray'
 import isEmpty from 'lodash/isEmpty'
 import isNumber from 'lodash/isNumber'
 import qs from 'qs'
-import { Fragment, lazy, Suspense, useMemo, useState } from 'react'
+import { Fragment, Suspense, useMemo, useState } from 'react'
 import { useLocation, useParams } from 'react-router-dom'
 
 import { ImpactedFileType, useCommit } from 'services/commit'
@@ -23,7 +23,7 @@ import Icon from 'ui/Icon'
 import Spinner from 'ui/Spinner'
 import TotalsNumber from 'ui/TotalsNumber'
 
-const CommitFileDiff = lazy(() => import('../shared/CommitFileDiff'))
+import CommitFileDiff from '../shared/CommitFileDiff'
 
 const columnHelper = createColumnHelper<ImpactedFileType>()
 
