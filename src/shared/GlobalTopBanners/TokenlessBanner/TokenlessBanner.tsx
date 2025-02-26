5a632e9eb5ff5efe46c8389bbfe7e49a1914eb91
Calvin <calvin.yau@sentry.io>
2025-02-26 10:17:03 -0800

    fix: Add fixes and new rules to tokenless banner show logic (#3764)

--- src/shared/GlobalTopBanners/TokenlessBanner/TokenlessBanner.tsx
+++ src/shared/GlobalTopBanners/TokenlessBanner/TokenlessBanner.tsx
@@ -1,16 +1,20 @@
+import { useSuspenseQuery as useSuspenseQueryV5 } from '@tanstack/react-queryV5'
+import React from 'react'
 import { useParams } from 'react-router-dom'
 
 import { ONBOARDING_SOURCE } from 'pages/TermsOfService/constants'
 import { useLocationParams } from 'services/navigation/useLocationParams'
 import { useUploadTokenRequired } from 'services/uploadTokenRequired'
 import { useUser } from 'services/user'
+import { TokenlessQueryOpts } from 'services/user/TokenlessQueryOpts'
+import { Provider } from 'shared/api/helpers'
 import { useFlags } from 'shared/featureFlags'
 
 import TokenNotRequiredBanner from './TokenNotRequiredBanner'
 import TokenRequiredBanner from './TokenRequiredBanner'
 
 type UseParams = {
-  provider: string
+  provider: Provider
   owner: string
 }
 
@@ -20,19 +24,30 @@ const TokenlessBanner: React.FC = () => {
   })
   const { provider, owner } = useParams<UseParams>()
   const { data } = useUploadTokenRequired({ provider, owner, enabled: !!owner })
+  const { data: ownerTokenlessData } = useSuspenseQueryV5(
+    TokenlessQueryOpts({ username: owner, provider })
+  )
   const { data: currentUser } = useUser()
   const { params } = useLocationParams()
   // @ts-expect-error useLocationParams needs to be typed
   const cameFromOnboarding = params['source'] === ONBOARDING_SOURCE
+  // showing the tokenless banner when the owner does not have any
+  // active repos nor any public repos does not give the user any value and
+  // potentially causes confusion
+  const hasActiveRepos = ownerTokenlessData?.hasActiveRepos
+  const hasPublicRepos = ownerTokenlessData?.hasPublicRepos
+  const relevantToCurrentOwner = hasActiveRepos && hasPublicRepos
 
   if (
     !tokenlessSection ||
     !owner ||
     !data ||
     !currentUser?.user ||
-    cameFromOnboarding
-  )
+    cameFromOnboarding ||
+    !relevantToCurrentOwner
+  ) {
     return null
+  }
 
   return data?.uploadTokenRequired ? (
     <TokenRequiredBanner />
