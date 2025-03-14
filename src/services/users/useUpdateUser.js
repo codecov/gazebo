ad46ee0b2470e83d97ae87dc5065fd932cddfb43
Spencer Murray <159931558+spalmurray-codecov@users.noreply.github.com>
2025-03-14 14:15:25 -0400

    fix: Various usability tweaks on the members page (#3805)

--- src/services/users/useUpdateUser.js
+++ src/services/users/useUpdateUser.js
@@ -2,6 +2,8 @@ import { useMutation, useQueryClient } from '@tanstack/react-query'
 
 import Api from 'shared/api'
 
+const UPDATE_USERS_TIMEOUT_KEY = 'update_users_timeout'
+
 function patchPathUsers({ provider, owner, targetUserOwnerid }) {
   return `/${provider}/${owner}/users/${targetUserOwnerid}/`
 }
@@ -11,12 +13,16 @@ export function useUpdateUser({ provider, owner, opts = {} }) {
   const queryClient = useQueryClient()
 
   const successHandler = (...args) => {
-    // The following cache busts will trigger react-query to retry the api call updating components depending on this data.
-    queryClient.invalidateQueries(['users'])
-    queryClient.invalidateQueries(['accountDetails'])
-    queryClient.invalidateQueries(['InfiniteUsers'])
-
     if (onSuccess) {
+      // The following cache busts will trigger react-query to retry the api call updating components depending on this data.
+      queryClient.invalidateQueries(['accountDetails'])
+      queryClient.invalidateQueries(['GetPlanData'])
+
+      sessionStorage.setItem(
+        UPDATE_USERS_TIMEOUT_KEY,
+        setTimeout(() => queryClient.invalidateQueries(['users']), 2000) // feels good to me
+      )
+
       // Execute passed onSuccess after invalidating queries
       onSuccess.apply(null, args)
     }
@@ -24,6 +30,10 @@ export function useUpdateUser({ provider, owner, opts = {} }) {
 
   return useMutation({
     mutationFn: ({ targetUserOwnerid, ...body }) => {
+      // Prevent refreshing users list until done chaining requests w/ timeout
+      clearTimeout(sessionStorage.getItem(UPDATE_USERS_TIMEOUT_KEY))
+      queryClient.cancelQueries(['users'])
+
       const path = patchPathUsers({ provider, owner, targetUserOwnerid })
       return Api.patch({ path, provider, body })
     },
