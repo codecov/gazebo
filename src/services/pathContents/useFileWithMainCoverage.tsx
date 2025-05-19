ca0515bd6569dd0e969dfceac0673c783d7ce977
ajay-sentry <159853603+ajay-sentry@users.noreply.github.com>
2025-05-19 08:49:32 -0700

    perf: Minor extract coverage util perf updates (#3869)

--- src/services/pathContents/useFileWithMainCoverage.tsx
+++ src/services/pathContents/useFileWithMainCoverage.tsx
@@ -11,8 +11,14 @@ import {
 } from './constants'
 import { extractCoverageFromResponse } from './utils'
 
-// There's only 1 hook per page table as of Feb 22, 2023. This is a limitation to tailor a table to a particular page. Each "page" should have an independent set of hooks for directory and file entries. Every usePrefetchFile<page name> hook needs to this hook's query and queryKey
-// There should be a coverageForFile + prefetch hook set per page. This function, due to how it's written, acts as 1 hook all pages use, and each file implements it's prefetch function accordingly. This is something that needs to be changed, likely in a subsequent PR, as we're getting rid of the individual line file type layout anyway and doing diff line only
+/* There's only 1 hook per page table as of Feb 22, 2023. This is a limitation to
+tailor a table to a particular page. Each "page" should have an independent set of
+hooks for directory and file entries. Every usePrefetchFile<page name> hook needs to
+this hook's query and queryKey. There should be a coverageForFile + prefetch hook set per page. 
+This function, due to how it's written, acts as 1 hook all pages use, and each file implements 
+it's prefetch function accordingly. This is something that needs to be changed, likely in a 
+subsequent PR, as we're getting rid of the individual line file type layout anyway and 
+doing diff line only */
 
 interface UseFileWithMainCoverageArgs {
   provider: string
--- src/services/pathContents/utils.ts
+++ src/services/pathContents/utils.ts
@@ -1,29 +1,27 @@
-import keyBy from 'lodash/keyBy'
-import mapValues from 'lodash/mapValues'
-
 import { PathContentsRepositorySchema } from './constants'
 
 export function extractCoverageFromResponse(
   repository: PathContentsRepositorySchema | undefined | null
 ) {
   if (!repository) return null
-  const commit = repository?.commit
-  const branch = repository?.branch?.head
-  const coverageSource = (commit || branch)?.coverageAnalytics
+
+  const commit = repository.commit
+  const branch = repository.branch?.head
+  const coverageSource = commit?.coverageAnalytics || branch?.coverageAnalytics
   const coverageFile = coverageSource?.coverageFile
+
   if (!coverageFile) return null
-  const lineWithCoverage = keyBy(coverageFile?.coverage, 'line')
-  const fileCoverage = mapValues(lineWithCoverage, 'coverage')
+
+  const fileCoverage = Object.fromEntries(
+    coverageFile.coverage?.map((item) => [item?.line, item?.coverage]) || []
+  )
   const coverageTotal = coverageFile?.totals?.percentCovered
   const hashedPath = coverageFile?.hashedPath
 
   return {
     content: coverageFile?.content,
     coverage: fileCoverage,
-    totals:
-      typeof coverageTotal !== 'number' || isNaN(coverageTotal)
-        ? 0
-        : coverageTotal,
+    totals: coverageTotal && !Number.isNaN(coverageTotal) ? coverageTotal : 0,
     flagNames: coverageSource?.flagNames ?? [],
     componentNames: coverageSource?.components?.map(({ name }) => name) ?? [],
     ...(hashedPath && { hashedPath }),
