f1d0a861bcd08ea96cabdfa7f0e61bdf60e70dfc
Calvin <calvin.yau@sentry.io>
2025-08-27 13:49:59 -0700

    fix: Use dynamic repo name for token step copy (#3926)

--- src/pages/RepoPage/CoverageOnboarding/GitHubActions/TokenStep.tsx
+++ src/pages/RepoPage/CoverageOnboarding/GitHubActions/TokenStep.tsx
@@ -120,7 +120,7 @@ function OrgOrRepoTokenSelector({
   handleValueChange,
   hasOrgUploadToken,
 }: OrgOrRepoTokenSelectorProps) {
-  const { owner } = useParams<URLParams>()
+  const { owner, repo } = useParams<URLParams>()
   const isAdmin = useIsCurrentUserAnAdmin({ owner })
   const { regenerateToken, isLoading } = useGenerateOrgUploadToken()
 
@@ -147,7 +147,7 @@ function OrgOrRepoTokenSelector({
         >
           <RadioTileGroup.Label>Repository token</RadioTileGroup.Label>
           <RadioTileGroup.Description>
-            Use it for uploading coverage reports in the enigma repository.
+            Use it for uploading coverage reports in the {repo} repository.
           </RadioTileGroup.Description>
         </RadioTileGroup.Item>
       </RadioTileGroup>
